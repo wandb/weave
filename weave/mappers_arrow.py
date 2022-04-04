171dfd259c7b0fbfc6dd474a99fe6289037354a9
Shawn Lewis <shlewis@gmail.com>
2022-04-04 12:34:49 -0700

    Make mappers_arrow one-way to simplify

--- weave/mappers_arrow.py
+++ weave/mappers_arrow.py
@@ -6,7 +6,6 @@ from . import mappers_python
 from . import arrow_util
 from . import weave_types as types
 from . import storage
-from . import refs
 
 
 class TypedDictToArrowStruct(mappers_python.TypedDictToPyDict):
@@ -16,26 +15,6 @@ class TypedDictToArrowStruct(mappers_python.TypedDictToPyDict):
             prop_result_type = property_serializer.result_type()
             fields.append(arrow_util.arrow_field(property_key, prop_result_type))
         return pa.struct(fields)
-
-
-class ArrowStructMapper(mappers.Mapper):
-    def __init__(self, type_, mapper, artifact, path):
-        prop_serializers = {}
-        for field in type_:
-            prop_serializers[field.name] = mapper(field, artifact, path)
-        self._property_serializers = prop_serializers
-
-
-class ArrowStructToTypedDict(ArrowStructMapper):
-    def result_type(self):
-        return types.TypedDict(self._property_serializers)
-
-    def apply(self, obj):
-        result = {}
-        for k, v in obj.items():
-            v = self._property_serializers[k].apply(v)
-            result[k] = v
-        return result
 
 
 class ObjectToArrowStruct(mappers_python.ObjectToPyDict):
@@ -51,51 +30,9 @@ class ObjectToArrowStruct(mappers_python.ObjectToPyDict):
         return arrow_util.arrow_type_with_metadata(arrow_type, metadata)
 
 
-class ArrowWeaveFieldToObject(mappers.Mapper):
-    def __init__(self, type_, mapper, artifact, path):
-        if type_.metadata is None or b"weave_type" not in type_.metadata:
-            raise Exception("invalid")
-        self._type_dict = json.loads(type_.metadata[b"weave_type"])
-        prop_serializers = {}
-        for field in type_.type:
-            prop_serializers[field.name] = mapper(field, artifact, path)
-        self._property_serializers = prop_serializers
-
-    def result_type(self):
-        return types.TypeRegistry.type_from_dict(self._type_dict)
-
-    def apply(self, obj):
-        result = {}
-        for k, v in obj.items():
-            v = self._property_serializers[k].apply(v)
-            result[k] = v
-        result_type = self.result_type()
-        for prop_name, prop_type in result_type.variable_property_types().items():
-            if isinstance(prop_type, types.Const):
-                result[prop_name] = prop_type.val
-        return self.result_type().instance_class(**result)
-
-
 class ListToArrowArr(mappers_python.ListToPyList):
     def result_type(self):
         return pa.list_(arrow_util.arrow_field("x", self._object_type.result_type()))
-
-
-class ArrowListMapper(mappers.Mapper):
-    def __init__(self, type_, mapper, artifact, path):
-        object_type = mapper(type_.value_type, artifact, path)
-        value_field = type_.value_field
-        if value_field.metadata is not None and b"weave_type" in value_field.metadata:
-            object_type = ArrowWeaveFieldToObject(value_field, mapper, artifact, path)
-        self._object_type = object_type
-
-
-class ArrowArrToList(ArrowListMapper):
-    def result_type(self):
-        return types.List(self._object_type)
-
-    def apply(self, obj):
-        return [self._object_type.apply(item) for item in obj]
 
 
 class IntToArrowInt(mappers_python.IntToPyInt):
@@ -103,33 +40,9 @@ class IntToArrowInt(mappers_python.IntToPyInt):
         return pa.int64()
 
 
-class ArrowIntMapper(mappers.Mapper):
-    pass
-
-
-class ArrowIntToInt(ArrowIntMapper):
-    def result_type(self):
-        return types.Int()
-
-    def apply(self, obj):
-        return obj
-
-
 class FloatToArrowFloat(mappers_python.FloatToPyFloat):
     def result_type(self):
         return pa.float64()
-
-
-class ArrowFloatMapper(mappers.Mapper):
-    pass
-
-
-class ArrowFloatToFloat(ArrowFloatMapper):
-    def result_type(self):
-        return types.Float()
-
-    def apply(self, obj):
-        return obj
 
 
 class StringToArrowString(mappers_python.StringToPyString):
@@ -137,26 +50,9 @@ class StringToArrowString(mappers_python.StringToPyString):
         return pa.string()
 
 
-class ArrowStringMapper(mappers.Mapper):
-    pass
-
-
-class ArrowStringToString(ArrowStringMapper):
-    def result_type(self):
-        return types.String()
-
-    def apply(self, obj):
-        return obj
-
-
 class NoneToArrowNone(mappers.Mapper):
     def result_type(self):
         return pa.null()
-
-
-class ArrowNoneToNone(mappers.Mapper):
-    def result_type(self):
-        return types.none_type
 
 
 class UnknownToArrowNone(mappers_python.UnknownToPyUnknown):
@@ -183,24 +79,6 @@ class DefaultToArrow(mappers.Mapper):
         return local_ref_str
 
 
-class DefaultFromArrow(mappers.Mapper):
-    def __init__(self, type_, mapper, artifact, path=[]):
-        self.type = type_
-        self._result_type = types.TypeRegistry.type_from_dict(
-            json.loads(type_.metadata[b"weave_ref_type"])
-        )
-        self._artifact = artifact
-        self._path = path
-
-    def result_type(self):
-        return self._result_type
-
-    def apply(self, obj):
-        return refs.LocalArtifactRef.from_local_ref(
-            self._artifact, obj, self._result_type
-        ).get()
-
-
 def map_to_arrow_(type, mapper, artifact, path=[]):
     if isinstance(type, types.TypedDict):
         return TypedDictToArrowStruct(type, mapper, artifact, path)
@@ -224,30 +102,27 @@ def map_to_arrow_(type, mapper, artifact, path=[]):
         return DefaultToArrow(type, mapper, artifact, path)
 
 
-def map_from_arrow_(arrow_type, mapper, artifact, path=[]):
-    if isinstance(arrow_type, pa.Field):
-        if arrow_type.metadata is not None and b"weave_type" in arrow_type.metadata:
-            return ArrowWeaveFieldToObject(arrow_type, mapper, artifact, path)
-        elif (
-            arrow_type.metadata is not None and b"weave_ref_type" in arrow_type.metadata
-        ):
-            return DefaultFromArrow(arrow_type, mapper, artifact, path)
-        else:
-            return mapper(arrow_type.type, artifact, path)
-    elif isinstance(arrow_type, pa.Schema) or pa.types.is_struct(arrow_type):
-        return ArrowStructToTypedDict(arrow_type, mapper, artifact, path)
-    elif pa.types.is_list(arrow_type):
-        return ArrowArrToList(arrow_type, mapper, artifact, path)
-    elif pa.types.is_integer(arrow_type):
-        return ArrowIntToInt(arrow_type, mapper, artifact, path)
-    elif pa.types.is_float64(arrow_type):
-        return ArrowFloatToFloat(arrow_type, mapper, artifact, path)
-    elif pa.types.is_string(arrow_type):
-        return ArrowStringToString(type, mapper, artifact, path)
-    elif pa.types.is_null(arrow_type):
-        return ArrowNoneToNone(type, mapper, artifact, path)
+def map_from_arrow_(type, mapper, artifact, path=[]):
+    if isinstance(type, types.TypedDict):
+        return mappers_python.TypedDictToPyDict(type, mapper, artifact, path)
+    elif isinstance(type, types.List):
+        return mappers_python.ListToPyList(type, mapper, artifact, path)
+    elif isinstance(type, types.ObjectType):
+        return mappers_python.ObjectDictToObject(type, mapper, artifact, path)
+    elif isinstance(type, types.Int):
+        return mappers_python.IntToPyInt(type, mapper, artifact, path)
+    elif isinstance(type, types.Float):
+        return mappers_python.FloatToPyFloat(type, mapper, artifact, path)
+    elif isinstance(type, types.Number):
+        return mappers_python.IntToPyInt(type, mapper, artifact, path)
+    elif isinstance(type, types.String):
+        return mappers_python.StringToPyString(type, mapper, artifact, path)
+    elif isinstance(type, types.NoneType):
+        return mappers_python.NoneToPyNone(type, mapper, artifact, path)
+    elif isinstance(type, types.UnknownType):
+        return UnknownToArrowNone(type, mapper, artifact, path)
     else:
-        raise Exception("not implemented", type)
+        return mappers_python.DefaultFromPy(type, mapper, artifact, path)
 
 
 map_to_arrow = mappers.make_mapper(map_to_arrow_)
