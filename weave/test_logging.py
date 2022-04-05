4eb4c4cde181ffc22c328eca38b9163d1d5cfd0d
Danny Goldstein <dgold@berkeley.edu>
2022-04-05 16:27:07 -0700

    create an option to enable capturing of logs to  stderr

--- weave/test_logging.py
+++ weave/test_logging.py
@@ -66,3 +66,25 @@ def test_log_2_app_instances_different_threads(fresh_server_logfile):
 
     assert len(threads) == 3
     assert threads[-2] != threads[-1]
+
+
+def test_capture_server_logs_captures_server_logs(fresh_server_logfile, capsys):
+    context.capture_weave_server_logs()
+
+    with context.local_http_client():
+        # run this to kick off a server
+        assert api.use(ops.Number.__add__(3, 9)) == 12
+
+        # check that the log file was created
+        assert os.path.exists(weave_server.default_log_filename)
+
+        with open(weave_server.default_log_filename, "r") as f:
+            content = f.read()
+
+        # check that it has a record of executing one call
+        assert "execute" in content and "200" in content
+
+        # check that the same stuff appears in the captured stream logs
+        capresult = capsys.readouterr()
+        stderr = capresult.err
+        assert "execute" in stderr and "200" in stderr
