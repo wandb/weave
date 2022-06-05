7f58c438368bbde7772258942bd45f7033786b28
Shawn Lewis <shlewis@gmail.com>
2022-06-05 10:14:12 -0700

    Script to make a uml class diagram for Weave Python

---
+++ weave/make_class_diagram.sh
@@ -0,0 +1,4 @@
+# pip install pylint pygraphviz
+
+# I can't figure out how to just get a class hierarchy of Types!
+pyreverse -p weave -my -o png -k --ignore mappers.py,mappers_arrow.py,mappers_python.py -s 0 **.py
