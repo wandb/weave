075ff9b0d8f7f0dd9579b31c485890e3d990dc05
Shawn Lewis <shlewis@gmail.com>
2022-07-08 13:20:42 -0700

    Fix up Image op examples, move some examples

--- weave/ops_domain/__init__.py
+++ weave/ops_domain/__init__.py
@@ -45,6 +45,9 @@ class RunType(types.Type):
     instance_classes = wandb_api.Run
     instance_class = wandb_api.Run
 
+    def to_dict(self):
+        return self.name
+
     def instance_to_dict(self, obj):
         return {
             "entity_name": obj._entity,
@@ -113,15 +116,14 @@ class Run:
         return run.jobType
 
 
+@dataclasses.dataclass
 class RunsType(types.Type):
     name = "runs"
 
     instance_classes = wandb_api.Runs
     instance_class = wandb_api.Runs
 
-    @property
-    def object_type(self):
-        return RunType()
+    object_type: types.Type = RunType()
 
     def instance_to_dict(self, obj):
         return {
