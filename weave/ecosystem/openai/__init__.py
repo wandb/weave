a15eb0a7a27e7db0cb6d29b96f4d6e18685d94fb
Shawn Lewis <shlewis@gmail.com>
2022-07-11 07:41:29 -0700

    Move OpenAI into folder

---
+++ weave/ecosystem/openai/__init__.py
@@ -0,0 +1,1 @@
+from .gpt3 import *
--- weave/ecosystem/openai/test_openai.py
+++ weave/ecosystem/openai/test_openai.py
@@ -1,15 +1,15 @@
-from . import openai
+from . import gpt3
 
-from .. import weave_types as types
+from ... import weave_types as types
 
 
 def test_gpt3model_inferred_type():
-    assert openai.Gpt3Model.complete.input_type.arg_types == {
-        "self": openai.Gpt3ModelType(),
+    assert gpt3.Gpt3Model.complete.input_type.arg_types == {
+        "self": gpt3.Gpt3ModelType(),
         "prompt": types.String(),
         "seed": types.Int(),
     }
-    assert openai.Gpt3Model.complete.output_type == types.TypedDict(
+    assert gpt3.Gpt3Model.complete.output_type == types.TypedDict(
         {
             "id": types.String(),
             "object": types.String(),
