a74275bd460b3c342d508a14cfd7917dd48f4b5f
Stacey Svetlichnaya <45573465+staceysv@users.noreply.github.com>
2022-07-11 20:12:12 -0700

    adding model_view and model_view_panel_render to bertviz functionality (#66)
    * adding model_view and model_view_panel_render to bertviz functionality
    
    * learning how to use this hook
    
    Co-authored-by: Anastasia Svetlichnaya <stacey@Anastasias-MacBook-Pro.local>

--- weave/ecosystem/bertviz/panels.py
+++ weave/ecosystem/bertviz/panels.py
@@ -32,7 +32,7 @@ def head_view(attention: huggingface.ModelOutputAttention) -> weave.ops.Html:
 
 
 # An op that returns a panel is treated by the WeaveUI code as a panel. It will
-# show up in the panel choices if the current expressoins' output type matches
+# show up in the panel choices if the current expression's output type matches
 # the type of the panel input (the first argument in the op declaration.)
 @weave.op()
 def head_view_panel_render(
@@ -44,4 +44,29 @@ def head_view_panel_render(
     # We add the lazy call as input to the returned Html panel. Nothing has been
     # computed so far. The UI's Html panel will perform a useNodeValue operation on its
     # input node. Only then will the head_view function finally be called.
+    return weave.panels.Html(html)
+
+
+@weave.op()
+def model_view(attention: huggingface.ModelOutputAttention) -> weave.ops.Html:
+    # Parallels head_view() to visualize the full matrix of attention heads as rows
+    # and layers as columns for each attention map
+
+    # walk to model from attention
+    model_output = attention._model_output
+    encoded_model_input = model_output._encoded_model_input
+    model = model_output._model
+
+    tokens = model.tokenizer().convert_ids_to_tokens(encoded_model_input[0])
+    html = bertviz.model_view(attention._attention, tokens, html_action="return")
+
+    # TODO: this would read better as weave.types.Html I think.
+    return weave.ops.Html(html.data)
+
+
+@weave.op()
+def model_view_panel_render(
+    attention: weave.Node[huggingface.ModelOutputAttention],
+) -> weave.panels.Html:
+    html = model_view(attention)
     return weave.panels.Html(html)
