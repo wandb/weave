3fd98c41de905f68bc579599e8c8cfd70e89cc08
Tim <tim.s.sweeney@gmail.com>
2022-07-29 18:08:56 -0700

    more;

--- weave/api.py
+++ weave/api.py
@@ -47,6 +47,25 @@ def save(node_or_obj, name=None):
         return _weave_internal.make_const_node(ref.type, ref.obj)
 
 
+# # The bad part about this is that the op chain is lost
+# # when we just do `use`.
+# def save(node_or_obj, name=None):
+#     if isinstance(node_or_obj, _graph.Node):
+#         from .ops_primitives.weave_api import save as op_save
+
+#         return op_save(node_or_obj, name=name)
+#     else:
+#         ref = _storage.save(node_or_obj, name=name)
+
+#         # TODO: This doesn't return op_get
+#         # But somehow we eventually get an op_get... how?
+#         from .ops_primitives.weave_api import get as op_get
+
+#         return op_get(str(ref))
+
+#         # return _weave_internal.make_const_node(ref.type, ref.obj)
+
+
 def publish(node_or_obj, name=None):
     if isinstance(node_or_obj, _graph.Node):
         # Why call use? wouldn't we want to pass the graph itself, not the result of use??
