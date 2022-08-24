1d2e914405d1e8e781ffa67aba23653c15d45bc2
Danny Goldstein <dgold@berkeley.edu>
2022-08-23 19:20:54 -0700

    move stuff around to fix tests

--- weave/ops_domain/__init__.py
+++ weave/ops_domain/__init__.py
@@ -4,9 +4,8 @@ import typing
 
 from wandb.apis import public as wandb_api
 
-from .. import panels
-from ..api import op, weave_class, type, use, get, type_of, Node
-from ..ops_primitives import TypedDict
+from .run_segment import RunSegment, run_segment_render
+from ..api import op, weave_class
 from .. import weave_types as types
 from . import wbartifact
 from . import file_wbartifact
@@ -336,83 +335,3 @@ class Project:
 @op(name="root-project")
 def project(entityName: str, projectName: str) -> wandb_api.Project:
     return wandb_public_api().project(name=projectName, entity=entityName)
-
-
-@type()
-class RunSegment:
-    name: str
-    prior_run_ref: typing.Optional[str]
-    resumed_from_step: int
-    metrics: typing.Any
-
-    @op(render_info={"type": "function"})
-    def refine_experiment_type(self) -> types.Type:
-        """Assuming a constant type over history rows for now."""
-        segment = self
-        metrics = segment.metrics
-        resumed_from_step = self.resumed_from_step
-        while len(metrics) == 0:
-            if segment.prior_run_ref is None:
-                # no history - return empty
-                return types.List(object_type=types.Any())
-
-            segment = use(get(segment.prior_run_ref))
-            metrics = segment.metrics[:resumed_from_step]
-            resumed_from_step = segment.resumed_from_step
-
-        # get the first row and use it to infer the type
-        example_row = metrics[0]
-        name_type = types.TypedDict({"name": types.String()})
-        return types.List(types.merge_types(type_of(example_row), name_type))
-
-    def _experiment_body(self, until: typing.Optional[int] = None) -> typing.Any:
-        prior_run_metrics: typing.Any = []
-        if self.prior_run_ref is not None:
-            # get the prior run
-            prior_run: RunSegment = use(get(self.prior_run_ref))
-            prior_run_metrics = prior_run._experiment_body(until=self.resumed_from_step)
-
-        own_metrics: typing.Any = [
-            {
-                "step": d["step"],
-                "name": self.name,
-                **d,  # type: ignore
-            }
-            for d in self.metrics[:until]
-        ]
-
-        return prior_run_metrics + own_metrics
-
-    @op(refine_output_type=refine_experiment_type)
-    def experiment(self) -> typing.Any:
-        return self._experiment_body()
-
-    @op()
-    def refine_history_type(self) -> types.Type:
-        return type_of(self.metrics)
-
-    @op(refine_output_type=refine_history_type)
-    def history(self) -> typing.Any:
-        return self.metrics
-
-
-@op()
-def run_segment_render(
-    run_segment_node: Node[RunSegment],
-) -> panels.Card:
-
-    # All methods callable on X are callable on weave.Node[X], but
-    # the types arent' setup properly, so cast to tell the type-checker
-    # TODO: Fix!
-    run_segment = typing.cast(RunSegment, run_segment_node)
-
-    return panels.Card(
-        title=run_segment.name,
-        subtitle="Weave Run Segment",
-        content=[
-            panels.CardTab(
-                name="History",
-                content=panels.Table(run_segment.metrics),  # type: ignore
-            ),
-        ],
-    )
---
+++ weave/ops_domain/run_segment.py
@@ -0,0 +1,83 @@
+import typing
+
+from weave import type, op, weave_types as types, use, get, type_of, Node, panels
+
+
+@type()
+class RunSegment:
+    name: str
+    prior_run_ref: typing.Optional[str]
+    resumed_from_step: int
+    metrics: typing.Any
+
+    @op(render_info={"type": "function"})
+    def refine_experiment_type(self) -> types.Type:
+        """Assuming a constant type over history rows for now."""
+        segment = self
+        metrics = segment.metrics
+        resumed_from_step = self.resumed_from_step
+        while len(metrics) == 0:
+            if segment.prior_run_ref is None:
+                # no history - return empty
+                return types.List(object_type=types.Any())
+
+            segment = use(get(segment.prior_run_ref))
+            metrics = segment.metrics[:resumed_from_step]
+            resumed_from_step = segment.resumed_from_step
+
+        # get the first row and use it to infer the type
+        example_row = metrics[0]
+        name_type = types.TypedDict({"name": types.String()})
+        return types.List(types.merge_types(type_of(example_row), name_type))
+
+    def _experiment_body(self, until: typing.Optional[int] = None) -> typing.Any:
+        prior_run_metrics: typing.Any = []
+        if self.prior_run_ref is not None:
+            # get the prior run
+            prior_run: RunSegment = use(get(self.prior_run_ref))
+            prior_run_metrics = prior_run._experiment_body(until=self.resumed_from_step)
+
+        own_metrics: typing.Any = [
+            {
+                "step": d["step"],
+                "name": self.name,
+                **d,  # type: ignore
+            }
+            for d in self.metrics[:until]
+        ]
+
+        return prior_run_metrics + own_metrics
+
+    @op(refine_output_type=refine_experiment_type)
+    def experiment(self) -> typing.Any:
+        return self._experiment_body()
+
+    @op()
+    def refine_history_type(self) -> types.Type:
+        return type_of(self.metrics)
+
+    @op(refine_output_type=refine_history_type)
+    def history(self) -> typing.Any:
+        return self.metrics
+
+
+@op()
+def run_segment_render(
+    run_segment_node: Node[RunSegment],
+) -> panels.Card:
+
+    # All methods callable on X are callable on weave.Node[X], but
+    # the types arent' setup properly, so cast to tell the type-checker
+    # TODO: Fix!
+    run_segment = typing.cast(RunSegment, run_segment_node)
+
+    return panels.Card(
+        title=run_segment.name,
+        subtitle="Weave Run Segment",
+        content=[
+            panels.CardTab(
+                name="History",
+                content=panels.Table(run_segment.metrics),  # type: ignore
+            ),
+        ],
+    )
