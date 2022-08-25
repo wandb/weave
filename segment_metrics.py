8ede318277c4bc26501bb53e8e99e976e91ea2a0
Danny Goldstein <dgold@berkeley.edu>
2022-08-25 14:29:26 -0700

    add docs

--- segment_metrics.py
+++ segment_metrics.py
@@ -6,15 +6,15 @@ import numpy as np
 # serializer = publish   # uses w&b artifacts intead of local artifacts
 serializer = storage.save
 
+N_METRICS = 100
+
 
 def random_metrics(n=10, starting_index=0):
-    """Creates an array of metrics of length n starting from step starting_index."""
+    """Create an array of metrics of length n starting from step starting_index."""
     return [
         {
             "step": starting_index + i + 1,
-            "metricA": np.random.random() + np.exp(i / 2),
-            "metricB": np.random.random() + np.exp(i / 4),
-            "metricC": np.random.choice(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")),
+            **{f"metric{j + 1}": np.random.random() for j in range(N_METRICS)},
         }
         for i in range(n)
     ]
@@ -22,10 +22,31 @@ def random_metrics(n=10, starting_index=0):
 
 def create_branch(
     name: str,
-    previous_segment: typing.Optional[RunSegment],
+    previous_segment: typing.Optional[RunSegment] = None,
     length=10,
     branch_frac=0.8,
 ) -> RunSegment:
+    """Create a new segment and optionally attach it to a previous segment.
+
+    Parameters
+    ----------
+    name: str
+       The name of the segment.
+    previous_segment: Optional[RunSegment], default None.
+       The parent run segment. If this is a root run segment, use None.
+    length: int, default = 10
+       The number of history rows to generate for the segment.
+    branch_frac: float between 0 and 1.
+       Parameter describing where in the previous segment to set the branch point.
+       A branch_frac of 0 sets the branch point at the previous segment's root,
+       whereas a branch_frac of 1 sets the branch point at the end of the previous
+       segment.
+
+    Returns
+    -------
+    segment: RunSegment
+        The new segment.
+    """
     if previous_segment:
         previous_metrics = previous_segment.metrics
         starting_index = previous_metrics[0]["step"] + min(
