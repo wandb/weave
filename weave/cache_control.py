2e34ca349aca708f7fa6432436a55d9df7f87968
Danny Goldstein <dgold@berkeley.edu>
2022-09-12 15:35:11 -0700

    add dummy cache controllers

--- weave/cache_control.py
+++ weave/cache_control.py
@@ -1,3 +1,11 @@
-from .registry_mem import memory_registry
+from .decorators import cache_control
 
-ops = memory_registry.list_ops()
+
+@cache_control("list")
+def list_cache_control(**l):
+    return len(l) > 1
+
+
+@cache_control("unnest")
+def unnest_cache_control(arr):
+    return False
--- weave/decorators.py
+++ weave/decorators.py
@@ -2,3 +2,4 @@ from .decorator_op import op
 from .decorator_class import weave_class
 from .decorator_mutation import mutation
 from .decorator_type import type
+from .decorator_cache_control import cache_control
