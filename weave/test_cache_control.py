7a861907e7cceb9eb55d4026533357aaae3156bc
Danny Goldstein <dgold@berkeley.edu>
2022-09-13 13:25:05 -0700

    feat: cache disallowlist (#140)

---
+++ weave/test_cache_control.py
@@ -0,0 +1,23 @@
+from .ops import list_, dict_
+from . import forward_graph
+from .execute import execute_forward
+from . import compile
+
+
+def test_cache_control():
+    target = {"t": 1, "b": [1, 2, 3, 4]}
+    node_that_should_not_cache = list_.make_list(**{"0": target})
+    second_node_that_should_not_cache = list_.unnest(node_that_should_not_cache)
+
+    nodes = compile.compile([second_node_that_should_not_cache])
+    fg = forward_graph.ForwardGraph(nodes)
+    stats = execute_forward(fg, no_cache=False)
+    summary = stats.summary()
+    assert sum([v["cache_used"] for v in summary.values()]) == 0
+
+    node_that_should_cache = dict_(a=second_node_that_should_not_cache)
+    nodes = compile.compile([node_that_should_cache])
+    fg = forward_graph.ForwardGraph(nodes)
+    stats = execute_forward(fg, no_cache=False)
+    summary = stats.summary()
+    assert sum([v["cache_used"] for v in summary.values()]) == 1
