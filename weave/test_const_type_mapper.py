514686be2615324d14b49e552bb4f0839b7786a7
Tim Sweeney <tim.s.sweeney@gmail.com>
2022-09-13 07:30:21 -0700

    fix(weave): Fix Const Node Access (#138)

---
+++ weave/test_const_type_mapper.py
@@ -0,0 +1,24 @@
+import weave
+from . import weave_internal
+import typing
+from . import context_state
+
+_loading_builtins_token = context_state.set_loading_built_ins()
+
+MetricRows = typing.TypeVar("MetricRows")
+
+
+@weave.type()
+class TestType(typing.Generic[MetricRows]):
+    metrics: MetricRows
+
+
+context_state.clear_loading_built_ins(_loading_builtins_token)
+
+
+def test_const_type_mapper():
+    node = weave_internal.make_const_node(
+        TestType.WeaveType(weave.types.List(weave.types.Number())), None
+    )
+    fn_node = weave.define_fn({"row": weave.types.Number()}, lambda row: row + 1)
+    node.metrics.map(fn_node)
