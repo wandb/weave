3b3b343c5c04216aebffef40b4aaa26245a2812b
Danny Goldstein <dgold@berkeley.edu>
2022-09-13 11:38:40 -0700

    rename test

--- weave/test_cache_control.py
+++ weave/test_cache_control.py
@@ -4,7 +4,7 @@ from .execute import execute_forward
 from . import compile
 
 
-def test_cache_control_decorator():
+def test_cache_control():
     target = {"t": 1, "b": [1, 2, 3, 4]}
     node_that_should_not_cache = list_.make_list(**{"0": target})
     second_node_that_should_not_cache = list_.unnest(node_that_should_not_cache)
