00ce52bf7b392a6f43942b0e603a460607f0d117
Tim Sweeney <tim.s.sweeney@gmail.com>
2022-09-14 10:58:23 -0700

    chore(dx): Add mypy type annotation requirements to the weave package (#142)
    * init: mypy
    
    * Moved tests and added exceptions
    
    * test path fix

---
+++ weave/tests/test_custom_types.py
@@ -0,0 +1,74 @@
+import pytest
+from PIL import Image
+
+from weave.ecosystem.wandb import geom
+
+from .. import api as weave
+from .. import storage
+from ..ops_primitives import arrow
+
+
+def test_mapped_method_on_custom_type():
+    segment = geom.LineSegment(0.0, 0.1, 0.9, 1.1)
+
+    len = weave.use(segment.length())
+    assert len == 1.81
+
+    mid = weave.use(segment.midpoint())
+    assert mid.x == 0.45
+    assert mid.y == 0.6
+
+    segments = arrow.to_arrow(
+        [
+            geom.LineSegment(0.0, 0.1, 0.9, 1.1),
+            geom.LineSegment(0.0, 0.2, 0.4, 1.1),
+        ]
+    )
+    lens = weave.use(segments.map(lambda seg: seg.length()))
+    assert weave.use(lens[0]) == 1.81
+    assert weave.use(lens[1]) == pytest.approx(0.97)
+
+    # mid = weave.use(segments.midpoint())
+    # mid = weave.use(segments.map(lambda seg: seg.midpoint()))
+    # assert mid[0].x == 0.45
+    # assert mid[0].y == 0.6
+    # assert mid[1].x == 0.2
+    # assert mid[1].y == pytest.approx(0.65)
+
+
+def test_mapped_method_returning_custom_type():
+    segments = arrow.to_arrow(
+        [
+            geom.LineSegment(0.0, 0.1, 0.9, 1.1),
+            geom.LineSegment(0.0, 0.2, 0.4, 1.1),
+        ]
+    )
+    mid = weave.use(segments.map(lambda seg: seg.midpoint()))
+
+    assert weave.use(mid[0].x) == 0.45
+    assert weave.use(mid[0].y) == 0.6
+    assert weave.use(mid[1].x) == 0.2
+    assert weave.use(mid[1].y) == pytest.approx(0.65)
+
+
+def test_mapped_on_fully_custom_type():
+    data = [
+        {"a": 5, "im": Image.linear_gradient("L").rotate(0)},
+        {"a": 6, "im": Image.linear_gradient("L").rotate(4)},
+    ]
+    arrow_arr = arrow.to_arrow(data)
+
+    assert weave.use(arrow_arr.map(lambda row: row["im"].width())).to_pylist() == [
+        256,
+        256,
+    ]
+
+
+def test_mapped_pick():
+    data = [
+        {"a": 5, "b": 9},
+        {"a": 6, "b": 10},
+    ]
+    arrow_arr = arrow.to_arrow(data)
+
+    assert weave.use(arrow_arr.pick("b")).to_pylist() == [9, 10]
---
+++ weave/tests/test_weave_types.py
@@ -0,0 +1,76 @@
+from .. import weave_types as types
+from .. import runs
+from rich import print
+
+
+def test_typeof_string():
+    t = types.TypeRegistry.type_of("x")
+    assert t == types.String()
+
+
+def test_typeof_list_const_string():
+    t = types.TypeRegistry.type_of(["x"])
+    assert t == types.List(types.String())
+
+
+def test_serialize_const_string():
+    t = types.Const(types.String(), "x")
+    ser = t.to_dict()
+    deser = types.TypeRegistry.type_from_dict(ser)
+    assert t == deser
+
+
+def test_typedict_assign_keys_are_stable():
+    for i in range(10):
+        t = types.TypedDict(
+            {"a": types.String(), "b": types.String(), "c": types.String()}
+        )
+        t2 = types.TypedDict(
+            {"a": types.String(), "b": types.String(), "c": types.String()}
+        )
+        r = t.assign_type(t2)
+        assert list(r.property_types.keys()) == ["a", "b", "c"]
+
+
+def test_typeof_bool():
+    assert types.TypeRegistry.type_of(False) == types.Boolean()
+
+
+def test_typeof_type():
+    assert types.TypeRegistry.type_of(types.Int()) == types.Type()
+
+
+def test_type_tofromdict():
+    assert types.Type().to_dict() == "type"
+    assert types.TypeRegistry.type_from_dict("type") == types.Type()
+
+
+def test_typeof_list_runs():
+    l = [
+        runs.Run("a", "op", inputs={"a": "x"}, output=4.9),
+        runs.Run("b", "op", inputs={"a": "x", "b": 9}, output=3.3),
+    ]
+    actual = types.TypeRegistry.type_of(l)
+    print("test_typeof_list_runs.actual", actual)
+
+    assert actual == types.List(
+        types.UnionType(
+            types.RunType(
+                inputs=types.TypedDict({"a": types.String()}),
+                history=types.List(types.UnknownType()),
+                output=types.Float(),
+            ),
+            types.RunType(
+                inputs=types.TypedDict({"a": types.String(), "b": types.Int()}),
+                history=types.List(types.UnknownType()),
+                output=types.Float(),
+            ),
+        )
+    )
+
+
+def test_typeof_list_dict_merge():
+    d = [{"a": 6, "b": "x"}, {"a": 5, "b": None}]
+    assert types.TypeRegistry.type_of(d) == types.List(
+        types.TypedDict({"a": types.Int(), "b": types.optional(types.String())})
+    )
