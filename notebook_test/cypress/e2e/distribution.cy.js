e41d89947c1c3b7b0c1e528d3590d47162baddfd
Shawn Lewis <shlewis@gmail.com>
2022-10-24 20:42:54 -0700

    Try to change working-directory

--- notebook_test/cypress/e2e/distribution.cy.js
+++ notebook_test/cypress/e2e/distribution.cy.js
@@ -2,7 +2,7 @@ describe('empty spec', () => {
   it('passes', () => {
     // If the notebook output is not clear, this will take too long.
     // TODO: auto-clear
-    cy.visit('http://localhost:8888/notebooks/examples/vis/Distribution.ipynb')
+    cy.visit('http://localhost:8888/notebooks/examples/vis/Distribution.ipynb?token=abcd')
 
     // Restart and run all
     cy.get('#kernellink').click()
