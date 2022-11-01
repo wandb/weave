37b645b61153c0e252b490fc208450351b09d97b
Shawn Lewis <shlewis@gmail.com>
2022-10-31 21:05:22 -0700

    Disable confusion

--- notebook_test/cypress/e2e/notebooks/confusion.cy.ts
+++ notebook_test/cypress/e2e/notebooks/confusion.cy.ts
@@ -1,6 +1,6 @@
 import {checkWeaveNotebookOutputs} from './notebooks';
 
-describe('confusion notebook test', () => {
+describe.skip('confusion notebook test', () => {
   it('passes', () =>
     checkWeaveNotebookOutputs('../examples/vis/Confusion.ipynb'));
 });
