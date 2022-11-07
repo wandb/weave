f962d16ad0993a9b3ef588695e964b4088ca0682
Shawn Lewis <shlewis@gmail.com>
2022-11-06 20:26:25 -0800

    Move Table Summary Example panel to broken

--- integration_test/cypress/e2e/notebooks/Table Summary Panel.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from './notebooks';
-
-describe('../examples/Table Summary Panel.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/Table Summary Panel.ipynb')
-    );
-});
