cd9090cd039b1f308fbd752976b4a944a3f4ac44
Shawn Lewis <shlewis@gmail.com>
2022-11-07 07:00:07 -0800

    Fix LabeledItem lint

--- weave/panels/panel_labeled_item.py
+++ weave/panels/panel_labeled_item.py
@@ -11,8 +11,8 @@ ItemType = typing.TypeVar("ItemType")
 
 @weave.type()
 class LabeledItemConfig(typing.Generic[ItemType]):
-    label: str = dataclasses.field(default_factory=lambda: False)
-    item: ItemType = dataclasses.field(default_factory=lambda: graph.VoidNode())
+    label: str
+    item: ItemType
 
 
 @weave.type()
@@ -24,30 +24,7 @@ class LabeledItem(panel.Panel):
         super().__init__(input_node=input_node, vars=vars)
         self.config = config
         if self.config is None:
-            self.config = LabeledItemConfig()
-        if "label" in options:
-            self.config.label = options["label"]
-        if "item" in options:
-            self.config.item = panel_util.child_item(options["item"])
-        # self._normalize()
-
-    # def set_label(self, label: str):
-    #     self._label = label
-
-    # # TODO: Type! Can be Panel | Node | any Weavable type
-    # #       get rid of the "any Weaveable type" bit by making this into
-    # #       an op!
-    # def set_item(self, item: typing.Any):
-    #     self._item = item
-
-    # def set_height(self, height: typing.Optional[int]):
-    #     self._height = height
-
-    # @property
-    # def config(self):
-    #     # TODO: handle item being a Panel!
-    #     return {
-    #         "label": self._label,
-    #         "item": panel_util.child_item(self._item).to_json(),
-    #         "height": self._height,
-    #     }
+            self.config = LabeledItemConfig(
+                options["label"],
+                panel_util.child_item(options["item"]),
+            )
