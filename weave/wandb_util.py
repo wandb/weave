15e6730f583f3339d27e20045866e8a7033bd2d4
Tim Sweeney <tim@wandb.com>
2022-11-15 16:51:40 -0800

    types: Implements remaining dtypes <> weave types  (#224)
    * init
    
    * fixed test

--- weave/wandb_util.py
+++ weave/wandb_util.py
@@ -14,12 +14,39 @@ def weave0_type_json_to_weave1_type(old_type_json: typing.Any) -> types.Type:
 
 
 def _convert_type(old_type: wandb_dtypes.Type) -> types.Type:
-    if isinstance(old_type, wandb_dtypes.TypedDictType):
+    if type(old_type) == wandb_dtypes.Type:
+        return types.Type()
+    elif isinstance(old_type, wandb_dtypes.TypedDictType):
         return types.TypedDict(
             {
                 prop_name: _convert_type(old_type)
                 for prop_name, old_type in old_type.params["type_map"].items()
             }
+        )
+    elif isinstance(old_type, wandb_dtypes.ListType):
+        return types.List(_convert_type(old_type.params["element_type"]))
+    elif isinstance(old_type, wandb_dtypes.InvalidType):
+        return types.Invalid()
+    elif isinstance(old_type, wandb_dtypes.AnyType):
+        return types.Any()
+    elif isinstance(old_type, wandb_dtypes.UnknownType):
+        return types.UnknownType()
+    elif isinstance(old_type, wandb_dtypes.TimestampType):
+        raise errors.WeaveInternalError(
+            "converting old Weave type not yet implemented: %s" % type(old_type)
+        )
+    elif isinstance(old_type, wandb_dtypes.BooleanType):
+        return types.Boolean()
+    elif isinstance(old_type, wandb_dtypes.PythonObjectType):
+        return types.type_class_type_name(old_type.params["class_name"])
+    elif isinstance(old_type, wandb_dtypes.ConstType):
+        val = old_type.params["val"]
+        if old_type.params["is_set"]:
+            val = set(val)
+        return types.Const(types.TypeRegistry.type_of(val), val)
+    elif isinstance(old_type, wandb_dtypes.NDArrayType):
+        raise errors.WeaveInternalError(
+            "converting old Weave type not yet implemented: %s" % type(old_type)
         )
     elif isinstance(old_type, wandb_dtypes.UnionType):
         return types.UnionType(
