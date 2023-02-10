f1dfae62c1b035c3f3f1cba755fb776c5254c0bb
Shawn <shlewis@gmail.com>
2023-02-10 11:57:50 -0800

    Fix optional lists in wandb Table (#549)

--- weave/tests/test_wb_tables.py
+++ weave/tests/test_wb_tables.py
@@ -2,6 +2,8 @@ import wandb
 import weave
 from weave.artifact_local import LocalArtifact, LocalArtifactType
 from weave.ops_domain.wandb_domain_gql import _make_alias
+from weave.ops_domain import wbmedia
+from weave.ops_domain import table
 from ..weave_internal import make_const_node
 
 from ..artifact_wandb import WandbArtifact, WandbArtifactType, WeaveWBArtifactURI
@@ -72,3 +74,35 @@ def test_wb_partitioned_table(fake_wandb):
     cell_node = rows_node[0]
     assert weave.use(rows_node.count()) == 3
     assert weave.use(cell_node) == {"a": 1.0, "b": 2.0, "c": 3.0}
+
+
+def test_convert_optional_list_cell(fake_wandb):
+    tab = wandb.Table(columns=["a"])
+    tab.add_data([wandb.Html("<p>hello</p>")])
+    art = wandb.Artifact("test_name", "test_type")
+    art.add(tab, "table")
+    art_node = fake_wandb.mock_artifact_as_node(art)
+    file_node = art_node.file("table.table.json")
+    table_node = file_node.table()
+    table_rows = table_node.rows()
+    awl = weave.use(table_rows)
+    # The wandb library makes everything optional, which is what we want to
+    # test here.
+    # Make sure the mapped Weave1 type is what we expect.
+    assert awl.object_type == weave.types.TypedDict(
+        {
+            "a": weave.types.optional(
+                weave.types.List(wbmedia.HtmlArtifactFileRef.WeaveType())
+            )
+        }
+    )
+    assert awl.to_pylist_raw() == [
+        {
+            "a": [
+                {
+                    "artifact": "wandb-artifact:///test_entity/test_project/test_name:v0",
+                    "path": "media/html/03ac15e611be692f058e.html",
+                }
+            ]
+        }
+    ]
