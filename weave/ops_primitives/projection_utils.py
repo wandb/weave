d55af078b286216c368faf9a07ded01e61a6ad29
Tim Sweeney <tim@wandb.com>
2023-02-16 11:24:07 -0800

    init (#585)

--- weave/ops_primitives/projection_utils.py
+++ weave/ops_primitives/projection_utils.py
@@ -40,14 +40,9 @@ def perform_2D_projection(
             np_array_of_embeddings, algorithmOptions.get("tnse", {})
         )
     elif projectionAlgorithm == "umap":
-        # Here, we are intentionally using PCA because UMAP is horribly slow
-        # on even some basic cases and it is causing problems with Weave1 service.
-        projection = perform_2D_projection_pca(
-            np_array_of_embeddings, algorithmOptions.get("pca", {})
+        projection = perform_2D_projection_umap(
+            np_array_of_embeddings, algorithmOptions.get("umap", {})
         )
-        # projection = perform_2D_projection_umap(
-        #     np_array_of_embeddings, algorithmOptions.get("umap", {})
-        # )
     else:
         raise Exception("Unknown projection algorithm: " + projectionAlgorithm)
     return projection
@@ -62,22 +57,11 @@ def limit_embedding_dimensions(
     return np_array_of_embeddings
 
 
-def fit_subsample(np_array_of_embeddings: np.ndarray, size: int = 1500) -> np.ndarray:
-    data_len = len(np_array_of_embeddings)
-    if data_len <= size:
-        return np_array_of_embeddings
-
-    sample_ids = np.random.randint(data_len, size=size)
-    return np_array_of_embeddings[sample_ids]
-
-
 def perform_2D_projection_pca(
     np_array_of_embeddings: np.ndarray, options: dict
 ) -> np.ndarray:
     model = PCA(n_components=2)
-    fit_data = fit_subsample(np_array_of_embeddings)
-    model.fit(fit_data)
-    return model.transform(np_array_of_embeddings)
+    return model.fit_transform(np_array_of_embeddings)
 
 
 def perform_2D_projection_tsne(
@@ -102,14 +86,13 @@ umap_lock = threading.Lock()
 def perform_2D_projection_umap(
     np_array_of_embeddings: np.ndarray, options: dict
 ) -> np.ndarray:
-    fit_data = fit_subsample(np_array_of_embeddings)
-    n_samples = len(fit_data)
     model = _get_umap().UMAP(
         n_components=2,
-        n_neighbors=min(n_samples - 1, options.get("neighbors", 15)),
-        min_dist=options.get("minDist", 0.1),
-        spread=options.get("spread", 1.0),
+        # Letting the library choose defaults
+        # n_neighbors=min(n_samples - 1, options.get("neighbors", 15)),
+        # min_dist=options.get("minDist", 0.1),
+        # spread=options.get("spread", 1.0),
+        init="random",
     )
     with umap_lock:
-        model.fit(fit_data)
-        return model.transform(np_array_of_embeddings)
+        return model.fit_transform(np_array_of_embeddings)
