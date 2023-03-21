36d9ff760110828fe50f291e685c1aa37c449ca7
Nick Penaranda <nick.penaranda@wandb.com>
2023-03-21 14:07:39 -0400

    restore build.sh

---
+++ weave/frontend/build.sh
@@ -0,0 +1,10 @@
+set -e
+
+cd "$WANDB_CORE"/frontends/app/weave-ui
+yarn build
+cd -
+rm -rf assets index.html
+cp -R "$WANDB_CORE"/frontends/app/weave-ui/build/* .
+git -C "$WANDB_CORE" describe --match="" --always --dirty > core_version
+if grep -q dirty core_version; then echo "!!! WARNING: frontend was built from dirty wandb/core !!!"; fi
+
