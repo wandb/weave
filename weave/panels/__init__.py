e00ae8b9d0875f5445aeeebb9861ba0d3c3fd1c5
Shawn Lewis <shlewis@gmail.com>
2023-04-11 13:34:27 -0700

    Organize panel imports

--- weave/panels/__init__.py
+++ weave/panels/__init__.py
@@ -1,19 +1,28 @@
 from ..panel import Panel
+
+# special
 from .panel_expression import *
 from .panel_auto import *
-from .panel_table import Table
-from .panel_query import Query
-from .panel_plot import Plot
-from .panel_facet import Facet
+
+# layout
 from .panel_labeled_item import LabeledItem
-from .panel_each import Each
 from .panel_card import Card, CardTab
 from .panel_group import Group
+
+# navigation
+from .panel_weavelink import WeaveLink
+
+from .panel_table import Table
+from .panel_plot import Plot
+from .panel_facet import Facet
+from .panel_each import Each
 from .panel_color import Color
 from .panel_html import PanelHtml
 from .panel_markdown import PanelMarkdown
-from .panel_weavelink import WeaveLink
 
+
+# sidebar specific
+from .panel_query import Query
 
 # editors
 from .panel_slider import Slider, SliderConfig
