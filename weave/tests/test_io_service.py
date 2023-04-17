f04cac340bf1f7f3dc193453467505796cc1e5b4
Danny Goldstein <danny@wandb.com>
2023-04-17 16:36:13 -0400

    remove time

--- weave/tests/test_io_service.py
+++ weave/tests/test_io_service.py
@@ -18,7 +18,6 @@ async def test_io_service_async_client(io_server_factory, process):
 
     results = []
     tasks = set()
-    start = time.time()
     async with client.connect() as conn:
         for _ in range(10):
             task = loop.create_task(conn.sleep(0.1))
@@ -30,13 +29,8 @@ async def test_io_service_async_client(io_server_factory, process):
             task.add_done_callback(task_done_callback)
         await asyncio.wait(tasks)
 
-    end = time.time()
-
     assert len(results) == 10
 
-    # if serial, this should be greater than 1 second (10 * 0.1)
-    # if executed concurrently, this should be between 0.1 and 0.2 (0.1 + overhead)
-    assert 0.1 < end - start < 0.2
     for result in results:
         assert result == 0.1
 
@@ -55,17 +49,14 @@ async def test_io_service_sync_client(io_server_factory, process):
         fs=fs,
     )
 
-    start = time.time()
     results = []
     for _ in range(10):
         results.append(client.sleep(0.1))
-    end = time.time()
 
     assert len(results) == 10
 
     # if serial, this should be greater than 1 second (10 * 0.1)
     # if executed concurrently, this should be between 0.1 and 0.2 (0.1 + overhead)
-    assert 1 < end - start < 1.1
     for result in results:
         assert result == 0.1
 
