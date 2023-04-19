21265de9e9dce7376b71b878b1e11f1e3c6e6901
Shawn Lewis <shlewis@gmail.com>
2023-04-19 13:22:04 -0700

    Add show changes missing from earlier commit

--- weave/show.py
+++ weave/show.py
@@ -17,6 +17,8 @@ from . import util
 from . import errors
 from . import usage_analytics
 from . import ref_base
+from . import artifact_fs
+from . import ops
 
 
 def make_varname_for_type(t: types.Type):
@@ -33,24 +35,29 @@ def make_container(
     return Group(preferHorizontal=True, showExpressions=True, items={name: obj})
 
 
-def make_show_obj(obj: typing.Any):
+def make_show_obj(obj: typing.Any) -> tuple[graph.Node, str]:
     node: graph.Node
     if obj is None:
-        return make_container(graph.VoidNode(), "panel0")
+        return graph.VoidNode(), "panel0"
     elif isinstance(obj, panel.Panel):
-        return make_container(obj, obj.id + "0")
+        return obj, obj.id + "0"
     elif isinstance(obj, graph.Node):
-        return make_container(obj, make_varname_for_type(obj.type) + "0")
+        return obj, make_varname_for_type(obj.type)
+    elif isinstance(obj, ref_base.Ref):
+        if isinstance(obj, artifact_fs.FilesystemArtifactRef):
+            name = obj.name
+            uri = obj.branch_uri
+        else:
+            name = make_varname_for_type(obj.type)
+            uri = obj.uri
+        return ops.get(uri), name
 
     if types.TypeRegistry.has_type(obj):
-        print("HAVE TYPE")
-        from weave import ops
-
         names = util.find_names(obj)
 
         ref = storage.save(obj, name=names[-1])
         node = ops.get(ref.uri)
-        return make_container(node, make_varname_for_type(ref.type))
+        return node, make_varname_for_type(ref.type)
 
     raise errors.WeaveTypeError(
         "%s not yet supported. Create a weave.Type to add support." % type(obj)
@@ -59,71 +66,24 @@ def make_show_obj(obj: typing.Any):
 
 # Broken out into to separate function for testing
 def _show_params(obj):
-    obj_in_container = make_show_obj(obj)
-    ref = storage.save(obj_in_container, name="panel:latest")
-    ref.artifact._version = "latest"
-    show_node = graph.OutputNode(
-        types.UnknownType(),
-        "get",
-        {"uri": graph.ConstNode(types.String(), str(ref))},
-    )
+    # Get a weave node (expression) that will return the object, and something to call it
+    node, var_name = make_show_obj(obj)
+
+    # Place the node inside a root group, so the user can add panels.
+    ui_root = make_container(node, var_name)
+
+    # Name the panel artifact after the object's variable name for now,
+    # and only create a new one if the name is not take yet.
+    # If the name exists, we'll render whatever is already stored there!
+    panel_name = "dashboard-" + var_name
+    panel_ref = storage.get_local_version_ref(panel_name, "latest")
+    if panel_ref is None:
+        panel_ref = storage.save(ui_root, name=f"{panel_name}:latest")
+
+    # convert panel_ref to a a get expression.
+    show_node = ops.get(panel_ref.branch_uri)
+
     return {"weave_node": weavejs_fixes.fixup_node(show_node)}
-
-    if obj is None:
-        return {"weave_node": graph.VoidNode()}
-    if isinstance(obj, graph.Node):
-        if isinstance(obj, graph.OutputNode) and obj.from_op.name == "get":
-            # If its a basic get op, remove the type from the node, the
-            # frontend will fetch it by refining. We could do this for all nodes
-            # since the frontend currently refines, but there may be compatibility bugs.
-            # So for now just do it for get(), which will definitely refine correctly.
-            # This allows us to send nodes of a fixed size (the length of a local artifact
-            # uri plus a little overhead) via get parameter. We can store anything in artifacts,
-            # so we can communicate any sized object to the browser this way.
-            obj = graph.OutputNode(
-                types.UnknownType(),
-                "get",
-                {"uri": obj.from_op.inputs["uri"]},
-            )
-        return {"weave_node": weavejs_fixes.fixup_node(obj)}
-
-    elif isinstance(obj, panel.Panel):
-        ref = storage.save(obj)
-        node = graph.OutputNode(
-            types.UnknownType(),
-            "get",
-            {"uri": graph.ConstNode(types.String(), str(ref))},
-        )
-        return {"weave_node": weavejs_fixes.fixup_node(node)}
-
-        # converted = storage.to_python(obj)["_val"]
-
-        # return {
-        #     "weave_node": weavejs_fixes.fixup_node(obj.input_node),
-        #     "panel_id": converted["id"],
-        #     "panel_config": weavejs_fixes.fixup_data(converted["config"]),
-        # }
-
-    elif isinstance(obj, storage.Ref):
-        from weave import ops
-
-        node = ops.get(obj.uri)
-        return {"weave_node": weavejs_fixes.fixup_node(node)}
-
-    elif types.TypeRegistry.has_type(obj):
-        from weave import ops
-
-        names = util.find_names(obj)
-
-        ref = storage.save(obj, name=names[-1])
-        node = ops.get(ref.uri)
-
-        return {"weave_node": weavejs_fixes.fixup_node(node)}
-
-    else:
-        raise errors.WeaveTypeError(
-            "%s not yet supported. Create a weave.Type to add support." % type(obj)
-        )
 
 
 def show_url(obj=None):
