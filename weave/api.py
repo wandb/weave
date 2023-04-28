ef7d95416fb4ce5e67a537fc4d7b73591827483a
Tim Sweeney <tim@wandb.com>
2023-04-28 11:26:16 -0700

    CI beginning work

--- weave/api.py
+++ weave/api.py
@@ -32,30 +32,30 @@ from .weave_internal import define_fn
 Node = _graph.Node
 
 
-# def save(node_or_obj, name=None):
-#     ref = _storage.save(node_or_obj, name=name)
-#     return _ops.get(str(ref))
-
-
-# def publish(node_or_obj, name=None):
-#     ref = _storage.publish(node_or_obj, name=name)
-#     return _ops.get(str(ref))
-
-
 def save(node_or_obj, name=None):
-    if isinstance(node_or_obj, _graph.Node):
-        return _ops.save(node_or_obj, name=name)
-    else:
-        ref = _storage.save(node_or_obj, name=name)
-        return _ops.get(str(ref))
+    ref = _storage.save(node_or_obj, name=name)
+    return _ops.get(str(ref))
 
 
 def publish(node_or_obj, name=None):
-    if isinstance(node_or_obj, _graph.Node):
-        node_or_obj = use(node_or_obj)
+    ref = _storage.publish(node_or_obj, name=name)
+    return _ops.get(str(ref))
 
-    ref = _storage.publish(node_or_obj, name)
-    return _weave_internal.make_const_node(ref.type, ref.obj)
+
+# def save(node_or_obj, name=None):
+#     if isinstance(node_or_obj, _graph.Node):
+#         return _ops.save(node_or_obj, name=name)
+#     else:
+#         ref = _storage.save(node_or_obj, name=name)
+#         return _ops.get(str(ref))
+
+
+# def publish(node_or_obj, name=None):
+#     if isinstance(node_or_obj, _graph.Node):
+#         node_or_obj = use(node_or_obj)
+
+#     ref = _storage.publish(node_or_obj, name)
+#     return _weave_internal.make_const_node(ref.type, ref.obj)
 
 
 def get(ref_str):
