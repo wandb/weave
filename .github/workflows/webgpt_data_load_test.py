5d5e8e84f301b91a7a644fc6b125eb75e863353f
Jessica Xiang <jessicaxiang1997@gmail.com>
2023-05-16 15:57:20 -0700

    add webgpt load test

---
+++ .github/workflows/webgpt_data_load_test.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+import weave
+
+dataset = load_dataset("openai/webgpt_comparisons", split="train")
+dataset = dataset.remove_columns(
+    ["quotes_0", "tokens_0", "score_0", "quotes_1", "tokens_1", "score_1"]
+)
+dataset = dataset.flatten()
+dataset = dataset.remove_columns(["question.id", "question.dataset"])
+
+weave_input = []
+for data in dataset:
+    weave_input.append(data)
+
+op = weave.save(weave_input, name="webgpt-data:main")
