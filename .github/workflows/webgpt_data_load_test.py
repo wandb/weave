059bbe5a589d62e2de25b32363c6e64335f19450
Tim Hays <tim.hays@wandb.com>
2023-05-17 09:54:09 -0700

    Add stress test to write random items for 5 minutes

--- .github/workflows/webgpt_data_load_test.py
+++ .github/workflows/webgpt_data_load_test.py
@@ -1,4 +1,7 @@
 from datasets import load_dataset
+import random
+import string
+import time
 import weave
 
 dataset = load_dataset("openai/webgpt_comparisons", split="train")
@@ -13,3 +16,16 @@ for data in dataset:
     weave_input.append(data)
 
 op = weave.save(weave_input, name="webgpt-data:main")
+
+# Write to random items
+
+t0 = time.time()
+num_rows = len(weave.use(op))
+i = 0
+while time.time() - t0 < 5*60:
+    idx = random.randint(0, num_rows)
+    # In reality we'll be setting the feedback column but this tests the same operation
+    rand_str = ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
+    op[idx]["answer_0"].set(rand_str)
+    i += 1
+print(f"Items modified after 5 minutes: {i}")
