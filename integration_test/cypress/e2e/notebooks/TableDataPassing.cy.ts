dbe5580fca289587f934cecc2840238a4ffa8b65
Tim Sweeney <tim@wandb.com>
2023-05-18 11:58:08 -0700

    Adds Table Data Passing to Notebook Tests (#788)
    * lint
    
    * fixes notebook
    
    * fixes notebook 2

---
+++ integration_test/cypress/e2e/notebooks/TableDataPassing.cy.ts
@@ -0,0 +1,5 @@
+import {checkWeaveNotebookOutputs} from './notebooks';
+
+describe('../examples/Table Data Passing.ipynb notebook test', () => {
+  it('passes', () => checkWeaveNotebookOutputs('../examples/Table Data Passing.ipynb'));
+});
