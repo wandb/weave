e3b942c15dcde8d7282571e6e02ef12e31907b4b
Danny Goldstein <danny@wandb.com>
2023-05-31 21:35:27 -0400

    make axis settings title node

--- weave-js/src/components/Panel2/PanelPlot/versions/v12.ts
+++ weave-js/src/components/Panel2/PanelPlot/versions/v12.ts
@@ -1,15 +1,35 @@
 import * as weave from '@wandb/weave/core';
+import * as v10 from './v10';
 import * as v11 from './v11';
 
-export type SeriesConfig = Omit<v11.SeriesConfig, 'constants'> & {
+export type SeriesConfig = Omit<
+  v11.SeriesConfig,
+  'constants' | 'axisSettings'
+> & {
   constants: Omit<v11.SeriesConfig['constants'], 'mark'> & {
     mark: weave.Node<'string'> | weave.VoidNode;
   };
 };
 
-export type PlotConfig = Omit<v11.PlotConfig, 'configVersion' | 'series'> & {
+export type AxisSettings = {
+  x: Omit<v10.AxisSetting, 'title'> & {
+    title: weave.Node<'string'> | weave.VoidNode;
+  };
+  y: Omit<v10.AxisSetting, 'title'> & {
+    title: weave.Node<'string'> | weave.VoidNode;
+  };
+  color: Omit<v10.AxisSetting, 'title'> & {
+    title: weave.Node<'string'> | weave.VoidNode;
+  };
+};
+
+export type PlotConfig = Omit<
+  v11.PlotConfig,
+  'configVersion' | 'series' | 'axisSettings'
+> & {
   configVersion: 12;
   series: SeriesConfig[];
+  axisSettings: AxisSettings;
 };
 
 export const migrate = (config: v11.PlotConfig): PlotConfig => {
@@ -26,5 +46,28 @@ export const migrate = (config: v11.PlotConfig): PlotConfig => {
             : weave.constNodeUnsafe('string', series.constants.mark),
       },
     })),
+    axisSettings: {
+      x: {
+        ...config.axisSettings.x,
+        title:
+          config.axisSettings.x.title == null
+            ? weave.voidNode()
+            : weave.constNodeUnsafe('string', config.axisSettings.x.title),
+      },
+      y: {
+        ...config.axisSettings.y,
+        title:
+          config.axisSettings.y.title == null
+            ? weave.voidNode()
+            : weave.constNodeUnsafe('string', config.axisSettings.y.title),
+      },
+      color: {
+        ...config.axisSettings.color,
+        title:
+          config.axisSettings.color.title == null
+            ? weave.voidNode()
+            : weave.constNodeUnsafe('string', config.axisSettings.color.title),
+      },
+    },
   };
 };
