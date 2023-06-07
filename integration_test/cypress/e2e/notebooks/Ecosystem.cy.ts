06810ae848f7bfa2d07c5b257376c94cc933598d
Anastasia Svetlichnaya <stacey@Stacey-Svetlichnaya-C02D64USMD6R.local>
2023-06-06 21:45:24 -0700

    cleaning up some tests for notebook renaming

--- integration_test/cypress/e2e/notebooks/Ecosystem.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from './notebooks';
-
-describe('../examples/Ecosystem.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/Ecosystem.ipynb')
-    );
-});
--- integration_test/cypress/e2e/notebooks/Layout panels.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from './notebooks';
-
-describe('../examples/Layout panels.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/Layout panels.ipynb')
-    );
-});
