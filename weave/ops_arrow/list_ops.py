f8dc093b087439503a468c9fe3348877738d8475
Leonardo Axel Setyanto <laxels@gmail.com>
2023-06-16 15:34:43 -0700

    merge_concat

--- weave/ops_arrow/list_ops.py
+++ weave/ops_arrow/list_ops.py
@@ -3,6 +3,7 @@ import pyarrow as pa
 import pyarrow.compute as pc
 import numpy as np
 import typing
+from builtins import map as builtin_map
 
 from ..api import op, type_of
 from ..decorator_arrow_op import arrow_op
@@ -675,15 +676,25 @@ def concat(arr):
     elif len(arr) == 1:
         return arrow_tags.pushdown_list_tags(arr[0])
 
-    res = arr[0]
-    res = typing.cast(ArrowWeaveList, res)
-    res = arrow_tags.pushdown_list_tags(res)
+    tagged = list(builtin_map(lambda x: arrow_tags.pushdown_list_tags(x), arr))
+    return merge_concat(tagged)
 
-    for i in range(1, len(arr)):
-        tagged = arrow_tags.pushdown_list_tags(arr[i])
-        res = res.concat(tagged)
-    return res
+def merge_concat(arr: list[ArrowWeaveList]) -> ArrowWeaveList:
+    if len(arr) == 0:
+        raise ValueError("arr must not be empty")
+    if len(arr) == 1:
+        return arr[0]
+    left, right = split(arr)
+    return merge(merge_concat(left), merge_concat(right))
 
+def split(arr: list[ArrowWeaveList]) -> tuple[list[ArrowWeaveList], list[ArrowWeaveList]]:
+    if len(arr) < 2:
+        raise ValueError("arr must have length of at least 2")
+    middle_index = len(arr) // 2
+    return arr[:middle_index], arr[middle_index:]
+
+def merge(left: ArrowWeaveList, right: ArrowWeaveList) -> ArrowWeaveList:
+    return left.concat(right)
 
 # # Putting this here instead of in number b/c it is just a map function
 # bin_type = types.TypedDict({"start": types.Number(), "stop": types.Number()})
