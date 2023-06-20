eb08bc034396cfa338007bb371128d79671bde41
Leonardo Axel Setyanto <laxels@gmail.com>
2023-06-20 16:56:50 -0700

    names

--- weave/parallelism.py
+++ weave/parallelism.py
@@ -50,12 +50,12 @@ def do_in_parallel(
     remaining_budget_per_thread = get_remaining_budget_per_thread(len(items))
     wandb_api_ctx = wandb_api.get_wandb_api_context()
     result_store = forward_graph.get_node_result_store()
-    outer_tls = execute.get_top_level_stats()
+    top_level_stats = execute.get_top_level_stats()
 
     def do_one_with_memo_and_parallel_budget(x):
         memo_token = memo._memo_storage.set(memo_ctx)
         thread_result_store = None
-        tls = None
+        thread_top_level_stats = None
         try:
             with parallel_budget_ctx(remaining_budget_per_thread):
                 with wandb_api.wandb_api_context(wandb_api_ctx):
@@ -63,14 +63,14 @@ def do_in_parallel(
                         with forward_graph.node_result_store(
                             result_store
                         ) as thread_result_store:
-                            with execute.top_level_stats() as tls:
+                            with execute.top_level_stats() as thread_top_level_stats:
                                 return do_one(x)
         finally:
             memo._memo_storage.reset(memo_token)
             if thread_result_store is not None:
                 result_store.merge(thread_result_store)
-            if outer_tls is not None and tls is not None:
-                outer_tls.merge(tls)
+            if top_level_stats is not None and thread_top_level_stats is not None:
+                top_level_stats.merge(thread_top_level_stats)
 
     return ThreadPoolExecutor(max_workers=parallel_budget).map(
         do_one_with_memo_and_parallel_budget, items
