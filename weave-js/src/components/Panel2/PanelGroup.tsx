1e321129d5c1f956f80d630f1dec712b963dc22e
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2023-06-26 00:20:58 -0700

    chore: immer 10 compatibility

--- weave-js/src/components/Panel2/PanelGroup.tsx
+++ weave-js/src/components/Panel2/PanelGroup.tsx
@@ -14,7 +14,7 @@ import {
   Stack,
   voidNode,
 } from '@wandb/weave/core';
-import produce, {Draft} from 'immer';
+import {Draft, produce} from 'immer';
 import * as _ from 'lodash';
 import React, {useCallback, useMemo} from 'react';
 import {Button} from 'semantic-ui-react';
--- weave-js/src/components/Panel2/PanelPanel.tsx
+++ weave-js/src/components/Panel2/PanelPanel.tsx
@@ -1,6 +1,6 @@
 import {useTraceUpdate} from '@wandb/weave/common/util/hooks';
 import {constNodeUnsafe, Node, NodeOrVoidNode} from '@wandb/weave/core';
-import produce from 'immer';
+import {produce} from 'immer';
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
 import _ from 'lodash';
