0c9cd03e451216e9b39a27d4624c008e04c71cee
Shawn Lewis <shlewis@gmail.com>
2023-06-30 05:16:53 -0700

    Tweaks

--- weave/ecosystem/wandb/run_chain.py
+++ weave/ecosystem/wandb/run_chain.py
@@ -52,63 +52,66 @@ class RunChain:
 
 
 # This is cacheable, since runs set their checkpoint artifact on startup.
-@weave.op()
+@weave.op(render_info={"type": "function"})
 def run_chain(run_path: str) -> RunChain:
-    entity, project, run_id = run_path.split("/")
-    proj = weave.ops.project(entity, project)
-    run = proj.run(run_id)
+    with op_def.no_refine():
+        entity, project, run_id = run_path.split("/")
+        proj = weave.ops.project(entity, project)
+        run = proj.run(run_id)
 
-    # Get the checkpoint artifact for this one if one exists
-    used_arts = run.usedArtifactVersions()
-    used_art_names = used_arts.name()
-    used_art_type_names = used_arts.artifactType().name()
-    res = weave.use((used_art_names, used_art_type_names))
+        # Get the checkpoint artifact for this one if one exists
+        used_arts = run.usedArtifactVersions()
+        used_art_names = used_arts.name()
+        used_art_type_names = used_arts.artifactType().name()
+        res = weave.use((used_art_names, used_art_type_names))
 
-    checkpoint_names = []
-    for name, type_name in zip(res[0], res[1]):
-        if type_name == "checkpoint":
-            checkpoint_names.append(name)
-    if len(checkpoint_names) == 0:
-        return RunChain(
-            entity_name=entity,
-            project_name=project,
-            segments=[RunChainSegmentInfo(run_name=run_id, final_step=None)],
-        )
-    if len(checkpoint_names) > 1:
-        raise Exception("Multiple checkpoints found")
-    checkpoint_name = checkpoint_names[0]
-
-    # Get the sequence of runs and checkpoints that lead here.
-    seq = gql_artifact_dag.get_run_checkpoint_chain(entity, project, checkpoint_name)
-
-    # Fetch the contents of the checkpoint file for all checkpoints so we can get
-    # the step for each.
-    checkpoint_artifact_info = [s[1] for s in seq]
-    nodes = []
-    for artifact_info in checkpoint_artifact_info:
-        checkpoint_json_node = (
-            proj.artifactVersion(
-                artifact_info["artifactSequenceName"],
-                artifact_info["artifactCommitHash"],
+        checkpoint_names = []
+        for name, type_name in zip(res[0], res[1]):
+            if type_name == "checkpoint":
+                checkpoint_names.append(name)
+        if len(checkpoint_names) == 0:
+            return RunChain(
+                entity_name=entity,
+                project_name=project,
+                segments=[RunChainSegmentInfo(run_name=run_id, final_step=None)],
             )
-            .file("checkpoint.json")
-            .contents()
+        if len(checkpoint_names) > 1:
+            raise Exception("Multiple checkpoints found")
+        checkpoint_name = checkpoint_names[0]
+
+        # Get the sequence of runs and checkpoints that lead here.
+        seq = gql_artifact_dag.get_run_checkpoint_chain(
+            entity, project, checkpoint_name
         )
-        nodes.append(checkpoint_json_node)
 
-    checkpoint_jsons = weave.use(nodes)
-    checkpoint_steps = [json.loads(c)["step"] for c in checkpoint_jsons]
-    run_infos = [s[0] for s in seq]
+        # Fetch the contents of the checkpoint file for all checkpoints so we can get
+        # the step for each.
+        checkpoint_artifact_info = [s[1] for s in seq]
+        nodes = []
+        for artifact_info in checkpoint_artifact_info:
+            checkpoint_json_node = (
+                proj.artifactVersion(
+                    artifact_info["artifactSequenceName"],
+                    artifact_info["artifactCommitHash"],
+                )
+                .file("checkpoint.json")
+                .contents()
+            )
+            nodes.append(checkpoint_json_node)
 
-    # Construct our segment objects.
-    segments = []
-    for run_info, checkpoint_step in zip(run_infos, checkpoint_steps):
+        checkpoint_jsons = weave.use(nodes)
+        checkpoint_steps = [json.loads(c)["step"] for c in checkpoint_jsons]
+        run_infos = [s[0] for s in seq]
+
+        # Construct our segment objects.
+        segments = []
+        for run_info, checkpoint_step in zip(run_infos, checkpoint_steps):
+            segments.append(
+                RunChainSegmentInfo(
+                    run_name=run_info["runName"], final_step=checkpoint_step
+                )
+            )
         segments.append(
-            RunChainSegmentInfo(
-                run_name=run_info["runName"], final_step=checkpoint_step
-            )
+            RunChainSegmentInfo(run_name=run_infos[-1]["runName"], final_step=None)
         )
-    segments.append(
-        RunChainSegmentInfo(run_name=run_infos[-1]["runName"], final_step=None)
-    )
-    return RunChain(entity_name=entity, project_name=project, segments=segments)
+        return RunChain(entity_name=entity, project_name=project, segments=segments)
