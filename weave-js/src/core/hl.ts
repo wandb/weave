6196ffca1607439d120a46afe49702519e381767
Shawn Lewis <shlewis@gmail.com>
2023-07-11 04:54:52 -0700

    Fix suggestions

--- weave-js/src/core/hl.ts
+++ weave-js/src/core/hl.ts
@@ -680,8 +680,10 @@ export async function refineNode(
 }
 
 function isProducibleType(type: Type) {
+  if (isFunction(type)) {
+    return isProducibleType(type.outputType);
+  }
   const PRODUCIBLE_TYPES: Type[] = [
-    functionType({}, 'any'),
     'string',
     'number',
     'boolean',
@@ -690,7 +692,7 @@ function isProducibleType(type: Type) {
     maybe('number'),
     maybe('boolean'),
   ];
-  return PRODUCIBLE_TYPES.find(t => isAssignableTo(type, t)) != null;
+  return PRODUCIBLE_TYPES.find(t => isAssignableTo(t, type)) != null;
 }
 
 /**
