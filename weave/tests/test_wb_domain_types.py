79c35f5443e82a73cb2f5f7807833d8b7c219bc2
Danny Goldstein <danny@wandb.com>
2023-07-18 19:04:00 -0700

    add more tests

--- weave/tests/test_wb_domain_types.py
+++ weave/tests/test_wb_domain_types.py
@@ -1,3 +1,5 @@
+from .. import api as weave
+from .. import storage
 from ..ops_domain import wb_domain_types as wdt
 from .. import weave_types as types
 
@@ -7,3 +9,28 @@ def test_with_keys_assignability():
     org_with_keys = org_type.with_keys({"orgName": types.String()})
     assert not org_with_keys().assign_type(org_type)
     assert org_type.assign_type(org_with_keys())
+
+
+def test_type_of_run_with_keys():
+    run = wdt.Run.from_gql({"a": "1"})
+    assert types.TypeRegistry.type_of(run) == wdt.RunType.with_keys(
+        {"a": types.String()}
+    )
+
+
+def serialize_deserialize_run_type():
+    run_type: types.Type = wdt.RunType.with_keys({"a": types.String()})
+    assert run_type.from_dict(run_type.to_dict()) == run_type
+
+
+def test_storage_on_type_with_keys():
+    run = wdt.Run.from_gql({"a": "1"})
+    obj_id = storage.save(run, "my-test-run")
+    loaded = storage.get(obj_id)
+    assert loaded == run
+
+
+def test_type_of_run_node_with_keys():
+    run = wdt.Run.from_gql({"a": "1"})
+    node = weave.save(run)
+    assert node.type == wdt.RunType.with_keys({"a": types.String()})()
