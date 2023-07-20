8bf8c1e437f082bcea254fdd330873f37fff675d
Leonardo Axel Setyanto <laxels@gmail.com>
2023-07-19 17:07:49 -0700

    fix(weave): properly load media from run files (#193)
    * fix(weave): properly load media from run files
    
    * ref path
    
    * format
    
    * revert   change
    
    * revert other changes

--- weave/ops_domain/wb_util.py
+++ weave/ops_domain/wb_util.py
@@ -115,6 +115,61 @@ def _process_run_dict_item(val, run_path: typing.Optional[RunPath] = None):
                 height=val["height"],
                 sha256=val["sha256"],
             )
+        if val["_type"] == "audio-file" and run_path is not None:
+            from . import AudioArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return AudioArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+        if val["_type"] == "bokeh-file" and run_path is not None:
+            from . import BokehArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return BokehArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+        if val["_type"] == "video-file" and run_path is not None:
+            from . import VideoArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return VideoArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+        if val["_type"] == "object3D-file" and run_path is not None:
+            from . import Object3DArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return Object3DArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+        if val["_type"] == "molecule-file" and run_path is not None:
+            from . import MoleculeArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return MoleculeArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+        if val["_type"] == "html-file" and run_path is not None:
+            from . import HtmlArtifactFileRef
+
+            fs_artifact_file = _filesystem_runfiles_from_run_path(run_path, val["path"])
+            return HtmlArtifactFileRef(  # type: ignore
+                fs_artifact_file.artifact,
+                fs_artifact_file.path,
+                sha256=val["sha256"],
+            )
+
         if val["_type"] == "images/separated" and run_path is not None:
             from . import ImageArtifactFileRef
 
