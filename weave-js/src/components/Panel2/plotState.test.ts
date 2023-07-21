654647f82ef71ef6d774952cd9995bb813366ddb
Vish Rajiv <vishwaesh.rajiv@gmail.com>
2023-07-21 10:05:44 -0700

    fixing test

--- weave-js/src/components/Panel2/plotState.test.ts
+++ weave-js/src/components/Panel2/plotState.test.ts
@@ -516,7 +516,7 @@ describe('test multiple series', () => {
       produce(config, draft => {
         draft.configOptionsExpanded.mark = false;
         draft.series = [draft.series[0], draft.series[0]];
-        draft.series[-1].seriesName = 'Series 2';
+        draft.series[draft.series.length - 1].seriesName = 'Series 2';
       })
     );
   });
