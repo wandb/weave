3660e7a6f5203f194dd060041d248f5edeef1c5d
Danny Goldstein <danny@wandb.com>
2023-07-20 19:35:04 -0700

    gpt4 improvements

--- weave/gql_to_weave.py
+++ weave/gql_to_weave.py
@@ -4,8 +4,9 @@ from graphql import (
     GraphQLObjectType,
     GraphQLList,
     GraphQLNonNull,
+    FieldNode,
+    OperationDefinitionNode,
 )
-
 import graphql
 import pathlib
 
@@ -19,25 +20,28 @@ def get_nested_return_type(
     return_type: graphql.GraphQLType, selection_set: graphql.SelectionSetNode
 ) -> graphql.GraphQLType:
     for selection in selection_set.selections:
-        if isinstance(return_type, (GraphQLList, GraphQLNonNull)):
-            return_type = return_type.of_type
+        if isinstance(selection, FieldNode):
+            if isinstance(return_type, (GraphQLList, GraphQLNonNull)):
+                return_type = return_type.of_type
 
-        if isinstance(return_type, GraphQLObjectType):
-            field = return_type.fields.get(selection.name.value)
-            if not field:
-                raise ValueError(f"Unknown field {selection.name.value}")
-            return_type = field.type
+            if isinstance(return_type, GraphQLObjectType):
+                field = return_type.fields.get(selection.name.value)
+                if not field:
+                    raise ValueError(f"Unknown field {selection.name.value}")
+                return_type = field.type
 
-        if hasattr(selection, "selection_set") and selection.selection_set:
-            return_type = get_nested_return_type(return_type, selection.selection_set)
+            if selection.selection_set:
+                return_type = get_nested_return_type(
+                    return_type, selection.selection_set
+                )
 
     return return_type
 
 
 def get_return_type(schema: graphql.GraphQLSchema, query: str) -> graphql.GraphQLType:
     document = parse(query)
-    operation_definition = document.definitions[0]
-    root_operation_type = get_operation_root_type(schema, operation_definition)
-    return get_nested_return_type(
-        root_operation_type, operation_definition.selection_set
-    )
+    for definition in document.definitions:
+        if isinstance(definition, OperationDefinitionNode):
+            root_operation_type = get_operation_root_type(schema, definition)
+            return get_nested_return_type(root_operation_type, definition.selection_set)
+    raise ValueError("No operation found in query")
