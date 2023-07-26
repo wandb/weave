9a6b280c6228790dedb0c7a04824930bb760ac33
Joyce Fang <103448723+jo-fang@users.noreply.github.com>
2023-07-26 15:15:31 -0400

    chore(style): add tailwind spacing (#227)
    * tailwind spacing
    
    * add even numbers based on core search
    
    * add comment about inheritance
    
    * use rem for 0-8
    
    * more
    
    * more

--- weave-js/tailwind.config.cjs
+++ weave-js/tailwind.config.cjs
@@ -4,6 +4,47 @@ module.exports = {
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     /**
+     * This overrides Tailwind's default spacing system.
+     * Inherited by padding, margin, width, height, maxHeight, gap,
+     * inset, space, translate, scrollMargin, and scrollPadding
+     */
+    spacing: {
+      0: '0rem',
+      px: '0.063rem',
+      1: '0.063rem',
+      2: '0.125rem',
+      3: '0.188rem',
+      4: '0.25rem',
+      5: '0.313rem',
+      6: '0.375rem',
+      7: '0.438rem',
+      8: '0.5rem',
+      10: '0.625rem',
+      12: '0.75rem',
+      14: '0.875rem',
+      16: '1rem',
+      18: '1.125rem',
+      20: '1.25rem',
+      22: '1.375rem',
+      24: '1.5rem',
+      26: '1.625rem',
+      28: '1.75rem',
+      30: '1.875rem',
+      32: '2rem',
+      34: '2.125rem',
+      36: '2.25rem',
+      38: '2.375rem',
+      40: '2.5rem',
+      44: '2.75rem',
+      48: '3rem',
+      56: '3.5rem',
+      60: '3.75rem',
+      64: '4rem',
+      72: '4.5rem',
+      80: '5rem',
+      96: '6rem',
+    },
+    /**
      * Keep these colors in sync with what is in color.styles.ts
      */
     colors: {
