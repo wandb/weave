2caf9f44c444a1ca3aa7ee48fd884cd38dc57bd5
Jamie Rasmussen <112953339+jamie-rasmussen@users.noreply.github.com>
2023-07-25 21:53:37 -0500

    chore: add tailwind dependency (#222)

---
+++ weave-js/src/components/Tailwind.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type TailwindProps = {
+  style?: React.CSSProperties;
+};
+
+// Simple wrapper component that has the tw-style class,
+// meaning that all elements inside of this wrapper will have
+// Tailwind's preflight CSS reset applied.
+//
+// How to use:
+// Put this wrapper around components when you want them to use
+// tailwind css for styling.
+//
+// DO NOT apply this as a wrapper around existing parts of the site unless
+// you are able to test that all the components/pages/etc still appear
+// correctly.
+//
+// There's nothing bad about using this Tailwind wrapper around
+// existing elements, as long as you can test that they still look good.
+export const Tailwind: React.FC<TailwindProps> = ({style, children}) => {
+  return (
+    <div className="tw-style" style={style}>
+      {children}
+    </div>
+  );
+};
