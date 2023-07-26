42c000cfd5a219c6ebf82963c3b177b07b90c5df
Joyce Fang <joyce.fang@wandb.com>
2023-07-26 15:14:40 -0400

    addd @deprecated

--- weave-js/src/common/css/globals.styles.ts
+++ weave-js/src/common/css/globals.styles.ts
@@ -1,5 +1,12 @@
 export * from './color.styles';
 export * from './utils';
 
+/* Hardcoded values */
+export const SEARCH_NAV_HEIGHT = '60px';
+
+// TODO - need to talk to designers before making changes to black and transparent
+export const BLACK = '#000000';
+export const TRANSPARENT = 'rgba(255, 255, 255, 0)';
+
 // We want to start moving off of the values in this file
 export * from './globals_deprecated.styles';
