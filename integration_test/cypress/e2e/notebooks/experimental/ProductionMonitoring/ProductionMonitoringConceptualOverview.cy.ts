e94806203485a285f52b3a03f22c50100dd7c09a
Anastasia Svetlichnaya <stacey@Stacey-Svetlichnaya-C02D64USMD6R.local>
2023-07-28 15:57:48 -0700

    don't generate prodmon example tests

--- integration_test/cypress/e2e/notebooks/experimental/ProductionMonitoring/ProductionMonitoringConceptualOverview.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../../notebooks';
-
-describe('../examples/experimental/ProductionMonitoring/ProductionMonitoringConceptualOverview.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/experimental/ProductionMonitoring/ProductionMonitoringConceptualOverview.ipynb')
-    );
-});
--- integration_test/cypress/e2e/notebooks/experimental/ProductionMonitoring/stream_table_api.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../../notebooks';
-
-describe('../examples/experimental/ProductionMonitoring/stream_table_api.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/experimental/ProductionMonitoring/stream_table_api.ipynb')
-    );
-});
