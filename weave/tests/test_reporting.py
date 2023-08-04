987e0091580ab52e27532112dcfedb1f013c7316
Danny Goldstein <danny@wandb.com>
2023-08-04 12:45:47 -0700

    add tests

---
+++ weave/tests/test_reporting.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+@pytest.fixture()
+def error_fixture():
+    raise Exception()
+
+
+def test_ok():
+    assert True
+
+
+def test_fail():
+    assert False
+
+
+def test_error(error_fixture):
+    pass
+
+
+def test_skip():
+    pytest.skip("skipping this test")
+
+
+def test_xfail():
+    pytest.xfail("xfailing this test")
+
+
+@pytest.mark.flaky(reruns=1)
+def test_rerun():
+    assert False
