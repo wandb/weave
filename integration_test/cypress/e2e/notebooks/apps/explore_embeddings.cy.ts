07853e8708df7b7eba2f97a17334c2a56b6241e4
Stacey Svetlichnaya <stacey@wandb.com>
2023-08-11 09:04:13 -0700

    cant test embeddings without api key

--- integration_test/cypress/e2e/notebooks/apps/explore_embeddings.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../notebooks';
-
-describe('../examples/apps/explore_embeddings.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/apps/explore_embeddings.ipynb')
-    );
-});
