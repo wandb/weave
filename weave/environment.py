f7748df66cdef9f81dc8557a5d7dc54b830a3f5d
Chris Van Pelt <vanpelt@wandb.com>
2023-08-18 18:47:36 -0400

    Make new dynamic env endpoint (#403)

--- weave/environment.py
+++ weave/environment.py
@@ -82,6 +82,19 @@ def weave_log_format(default: "logs.LogFormat") -> "logs.LogFormat":
     return LogFormat(os.getenv("WEAVE_LOG_FORMAT", default))
 
 
+def weave_link_prefix() -> str:
+    """When running in server we mount index under /weave"""
+    if os.getenv("GORILLA_ONPREM") == "true":
+        return "/weave"
+    return ""
+
+
+def analyics_disabled() -> bool:
+    if os.getenv("WEAVE_DISABLE_ANALYTICS") == "true":
+        return True
+    return False
+
+
 def weave_server_url() -> str:
     base_url = wandb_base_url()
     default = "https://weave.wandb.ai"
