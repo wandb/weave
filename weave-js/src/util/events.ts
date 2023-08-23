b426adfafb82c67611b7b6e5441aa13f90c5c84c
Vish Rajiv <vishwaesh.rajiv@gmail.com>
2023-08-22 17:10:14 -0700

    lint

--- weave-js/src/util/events.ts
+++ weave-js/src/util/events.ts
@@ -2,5 +2,5 @@
  * TODO: Connect to router.
  */
 export function trackPage(properties: object, options: object) {
-  window.analytics?.page?.(properties, options);
+  (window.analytics as any).page?.(properties, options);
 }
