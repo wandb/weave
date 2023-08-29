0adde532edb4f0c867b6d4330c9836ac5fa20cc9
Danny Goldstein <danny@wandb.com>
2023-08-29 14:39:30 -0700

    lint

--- weave/gql_schema.py
+++ weave/gql_schema.py
@@ -9,17 +9,16 @@ _GQL_SCHEMA_CACHE: dict[typing.Optional[str], graphql.GraphQLSchema] = {}
 
 def gql_schema() -> graphql.GraphQLSchema:
     schema_path = environment.gql_schema_path()
-    should_introspect = schema_path is None
     gql_schema = _GQL_SCHEMA_CACHE.get(schema_path)
 
     if gql_schema is None:
-        if should_introspect:
-            gql_schema = wandb_client_api.introspect_server_schema()
-
-        else:
+        if schema_path is not None:
             with open(schema_path, "r") as f:
                 schema_str = f.read()
 
             gql_schema = graphql.build_schema(schema_str)
+        else:
+            gql_schema = wandb_client_api.introspect_server_schema()
+
     _GQL_SCHEMA_CACHE[schema_path] = gql_schema
     return gql_schema
