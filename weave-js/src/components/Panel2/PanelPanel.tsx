04f927c0b64a2a29a1cee2e738a759ef0abbec45
Shawn Lewis <shlewis@gmail.com>
2023-08-30 20:39:49 -0700

    Remove old comment.

--- weave-js/src/components/Panel2/PanelPanel.tsx
+++ weave-js/src/components/Panel2/PanelPanel.tsx
@@ -491,13 +491,6 @@ export const PanelPanel: React.FC<PanelPanelProps> = props => {
     updateInput as any
   );
 
-  // Unfortunately we currently do this twice, in parallel!
-  // That happens because we render the Panel's regular render component
-  // and its config component (hidden by the sidebar) in parallel.
-  // Fortunately the Weave backend requests for these two parallel initializations
-  // are deduped.
-  // TODO: fix, this should really be the true UI root.
-
   useEffect(() => {
     if (!panelQuery.loading) {
       const doLoad = async () => {
