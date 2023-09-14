04e6a72424f5998bdaa7cf4e86eb553b5b31f9fd
Tim Sweeney <tim@wandb.com>
2023-09-13 20:37:31 -0700

    fixed auth issue with user is employee

--- weave-js/src/common/hooks/useViewerIsWandbEmployee.tsx
+++ weave-js/src/common/hooks/useViewerIsWandbEmployee.tsx
@@ -1,12 +1,17 @@
+import {useIsAuthenticated} from '@wandb/weave/components/PagePanelComponents/util';
 import {opRootViewer, opUserEmail} from '@wandb/weave/core';
 import {useNodeValue} from '@wandb/weave/react';
 
 export const useIsViewerWandbEmployee = () => {
+  // Temp hack to avoid making authenticated queries without needing to
+  // We should make the server handle this more gracefully
+  const isAuthed = useIsAuthenticated();
   const viewerOp = opRootViewer({});
   const emailOp = opUserEmail({user: viewerOp});
-  const {result: viewerEmail} = useNodeValue(emailOp);
 
-  if (!viewerEmail) {
+  const {result: viewerEmail} = useNodeValue(emailOp, {skip: !isAuthed});
+
+  if (!viewerEmail || !isAuthed) {
     return false;
   }
 
