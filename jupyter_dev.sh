a4c2eedf4dc4e88c198b3856d85a3f877a26d280
Tim Sweeney <tim@wandb.com>
2023-09-15 02:54:56 -0700

    minor cleanup

--- jupyter_dev.sh
+++ jupyter_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 (
-    source ./config/auth_modes.env
+    source ./config/auth_modes.sh
     WEAVE_FRONTEND_DEVMODE=true jupyter notebook
 )
--- weave_server.sh
+++ weave_server.sh
@@ -3,6 +3,6 @@
 # We could set WEAVE_BACKEND_HOST=http://localhost:9994/__weave but the frontend dev server
 # automatically forwards so it shouldn't be necessary.
 (
-    source ./config/auth_modes.env
+    source ./config/auth_modes.sh
     WEAVE_DISABLE_ANALYTICS=true WEAVE_SERVER_ENABLE_LOGGING=true FLASK_DEBUG=1 FLASK_APP=weave.weave_server flask run --port 9994
 )
