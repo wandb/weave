7ffd432c1161a9ad222eef90ca5c0b2fbf1522ba
Tim Sweeney <tim@wandb.com>
2023-09-15 00:10:42 -0700

    added wb_auth

--- jupyter_dev.sh
+++ jupyter_dev.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 (
-    if [[ "${USE_ADMIN_ENV}" ]];
+    if [[ "${USE_ADMIN_AUTH}" ]];
     then 
-        echo "Using admin env"
+        echo "Using admin auth"
         source .admin.env 
+    else
+        if [[ "${USE_USER_AUTH}" ]];
+        then 
+            echo "Using user auth"
+            source .wb_api.env 
+        else
+            echo "Using no auth"
+        fi 
     fi 
     WEAVE_FRONTEND_DEVMODE=true jupyter notebook
 )
--- weave_server.sh
+++ weave_server.sh
@@ -3,10 +3,18 @@
 # We could set WEAVE_BACKEND_HOST=http://localhost:9994/__weave but the frontend dev server
 # automatically forwards so it shouldn't be necessary.
 (
-    if [[ "${USE_ADMIN_ENV}" ]];
+    if [[ "${USE_ADMIN_AUTH}" ]];
     then 
-        echo "Using admin env"
+        echo "Using admin auth"
         source .admin.env 
+    else
+        if [[ "${USE_USER_AUTH}" ]];
+        then 
+            echo "Using user auth"
+            source .wb_api.env 
+        else
+            echo "Using no auth"
+        fi 
     fi 
     WEAVE_DISABLE_ANALYTICS=true WEAVE_SERVER_ENABLE_LOGGING=true FLASK_DEBUG=1 FLASK_APP=weave.weave_server flask run --port 9994
 )
