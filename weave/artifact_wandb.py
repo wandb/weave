363c46c5bb9d172f8d69a06ce7f9c4e4724fa6dd
Danny Goldstein <danny@wandb.com>
2023-09-20 16:27:17 -0700

    lint

--- weave/artifact_wandb.py
+++ weave/artifact_wandb.py
@@ -195,7 +195,7 @@ def _collection_and_alias_id_mapping_to_uri(
     )
     res = wandb_client_api._query_with_retry(
         query,
-        variable_values={
+        variables={
             "id": client_collection_id,
             "aliasName": alias_name,
         },
