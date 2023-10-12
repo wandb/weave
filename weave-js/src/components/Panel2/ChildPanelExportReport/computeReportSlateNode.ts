079e5c05f553e4787569e40722cfe70d4d8f0097
Connie Lee <connie.lee@wandb.com>
2023-10-12 14:19:10 -0700

    feat(wir): Include vars for exported panels

--- weave-js/src/components/Panel2/ChildPanelExportReport/computeReportSlateNode.ts
+++ weave-js/src/components/Panel2/ChildPanelExportReport/computeReportSlateNode.ts
@@ -1,6 +1,6 @@
-import {ID} from '@wandb/weave/core';
+import {ID, dereferenceAllVars} from '@wandb/weave/core';
 import {ChildPanelFullConfig} from '../ChildPanel';
-import {getConfigForPath, makeGroup, makePanel} from '../panelTree';
+import {getConfigForPath, makeGroup, makePanel, mapPanels} from '../panelTree';
 import {toWeaveType} from '../toWeaveType';
 
 export type WeavePanelSlateNode = {
@@ -38,8 +38,32 @@ export const computeReportSlateNode = (
   targetPath: string[]
 ): WeavePanelSlateNode => {
   const targetConfig = getConfigForPath(fullConfig, targetPath);
+
+  const varsItems: Record<string, ChildPanelFullConfig> = {};
+  // perf: need func that just gets stack for target instead of map over full config
+  mapPanels(fullConfig, [], (config, stack) => {
+    if (config === targetConfig) {
+      const {usedStack} = dereferenceAllVars(config.input_node, stack);
+      // issue: usedStack doesn't include tables & plots
+      usedStack.forEach(frame => {
+        varsItems[frame.name] = makePanel(
+          'Expression', // issue: not all vars are expressions
+          undefined,
+          frame.value
+        );
+      });
+    }
+    return config;
+  });
+
   const inputNodeVal = makeGroup(
     {
+      // NOTE: order matters! `vars` must come before `panel`
+      vars: makeGroup(varsItems, {
+        allowedPanels: ['Expression'], // issue: again, not all vars are expressions
+        childNameBase: 'var',
+        layoutMode: 'vertical',
+      }),
       panel: targetConfig,
     },
     {
