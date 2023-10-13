1467775860b3c3a9169396bfebfcdd0aa2584316
Shawn Lewis <shlewis@gmail.com>
2023-10-13 16:44:07 -0700

    Update cli, remove serve for now

--- weave/__main__.py
+++ weave/__main__.py
@@ -4,15 +4,13 @@ from weave import server
 
 
 def start_ui() -> None:
+    print("Starting server...")
     serv = server.HttpServer(port=3000)  # type: ignore
     serv.start()
+    print("Server started")
     print("http://localhost:3000/browse2")
     while True:
         time.sleep(10)
-
-
-def serve_model(model_uri: str) -> None:
-    print("SERVE MODEL", model_uri)
 
 
 def main() -> None:
@@ -20,17 +18,11 @@ def main() -> None:
     subparsers = parser.add_subparsers(dest="command")
 
     ui_parser = subparsers.add_parser("ui")
-    serve_parser = subparsers.add_parser("serve")
-    serve_parser.add_argument(
-        "--model", type=str, required=True, help="URI to the model"
-    )
 
     args = parser.parse_args()
 
     if args.command == "ui":
         start_ui()
-    elif args.command == "serve":
-        serve_model(args.model)
     else:
         parser.print_help()
 
