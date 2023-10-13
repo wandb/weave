75642ec1332e9822f8e0ee1f0c757eeb2c4b5b24
Danny Goldstein <danny@wandb.com>
2023-10-13 12:27:37 -0700

    feat: allow specifying weave_wandb_gql_headers (#749)

--- weave/environment.py
+++ weave/environment.py
@@ -5,7 +5,7 @@
 import configparser
 import enum
 import os
-import pathlib
+import json
 import typing
 from . import util
 from . import errors
@@ -149,6 +149,19 @@ def memdump_sighandler_enabled() -> bool:
 
 def sigterm_sighandler_enabled() -> bool:
     return util.parse_boolean_env_var("WEAVE_ENABLE_SIGTERM_SIGHANDLER")
+
+
+def weave_wandb_gql_headers() -> typing.Dict[str, str]:
+    # expects a json string
+    raw = os.environ.get("WEAVE_WANDB_GQL_HEADERS")
+    if raw:
+        try:
+            return json.loads(raw)
+        except json.JSONDecodeError:
+            raise errors.WeaveConfigurationError(
+                "WEAVE_WANDB_GQL_HEADERS should be a json string"
+            )
+    return {}
 
 
 def weave_wandb_cookie() -> typing.Optional[str]:
