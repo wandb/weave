604870541210c1c2bbc53bacfcdf95dae28529d1
Danny Goldstein <danny@wandb.com>
2023-11-09 12:10:31 -0800

    increase timeout

--- integration_test/cypress/e2e/notebooks/notebooks.ts
+++ integration_test/cypress/e2e/notebooks/notebooks.ts
@@ -66,18 +66,21 @@ function forEachWeaveOutputCellInNotebook(
         // TODO: This switches depending on if in devmode
         cy.visit('/__frontend/weave_frontend' + url.search);
 
-        // Cypress is erroring on ResizeObserver loop limit exceeded, but 
+        // Cypress is erroring on ResizeObserver loop limit exceeded, but
         // we are not seeing anything like this locally. I think it might
         // have to do with cypress itself. Similar issues have been noted
         // here: https://github.com/cypress-io/cypress/issues/8418, with similar
         // patches resolving the issue. I'm not sure if this is the best way
         // to handle this, but it seems to work.
         cy.on('uncaught:exception', err => {
-          if (err.name.includes('ResizeObserver') || err.message.includes('ResizeObserver')) {
-            return false
+          if (
+            err.name.includes('ResizeObserver') ||
+            err.message.includes('ResizeObserver')
+          ) {
+            return false;
           }
-        })
-        
+        });
+
         // cy.visit('http://localhost:3000/' + url.search);
 
         cellTest(cell.id);
@@ -88,10 +91,10 @@ function forEachWeaveOutputCellInNotebook(
 
 function executeNotebook(notebookPath: string) {
   exec(
-    'pytest --nbmake --nbmake-timeout=150000 --overwrite "' +
+    'pytest --nbmake --nbmake-timeout=1500000 --overwrite "' +
       notebookPath +
       '"',
-    150000
+    1500000
   );
 }
 
