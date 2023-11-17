1d7a190ac5d67d015418abec2ccc46d7aaec9b32
Tim Sweeney <tim@wandb.com>
2023-11-17 09:47:10 -0600

    chore(weave): Fix report export with groups & single vars (#850)
    * init
    
    * make node work
    
    * address comments
    
    * lint
    
    ---------
    
    Co-authored-by: Connie Lee <connie.lee0317@gmail.com>

--- weave-js/src/components/Panel2/ChildPanelExportReport/computeReportSlateNode.ts
+++ weave-js/src/components/Panel2/ChildPanelExportReport/computeReportSlateNode.ts
@@ -1,4 +1,11 @@
-import {ID, isAssignableTo, isConstNode, isVarNode} from '@wandb/weave/core';
+import {
+  ID,
+  isAssignableTo,
+  isConstNode,
+  isNonVoidNode,
+  isVarNode,
+} from '@wandb/weave/core';
+import _ from 'lodash';
 
 import {weaveTypeIsPanel} from '../../PagePanelComponents/util';
 import {ChildPanelFullConfig} from '../ChildPanel';
@@ -60,7 +67,17 @@ const getVarItemsForPath = (
 
   // First, extract the stack for a given panel.
   mapPanels(fullConfig, [], (config, stack) => {
-    if (config === targetConfig) {
+    if (
+      // mapPanels does not guarantee reference equality, so we need to use
+      // _.isEqual to compare the configs. This is at least the case for Group
+      // panels, which are cloned in mapPanels.
+      _.isEqual(config, targetConfig) ||
+      // Additionally, the target config might be a direct node itself, so we
+      // need to also check for equality with the input_node of the target
+      // config.
+      (isNonVoidNode(targetConfig) &&
+        _.isEqual(config.input_node, targetConfig))
+    ) {
       stack.forEach(frame => {
         if (isConstNode(frame.value) && weaveTypeIsPanel(frame.value.type)) {
           if (!isAssignableTo(frame.value.type, {type: 'Group' as any})) {
