9940dd4717bacf7f30e469318951ba911f3ceba5
Tim Sweeney <tim@wandb.com>
2023-11-17 14:47:07 -0600

    back down to node 16

--- weave-js/src/contextProviders.tsx
+++ weave-js/src/contextProviders.tsx
@@ -82,7 +82,7 @@ const useRemoteEcosystemClient = (
       makeEcosystemMixedOpStore(StaticOpStore.getInstance(), remoteOpStore),
       getCookie('anon_api_key'),
       // Always use off of window object, in case of fetch wrappers
-      // `as any` required as part of node 16 -> 18 upgrade
+      // Why is this as any needed all of a sudden?
       (input, init) => window.fetch(input as any, init)
     );
   }, [isAdmin, remoteOpStore, tokenFunc, loading]);
