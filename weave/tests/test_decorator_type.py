8cd0fb3fc05747047438cbcbed81f5f51a16b2e2
Shawn Lewis <shlewis@gmail.com>
2023-11-19 14:37:05 -0800

    Remove invalid test

--- weave/tests/test_decorator_type.py
+++ weave/tests/test_decorator_type.py
@@ -3,14 +3,6 @@ import typing
 import weave
 
 
-def test_no_type_vars_in_dict():
-    @weave.type()
-    class TestNoTypeVarsInDictType:
-        v: dict[str, int]
-
-    assert not TestNoTypeVarsInDictType.WeaveType.type_attrs()
-
-
 def test_type_var_in_dict_any():
     @weave.type()
     class TestTypeVarsInDictType:
