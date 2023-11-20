a57681abbf28e4b8f0ab8b8c8ec5c5f9a038bcd1
Tim Sweeney <tim@wandb.com>
2023-11-20 12:32:22 -0600

    typing: run.py

--- weave/run.py
+++ weave/run.py
@@ -39,37 +39,37 @@ class Run:
 
     @property
     def attributes(self) -> dict[str, typing.Any]:
-        return {k: v for k, v in self._attrs.get("attributes", {}).items() if v != None}
+        attrs_dict = self._attrs.get("attributes", {})
+        if not isinstance(attrs_dict, dict):
+            return {}
+        return {k: v for k, v in attrs_dict.items() if v != None}
 
     @property
     def inputs(self) -> dict[str, typing.Any]:
-        keys = self._attrs["inputs"].get("_keys")
+        input_dict = self._attrs.get("input", {})
+        if not isinstance(input_dict, dict):
+            return {}
+        keys = input_dict.get("_keys")
         if keys is None:
-            keys = [
-                k
-                for k in self._attrs["inputs"].keys()
-                if self._attrs["inputs"][k] != None
-            ]
-        return {k: self._attrs["inputs"][k] for k in keys}
+            keys = [k for k in input_dict.keys() if input_dict[k] != None]
+        return {k: input_dict[k] for k in keys}
 
     @property
     def output(self) -> typing.Any:
         if self.status_code != "SUCCESS":
             return None
-        keys = self._attrs["output"].get("_keys")
-        if keys is None:
-            keys = [
-                k
-                for k in self._attrs["output"].keys()
-                if self._attrs["output"][k] != None
-            ]
-        output = {k: self._attrs["output"][k] for k in keys}
-        if "_result" in output:
-            return output["_result"]
+        output_dict = self._attrs.get("output")
+        if isinstance(output_dict, dict):
+            keys = output_dict.get("_keys")
+            if keys is None:
+                keys = [k for k in output_dict.keys() if output_dict[k] != None]
+            output = {k: output_dict.get(k) for k in keys}
+            if "_result" in output:
+                return output["_result"]
         return output
 
     @property
-    def parent_id(self) -> str:
+    def parent_id(self) -> typing.Optional[str]:
         return self._attrs["parent_id"]
 
     def add_feedback(self, feedback: dict[str, typing.Any]) -> None:
@@ -82,7 +82,7 @@ class Run:
 
     def parent(self) -> typing.Optional["Run"]:
         client = graph_client_context.require_graph_client()
-        if self.parent_id == None:
+        if self.parent_id is None:
             return None
         return client.run(self.parent_id)
 
