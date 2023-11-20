a5820d8af88f0e3e24798806632e639adce17d88
Danny Goldstein <danny@wandb.com>
2023-11-20 13:22:06 -0800

    uncomment runchain

--- integration_test/cypress/e2e/notebooks/experimental/app/RunChain.cy.ts
+++ integration_test/cypress/e2e/notebooks/experimental/app/RunChain.cy.ts
@@ -2,6 +2,8 @@ import {checkWeaveNotebookOutputs} from '../../notebooks';
 
 describe('../examples/experimental/app/RunChain.ipynb notebook test', () => {
   it('passes', () => {
-    // return checkWeaveNotebookOutputs('../examples/experimental/app/RunChain.ipynb');
+    return checkWeaveNotebookOutputs(
+      '../examples/experimental/app/RunChain.ipynb'
+    );
   });
 });
