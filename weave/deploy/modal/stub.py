68c90deb1103ecbc95b578e995a28798d4b8615e
Tim Sweeney <tim@wandb.com>
2023-11-20 13:52:37 -0600

    typing: stub.py

--- weave/deploy/modal/stub.py
+++ weave/deploy/modal/stub.py
@@ -1,4 +1,6 @@
+from fastapi import FastAPI
 from modal import Image, Secret, Stub, asgi_app
+from weave.artifact_wandb import WandbArtifactRef
 from weave.uris import WeaveURI
 from weave.deploy.util import safe_name
 import os
@@ -21,13 +23,16 @@ uri = WeaveURI.parse("$MODEL_REF")
 
 @stub.function(image=image, secret=Secret.from_dotenv(__file__))
 @asgi_app(label=safe_name(uri.name))
-def fastapi_app():
+def fastapi_app() -> FastAPI:
     from weave.serve_fastapi import object_method_app
     from weave.uris import WeaveURI
     from weave import api
 
     uri = WeaveURI.parse(os.environ["MODEL_REF"])
-    app = object_method_app(uri.to_ref(), auth_entity=os.environ.get("AUTH_ENTITY"))
+    uri_ref = uri.to_ref()
+    if not isinstance(uri_ref, WandbArtifactRef):
+        raise ValueError(f"Expected a wandb artifact ref, got {type(uri_ref)}")
+    app = object_method_app(uri_ref, auth_entity=os.environ.get("AUTH_ENTITY"))
 
     api.init(os.environ["PROJECT_NAME"])
     # TODO: hookup / provide more control over attributes
