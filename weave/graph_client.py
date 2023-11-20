d95b2a1bac9a50d2ec31cfa38c7849ef895401e7
Tim Sweeney <tim@wandb.com>
2023-11-20 13:30:19 -0600

    typing: graph_client.py

--- weave/graph_client.py
+++ weave/graph_client.py
@@ -43,52 +43,49 @@ class GraphClient:
 
     def run(self, run_id: str) -> typing.Optional[Run]:
         with context_state.lazy_execution():
-            run_attrs: stream_data_interfaces.TraceSpanDict = weave_internal.use(
-                select_all(
-                    self.runs_st.rows().filter(lambda row: row["span_id"] == run_id)[0]
-                )
-            )
+            rows_node = self.runs_st.rows()
+            filter_node = rows_node.filter(lambda row: row["span_id"] == run_id)[0]  # type: ignore
+            run_attrs = weave_internal.use(select_all(filter_node))
+            if not isinstance(run_attrs, dict):
+                return None
             if run_attrs["span_id"] == None:
                 return None
+            run_attrs = typing.cast(stream_data_interfaces.TraceSpanDict, run_attrs)
             return Run(run_attrs)
 
     def run_children(self, run_id: str) -> WeaveIter[Run]:
         with context_state.lazy_execution():
-            return WeaveIter(
-                self.runs_st.rows().filter(lambda row: row["parent_id"] == run_id),
-                cls=Run,
-            )
+            rows_node = self.runs_st.rows()
+            filter_node = rows_node.filter(lambda row: row["parent_id"] == run_id)  # type: ignore
+            return WeaveIter(filter_node, cls=Run)
 
     # Hmm... I want this to be a ref to an op I think?
     def op_runs(self, op_def: op_def.OpDef) -> WeaveIter[Run]:
         with context_state.lazy_execution():
-            return WeaveIter(
-                self.runs_st.rows().filter(
-                    lambda row: row["name"] == str(op_def.location)
-                ),
-                cls=Run,
+            rows_node = self.runs_st.rows()
+            filter_node = rows_node.filter(  # type: ignore
+                lambda row: row["name"] == str(op_def.location)
             )
+            return WeaveIter(filter_node, cls=Run)
 
     def ref_input_to(self, ref: artifact_wandb.WandbArtifactRef) -> WeaveIter[Run]:
         with context_state.lazy_execution():
-            return WeaveIter(
-                self.runs_st.rows().filter(lambda row: row["inputs._ref0"] == ref),
-                cls=Run,
-            )
+            rows_node = self.runs_st.rows()
+            filter_node = rows_node.filter(lambda row: row["inputs._ref0"] == ref)  # type: ignore
+            return WeaveIter(filter_node, cls=Run)
 
     def ref_output_of(
         self, ref: artifact_wandb.WandbArtifactRef
     ) -> typing.Optional[Run]:
         with context_state.lazy_execution():
-            run_attrs = weave_internal.use(
-                select_all(
-                    self.runs_st.rows().filter(lambda row: row["outputs._ref0"] == ref)[
-                        0
-                    ]
-                )
-            )
+            rows_node = self.runs_st.rows()
+            filter_node = rows_node.filter(lambda row: row["outputs._ref0"] == ref)[0]  # type: ignore
+            run_attrs = weave_internal.use(select_all(filter_node))
+            if not isinstance(run_attrs, dict):
+                return None
             if run_attrs["span_id"] == None:
                 return None
+            run_attrs = typing.cast(stream_data_interfaces.TraceSpanDict, run_attrs)
             return Run(run_attrs)
 
     def add_feedback(self, run_id: str, feedback: dict[str, typing.Any]) -> None:
@@ -99,19 +96,17 @@ class GraphClient:
 
     def run_feedback(self, run_id: str) -> WeaveIter[dict[str, typing.Any]]:
         with context_state.lazy_execution():
-            return WeaveIter(
-                self.run_feedback_st.rows().filter(lambda row: row["run_id"] == run_id)
-            )
+            rows_node = self.run_feedback_st.rows()
+            filter_node = rows_node.filter(lambda row: row["run_id"] == run_id)  # type: ignore
+            return WeaveIter(filter_node)
 
     def feedback(self, feedback_id: str) -> typing.Optional[dict[str, typing.Any]]:
         with context_state.lazy_execution():
-            feedback_attrs = weave_internal.use(
-                select_all(
-                    self.run_feedback_st.rows().filter(
-                        lambda row: row["feedback_id"] == feedback_id
-                    )[0]
-                )
-            )
+            rows_node = self.run_feedback_st.rows()
+            filter_node = rows_node.filter(lambda row: row["feedback_id"] == feedback_id)[0]  # type: ignore
+            feedback_attrs = weave_internal.use(select_all(filter_node))
+            if not isinstance(feedback_attrs, dict):
+                return None
             if feedback_attrs["feedback_id"] == None:
-                raise None
+                return None
             return feedback_attrs
