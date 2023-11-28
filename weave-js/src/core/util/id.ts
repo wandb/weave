c8a49c298c7248f8a73f61924b5f8c87c8a0c7df
Danny Goldstein <danny@wandb.com>
2023-11-27 16:57:47 -0800

    lint

--- weave-js/src/core/util/id.ts
+++ weave-js/src/core/util/id.ts
@@ -5,13 +5,10 @@ export function ID(length = 9) {
   return Math.random().toString(36).substr(2, length);
 }
 
-export function uuidv4(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-    /[xy]/g,
-    function (c: string): string {
-      const r = (Math.random() * 16) | 0;
-      const v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    }
-  );
-}
+export const uuidv4 = (): string => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.floor(Math.random() * 16);
+    const v = c === 'x' ? r : (r % 4) + 8;
+    return v.toString(16);
+  });
+};
