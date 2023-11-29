85f337ceb099c4cb3e415776c29c4f386064948e
Tim Sweeney <tim@wandb.com>
2023-11-29 17:53:03 -0600

    tables

--- weave-js/src/components/PagePanelComponents/Home/Browse2/pages/TablesPage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse2/pages/TablesPage.tsx
@@ -1,80 +1,40 @@
-import {Typography} from '@material-ui/core';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import _ from 'lodash';
-import * as React from 'react';
-import {useLocation} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return {name, calories, fat, carbs, protein};
-}
+import {useEPPrefix} from './util';
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function useQuery() {
-  const {search} = useLocation();
-
-  return React.useMemo(() => {
-    const params = new URLSearchParams(search);
-    const entries = Array.from(params.entries());
-    const searchDict = _.fromPairs(entries);
-    return searchDict;
-  }, [search]);
-}
-
-export const TablesPage: React.FC = props => {
-  const search = useQuery();
-  const filter = search.filter;
+export const TablesPage: React.FC = () => {
+  const prefix = useEPPrefix();
   return (
-    <>
-      <Typography variant="h3" component="h3" gutterBottom>
-        Tables {filter}
-      </Typography>
-      <TableContainer component={Paper}>
-        <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => (
-              <TableRow
-                key={row.name}
-                sx={{'&:last-child td, &:last-child th': {border: 0}}}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <div>
+      <h1>TablesPage Placeholder</h1>
+      <div>This is the listing page for tables</div>
+      <div>Migration Notes:</div>
+      <ul>
+        <li>
+          We should try to reuse as much as possible from the homepage tables
+          table view. Most of the query layer should be reusable.
+        </li>
+      </ul>
+      <div>Links:</div>
+      <ul>
+        <li>
+          Each row should link to the associated table:{' '}
+          <Link to={prefix('/tables/my_table')}>/tables/my_table</Link>
+        </li>
+      </ul>
+      <div>Inspiration</div>
+      <a href="https://weave.wandb.test/browse/wandb/timssweeney/weave/table">
+        Link
+      </a>
+      <br />
+      <img
+        src="https://github.com/wandb/weave/blob/562a679a24ede63dcf4295476a52d7dc38d4bd04/weave-js/src/components/PagePanelComponents/Home/Browse2/pages/example_media/tables_example.png?raw=true"
+        style={{
+          width: '100%',
+          maxWidth: '800px',
+        }}
+      />
+    </div>
   );
 };
