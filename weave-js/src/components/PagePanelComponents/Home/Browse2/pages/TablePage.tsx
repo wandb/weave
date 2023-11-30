3d4cc6a30004e71217482ed67202fbc4bec08a83
Tim Sweeney <tim@wandb.com>
2023-11-29 19:31:33 -0600

    tablepage

--- weave-js/src/components/PagePanelComponents/Home/Browse2/pages/TablePage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse2/pages/TablePage.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import {useEPPrefix} from './util';
 
 export const TablePage: React.FC = () => {
-  return <div>TablePage placeholder</div>;
+  const prefix = useEPPrefix();
+  return (
+    <div>
+      <h1>TablePage Placeholder</h1>
+      <div>This is fullpage view for a table</div>
+      <div>Migration Notes:</div>
+      <ul>
+        <li>Very similar to the preview view in current weave home</li>
+      </ul>
+      <div>Links:</div>
+      <ul>
+        <li>
+          Open in Board
+          <Link to={prefix('/boards/_new_board_')}>/boards/_new_board_</Link>
+        </li>
+        <li>
+          Back to all tables. <Link to={prefix('/tables')}>/tables</Link>
+        </li>
+      </ul>
+      <div>Inspiration</div>
+      This page will basically be a simple table of Op Versions, with some
+      lightweight filtering on top.
+      <br />
+      <img
+        src="https://github.com/wandb/weave/blob/0fe070497bbde538475efad27cbf147823bc959b/weave-js/src/components/PagePanelComponents/Home/Browse2/pages/example_media/table_example.png?raw=true"
+        style={{
+          width: '100%',
+        }}
+      />
+    </div>
+  );
 };
-
-// https://weave.wandb.ai/?exp=get%28%0A++++%22wandb-artifact%3A%2F%2F%2Ftimssweeney%2Fweave%2Ftrace_span_stream%3Alatest%2Fobj%22%29%0A++.rows
