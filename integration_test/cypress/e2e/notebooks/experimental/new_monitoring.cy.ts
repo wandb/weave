3bbcc70738c76260da59a75b5c45c93e2a426adf
Andrew Truong <itsandrewtruong@gmail.com>
2023-12-05 10:59:42 -0500

    test

--- integration_test/cypress/e2e/notebooks/experimental/new_monitoring.cy.ts
+++
@@ -1,6 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../notebooks';
-
-describe('../examples/experimental/new_monitoring.ipynb notebook test', () => {
-  it('passes', () =>
-    checkWeaveNotebookOutputs('../examples/experimental/new_monitoring.ipynb'));
-});
