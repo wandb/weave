971e23bcbe416be8c0ea45210f5d57fac3e30feb
Jamie Rasmussen <112953339+jamie-rasmussen@users.noreply.github.com>
2023-12-05 16:07:51 -0600

    fix: case-insensitive suggestion reordering (#909)

--- weave-js/src/core/util/string.test.ts
+++ weave-js/src/core/util/string.test.ts
@@ -1,4 +1,19 @@
-import {trimEndChar} from './string';
+import {trimEndChar, trimStartChar} from './string';
+
+describe('trimStartChar', () => {
+  it('handles start char does not exist', () => {
+    expect(trimStartChar('abc', 'd')).toEqual('abc');
+  });
+  it('ignores trim char not at start', () => {
+    expect(trimStartChar('abc', 'b')).toEqual('abc');
+  });
+  it('removes all trim char', () => {
+    expect(trimStartChar('aaaaabc', 'a')).toEqual('bc');
+  });
+  it('returns empty string if entirely trim char', () => {
+    expect(trimStartChar('aaaaa', 'a')).toEqual('');
+  });
+});
 
 describe('trimEndChar', () => {
   it('handles end char does not exist', () => {
