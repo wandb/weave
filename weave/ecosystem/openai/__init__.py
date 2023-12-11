c9b05d619bcf686ecf9e7ec80202cfef55b45f92
Danny Goldstein <danny@wandb.com>
2023-12-11 14:24:58 -0800

    Revert "chore(weave): OpenAI SDK and Monitoring improvements (#851)"
    This reverts commit 3dedaa6db9d67753d9eddf8f556a106289638cd9.

---
+++ weave/ecosystem/openai/__init__.py
@@ -0,0 +1,13 @@
+from weave import context_state
+import logging
+
+logging.getLogger("ecosystem_example").setLevel(logging.ERROR)
+
+loading_builtins_token = context_state.set_loading_built_ins()
+
+try:
+    from .gpt3 import *
+    from .embedding import *
+
+finally:
+    context_state.clear_loading_built_ins(loading_builtins_token)
