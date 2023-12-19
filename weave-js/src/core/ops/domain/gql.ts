da1fe67cf6630b6966ec05e0bf0457e00681e74f
Estella Xin <estellazx@gmail.com>
2023-12-19 16:47:44 -0500

    chore(artifacts): add entity artifactTTLDurationSeconds and TTLisInherited ops (#934)
    * add entity ttl duration
    
    * version ttl and entity ttl fields

--- weave-js/src/core/ops/domain/gql.ts
+++ weave-js/src/core/ops/domain/gql.ts
@@ -499,6 +499,8 @@ export const toGqlField = (
     ];
   } else if (forwardOp.op.name === 'entity-org') {
     return [gqlObjectField(forwardGraph, forwardOp, 'organization')];
+  } else if (forwardOp.op.name === 'entity-artifactTTLDurationSeconds') {
+    return gqlBasicField('artifactTTLDurationSeconds');
   } else if (forwardOp.op.name === 'project-id') {
     return gqlBasicField('id');
   } else if (forwardOp.op.name === 'project-entity') {
@@ -1108,6 +1110,8 @@ export const toGqlField = (
     return gqlBasicField('metadata');
   } else if (forwardOp.op.name === 'artifactVersion-ttlDurationSeconds') {
     return gqlBasicField('ttlDurationSeconds');
+  } else if (forwardOp.op.name === 'artifactVersion-ttlIsInherited') {
+    return gqlBasicField('ttlIsInherited');
   } else if (forwardOp.op.name === 'artifactVersion-artifactType') {
     return [gqlObjectField(forwardGraph, forwardOp, 'artifactType')];
   } else if (forwardOp.op.name === 'artifactVersion-artifactCollections') {
