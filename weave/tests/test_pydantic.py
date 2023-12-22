c3efe7ebb9eaddbb82bc86f85a5eef1e04436ea1
Danny Goldstein <danny@wandb.com>
2023-12-21 18:42:52 -0800

    add test

--- weave/tests/test_pydantic.py
+++ weave/tests/test_pydantic.py
@@ -8,3 +8,32 @@ def test_jsonschema_to_weave_type():
     assert json_schema_to_weave_type(
         {"type": "array", "items": {"type": "integer"}}
     ) == types.List(types.Int())
+
+    assert json_schema_to_weave_type(
+        {
+            "type": "object",
+            "properties": {"a": {"type": "integer"}},
+            "required": ["a"],
+        }
+    ) == types.TypedDict({"a": types.Int()})
+
+    assert json_schema_to_weave_type({"type": "null"}) == types.NoneType()
+
+    assert json_schema_to_weave_type(
+        {
+            "type": "object",
+            "properties": {
+                "a": {
+                    "type": "integer",
+                },
+                "b": {"type": "object", "properties": {"b": {"type": "string"}}},
+            },
+            "required": ["a"],
+        }
+    ) == types.TypedDict(
+        {
+            "a": types.Int(),
+            "b": types.TypedDict({"b": types.String()}, not_required_keys={"b"}),
+        },
+        not_required_keys={"b"},
+    )
