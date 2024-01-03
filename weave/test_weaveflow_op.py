2236c7155a0618d8b7146721aed51bd3c76382a5
Tim Sweeney <tim@wandb.com>
2024-01-02 16:23:09 -0800

    lint

--- weave/test_weaveflow_op.py
+++ weave/test_weaveflow_op.py
@@ -1,8 +1,11 @@
 import weave
 
+
 def test_weaveflow_basic_addition(user_by_api_key_in_env):
     from weave import weaveflow
+
     weave.init("weaveflow_example")
+
     @weave.op()
     def custom_adder(a: int, b: int) -> int:
         return a + b
@@ -13,13 +16,13 @@ def test_weaveflow_basic_addition(user_by_api_key_in_env):
 
 def test_weaveflow_return_list(user_by_api_key_in_env):
     from weave import weaveflow
+
     project = "weaveflow_example"
     weave.init(project)
+
     @weave.op()
     def custom_adder(a: int, b: int) -> list[int]:
         return [a + b]
 
     res = custom_adder(1, 2)
     assert res == [3]
-
-
