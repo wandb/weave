b194c72ad3e15532c6eb7ff2e23e63afeb32de22
Tim Sweeney <tim@wandb.com>
2024-01-05 16:43:22 -0800

    init

--- weave/monitoring/langchain.py
+++ weave/monitoring/langchain.py
@@ -8,6 +8,7 @@ from wandb.sdk.data_types import trace_tree
 
 from weave.monitoring import StreamTable
 from weave import stream_data_interfaces
+from weave import graph_client_context as _graph_client_context
 
 # We should move this logic into our built-in langchain integration
 from langchain.callbacks.tracers.base import BaseTracer
@@ -23,23 +24,23 @@ patched = False
 
 
 # This should be committed to the langchain repo
-def patch_serialize() -> None:
-    global patched
-    if patched:
-        return
-    patched = True
+# def patch_serialize() -> None:
+#     global patched
+#     if patched:
+#         return
+#     patched = True
 
-    old_serialize_io = LCW._serialize_io
+#     old_serialize_io = LCW._serialize_io
 
-    def new_serialize_io(run_inputs):  # type: ignore
-        if run_inputs is None:
-            return {}
-        return old_serialize_io(run_inputs)
+#     def new_serialize_io(run_inputs):  # type: ignore
+#         if run_inputs is None:
+#             return {}
+#         return old_serialize_io(run_inputs)
 
-    LCW._serialize_io = new_serialize_io
+#     LCW._serialize_io = new_serialize_io
 
 
-patch_serialize()
+# patch_serialize()
 
 
 class WeaveTracer(BaseTracer):
@@ -63,3 +64,32 @@ class WeaveTracer(BaseTracer):
         spans = stream_data_interfaces.wb_span_to_weave_spans(root_span)
         for span in spans:
             self._st.log(span)
+
+
+from weave.run_streamtable_span import RunStreamTableSpan
+
+class WeaveflowTracer(BaseTracer):
+    def __init__(self, **kwargs: typing.Any) -> None:
+        super().__init__(**kwargs)
+        self.run_processor = LCW.RunProcessor(wandb, trace_tree)
+
+    def _persist_run(self, run: Run) -> None:
+        client = _graph_client_context.get_graph_client()
+        if not client:
+            raise ValueError("Call weave.init() first")
+        root_span = self.run_processor.process_span(run)
+        if root_span is None:
+            return
+        model_dict = self.run_processor.process_model(run)
+        model_str = json.dumps(model_dict)
+        spans = stream_data_interfaces.wb_span_to_weave_spans(root_span)
+        for span in spans:
+            try:
+                client.finish_run(
+                    run = RunStreamTableSpan(span),
+                    output = span['output'],
+                    output_refs = [],
+                )
+            except Exception as e:
+                print(e)
+                pass
