9498cf8f1c2c5d79bde94e5882abad50c61166ff
Tim Sweeney <tim@wandb.com>
2024-01-10 00:09:49 -0800

    lint

--- weave/tests/test_union_array.py
+++ weave/tests/test_union_array.py
@@ -1,5 +1,9 @@
 import pyarrow as pa
-from weave.ops_domain.run_history.run_history_v3_parquet_stream_optimized import _consolidate_matching_types_in_dense_union_array, _dedup_dense_union_array, _flatten_dense_union_array
+from weave.ops_arrow.convert import (
+    _consolidate_matching_types_in_dense_union_array,
+    _dedup_dense_union_array,
+    _flatten_dense_union_array,
+)
 
 
 def test_union_simple():
@@ -9,6 +13,7 @@ def test_union_simple():
         [pa.array([1, 2, 3]), pa.array([4, 5, 6]), pa.array([7, 8, 9])],
     )
     do_union_array_test(union_arr)
+
 
 def test_union_hard():
     union_arr = pa.UnionArray.from_dense(
@@ -22,6 +27,7 @@ def test_union_hard():
         [union_arr, pa.array([5, 6, None, 7])],
     )
     do_union_array_test(union_arr)
+
 
 def do_union_array_test(union_arr):
     flattened = _flatten_dense_union_array(union_arr)
