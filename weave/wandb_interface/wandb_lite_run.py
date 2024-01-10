4eda788edfe2b692301cdd692c69b2f4a1ba5245
Jason Zhao <jason@wandb.com>
2024-01-10 09:40:46 -0700

    wait for result

--- weave/wandb_interface/wandb_lite_run.py
+++ weave/wandb_interface/wandb_lite_run.py
@@ -218,7 +218,7 @@ class InMemoryLazyLiteRun:
             )
 
         with tracer.trace("ArtifactSaver.save"):
-            res = saver.save(
+            res, future = saver.save(
                 type=artifact_type_name,
                 name=artifact_name,
                 client_id=artifact._client_id,
@@ -228,6 +228,8 @@ class InMemoryLazyLiteRun:
                 aliases=["latest"] + additional_aliases,
                 use_after_commit=False,
             )
+            # wait for the artifact to be committed before returning the result
+            future.result()
         return res
 
     def upsert_project(self) -> None:
