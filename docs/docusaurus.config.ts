3d9edb1ffb907b0db99df1d0e56d92eccfba08d3
jwlee64 <josiah.lee@wandb.com>
2024-01-23 12:14:35 -0800

    fix title for preview

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "W&B Prompts",
+  title: "W&B Weave",
   tagline: "Confidently ship LLM applications.",
   favicon: "img/favicon.ico",
 
