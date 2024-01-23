1245ff232147aaaccf1bbc4007bb489e88edae6a
Tim Sweeney <tim@wandb.com>
2024-01-22 17:18:28 -0800

    init

--- weave/context_state.py
+++ weave/context_state.py
@@ -33,11 +33,13 @@ if patch_context:
         for k, v in _context.items():
             var = glbs.get("_" + k)
             if var is not None:
+                print("Restoring context", k, v)
                 var.set(v)
 
     def weave_post_run():
         _context.clear()
         for k, v in contextvars.copy_context().items():
+            print("Saving context", k.name, v)
             _context[k.name] = v
 
     ipython = get_ipython()
