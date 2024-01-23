2c035527ff726257bb617c4ac48bdfd5c7168d18
Josiah Lee <josiahwlee@gmail.com>
2024-01-22 16:55:38 -0800

    fix edit doc link to go to correct page (#1104)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/wandb/weave/blob/master/docs/",
           routeBasePath: "/",
         },
         theme: {
