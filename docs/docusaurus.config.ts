6a2e7ac206f0c2ebefd3e462a7dc4674e5749e19
Josiah Lee <josiahwlee@gmail.com>
2024-01-22 16:53:49 -0800

    chore(docs): add working directory (#1110)
    * add working directory
    
    * try without dot
    
    * add cache dependency path
    
    * fix build error
    
    * move permissions to job
    
    * remove github actions bot

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://wandb.github.io/weave",
+  url: "https://wandb.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/weave",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
