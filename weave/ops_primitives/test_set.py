838f4c44e171e7cec6a8a21a54fc5d20546e7edf
Shawn Lewis <shlewis@gmail.com>
2024-01-26 06:11:39 -0800

    Fix test

--- weave/ops_primitives/test_set.py
+++ weave/ops_primitives/test_set.py
@@ -1,13 +1,13 @@
 import weave
 
-from .. import weave_internal
+from .. import val_const
 from .. import ops_primitives
 
 
 def test_difference_type():
     diff = ops_primitives.difference(
-        [weave_internal.const("a"), weave_internal.const("b")],
-        [weave_internal.const("b")],
+        [val_const.const("a"), val_const.const("b")],
+        [val_const.const("b")],
     )
     # When difference operations on Const Union types, we get a const union
     # type back with the difference applied (ie difference works at type level
