2a24b9c7729d634a75bec4ef2efacd65fcae7330
Shawn Lewis <shlewis@gmail.com>
2024-01-27 09:33:38 -0800

    Fix some notebook tests, remove others

--- integration_test/cypress/e2e/notebooks/experimental/01 Tutorial - Operations.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../notebooks';
-
-describe('../examples/experimental/01 Tutorial - Operations.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/experimental/01 Tutorial - Operations.ipynb')
-    );
-});
--- integration_test/cypress/e2e/notebooks/getting_started/2_images_gen.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../notebooks';
-
-describe('../examples/getting_started/2_images_gen.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/getting_started/2_images_gen.ipynb')
-    );
-});
