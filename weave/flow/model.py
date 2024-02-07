54f792808f4244c93168fead789b0e9702dae3a4
Shawn Lewis <shlewis@gmail.com>
2024-02-06 16:31:40 -0800

    Fix lint

--- weave/flow/model.py
+++ weave/flow/model.py
@@ -5,4 +5,5 @@ from weave.flow.obj import Object
 
 class Model(Object):
     @weave.op()
-    async def predict(self, input: typing.Any) -> typing.Any: ...
+    async def predict(self, input: typing.Any) -> typing.Any:
+        ...
