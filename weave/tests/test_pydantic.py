46a83dd80b51b78e29ebad906819b686c7afeb79
Shawn Lewis <shlewis@gmail.com>
2024-02-07 06:24:14 -0800

    Fix tests

--- weave/tests/test_pydantic.py
+++ weave/tests/test_pydantic.py
@@ -85,10 +85,10 @@ def test_pydantic_saveload():
     a_type = weave.type_of(a)
     assert a_type.root_type_class().__name__ == "A"
 
-    weave.init_local_client()
-    weave.publish(a, name="my-a")
+    with weave.local_client():
+        weave.publish(a, name="my-a")
 
-    a2 = weave.ref("my-a").get()
-    assert a2.name == "my-a"
-    assert a2.description == None
-    assert a2.model_name == "my-model"
+        a2 = weave.ref("my-a").get()
+        assert a2.name == "my-a"
+        assert a2.description == None
+        assert a2.model_name == "my-model"
