9dd9807deaa5c8178246ff442506407bce60ebca
Tim Sweeney <tim@wandb.com>
2024-02-19 14:48:29 -0800

    put back a smaller fix

--- weave/api.py
+++ weave/api.py
@@ -179,6 +179,9 @@ def local_client() -> typing.Iterator[_graph_client.GraphClient]:
 def init_sql_client() -> _graph_client.GraphClient:
     return _weave_init.init_sql().client
 
+def init_trace_client() -> _graph_client.GraphClient:
+    return _weave_init.init_trace_remote().client
+
 @contextlib.contextmanager
 def sql_client() -> typing.Iterator[_graph_client.GraphClient]:
     inited_client = _weave_init.init_sql()
