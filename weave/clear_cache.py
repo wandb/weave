bed1423c2c91dfca7205eee7fdd6e501df55565e
jwlee64 <josiah.lee@wandb.com>
2024-02-20 16:39:29 -0800

    use print

--- weave/clear_cache.py
+++ weave/clear_cache.py
@@ -1,19 +1,17 @@
 from weave import cache
 
-import logging
 import time
 import os
 
-logger = logging.getLogger(__name__)
 
 # Script to run to delete expired caches
 if __name__ == "__main__":
-    logger.info("Starting clear cache job")
+    print("Starting clear cache job")
     hour_interval = int(os.getenv("WEAVE_CACHE_CLEAR_INTERVAL", 2))
-    logger.info("Clearing expiredcaches  every " + str(hour_interval) + " hours")
+    print("Clearing expiredcaches  every " + str(hour_interval) + " hours")
     while True:
         curTime = time.strftime("%x, %X", time.localtime())
-        logger.info("Clearing cache " + curTime)
+        print("Clearing cache " + curTime)
         cache.clear_cache()
         time.sleep(60*60*hour_interval)
 
