6d7fd209701b14e814b163fe0baffa38f36cf23b
jwlee64 <josiah.lee@wandb.com>
2024-02-20 16:53:52 -0800

    try setting ddtrace config

--- weave/weave_server.py
+++ weave/weave_server.py
@@ -72,11 +72,10 @@ if engine_trace.datadog_is_enabled():
     import ddtrace
 
     # patch all libraries to datadog except logging
-    ddtrace.patch_all(logging=False)
-
-    if not environment.disable_weave_pii():
-        # patch logging to datadog, if not disabled
-        ddtrace.patch(logging=True)
+    ddtrace.patch_all(logging=True)
+    
+    if environment.disable_weave_pii():
+        ddtrace.config.logs_injection = False
 
     custom_dd_patch()
 
