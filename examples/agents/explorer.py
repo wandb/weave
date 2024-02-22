71b3690f8c230addc9992fdd1e2501cc258fda5d
Shawn Lewis <shlewis@gmail.com>
2024-02-22 12:27:05 -0800

    Update explorer

--- examples/agents/explorer.py
+++ examples/agents/explorer.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 from rich import print
 
@@ -11,6 +12,8 @@ Agent never stops exploring, it is completely autonomous.
 Agent documents its learnings in a file call "journal.txt".
 """
 
+LENGTH_LIMIT = 1000
+
 
 @weave.op()
 def run_command(command: str) -> str:
@@ -22,11 +25,34 @@ def run_command(command: str) -> str:
     Returns:
         The output of the command.
     """
-    return subprocess.check_output(command, shell=True).decode("utf-8")
+    try:
+        completed_process = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            shell=True,
+        )
+        exit_code = completed_process.returncode
+        stdout = completed_process.stdout.strip()
+        stderr = completed_process.stderr.strip()
+    except Exception as e:
+        exit_code = -1
+        stdout = ""
+        stderr = str(e)
+
+    if len(stdout) > LENGTH_LIMIT:
+        stdout = stdout[:LENGTH_LIMIT]
+        stdout += "\n... (truncated)"
+    if len(stderr) > LENGTH_LIMIT:
+        stderr = stderr[:LENGTH_LIMIT]
+        stderr += "\n... (truncated)"
+
+    return json.dumps({"exit_code": exit_code, "stdout": stdout, "stderr": stderr})
 
 
 if __name__ == "__main__":
-    # weave.init("wf-agent5")
+    # weave.init("wf-explorer1")
 
     agent = Agent(
         model_name="gpt-4-0125-preview",
@@ -34,13 +60,26 @@ if __name__ == "__main__":
         tools=[run_command],
     )
 
-    initial_state = AgentState(
+    state = AgentState(
         history=[
             {
                 "role": "user",
-                "content": "Explore everything you can find. You are completely autonomous, so don't stop to ask for input, just keep exploring.",
+                "content": "Explore everything you can find, using your tools. You are completely autonomous, so don't stop to ask for input, just keep exploring.",
             },
         ]
     )
 
-    agent.run(initial_state)
+    while True:
+        state = agent.step(state)
+        last_message = state.history[-1]
+        if last_message["role"] == "assistant" and "tool_calls" not in last_message:
+            user_input = input("User input: ")
+            state = AgentState(
+                history=state.history
+                + [
+                    {
+                        "role": "user",
+                        "content": user_input,
+                    }
+                ]
+            )
