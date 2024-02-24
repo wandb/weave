3c0e89bed493bc8355f511737db26cbf5fdd3c5e
Tim Sweeney <tim@wandb.com>
2024-02-24 10:42:02 -0800

    lint

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/trace_server_client.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/trace_server_client.ts
@@ -3,7 +3,7 @@
  * The intention is that the implementation is a 1-1 mapping to the trace
  * server's API. This file should not contain any business logic. If possible,
  * we could generate this from `trace_server.py`. Which in effect is a perfect
- * mapping of `weave/trace_server/trace_server_interface.py` as a web service. 
+ * mapping of `weave/trace_server/trace_server_interface.py` as a web service.
  *
  * These types MUST be kept in sync with the types defined in
  * `weave/trace_server/trace_server_interface.py`. Please modify with care.
