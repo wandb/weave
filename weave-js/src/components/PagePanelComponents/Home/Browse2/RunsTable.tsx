e766d73a6377f5e3053f7109b2288470b7cebd42
Jamie Rasmussen <jamie.rasmussen@wandb.com>
2024-02-27 12:31:56 -0600

    fix: columns from other evaluations getting displayed

--- weave-js/src/components/PagePanelComponents/Home/Browse2/RunsTable.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse2/RunsTable.tsx
@@ -122,11 +122,19 @@ export const RunsTable: FC<{
   const tableData = useMemo(() => {
     return spans.map((call: CallSchema) => {
       const argOrder = call.rawSpan.inputs._input_order;
-      let args = _.fromPairs(
-        Object.entries(call.rawSpan.inputs).filter(
-          ([k, c]) => c != null && !k.startsWith('_')
-        )
-      );
+      let args: Record<string, any> = {};
+      if (call.rawSpan.inputs._keys) {
+        for (const key of call.rawSpan.inputs._keys) {
+          args[key] = call.rawSpan.inputs[key];
+        }
+      } else {
+        args = _.fromPairs(
+          Object.entries(call.rawSpan.inputs).filter(
+            ([k, c]) => c != null && !k.startsWith('_')
+          )
+        );
+      }
+
       if (argOrder) {
         args = _.fromPairs(argOrder.map((k: string) => [k, args[k]]));
       }
@@ -338,6 +346,7 @@ export const RunsTable: FC<{
       const inputOrder = !isSingleOpVersion
         ? getInputColumns(tableStats)
         : row0.rawSpan.inputs._arg_order ??
+          row0.rawSpan.inputs._keys ??
           Object.keys(_.omitBy(row0.rawSpan.inputs, v => v == null)).filter(
             k => !k.startsWith('_')
           );
