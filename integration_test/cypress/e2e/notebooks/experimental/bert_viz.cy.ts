e6416c329caa49611c274485487215e7e46d652e
Shawn <shlewis@gmail.com>
2024-02-28 16:08:46 -0800

    Disable huggingfacde notebook tests (#1264)
    Co-authored-by: Tim Sweeney <tim@wandb.com>

--- integration_test/cypress/e2e/notebooks/experimental/bert_viz.cy.ts
+++
@@ -1,7 +0,0 @@
-import {checkWeaveNotebookOutputs} from '../notebooks';
-
-describe('../examples/experimental/bert_viz.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/experimental/bert_viz.ipynb')
-    );
-});
