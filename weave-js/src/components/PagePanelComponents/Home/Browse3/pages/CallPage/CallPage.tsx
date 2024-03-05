195b2b852cd1bbdebae87c08ee0b5dcf458a0dc7
Jamie Rasmussen <112953339+jamie-rasmussen@users.noreply.github.com>
2024-03-05 09:42:56 -0600

    chore: remove CallPageInnerHorizontal (#1285)

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
@@ -1,26 +1,18 @@
 import {Box} from '@material-ui/core';
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 import {useHistory} from 'react-router-dom';
 
 import {Button} from '../../../../../Button';
 import {Browse2OpDefCode} from '../../../Browse2/Browse2OpDefCode';
 import {queryGetBoolean, queryToggleBoolean} from '../../urlQueryUtil';
 import {CenteredAnimatedLoader} from '../common/Loader';
-import {
-  SimplePageLayout,
-  SimplePageLayoutContext,
-  SimplePageLayoutWithHeader,
-} from '../common/SimplePageLayout';
-import {truncateID} from '../util';
+import {SimplePageLayoutWithHeader} from '../common/SimplePageLayout';
 import {useWFHooks} from '../wfReactInterface/context';
 import {CallSchema} from '../wfReactInterface/wfDataModelHooksInterface';
 import {CallDetails} from './CallDetails';
 import {CallOverview} from './CallOverview';
 import {CallSummary} from './CallSummary';
 import {CallTraceView} from './CallTraceView';
-
-// % of screen to give the trace view in horizontal mode
-const TRACE_PCT = 40;
 
 export const CallPage: FC<{
   entity: string;
@@ -34,28 +26,12 @@ export const CallPage: FC<{
     project: props.project,
     callId: props.callId,
   });
-  const [verticalLayout, setVerticalLayout] = useState(true);
   if (call.loading) {
     return <CenteredAnimatedLoader />;
   } else if (call.result === null) {
     return <div>Call not found</div>;
   }
-  if (verticalLayout) {
-    return (
-      <CallPageInnerVertical
-        {...props}
-        setVerticalLayout={setVerticalLayout}
-        call={call.result}
-      />
-    );
-  }
-  return (
-    <CallPageInnerHorizontal
-      {...props}
-      setVerticalLayout={setVerticalLayout}
-      call={call.result}
-    />
-  );
+  return <CallPageInnerVertical {...props} call={call.result} />;
 };
 
 const useCallTabs = (call: CallSchema) => {
@@ -80,70 +56,9 @@ const useCallTabs = (call: CallSchema) => {
   ];
 };
 
-const CallPageInnerHorizontal: FC<{
-  call: CallSchema;
-  setVerticalLayout: (vertical: boolean) => void;
-}> = ({call, setVerticalLayout}) => {
-  const {traceId, callId, spanName} = call;
-
-  const title = `${spanName}: ${truncateID(callId)}`;
-  const traceTitle = `Trace: ${truncateID(traceId)}`;
-
-  const callTabs = useCallTabs(call);
-
-  return (
-    <SimplePageLayout
-      title={traceTitle}
-      tabs={[
-        {
-          label: 'Trace',
-          content: (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                flex: '1 1 auto',
-                overflow: 'hidden',
-              }}>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  flex: `1 1 ${TRACE_PCT}%`,
-                  height: TRACE_PCT,
-                  overflow: 'hidden',
-                }}>
-                <CallTraceView call={call} />
-              </Box>
-              <Box
-                sx={{
-                  borderTop: '1px solid #e0e0e0',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  flex: `1 1 ${100 - TRACE_PCT}%`,
-                  height: 100 - TRACE_PCT,
-                  overflow: 'hidden',
-                }}>
-                <SimplePageLayoutContext.Provider value={{}}>
-                  <SimplePageLayout
-                    title={title}
-                    // menuItems={callMenuItems}
-                    tabs={callTabs}
-                  />
-                </SimplePageLayoutContext.Provider>
-              </Box>
-            </Box>
-          ),
-        },
-      ]}
-    />
-  );
-};
-
 const CallPageInnerVertical: FC<{
   call: CallSchema;
-  setVerticalLayout: (vertical: boolean) => void;
-}> = ({call, setVerticalLayout}) => {
+}> = ({call}) => {
   const callTabs = useCallTabs(call);
   const history = useHistory();
   const showTraceTree = queryGetBoolean(history, 'tracetree', true);
