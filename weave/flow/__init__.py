1c81545c0ad58818a6e2fd5544daba1c2e88c4f3
Shawn <shlewis@gmail.com>
2024-03-08 08:35:15 -0700

    feat: Improved Eval API (#1297)
    * text-extract example data
    
    * New eval
    
    * Remove tui
    
    * OpDef as scorer
    
    * Fix error check
    
    * Fix error
    
    * Evaluate tests
    
    * Reorganize
    
    * Fixes and tests
    
    * Fix lint
    
    * scores -> scorers
    
    * Address code review
    
    * PR Comments

--- weave/flow/__init__.py
+++ weave/flow/__init__.py
@@ -1,5 +1,6 @@
 from .obj import Object
 from .dataset import Dataset
 from .model import Model
-from .evaluate import Evaluation
+from .eval import Evaluation, evaluate
+from .scorer import Scorer
 from .agent import Agent, AgentState
--- weave/tests/test_evaluate.py
+++ weave/tests/test_evaluate.py
@@ -2,39 +2,48 @@ import asyncio
 import pytest
 import weave
 from weave import ref_base
-from weave.flow import Dataset, Model, Evaluation
+from weave.flow.scorer import MulticlassF1Score
+from weave import Dataset, Model, Evaluation
+
+dataset_rows = [{"input": "1 + 2", "target": 3}, {"input": "2**4", "target": 15}]
+dataset = Dataset(rows=dataset_rows)
+
+expected_eval_result = {
+    "prediction": {"mean": 9.5},
+    "score": {"true_count": 1, "true_fraction": 0.5},
+}
+
+
+class EvalModel(Model):
+    @weave.op()
+    async def predict(self, input) -> str:
+        return eval(input)
+
+
+@weave.op()
+def score(target, prediction):
+    return target == prediction
+
+
+@weave.op()
+def example_to_model_input(example):
+    return {"input": example["input"]}
 
 
 def test_evaluate_basic():
     with weave.local_client():
-        dataset = Dataset(
-            rows=[{"input": "1 + 2", "output": "3"}, {"input": "2**4", "output": "16"}]
-        )
-
-        class EvalModel(Model):
-            @weave.op()
-            async def predict(self, input: str) -> str:
-                return eval(input)
-
-        @weave.op()
-        def score(example: dict, prediction: str) -> int:
-            return int(example["output"] == prediction)
-
-        @weave.op()
-        def example_to_model_input(example):
-            return example["input"]
 
         evaluation = Evaluation(
             dataset=dataset,
-            scores=[score],
-            example_to_model_input=example_to_model_input,
+            scorers=[score],
+            preprocess_model_input=example_to_model_input,
         )
         model = EvalModel()
         result = asyncio.run(evaluation.evaluate(model))
 
-        assert result == {"prediction": {"mean": 9.5}, "score": {"mean": 0.0}}
+        assert result == expected_eval_result
 
-        example_to_model_input0_run = evaluation.example_to_model_input.runs()[0]
+        example_to_model_input0_run = evaluation.preprocess_model_input.runs()[0]
         example_to_model_input0_run_example = example_to_model_input0_run.inputs[
             "example"
         ]
@@ -49,7 +58,7 @@ def test_evaluate_basic():
         ]
         assert example_to_model_input0_run_example.get() == {
             "input": "1 + 2",
-            "output": "3",
+            "target": 3,
         }
 
         predict0_run = EvalModel.predict.runs()[0]
@@ -89,3 +98,146 @@ def test_evaluate_basic():
         #     - fetch all preds from row
         #   - add col to dataset
         #   - update row in dataset
+
+
+def test_evaluate_callable_as_model():
+    @weave.op()
+    async def model_predict(input) -> str:
+        return eval(input)
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[score],
+        )
+        result = asyncio.run(evaluation.evaluate(model_predict))
+        assert result == expected_eval_result
+
+
+def test_predict_can_receive_other_params():
+    @weave.op()
+    async def model_predict(input, target) -> str:
+        return eval(input) + target
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[score],
+        )
+        result = asyncio.run(evaluation.evaluate(model_predict))
+        assert result == {
+            "prediction": {"mean": 18.5},
+            "score": {"true_count": 0, "true_fraction": 0.0},
+        }
+
+
+def test_can_preprocess_model_input():
+    @weave.op()
+    async def model_predict(x) -> str:
+        return eval(x)
+
+    @weave.op()
+    def preprocess(example):
+        return {"x": example["input"]}
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[score],
+            preprocess_model_input=preprocess,
+        )
+        result = asyncio.run(evaluation.evaluate(model_predict))
+        assert result == expected_eval_result
+
+
+def test_evaluate_rows_only():
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[score],
+        )
+        model = EvalModel()
+        result = asyncio.run(evaluation.evaluate(model))
+        assert result == expected_eval_result
+
+
+def test_evaluate_other_model_method_names():
+    class EvalModel(Model):
+        @weave.op()
+        async def infer(self, input) -> str:
+            return eval(input)
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[score],
+        )
+        model = EvalModel()
+        result = asyncio.run(evaluation.evaluate(model))
+        assert result == expected_eval_result
+
+
+def test_score_as_class():
+    class MyScorer(weave.Scorer):
+        def score(self, target, prediction):
+            return target == prediction
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[MyScorer()],
+        )
+        model = EvalModel()
+        result = asyncio.run(evaluation.evaluate(model))
+        assert result == {
+            "prediction": {"mean": 9.5},
+            "MyScorer": {"true_count": 1, "true_fraction": 0.5},
+        }
+
+
+def test_score_with_custom_summarize():
+    class MyScorer(weave.Scorer):
+        def summarize(self, score_rows):
+            assert list(score_rows) == [True, False]
+            return {"awesome": 3}
+
+        def score(self, target, prediction):
+            return target == prediction
+
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=dataset_rows,
+            scorers=[MyScorer()],
+        )
+        model = EvalModel()
+        result = asyncio.run(evaluation.evaluate(model))
+        assert result == {
+            "prediction": {"mean": 9.5},
+            "MyScorer": {"awesome": 3},
+        }
+
+
+def test_multiclass_f1_score():
+    with weave.local_client():
+        evaluation = Evaluation(
+            dataset=[
+                {"target": {"a": False, "b": True}, "pred": {"a": True, "b": False}}
+            ],
+            scorers=[MulticlassF1Score(class_names=["a", "b"])],
+        )
+
+        @weave.op()
+        def return_pred(pred):
+            return pred
+
+        result = asyncio.run(evaluation.evaluate(return_pred))
+        assert result == {
+            "prediction": {
+                "a": {"true_count": 1, "true_fraction": 1.0},
+                "b": {"true_count": 0, "true_fraction": 0.0},
+            },
+            "MulticlassF1Score": {
+                "a": {"f1": 0, "precision": 0.0, "recall": 0},
+                "b": {"f1": 0, "precision": 0, "recall": 0.0},
+            },
+        }
