9e06fc3b2411ea787f9108fbdae05d2010abcb79
Jamie Rasmussen <jamie.rasmussen@wandb.com>
2024-03-21 16:14:08 -0500

    chore: use common Alert component instead of Material's

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/TabUseModel.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/TabUseModel.tsx
@@ -1,8 +1,9 @@
-import {Alert, Box} from '@mui/material';
+import {Box} from '@mui/material';
 import React from 'react';
 
 import {isValidVarName} from '../../../../../core/util/var';
 import {parseRef} from '../../../../../react';
+import {Alert} from '../../../../Alert';
 import {CopyableText} from '../../../../CopyableText';
 import {DocLink} from './common/Links';
 
@@ -20,7 +21,7 @@ export const TabUseModel = ({name, uri, projectName}: TabUseModelProps) => {
 
   return (
     <Box m={2}>
-      <Alert severity="info" variant="outlined">
+      <Alert icon="lightbulb-info">
         See{' '}
         <DocLink
           path="guides/tracking/objects#getting-an-object-back"
