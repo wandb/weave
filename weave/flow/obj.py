6e83b51c463743b748ed7fb785d56c94383234b4
Shawn Lewis <shlewis@gmail.com>
2024-03-24 10:40:06 -0700

    Remove unused code

--- weave/flow/obj.py
+++ weave/flow/obj.py
@@ -1,24 +1,15 @@
-import typing
-from typing import Optional, Type, Tuple, Dict, Any
-from pydantic import (
-    ConfigDict,
-    model_validator,
-    ValidatorFunctionWrapHandler,
-    ValidationInfo,
-)
-from pydantic._internal._model_construction import ModelMetaclass
+from typing import Optional
+from pydantic import ConfigDict
 import pydantic
 
 from weave.trace.op import Op
-from .. import box
-from .. import ref_base
 
 
 class Object(pydantic.BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
 
-    # Allow OpDef attributes
+    # Allow Op attributes
     model_config = ConfigDict(
         ignored_types=(Op,),
         arbitrary_types_allowed=True,
@@ -27,32 +18,6 @@ class Object(pydantic.BaseModel):
     )
 
     __str__ = pydantic.BaseModel.__repr__
-
-    # This is a "wrap" validator meaning we can run our own logic before
-    # and after the standard pydantic validation.
-    @model_validator(mode="wrap")
-    @classmethod
-    def handle_relocatable_object(
-        cls, v: Any, handler: ValidatorFunctionWrapHandler, info: ValidationInfo
-    ) -> Any:
-        if hasattr(v, "_weave_obj_fields"):
-            # This is a relocated weave object, so destructure it into a dictionary
-            # so pydantic can validate it.
-            fields = {}
-            for k in v._weave_obj_fields:
-                val = getattr(v, k)
-                if isinstance(val, box.BoxedNone):
-                    val = None
-                fields[k] = val
-            # pydantic validation will construct a new pydantic object
-            new_obj = handler(fields)
-            # transfer ref to new object
-            ref = ref_base.get_ref(v)
-            if ref is not None:
-                ref_base._put_ref(new_obj, ref)
-            return new_obj
-        # otherwise perform standard pydantic validation
-        return handler(v)
 
 
 # We don't define this directly in the class definition so that VSCode
