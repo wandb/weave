4f2ee13c43f873c071f43fae431557b1a264254f
Tim Sweeney <tim@wandb.com>
2024-03-27 18:32:31 -0700

    fixed

--- weave/flow/dataset.py
+++ weave/flow/dataset.py
@@ -1,9 +1,10 @@
-from typing import Union, Any
+from typing import Any
 
 from pydantic import field_validator
 import weave
 
 from weave.flow.obj import Object
+from weave.trace.vals import TraceTable
 
 
 class Dataset(Object):
@@ -12,5 +13,7 @@ class Dataset(Object):
     @field_validator("rows", mode="before")
     def convert_to_table(cls, rows: Any) -> weave.Table:
         if not isinstance(rows, weave.Table):
+            if isinstance(rows, TraceTable):
+                rows = list(rows)
             return weave.Table(rows)
         return rows
