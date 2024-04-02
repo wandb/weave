8c431b8d8ac2be289591fa5a2c9d044674b02f2c
Tim Sweeney <tim@wandb.com>
2024-04-01 21:47:04 -0700

    init test

--- weave/tests/test_client_trace.py
+++ weave/tests/test_client_trace.py
@@ -766,3 +766,16 @@ def test_dataset_row_type(client):
         d = weave.Dataset(rows=[{"a": 1}, "a", "b"])
     with pytest.raises(ValidationError):
         d = weave.Dataset(rows=[{"a": 1}, {}])
+
+
+def test_dataset_row_ref(client):
+    d = weave.Dataset(rows=[{"a": 5, "b": 6}, {"a": 7, "b": 10}])
+    ref = weave.publish(d)
+    d2 = weave.ref(ref.uri()).get()
+    
+    inner = d2.rows[0]["a"]
+    exp_ref = 'weave:///shawn/test-project/object/Dataset:aF7lCSKo9BTXJaPxYHEBsH51dOKtwzxS6Hqvw4RmAdc/attr/rows/id/XfhC9dNA5D4taMvhKT4MKN2uce7F56Krsyv4Q6mvVMA/key/a'
+    assert inner == 5
+    assert inner.ref.uri() == exp_ref
+    gotten = weave.ref(exp_ref).get()
+    assert gotten == 5
