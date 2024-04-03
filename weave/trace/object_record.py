762d1fcc33e5dc47da636d0afb64cf3560d92463
jwlee64 <josiah.lee@wandb.com>
2024-04-02 18:09:09 -0700

    catch exceptions in getmembers, have a better error message for instruct or folks

--- weave/trace/object_record.py
+++ weave/trace/object_record.py
@@ -1,6 +1,7 @@
 import dataclasses
-import inspect
 import pydantic
+import types
+from inspect import isclass, getmro
 
 from typing import Any, Callable
 
@@ -47,9 +48,8 @@ def class_all_bases_names(cls: type) -> list[str]:
 
 def pydantic_object_record(obj: pydantic.BaseModel) -> ObjectRecord:
     attrs = pydantic_asdict_one_level(obj)
-    # print('asdfasdfasd', flush=True)
-    # for k, v in inspect.getmembers(obj, lambda x: isinstance(x, Op)):
-    #     attrs[k] = v
+    for k, v in getmembers(obj, lambda x: isinstance(x, Op)):
+        attrs[k] = v
     attrs["_class_name"] = obj.__class__.__name__
     attrs["_bases"] = class_all_bases_names(obj.__class__)
     return ObjectRecord(attrs)
@@ -65,8 +65,65 @@ def dataclass_object_record(obj: Any) -> ObjectRecord:
     if not dataclasses.is_dataclass(obj):
         raise ValueError(f"{obj} is not a dataclass")
     attrs = dataclass_asdict_one_level(obj)
-    for k, v in inspect.getmembers(obj, lambda x: isinstance(x, Op)):
+    for k, v in getmembers(obj, lambda x: isinstance(x, Op)):
         attrs[k] = v
     attrs["_class_name"] = obj.__class__.__name__
     attrs["_bases"] = class_all_bases_names(obj.__class__)
     return ObjectRecord(attrs)
+
+
+# This is an exact copy of the getmembers function from the inspect module
+# with the addition of handling exceptions when calling getattr
+def getmembers(object, predicate=None):
+    """Return all members of an object as (name, value) pairs sorted by name.
+    Optionally, only return members that satisfy a given predicate."""
+    if isclass(object):
+        mro = (object,) + getmro(object)
+    else:
+        mro = ()
+    results = []
+    processed = set()
+    names = dir(object)
+    # :dd any DynamicClassAttributes to the list of names if object is a class;
+    # this may result in duplicate entries if, for example, a virtual
+    # attribute with the same name as a DynamicClassAttribute exists
+    try:
+        for base in object.__bases__:
+            for k, v in base.__dict__.items():
+                if isinstance(v, types.DynamicClassAttribute):
+                    names.append(k)
+    except AttributeError:
+        pass
+    for key in names:
+        # First try to get the value via getattr.  Some descriptors don't
+        # like calling their __get__ (see bug #1785), so fall back to
+        # looking in the __dict__.
+        try:
+            value = getattr(object, key)
+            # handle the duplicate key
+            if key in processed:
+                raise AttributeError
+        except AttributeError:
+            for base in mro:
+                if key in base.__dict__:
+                    value = base.__dict__[key]
+                    break
+            else:
+                # could be a (currently) missing slot member, or a buggy
+                # __dir__; discard and move on
+                continue
+
+        # This is where the modified code begins
+        except Exception as e:
+            # This is for the one off case where the xmltodict library is not installed in Instructor's environment
+            if str(e) == "name 'ET' is not defined":
+                print("ImportError: cannot import name xmltodict")
+            else:
+                print(f"Error while calling getattr: {e}")
+        # This is where the modified code ends
+
+        if not predicate or predicate(value):
+            results.append((key, value))
+        processed.add(key)
+    results.sort(key=lambda pair: pair[0])
+    return results
