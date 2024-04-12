e514b97e6ceeb8368623acbe71d3817e3db4b4cc
Tim Sweeney <tim@wandb.com>
2024-04-12 16:53:46 -0700

    init

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/heuristics.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/heuristics.ts
@@ -1,8 +1,12 @@
+const stringIsLikelyURL = (str: string) => {
+    return str.startsWith('http://') || str.startsWith('https://');
+}
+
 export const stringIsLikelyImageURL = (str: string) => {
     return (
         // OpenAI DALL-E API image URLs
         str.startsWith('https://oaidalleapiprodscus.blob.core.windows.net') || 
         // Other image URLs
-        (str.startsWith('https://') && str.match(/\.(jpeg|jpg|gif|png)$/) !== null)
+        (stringIsLikelyURL(str) && str.match(/\.(jpeg|jpg|gif|png)$/) !== null)
     );
 }
