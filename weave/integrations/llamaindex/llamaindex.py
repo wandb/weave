35224b53773acdb648f157c57f8735c136cc352d
Tim Sweeney <tim@wandb.com>
2024-05-08 16:58:14 -0700

    a bunch of comments

--- weave/integrations/llamaindex/llamaindex.py
+++ weave/integrations/llamaindex/llamaindex.py
@@ -25,7 +25,12 @@ if not import_failed:
             event_starts_to_ignore: Optional[List[CBEventType]] = None,
             event_ends_to_ignore: Optional[List[CBEventType]] = None,
         ) -> None:
+            # Map from event id to call object - this is used for bookkeeping
+            # and eventually closing the call.
             self._call_map: Dict[str, Call] = {}
+
+            # Everything below here is just boilerplate for inheriting from
+            # BaseCallbackHandler.
             event_starts_to_ignore = (
                 event_starts_to_ignore if event_starts_to_ignore else []
             )
@@ -44,29 +49,52 @@ if not import_failed:
             **kwargs: Any,
         ) -> str:
             """Run when an event starts and return id of event."""
+            # Get a handle to the internal graph client.
             gc = graph_client_context.require_graph_client()
 
+            # Check to see if the event is an exception.
             if event_type == CBEventType.EXCEPTION:
+
+                # If the event is an exception, and we are actively tracking the corresponding
+                # call, finish the call with the exception.
                 if event_id in self._call_map:
+
+                    # Pop the call from the call map.
                     call = self._call_map.pop(event_id)
+
+                    # Get the exception from the payload if it exists, otherwise use a default message.
                     if payload:
                         exception = payload.get("EXCEPTION")
                     else:
                         exception = "Unknown exception occurred."
+
+                    # Finish the call with the exception.
                     gc.finish_call(call, None, exception=exception)
             else:
+                # Check if the event is a valid root event or child event.
+                # Here, we only allow a subset of event types as the root since not
+                # all event types are meaningful in the weave context.
                 is_valid_root = (
                     parent_id == "root" and event_type in ALLOWED_ROOT_EVENT_TYPES
                 )
+                # Since we don't track all calls, it is possible that the parent_id
+                # is not in the call map. In this case, we don't want to track the child event.
                 is_valid_child = parent_id != "root" and parent_id in self._call_map
+
+                # If the event is valid, create a call and add it to the call map.
                 if is_valid_root or is_valid_child:
+
+                    # Create a call object.
                     call = gc.create_call(
                         "llama_index." + event_type.name.lower(),
                         self._call_map.get(parent_id),
                         process_payload(payload),
                     )
 
+                    # Add the call to the call map.
                     self._call_map[event_id] = call
+
+            # Return the event id (this is just part of interface requirements)
             return event_id
 
         def on_event_end(
@@ -77,12 +105,18 @@ if not import_failed:
             **kwargs: Any,
         ) -> None:
             """Run when an event ends."""
+            # Get a handle to the internal graph client.
+            gc = graph_client_context.require_graph_client()
+
+            # If the event is in the call map, finish the call.
             if event_id in self._call_map:
-                gc = graph_client_context.require_graph_client()
+
+                # Finish the call.
                 gc.finish_call(self._call_map.pop(event_id), process_payload(payload))
 
         def start_trace(self, trace_id: Optional[str] = None) -> None:
             """Run when an overall trace is launched."""
+            # Not implemented - required by interface.
             pass
 
         def end_trace(
@@ -91,6 +125,7 @@ if not import_failed:
             trace_map: Optional[Dict[str, List[str]]] = None,
         ) -> None:
             """Run when an overall trace is exited."""
+            # Not implemented - required by interface.
             pass
 
 else:
