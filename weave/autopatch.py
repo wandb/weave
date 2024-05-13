12d0376f156eb457087bd6d930d84aa20fdadf24
Tim Sweeney <tim@wandb.com>
2024-05-13 09:44:53 -0700

    chore(weave): LiteLLM Support + Stream-by-flag (#1609)
    * init tests
    
    * init impl
    
    * lint
    
    * getting closer
    
    * added switch logic for the accumulator
    
    * finished
    
    * set name
    
    * set name
    
    * correct default values
    
    * attempt to fix the problem
    
    * attempting different requirements
    
    * attempt fix
    
    * move api key

--- weave/autopatch.py
+++ weave/autopatch.py
@@ -25,5 +25,7 @@ def autopatch() -> None:
     autopatch_openai()
 
     from .integrations.mistral.mistral import mistral_patcher
+    from .integrations.litellm.litellm import litellm_patcher
 
     mistral_patcher.attempt_patch()
+    litellm_patcher.attempt_patch()
