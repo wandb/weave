074b210705aa7c910f3c53b3bef1f783f1e42b1a
Tim Sweeney <tim@wandb.com>
2024-05-13 14:30:51 -0700

    chore(weave): Basic LLamaIndex support + integration nicities (#1613)
    * python side complete
    
    * python side complete
    
    * python side complete
    
    * python side complete
    
    * init llama index work
    
    * lint
    
    * some initial work
    
    * lint city
    
    * hmmm getting a bit closer - wish there was a better way to save the "model" and "index"
    
    * added the async work
    
    * fixed tests
    
    * moved some more of the logic down (anon ops and stack management)... lets see what breaks!
    
    * fix pytest versions
    
    * a bunch of comments
    
    * finished
    
    * fixed again
    
    * change everything back once again
    
    * some test fixes
    
    * fixed some test
    
    * fixed some other tests
    
    * fixed some other tests 3
    
    * fixed some other tests 4
    
    * fixed some other tests 5
    
    * fixed some other tests 6
    
    * lint
    
    * undo previous changes to tests & lint

--- weave/run_context.py
+++ weave/run_context.py
@@ -2,6 +2,7 @@ import contextlib
 import contextvars
 import typing
 import copy
+import logging
 
 if typing.TYPE_CHECKING:
     # from .run import Run
@@ -10,6 +11,8 @@ if typing.TYPE_CHECKING:
 _run_stack: contextvars.ContextVar[list["Call"]] = contextvars.ContextVar(
     "run", default=[]
 )
+
+logger = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
@@ -24,6 +27,40 @@ def current_run(
         yield new_stack
     finally:
         _run_stack.reset(token)
+
+
+def push_call(run: "Call") -> None:
+    new_stack = copy.copy(_run_stack.get())
+    new_stack.append(run)
+    _run_stack.set(new_stack)
+
+
+def pop_call(call_id: typing.Optional[str]) -> None:
+    new_stack = copy.copy(_run_stack.get())
+    if len(new_stack) == 0:
+        logger.warning(
+            f"weave pop_call error: Found empty callstack when popping call_id: {call_id}."
+        )
+        # raise ValueError("Call stack is empty")
+        return
+    if call_id:
+        # assert that the call_id is in the stack
+        for i in range(len(new_stack)):
+            target_index = -(i + 1)
+            call = new_stack[target_index]
+            if call.id == call_id:
+                # Actually do the slice
+                new_stack = new_stack[:target_index]
+                break
+        else:
+            logger.warning(
+                f"weave pop_call error: Call with id {call_id} not found in stack."
+            )
+            # raise ValueError(f"Call with id {call_id} not found in stack")
+            return
+    else:
+        new_stack.pop()
+    _run_stack.set(new_stack)
 
 
 def get_current_run() -> typing.Optional["Call"]:
