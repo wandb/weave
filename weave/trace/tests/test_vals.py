6a5ac3bee7a254f0a28f832ec0d4d2a21bef6846
Tim Sweeney <tim@wandb.com>
2024-05-14 14:24:51 -0700

    chore(weave): Fixes issue where graph client is closed but trace object still in memory (#1634)
    * init
    
    * added next test
    
    * fixed pydantic

--- weave/trace/tests/test_vals.py
+++ weave/trace/tests/test_vals.py
@@ -1,6 +1,7 @@
 import pytest
 
 from weave.trace.vals import TraceObject
+import weave
 
 
 def test_traceobject_properties():
@@ -11,3 +12,28 @@ def test_traceobject_properties():
 
     to = TraceObject(A(), None, None, None)
     assert to.x == 1
+
+
+def test_traceobject_access_after_init_termination(client):
+    my_obj = None
+
+    class MyObj(weave.Object):
+        val: int
+
+    @weave.op()
+    def my_op(obj: MyObj) -> None:
+        nonlocal my_obj
+        my_obj = obj
+
+    my_op(MyObj(val=1))
+
+    assert my_obj.val == 1
+
+    # Here we explicitly close the client in order to
+    # simulate a situation where the client is closed
+    # but a reference to a trace object still exists.
+    from weave import context_state
+
+    context_state._graph_client.set(None)
+
+    assert my_obj.val == 1
