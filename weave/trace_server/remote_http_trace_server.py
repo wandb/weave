8e46870977d768bc502f4f702b33943a994d2817
Nick Penaranda <nick.penaranda@wandb.com>
2024-05-20 10:44:09 -0400

    Only retry non-4xx http and other unknown errors

--- weave/trace_server/remote_http_trace_server.py
+++ weave/trace_server/remote_http_trace_server.py
@@ -41,7 +41,12 @@ REMOTE_REQUEST_RETRY_MAX_INTERVAL = 60 * 5  # 5 minutes
 
 
 def _is_retryable_exception(e: Exception) -> bool:
-    # TODO: Identify unretriable exceptions
+    if isinstance(e, requests.HTTPError):
+        code_class = e.response.status_code // 100
+        if code_class == 4 and e.response_status_code != 429:
+            return False  # Bad request, not rate limited
+
+    # Retry on all other exceptions
     return True
 
 
