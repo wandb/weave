cb62a8db1000db6bcb5b5f4fb015702d25dc29ab
Tim Sweeney <tim@wandb.com>
2024-05-20 19:03:46 -0700

    lint

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/context.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/context.tsx
@@ -51,6 +51,7 @@ const WFDataModelFromTraceServerCallsOnlyProvider: FC = ({children}) => {
     return {
       useCall: tsWFDataModelHooks.useCall,
       useCalls: tsWFDataModelHooks.useCalls,
+      useCallsStats: tsWFDataModelHooks.useCallsStats,
       useOpVersion: cgWFDataModelHooks.useOpVersion,
       useOpVersions: cgWFDataModelHooks.useOpVersions,
       useObjectVersion: cgWFDataModelHooks.useObjectVersion,
