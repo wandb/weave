e312d767dab28f36c741897e3a7817015e71a42e
gtarpenning <griffin.tarpenning@wandb.com>
2024-05-28 11:24:40 -0400

    wip

---
+++ weave/trace_server/migrations/003_rename_calls.up.sql
@@ -0,0 +1,21 @@
+ALTER TABLE calls_merged_view MODIFY QUERY
+    SELECT project_id,
+        id,
+        anySimpleState(wb_run_id) as wb_run_id,
+        anySimpleStateIf(wb_user_id, isNotNull(call_parts.started_at)) as wb_user_id,
+        anySimpleState(trace_id) as trace_id,
+        anySimpleState(parent_id) as parent_id,
+        -- **** use most recent op_name ****
+        argMaxSimpleState(op_name, call_parts.created_at) as op_name,
+        anySimpleState(started_at) as started_at,
+        anySimpleState(attributes_dump) as attributes_dump,
+        anySimpleState(inputs_dump) as inputs_dump,
+        array_concat_aggSimpleState(input_refs) as input_refs,
+        anySimpleState(ended_at) as ended_at,
+        anySimpleState(output_dump) as output_dump,
+        anySimpleState(summary_dump) as summary_dump,
+        anySimpleState(exception) as exception,
+        array_concat_aggSimpleState(output_refs) as output_refs
+    FROM call_parts
+    GROUP BY project_id,
+        id;
