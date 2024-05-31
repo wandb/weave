55164444ca41f1b0eb365fc7db306023e402e6c4
soumik12345 <19soumik.rakshit96@gmail.com>
2024-05-31 14:13:31 +0000

    add: diffusers integration

---
+++ weave/integrations/diffusers/diffusers.py
@@ -0,0 +1,17 @@
+import importlib
+
+import weave
+from weave.trace.patcher import SymbolPatcher, MultiPatcher
+
+from diffusers import StableDiffusionPipeline
+
+
+diffusers_patcher = MultiPatcher(
+    [
+        SymbolPatcher(
+            get_base_symbol=lambda: importlib.import_module("diffusers"),
+            attribute_name="StableDiffusionPipeline.__call__",
+            make_new_value=weave.op(),
+        )
+    ]
+)
