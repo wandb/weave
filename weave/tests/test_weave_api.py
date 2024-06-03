a723132e031ecfa39b849025bc7d6ac11b28c76b
jwlee64 <josiah.lee@wandb.com>
2024-06-03 16:34:10 -0700

    tests

--- weave/tests/test_weave_api.py
+++ weave/tests/test_weave_api.py
@@ -28,3 +28,27 @@ def test_create_list_rename_delete():
     weave.ops.delete_artifact(art_node)
     arts = weave.use(weave.ops.local_artifacts())
     assert len(arts) == 0
+
+
+def test_weave_finish_unsets_client(client):
+    @weave.op()
+    def test_op(message: str) -> str:
+        return message
+
+    calls = list(client.calls())
+    assert len(calls) == 0
+
+    weave.init("test-project")
+    assert test_op("hello") == "hello"
+    calls = list(client.calls())
+    assert len(calls) == 1
+
+    weave.finish()
+    assert test_op("hello2") == "hello2"
+    calls = list(client.calls())
+    assert len(calls) == 1
+
+    weave.init("test-project")
+    assert test_op("hello3") == "hello3"
+    calls = list(client.calls())
+    assert len(calls) == 2
