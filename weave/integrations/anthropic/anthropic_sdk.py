6082188ca297a91eadb649bec4ba1fa5552755c1
Thomas Capelle <tcapelle@pm.me>
2024-06-03 18:50:26 +0200

    lint

--- weave/integrations/anthropic/anthropic_sdk.py
+++ weave/integrations/anthropic/anthropic_sdk.py
@@ -59,8 +59,8 @@ def anthropic_accumulator(
 
     return acc
 
-# TODO: Add accumulator for beta.messages for tool usage
 
+# TODO: Add accumulator for beta.messages for tool usage
 
 
 # Unlike other integrations, streaming is based on input flag
@@ -68,15 +68,14 @@ def should_use_accumulator(inputs: typing.Dict) -> bool:
     return isinstance(inputs, dict) and bool(inputs.get("stream"))
 
 
-def create_wrapper(name:str):
+def create_wrapper(name: str):
     def wrapper(fn: typing.Callable) -> typing.Callable:
         "We need to do this so we can check if `stream` is used"
         op = weave.op()(fn)
-        op.name = name # type: ignore
+        op.name = name  # type: ignore
         return add_accumulator(
-            op, 
-            anthropic_accumulator, 
-            should_accumulate=should_use_accumulator)
+            op, anthropic_accumulator, should_accumulate=should_use_accumulator
+        )
 
     return wrapper
 
--- weave/integrations/anthropic/anthropic_test.py
+++ weave/integrations/anthropic/anthropic_test.py
@@ -10,6 +10,7 @@ from typing import Any, Generator
 model = "claude-3-haiku-20240307"
 # model = "claude-3-opus-20240229"
 
+
 def _get_call_output(call: tsi.CallSchema) -> Any:
     """This is a hack and should not be needed. We should be able to auto-resolve this for the user.
 
@@ -20,8 +21,10 @@ def _get_call_output(call: tsi.CallSchema) -> Any:
         return weave.ref(call_output).get()
     return call_output
 
+
 @pytest.mark.vcr(
-    filter_headers=["authorization"], allowed_hosts=["api.wandb.ai", "localhost"],
+    filter_headers=["authorization"],
+    allowed_hosts=["api.wandb.ai", "localhost"],
 )
 def test_anthropic(
     client: weave.weave_client.WeaveClient,
@@ -46,7 +49,7 @@ def test_anthropic(
     output = _get_call_output(call)
     assert output.id == message.id
     assert output.model == message.model
-    assert output.stop_reason== "end_turn"
+    assert output.stop_reason == "end_turn"
     assert output.stop_sequence == None
     assert output.content[0].text == exp
     summary = call.summary
@@ -92,7 +95,7 @@ def test_anthropic_stream(
     output = _get_call_output(call)
     assert output.id == message.id
     assert output.model == message.model
-    assert output.stop_reason== "end_turn"
+    assert output.stop_reason == "end_turn"
     assert output.stop_sequence == None
     assert output.content[0].text == exp
     summary = call.summary
@@ -133,7 +136,7 @@ async def test_async_anthropic(
     output = _get_call_output(call)
     assert output.id == message.id
     assert output.model == message.model
-    assert output.stop_reason== "end_turn"
+    assert output.stop_reason == "end_turn"
     assert output.stop_sequence == None
     assert output.content[0].text == exp
     summary = call.summary
@@ -142,6 +145,7 @@ async def test_async_anthropic(
     assert model_usage["requests"] == 1
     assert output.usage.output_tokens == model_usage["output_tokens"] == 19
     assert output.usage.input_tokens == model_usage["input_tokens"] == 10
+
 
 @pytest.mark.vcr(
     filter_headers=["authorization"],
@@ -181,7 +185,7 @@ async def test_async_anthropic_stream(
     output = _get_call_output(call)
     assert output.id == message.id
     assert output.model == message.model
-    assert output.stop_reason== "end_turn"
+    assert output.stop_reason == "end_turn"
     assert output.stop_sequence == None
     assert output.content[0].text == exp
     summary = call.summary
