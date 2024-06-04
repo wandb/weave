065d917962a2755eb1905c95675a674de903980d
gtarpenning <griffin.tarpenning@wandb.com>
2024-06-04 13:25:12 -0400

    editable inline

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallOverview.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallOverview.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import EditableField from '@wandb/weave/common/components/EditableField';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 import {CallId} from '../common/CallId';
 import {opNiceName} from '../common/Links';
 import {StatusChip} from '../common/StatusChip';
+import {useWFHooks} from '../wfReactInterface/context';
 import {CallSchema} from '../wfReactInterface/wfDataModelHooksInterface';
 import {ExceptionAlert} from './Exceptions';
 import {OverflowMenu} from './OverflowMenu';
@@ -39,10 +41,31 @@ export const CallOverview: React.FC<{
 
   const statusCode = call.rawSpan.status_code;
 
+  const [curOpName, setCurOpName] = useState(opName);
+  useEffect(() => {
+    setCurOpName(opName);
+  }, [opName]);
+  const {useCallRenameFunc} = useWFHooks();
+  const callRename = useCallRenameFunc();
+
   return (
     <>
       <Overview>
-        <CallName>{opName}</CallName>
+        <CallName>
+          <EditableField
+            value={curOpName}
+            save={newName => {
+              callRename(
+                `${call.entity}/${call.project}`,
+                call.callId,
+                newName
+              );
+              setCurOpName(newName);
+            }}
+            placeholder=""
+            updateValue={true}
+          />
+        </CallName>
         <CallId callId={call.callId} />
         <StatusChip value={statusCode} iconOnly />
         <OverflowBin>
