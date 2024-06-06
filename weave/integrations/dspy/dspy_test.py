42591c9a4dc51ed8ca5a64e202f2dfbc41761a27
Tim Sweeney <tim@wandb.com>
2024-06-06 15:04:35 -0700

    more fixes

--- weave/integrations/dspy/dspy_test.py
+++ weave/integrations/dspy/dspy_test.py
@@ -66,7 +66,7 @@ def test_dspy_language_models(client: WeaveClient, fake_api_key: None) -> None:
 
     gpt3_turbo = dspy.OpenAI(model="gpt-3.5-turbo-1106", max_tokens=300)
     dspy.configure(lm=gpt3_turbo)
-    gpt3_turbo("Hello! this is a raw prompt to GPT-3.5")
+    gpt3_turbo("hello! this is a raw prompt to GPT-3.5")
     assert_calls(
         client,
         expected_calls=[
