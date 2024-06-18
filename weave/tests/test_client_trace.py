26c92877a58382c928ad3a7dc554f0bd727c20b8
Tim Sweeney <tim@wandb.com>
2024-06-18 15:37:12 -0700

    added name

--- weave/tests/test_client_trace.py
+++ weave/tests/test_client_trace.py
@@ -2044,7 +2044,7 @@ def test_call_query_stream_equality(client):
     assert i == len(calls.calls)
 
 
-def test_sort_through_refs(client):
+def test_sort_and_filter_through_refs(client):
     @weave.op()
     def test_op(label, val):
         return val
