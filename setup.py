a95d9cbb60bbc428414de2265a6876d71de27268
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-06-19 12:02:37 +0000

    update: replace integration vendors dependencies with extras in setup.py

--- setup.py
+++ setup.py
@@ -108,4 +108,10 @@ class Sdist(sdist):  # type: ignore
 
 setup(
     cmdclass={"build": Build, "editable_wheel": EditableWheel, "sdist": Sdist},
+    extras_require={
+        "mistral": ["mistralai>=0.1.8"],
+        "litellm": ["litellm>=1.36.1"],
+        "llama-index": ["llama-index>=0.10.35"],
+        "anthropic": ["anthropic>=0.28.0"],
+    },
 )
