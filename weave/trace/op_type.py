8c5fae977f15007e14f76819c4e1cc444379403c
Andrew Truong <itsandrewtruong@gmail.com>
2024-06-25 00:17:08 -0400

    test

--- weave/trace/op_type.py
+++ weave/trace/op_type.py
@@ -20,7 +20,8 @@ from .. import environment, errors, storage
 from . import serializer
 from .op import Op
 
-weave_op_pattern = re.compile("@weave\.op(\(\))?")
+weave_op_pattern = re.compile(r"@weave\.op(\(\))?")
+weave_op_no_paren_pattern = re.compile(r"@weave\.op(?!\()")
 
 
 def type_code(type_: Any) -> str:
@@ -388,8 +389,13 @@ def save_instance(
             pass
 
     op_function_code = textwrap.dedent(inspect.getsource(obj.resolve_fn))
+
     if not weave_op_pattern.search(op_function_code):
-        op_function_code = "@weave.op\n" + op_function_code
+        op_function_code = "@weave.op()\n" + op_function_code
+    else:
+        op_function_code = weave_op_no_paren_pattern.sub(
+            "@weave.op()", op_function_code
+        )
     code.append(op_function_code)
 
     with artifact.new_file(f"{name}.py") as f:
