381f23894d5a9cb5e46fc8b81682b112b4e8d163
Tim Sweeney <tim@wandb.com>
2024-07-02 13:55:10 -0700

    cleanedup todos

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CompareEvaluationsPage/CompareEvaluationsPage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CompareEvaluationsPage/CompareEvaluationsPage.tsx
@@ -1,3 +1,41 @@
+/**
+ * This is the entrypoint for the Evaluation Comparison Page.
+ *
+ * TODO:
+ * - [ ] Reflect local state in URL (dimension, range, & selection)
+ * - [ ] Code Cleanup: There is a lot of dead / messy code
+ * - [ ] Refs in Model Property table should be expanded. Example:  https://app.wandb.test/wandb-designers/signal-maven/weave/compare-evaluations?evaluationCallIds=%5B%22bf5188ba-48cd-4c6d-91ea-e25464570c13%22%2C%222f4544f3-9649-487e-b083-df6985e21b12%22%2C%228cbeccd6-6ff7-4eac-a305-6fb6450530f1%22%5D
+ * - [ ] Refs in Call Table should be links
+ * - [ ] Arrays are not supported well in the model property table or the call table. Example: https://app.wandb.test/wandb-smle/weave-rag-lc-demo/weave/compare-evaluations?evaluationCallIds=%5B%2221e8ea02-3109-434c-95d0-cb2c7c542f74%22%5D&peekPath=%2Fwandb-smle%2Fweave-rag-lc-demo%2Fcalls%2F21e8ea02-3109-434c-95d0-cb2c7c542f74
+ * - [ ] "Comparison Table" should use Baseline model input for input fields, NOT the dataset input
+ * - [ ] Comparison table needs more vertical space allocated to focus on text (look at langsmith)
+ * Bugs:
+ * - [ ] Empty selection filter causes crash
+ * - [ ] Selection Filter seems to reset periodically? on data refresh
+ * - [ ] Plots do not respect the evaluation ordering
+ * - [ ] `isProbablyXCall` is incorrect - at least the predict one (eg. for infer_method_names in ("predict", "infer", "forward"):)
+ * Performance:
+ * - [ ] Audit the queries to determine areas of improvement and parallelization
+ * Data Model:
+ * - [ ] Audit the data model to correctly reflect ScoreDimensions (this is too loose right now)
+ * - [ ] find all cases of the `_raw*` cases and remove them, this is an example of insufficient data model
+ * - [ ] Latency, Tokens, and Cost are should "feel" like first class ScoringDimensions
+ * - [ ] Verify that nested scorers are supported
+ * - [ ] Change "Target Metric" to an ordered list of metrics. Default to first metric + latency
+ * Styling / UX:
+ * - [ ] Add a better loading state
+ * - [ ] Header controls don't scale well to smaller sized
+ * - [ ] Use Shawn's color Pallet
+ * - [ ] Should pull in EvaluationCall display name if it exists
+ * - [ ] Hover tooltip on charts to show values
+ * - Scatterplot Filter UX
+ *    - [ ] Move the dimension selector closer to the plot of interest
+ *    - [ ] Scatterplot filter should have 1-1 aspect ratio
+ *    - [ ] Scatterplot Filter dimensions are not quite obvious
+ *    - [ ] Scatterplot filter would benefit from a title / help text
+ *    - [ ] Scatterplot Filter should not zoom, just show selection range
+ */
+
 import {Box, FormControl} from '@material-ui/core';
 import {Circle} from '@mui/icons-material';
 import {Autocomplete} from '@mui/material';
@@ -1047,49 +1085,3 @@ const RowComparison: React.FC<{state: EvaluationComparisonState}> = props => {
     />
   );
 };
-
-/**
- * TOOD:
- * - [ ] Allow user to select primary metric & save to local storage + URL
- * - [ ] Wireup the baseline replace button
- * - [ ] Fix Plot to show correct data
- * - [ ] Build grouping
- * - [ ] Add scorer links in scorecard
- * - [ ] Definition header does not scale small enough
- * - [ ] The data model has gotten messy - figure out a good way to include costs
- * - [ ] Auto-expand first-level properties (see prompt here: https://app.wandb.test/wandb-designers/signal-maven/weave/compare-evaluations?evaluationCallIds=%5B%22bf5188ba-48cd-4c6d-91ea-e25464570c13%22%2C%222f4544f3-9649-487e-b083-df6985e21b12%22%2C%228cbeccd6-6ff7-4eac-a305-6fb6450530f1%22%5D)
- * - [ ] The damn thing is slow with all the upfront loading
- * - [ ] // TODO: find all cases of the `_raw*` cases and remove them!'
- * - [ ] Support arbitrarily nested scorers
- * - [ ] Add latency and tokens (and cost) to the scoring / plotting system more generally
- * - [ ] Failed evals break!
- * - [ ] Objects/Arrays/Refs are not suppported. see https://app.wandb.test/wandb-smle/weave-rag-lc-demo/weave/compare-evaluations?evaluationCallIds=%5B%2221e8ea02-3109-434c-95d0-cb2c7c542f74%22%5D&peekPath=%2Fwandb-smle%2Fweave-rag-lc-demo%2Fcalls%2F21e8ea02-3109-434c-95d0-cb2c7c542f74
- * - [ ] Big: empty selection
- * - [ ] Selection Filter seems to reset periodically? on data refresh
- * - [ ] Plots do not respect the ordering
- * TEST:
- * - [ ] Single Case
- * - [ ] Dual Case
- * - [ ] Multi Case
- * 
- * * Move Target Metric Below
-* Hover tooltip on charts to show values
-* Axis labels are a bit confusing
-* Filter chart does not have a name
-
-* Plotly select only, no zoom
-* Stretching of the plot not ideal
-
-* Let the user select a list of metrics
-* Allow the user to provide an ordering of metrics?
-
-* Need to add model latency
-
-* use baseline model input as input, not full input
----
-
----
-              // for infer_method_names in ("predict", "infer", "forward"):
-              // TODO: make this more robust
-Vertical space on the table - More of a string viewer and avoid secondary drill down
- */
