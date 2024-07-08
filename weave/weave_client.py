e93e435a20d58e253b4c8b2f27eacdcfabdeb69d
Andrew Truong <itsandrewtruong@gmail.com>
2024-07-08 16:36:49 -0400

    fix(weave): Make refs immutable (#1890)

--- weave/weave_client.py
+++ weave/weave_client.py
@@ -335,7 +335,6 @@ class WeaveClient:
                 raise ValueError(f"Unable to find object for ref uri: {ref.uri()}")
             raise
 
-        # Probably bad form to mutate the ref here
         # At this point, `ref.digest` is one of three things:
         # 1. "latest" - the user asked for the latest version of the object
         # 2. "v###" - the user asked for a specific version of the object
@@ -343,7 +342,7 @@ class WeaveClient:
         #
         # However, we always want to resolve the ref to the digest. So
         # here, we just directly assign the digest.
-        ref.digest = read_res.obj.digest
+        ref = dataclasses.replace(ref, digest=read_res.obj.digest)
 
         data = read_res.obj.val
 
