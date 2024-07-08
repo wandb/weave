9b58f45bc075175aad356614c5a4b108edaaa94f
Andrew Truong <itsandrewtruong@gmail.com>
2024-07-08 09:46:38 -0400

    test

--- weave/trace/op.py
+++ weave/trace/op.py
@@ -20,7 +20,7 @@ from typing import (
 
 from weave import call_context, client_context
 from weave.legacy import context_state
-from weave.trace.box import box
+from weave.trace import box
 from weave.trace.context import call_attributes
 from weave.trace.errors import OpCallError
 from weave.trace.refs import ObjectRef
