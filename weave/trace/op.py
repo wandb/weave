cff3daf97f034fe98fde4901847dbea77a7c9dd5
gtarpenning <griffin.tarpenning@wandb.com>
2024-07-08 16:15:40 -0400

    chore(weave): create a weave_options dict for setting display name on call

--- weave/trace/op.py
+++ weave/trace/op.py
@@ -74,10 +74,11 @@ class Op:
         if maybe_client is None:
             return self.resolve_fn(*args, **kwargs)
 
-        call = self._create_call(*args, **kwargs)
+        weave_options = kwargs.pop("weave_options", {})
+        call = self._create_call(weave_options, *args, **kwargs)
         return self._execute_call(call, *args, **kwargs)
 
-    def _create_call(self, *args: Any, **kwargs: Any) -> Any:
+    def _create_call(self, weave_options, *args: Any, **kwargs: Any) -> Any:
         client = client_context.weave_client.require_weave_client()
 
         try:
@@ -101,6 +102,7 @@ class Op:
             inputs_with_defaults,
             parent_call,
             attributes=attributes,
+            display_name=weave_options.get("display_name", None),
         )
 
     def _execute_call(self, call: Any, *args: Any, **kwargs: Any) -> Any:
