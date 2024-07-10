3db957d67ff4a4c1be997de470c6f81135b320c4
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-07-10 17:03:51 +0000

    update: tests

--- weave/integrations/groq/groq_sdk.py
+++ weave/integrations/groq/groq_sdk.py
@@ -1,36 +1,15 @@
 import importlib
-from typing import TYPE_CHECKING, Callable, Dict, Optional, Union
+from typing import Callable
 
 import weave
-from weave.trace.op_extensions.accumulator import add_accumulator
 from weave.trace.patcher import MultiPatcher, SymbolPatcher
-
-if TYPE_CHECKING:
-    from groq.resources.chat.completions import AsyncCompletions, Completions
-
-
-def groq_accumulator(
-    acc: Optional[Union["Completions", "AsyncCompletions"]],
-    value: Union["Completions", "AsyncCompletions"],
-) -> "Completions":
-    if acc is None:
-        acc = value
-    return value
-
-
-def should_use_accumulator(inputs: Dict) -> bool:
-    return isinstance(inputs, dict) and bool(inputs.get("stream"))
 
 
 def groq_wrapper(name: str) -> Callable[[Callable], Callable]:
     def wrapper(fn: Callable) -> Callable:
         op = weave.op()(fn)
         op.name = name  # type: ignore
-        return add_accumulator(
-            op,  # type: ignore
-            groq_accumulator,
-            should_accumulate=should_use_accumulator,
-        )
+        return op
 
     return wrapper
 
--- weave/integrations/groq/groq_test.py
+++ weave/integrations/groq/groq_test.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 import os
 from typing import Any, Optional
 
@@ -8,8 +7,6 @@ from groq import AsyncGroq, Groq
 
 import weave
 from weave.trace_server import trace_server_interface as tsi
-
-from .groq_sdk import groq_patcher
 
 
 def _get_call_output(call: tsi.CallSchema) -> Any:
@@ -98,196 +95,12 @@ def test_groq_quickstart(
     filter_headers=["authorization", "x-api-key"],
     allowed_hosts=["api.wandb.ai", "localhost", "trace.wandb.ai"],
 )
-def test_groq_function_calling_example(client: weave.weave_client.WeaveClient) -> None:
-    groq_client = Groq(api_key=os.getenv("GROQ_API_KEY", "DUMMY_API_KEY"))
-    MODEL = "llama3-70b-8192"
-    SEED = 42
-
-    @weave.op()
-    def get_game_score(team_name):
-        """Get the current score for a given NBA game"""
-        if "warriors" in team_name.lower():
-            return json.dumps(
-                {
-                    "game_id": "401585601",
-                    "status": "Final",
-                    "home_team": "Los Angeles Lakers",
-                    "home_team_score": 121,
-                    "away_team": "Golden State Warriors",
-                    "away_team_score": 128,
-                }
-            )
-        elif "lakers" in team_name.lower():
-            return json.dumps(
-                {
-                    "game_id": "401585601",
-                    "status": "Final",
-                    "home_team": "Los Angeles Lakers",
-                    "home_team_score": 121,
-                    "away_team": "Golden State Warriors",
-                    "away_team_score": 128,
-                }
-            )
-        elif "nuggets" in team_name.lower():
-            return json.dumps(
-                {
-                    "game_id": "401585577",
-                    "status": "Final",
-                    "home_team": "Miami Heat",
-                    "home_team_score": 88,
-                    "away_team": "Denver Nuggets",
-                    "away_team_score": 100,
-                }
-            )
-        elif "heat" in team_name.lower():
-            return json.dumps(
-                {
-                    "game_id": "401585577",
-                    "status": "Final",
-                    "home_team": "Miami Heat",
-                    "home_team_score": 88,
-                    "away_team": "Denver Nuggets",
-                    "away_team_score": 100,
-                }
-            )
-        else:
-            return json.dumps({"team_name": team_name, "score": "unknown"})
-
-    @weave.op()
-    def run_conversation(user_prompt):
-        # Step 1: send the conversation and available functions to the model
-        messages = [
-            {
-                "role": "system",
-                "content": "You are a function calling LLM that uses the data extracted from the get_game_score function to answer questions around NBA game scores. Include the team and their opponent in your response.",
-            },
-            {
-                "role": "user",
-                "content": user_prompt,
-            },
-        ]
-        tools = [
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_game_score",
-                    "description": "Get the score for a given NBA game",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "team_name": {
-                                "type": "string",
-                                "description": "The name of the NBA team (e.g. 'Golden State Warriors')",
-                            }
-                        },
-                        "required": ["team_name"],
-                    },
-                },
-            }
-        ]
-        response = groq_client.chat.completions.create(
-            model=MODEL,
-            messages=messages,
-            tools=tools,
-            tool_choice="auto",
-            max_tokens=4096,
-            seed=SEED,
-        )
-
-        response_message = response.choices[0].message
-        tool_calls = response_message.tool_calls
-        # Step 2: check if the model wanted to call a function
-        if tool_calls:
-            # Step 3: call the function
-            # Note: the JSON response may not always be valid; be sure to handle errors
-            available_functions = {
-                "get_game_score": get_game_score,
-            }  # only one function in this example, but you can have multiple
-            messages.append(
-                response_message
-            )  # extend conversation with assistant's reply
-            # Step 4: send the info for each function call and function response to the model
-            for tool_call in tool_calls:
-                function_name = tool_call.function.name
-                function_to_call = available_functions[function_name]
-                function_args = json.loads(tool_call.function.arguments)
-                function_response = function_to_call(
-                    team_name=function_args.get("team_name")
-                )
-                messages.append(
-                    {
-                        "tool_call_id": tool_call.id,
-                        "role": "tool",
-                        "name": function_name,
-                        "content": function_response,
-                    }
-                )  # extend conversation with function response
-            second_response = groq_client.chat.completions.create(
-                model=MODEL, messages=messages, seed=SEED
-            )  # get a new response from the model where it can see the function response
-            return second_response.choices[0].message.content
-
-    user_prompt = "What was the score of the Warriors game?"
-    result = run_conversation(user_prompt)
-    assert "128-121" in result
-    weave_server_respose = client.server.calls_query(
-        tsi.CallsQueryReq(project_id=client._project_id())
-    )
-    assert len(weave_server_respose.calls) == 4
-
-    flatened_calls_list = [
-        (op_name_from_ref(c.op_name), d)
-        for (c, d) in flatten_calls(weave_server_respose.calls)
-    ]
-    assert flatened_calls_list == [
-        ("run_conversation", 0),
-        ("groq.resources.chat.completions.Completions.create", 1),
-        ("get_game_score", 1),
-        ("groq.resources.chat.completions.Completions.create", 1),
-    ]
-
-    call_1 = weave_server_respose.calls[1]
-    assert call_1.exception is None and call_1.ended_at is not None
-    output_1 = _get_call_output(call_1)
-    assert output_1.model == MODEL
-    assert output_1.usage.completion_tokens == 47
-    assert output_1.usage.prompt_tokens == 973
-    assert output_1.usage.total_tokens == 1020
-    assert (
-        output_1.choices[0].message.tool_calls[0].function.arguments
-        == '{"team_name":"Golden State Warriors"}'
-    )
-
-    call_2 = weave_server_respose.calls[2]
-    assert call_2.exception is None and call_2.ended_at is not None
-    output_2 = _get_call_output(call_2)
-    assert (
-        output_2
-        == '{"game_id": "401585601", "status": "Final", "home_team": "Los Angeles Lakers", "home_team_score": 121, "away_team": "Golden State Warriors", "away_team_score": 128}'
-    )
-
-    call_3 = weave_server_respose.calls[3]
-    assert call_3.exception is None and call_3.ended_at is not None
-    output_3 = _get_call_output(call_3)
-    assert output_3.model == MODEL
-    assert output_3.usage.completion_tokens == 20
-    assert (
-        output_3.choices[0].message.content
-        == "The Golden State Warriors played against the Los Angeles Lakers and won the game 128-121."
-    )
-
-
-@pytest.mark.skip_clickhouse_client  # TODO:VCR recording does not seem to allow us to make requests to the clickhouse db in non-recording mode
-@pytest.mark.vcr(
-    filter_headers=["authorization", "x-api-key"],
-    allowed_hosts=["api.wandb.ai", "localhost", "trace.wandb.ai"],
-)
 def test_groq_async_chat_completion(
     client: weave.weave_client.WeaveClient,
 ) -> None:
     groq_client = AsyncGroq(api_key=os.environ.get("GROQ_API_KEY", "DUMMY_API_KEY"))
 
-    async def complete_chat():
+    async def complete_chat() -> None:
         chat_completion = await groq_client.chat.completions.create(
             messages=[
                 {
