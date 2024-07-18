55823b55318e6f3e60275230bd1d7864859e2d08
gtarpenning <griffin.tarpenning@wandb.com>
2024-07-18 12:48:59 -0700

    chore: inaccesable project IDs set to special private str

--- weave/trace_server/refs_internal.py
+++ weave/trace_server/refs_internal.py
@@ -10,6 +10,7 @@ from typing import Union
 
 WEAVE_INTERNAL_SCHEME = "weave-trace-internal"
 WEAVE_SCHEME = "weave"
+PRIVATE_WEAVE_STR = "<private>"
 
 DICT_KEY_EDGE_NAME = "key"
 LIST_INDEX_EDGE_NAME = "index"
