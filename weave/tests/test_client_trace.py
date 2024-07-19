b5a52a235e66b28dfd4d2cf31ee05112a5ef7fa5
Andrew Truong <itsandrewtruong@gmail.com>
2024-07-19 19:18:57 -0400

    test

--- weave/tests/test_client_trace.py
+++ weave/tests/test_client_trace.py
@@ -1664,6 +1664,7 @@ def map_with_copying_thread_executor(fn, vals):
 
 
 # TODO: Make an async version of this
+@pytest.mark.flaky(retries=3)  # <-- Flakes in CI
 @pytest.mark.parametrize(
     "mapper",
     [
