38e21e45a182449ea884d361f474adb409de2c5e
Tim Sweeney <tim@wandb.com>
2024-07-24 10:36:35 -0700

    chore: Fixes bug in Eval Compare page when the model op is not called `predict` (#2010)
    * init
    
    * lint

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/tsDataModelHooksEvaluationComparison.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/tsDataModelHooksEvaluationComparison.ts
@@ -461,9 +461,11 @@ const fetchEvaluationComparisonData = async (
             const rowDigest = maybeDigest;
             const possiblePredictNames = ['predict', 'infer', 'forward'];
             const isProbablyPredictCall =
-              _.some(possiblePredictNames, name =>
+              (_.some(possiblePredictNames, name =>
                 traceCall.op_name.includes(`.${name}:`)
-              ) && modelRefs.includes(traceCall.inputs.self);
+              ) &&
+                modelRefs.includes(traceCall.inputs.self)) ||
+              modelRefs.includes(traceCall.op_name);
 
             const isProbablyScoreCall = scorerRefs.has(traceCall.op_name);
             // WOW - super hacky. we have to do this b/c we support both instances and ops for scorers!
