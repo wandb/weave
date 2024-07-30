c6e2a138376a460cdfaa134767302878b33cf6a0
tonyyli-wandb <tony.li@wandb.com>
2024-07-30 10:13:54 -0700

    chore(weave): drop unused Tag type for artifacts

--- weave/legacy/ops_domain/wb_domain_types.py
+++ weave/legacy/ops_domain/wb_domain_types.py
@@ -190,19 +190,6 @@ ArtifactAliasType = ArtifactAlias.WeaveType()  # type: ignore
 ArtifactAliasType = typing.cast(PartialObjectTypeGeneratorType, ArtifactAliasType)
 
 
-@gql_type("tag")
-class Tag(GQLBase):
-    REQUIRED_FRAGMENT = """
-        id
-        name
-        tagCategoryName
-        attributes
-    """
-
-
-TagType = Tag.WeaveType()  # type: ignore
-TagType = typing.cast(PartialObjectTypeGeneratorType, TagType)
-
 @gql_type("report")
 class Report(GQLBase):
     REQUIRED_FRAGMENT = """
