e0a11507dd16f54fa7231383e3e277dc1be980e8
Andrew Truong <itsandrewtruong@gmail.com>
2024-07-30 23:43:19 -0400

    test

--- weave/trace/settings.py
+++ weave/trace/settings.py
@@ -1,11 +1,17 @@
+"""Settings for Weave.
+
+To add new settings:
+1. Add a new field to `UserSettings`
+2. Add a new `should_{xyz}` function
+"""
+
 import os
 from contextvars import ContextVar
 from typing import Any, Optional, Union
 
 from pydantic import BaseModel, ConfigDict, PrivateAttr
 
-disabled = ContextVar("disabled", default=False)
-print_call_link = ContextVar("disabled", default=True)
+SETTINGS_PREFIX = "WEAVE_"
 
 
 class UserSettings(BaseModel):
@@ -30,8 +36,8 @@ class UserSettings(BaseModel):
     _is_first_apply: bool = PrivateAttr(True)
 
     def _reset(self) -> None:
-        self.disabled = False
-        self.print_call_link = True
+        for name, field in self.model_fields.items():
+            setattr(self, name, field.default)
 
     def apply(self) -> None:
         if self._is_first_apply:
@@ -39,24 +45,17 @@ class UserSettings(BaseModel):
         else:
             self._reset()
 
-        disabled.set(self.disabled)
-        print_call_link.set(self.print_call_link)
-
-
-def str2bool_truthy(v: str) -> bool:
-    return v.lower() in ("yes", "true", "1", "on")
+        for name in self.model_fields:
+            context_var = _context_vars.get(name)
+            context_var.set(getattr(self, name))
 
 
 def should_disable_weave() -> bool:
-    if env := os.getenv("WEAVE_DISABLED"):
-        return str2bool_truthy(env)
-    return disabled.get()
+    return _should("disabled")
 
 
 def should_print_call_link() -> bool:
-    if env := os.getenv("WEAVE_PRINT_CALL_LINK"):
-        return str2bool_truthy(env)
-    return print_call_link.get()
+    return _should("print_call_link")
 
 
 def parse_and_apply_settings(
@@ -70,3 +69,19 @@ def parse_and_apply_settings(
         user_settings = settings
 
     user_settings.apply()
+
+
+_context_vars = {
+    name: ContextVar(name, default=field.default)
+    for name, field in UserSettings.model_fields.items()
+}
+
+
+def _str2bool_truthy(v: str) -> bool:
+    return v.lower() in ("yes", "true", "1", "on")
+
+
+def _should(name: str) -> bool:
+    if env := os.getenv(f"{SETTINGS_PREFIX}{name.upper()}"):
+        return _str2bool_truthy(env)
+    return _context_vars[name].get()
