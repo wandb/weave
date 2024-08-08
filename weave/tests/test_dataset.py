c8a2029ce20311194c8f194049f3458b1d70b3af
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-07 22:38:22 -0400

    test

--- weave/tests/test_dataset.py
+++ weave/tests/test_dataset.py
@@ -1,24 +1,9 @@
+import pytest
+
 import weave
 
-# TODO: This is not ideal because the user must reach into Dataset and
-# operate on `rows`, which is an implementation detail.
 
-# Ideally, the user can do `Dataset.append` and it will dispatch to
-# rows transparently, but this is incompatible with ref-get-Datasets because:
-# 1. If `Dataset.append` is not an Op, then it won't be available on the
-#    ref-get-Dataset; and
-# 2. If `Dataset.append` is an Op, each append will be traced (noisy), and
-#    it also breaks our rule that Ops should be pure.
-
-
-# I think the ideal state is for `append` to track, but only at the end
-# when the object is published -- if you call `Dataset.append` 3 times,
-# only 1 call is traced which contains the 3 appends.  This pattern is
-# similar to "add" vs. "commit" in git.  This is also the pattern we'll
-# want for server-side mutations, so we should revisit this when we add that.
-
-
-def test_dataset_append(client):
+def test_dataset_append_basic(client):
     ds = weave.Dataset(rows=[{"a": 1, "b": 2}])
     ds.append({"a": 3, "b": 4})
     ref = weave.publish(ds)
@@ -36,6 +21,54 @@ def test_dataset_append(client):
         {"a": 1, "b": 2},
         {"a": 3, "b": 4},
         {"a": 5, "b": 6},
+    ]
+
+
+@pytest.mark.xfail(reason="Table API does not properly handle objects yet")
+def test_dataset_append_objects(client):
+    class Thing(weave.Object):
+        val: int
+
+    ds = weave.Dataset(rows=[{"a": Thing(val=1), "b": Thing(val=2)}])
+    ds.append({"a": Thing(val=3), "b": Thing(val=4)})
+    ref = weave.publish(ds)
+
+    ds2 = ref.get()
+    assert ds2 == [
+        {"a": Thing(val=1), "b": Thing(val=2)},
+        {"a": Thing(val=3), "b": Thing(val=4)},
+    ]
+
+    ds2.append({"a": Thing(val=5), "b": Thing(val=6)})
+    ref2 = weave.publish(ds2)
+
+    ds3 = ref2.get()
+    assert ds3 == [
+        {"a": Thing(val=1), "b": Thing(val=2)},
+        {"a": Thing(val=3), "b": Thing(val=4)},
+        {"a": Thing(val=5), "b": Thing(val=6)},
+    ]
+
+
+def test_dataset_append_nested(client):
+    ds = weave.Dataset(rows=[{"a": {"b": 1}, "c": 2, "d": None}])
+    ds.append({"a": {"b": 3}, "c": 4, "d": None})
+    ref = weave.publish(ds)
+
+    ds2 = ref.get()
+    assert ds2 == [
+        {"a": {"b": 1}, "c": 2, "d": None},
+        {"a": {"b": 3}, "c": 4, "d": None},
+    ]
+
+    ds2.append({"a": {"b": 5}, "c": 6, "d": None})
+    ref2 = weave.publish(ds2)
+
+    ds3 = ref2.get()
+    assert ds3 == [
+        {"a": {"b": 1}, "c": 2, "d": None},
+        {"a": {"b": 3}, "c": 4, "d": None},
+        {"a": {"b": 5}, "c": 6, "d": None},
     ]
 
 
