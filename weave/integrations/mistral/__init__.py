130e83f472507c38ccf17dd5a2e92430b3fcd53e
Thomas Capelle <tcapelle@pm.me>
2024-08-08 18:31:19 +0200

    always return a patcher

--- weave/integrations/mistral/__init__.py
+++ weave/integrations/mistral/__init__.py
@@ -4,10 +4,10 @@ from packaging import version
 try:
     mistral_version = metadata.version("mistralai")
 except metadata.PackageNotFoundError:
-    raise ImportError("MistralAI is not installed. You can install it by doing `pip install mistralai`.")
+    mistral_version = "1.0" # we need to return a patching function
+
+if version.parse(mistral_version) < version.parse("1.0.0"):
+    from .v0.mistral import mistral_patcher
+    print(f"Using MistralAI version {mistral_version}. Please consider upgrading to version 1.0.0 or later.")
 else:
-    if version.parse(mistral_version) < version.parse("1.0.0"):
-        from .v0.mistral import mistral_patcher
-        print(f"Using MistralAI version {mistral_version}. Please consider upgrading to version 1.0.0 or later.")
-    else:
-        from .v1.mistral import mistral_patcher
+    from .v1.mistral import mistral_patcher
