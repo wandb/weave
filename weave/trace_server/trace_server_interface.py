9fd0f24063d64cb9078f0e575577531e32078d12
gtarpenning <griffin.tarpenning@wandb.com>
2024-08-14 11:28:22 -0700

    review comment

--- weave/trace_server/trace_server_interface.py
+++ weave/trace_server/trace_server_interface.py
@@ -257,7 +257,7 @@ class CallsQueryReq(BaseModel):
     # SortBy and thus GetFieldOperator.get_field_ (without direction)
     columns: typing.Optional[typing.List[str]] = None
     # columns to expand, i.e. refs to other objects, can be nested
-    expanded_columns: typing.Optional[typing.List[str]] = Field(
+    expand_columns: typing.Optional[typing.List[str]] = Field(
         default=None,
         examples=[["inputs.self.message", "inputs.model.prompt"]],
         description="Columns to expand, i.e. refs to other objects",
