4e7a6fb8eba3543f91f4913852939a6af3e36b74
Tim Sweeney <tim@wandb.com>
2024-08-16 14:30:46 -0700

    fixed

--- weave-js/src/components/PagePanelComponents/Home/Browse3/NotFoundPanel.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/NotFoundPanel.tsx
@@ -1,15 +1,16 @@
 import {ErrorPanel} from '@wandb/weave/components/ErrorPanel';
-import React, {FC} from 'react';
+import React, {FC, useContext} from 'react';
 
 import {Button} from '../../../Button';
-import {useClosePeek} from './context';
+import {useClosePeek, WeaveflowPeekContext} from './context';
 
 export const NotFoundPanel: FC<{title: string}> = ({title}) => {
   const close = useClosePeek();
+  const {isPeeking} = useContext(WeaveflowPeekContext);
   return (
     <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
       <div style={{alignSelf: 'flex-end', margin: 10}}>
-        <Button icon="close" variant="ghost" onClick={close} />
+        {isPeeking && <Button icon="close" variant="ghost" onClick={close} />}
       </div>
       <div style={{flex: 1}}>
         <ErrorPanel title={title} subtitle="" subtitle2="" />
