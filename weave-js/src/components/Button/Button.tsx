668f17d912413f194450bdc1c36411d5521d131e
onx2 <jeffrey.rooks@wandb.com>
2024-08-20 11:14:03 -0400

    type constraints, class maps, and runtime validation of props

--- weave-js/src/components/Button/Button.tsx
+++ weave-js/src/components/Button/Button.tsx
@@ -4,30 +4,38 @@
  * https://www.figma.com/file/01KWBdMZg5QM9SRS1pQq0z/Design-System----Robot-Styles?type=design&node-id=5956-31813&mode=design&t=Gm4WWGWwgjdfUUTe-0
  */
 
-import {TooltipContentProps} from '@radix-ui/react-tooltip';
+import {type TooltipContentProps} from '@radix-ui/react-tooltip';
 import classNames from 'classnames';
-import React, {ReactElement, useState} from 'react';
+import React, {
+  CSSProperties,
+  forwardRef,
+  type ReactElement,
+  useState,
+} from 'react';
 import {twMerge} from 'tailwind-merge';
 
-import {Icon, IconName} from '../Icon';
+import {Icon, type IconName} from '../Icon';
 import * as Tooltip from '../RadixTooltip';
 import {Tailwind} from '../Tailwind';
-import {ButtonSize, ButtonVariant} from './types';
+import type {ButtonSize, ButtonVariant} from './types';
+
+type IconButtonProps = {
+  icon?: IconName;
+  children?: never;
+  startIcon?: never;
+  endIcon?: never;
+  tooltip: string;
+};
+
+type LabelButtonProps = {
+  icon?: never;
+  children: ReactElement | string;
+  startIcon?: IconName;
+  endIcon?: IconName;
+  tooltip?: string;
+};
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  /**
-   * Shorthand alias for `startIcon` since most button icons are startIcons.
-   * If both props are provided, `startIcon` value will be used out of the two.
-   */
-  icon?: IconName;
-  /**
-   * Icon displayed before the children
-   */
-  startIcon?: IconName;
-  /**
-   * Icon displayed after the children
-   */
-  endIcon?: IconName;
   size?: ButtonSize;
   variant?: ButtonVariant;
   children?: ReactElement | string;
@@ -35,114 +43,104 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   tooltip?: string;
   tooltipProps?: TooltipContentProps;
   twWrapperStyles?: React.CSSProperties;
+} & (IconButtonProps | LabelButtonProps);
+
+const sizeMap = {
+  small:
+    'gap-6 px-6 py-3 text-sm leading-[18px] [&_svg]:h-16 [&_svg]:w-16 h-24 w-24 p-0',
+  medium: 'gap-10 px-10 py-4 text-base [&_svg]:h-18 [&_svg]:w-18 h-32 w-32 p-0',
+  large: 'gap-8 px-12 py-8 text-base h-40 w-40 p-0',
 };
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const variantMap = {
+  primary: 'bg-teal-500 text-white hover:bg-teal-450',
+  secondary:
+    'bg-oblivion/[0.05] dark:bg-moonbeam/[0.05] text-moon-800 dark:text-moon-200 hover:bg-teal-300/[0.48] hover:text-teal-600 dark:hover:bg-teal-700/[0.48] dark:hover:text-teal-400',
+  ghost:
+    'bg-oblivion/[0.05] dark:bg-moonbeam/[0.05] text-moon-800 dark:text-moon-200 hover:bg-teal-300/[0.48] hover:text-teal-600 dark:hover:bg-teal-700/[0.48] dark:hover:text-teal-400',
+  quiet:
+    'text-moon-500 hover:text-moon-800 dark:hover:text-moon-200 hover:bg-oblivion/[0.05] dark:hover:bg-moonbeam/[0.05]',
+  destructive: 'bg-red-500 text-white hover:bg-red-450',
+};
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
       size = 'medium',
       variant = 'primary',
       icon,
-      startIcon = icon,
+      startIcon,
       endIcon,
       active,
       children,
       className = '',
       tooltip,
       tooltipProps,
-      twWrapperStyles = {},
+      twWrapperStyles,
       ...htmlAttributes
     },
     ref
   ) => {
-    const hasIcon = startIcon || endIcon;
-    if (!children && !hasIcon) {
-      console.error('Button: requires either children or an icon.');
-    }
-
-    const isSmall = size === 'small';
-    const isMedium = size === 'medium';
-    const isLarge = size === 'large';
-    const isPrimary = variant === 'primary';
-    const isSecondary = variant === 'secondary';
-    const isGhost = variant === 'ghost';
-    const isQuiet = variant === 'quiet';
-    const isDestructive = variant === 'destructive';
-
-    const hasBothIcons = startIcon && endIcon;
-    const hasOnlyOneIcon = hasIcon && !hasBothIcons;
-    const isIconOnly = hasOnlyOneIcon && !children;
-
     /**
      * The Tailwind wrapper is a div, which is a block-element. However, a normal button
      * wouldn't be wrapped and would be inline-block by default. These styles are a
      * necessary workaround to ensure Button is styled correctly despite the wrapper.
      */
-    const wrapperStyles = {
+    const wrapperStyles: CSSProperties = {
       display: 'inline-flex',
       width: className.includes('w-full') ? '100%' : undefined,
       ...twWrapperStyles,
     };
 
-    const button = (
+    /**
+     * Runtime validation of props' type constraints
+     */
+    const ButtonInner = () => {
+      if (icon) {
+        if (children || startIcon || endIcon) {
+          console.warn(
+            'Button: `startIcon`, `endIcon`, and `children` are ignored when using `icon`.'
+          );
+        }
+        return <Icon name={icon} role="presentation" />;
+      }
+
+      if (!children) {
+        console.error('Button: `children` is required when not using `icon`.');
+        return null;
+      }
+
+      return (
+        <>
+          {startIcon ? <Icon name={startIcon} role="presentation" /> : null}
+          {children}
+          {endIcon ? <Icon name={endIcon} role="presentation" /> : null}
+        </>
+      );
+    };
+
+    const ButtonInternal = () => (
       <button
         ref={ref}
+        {...htmlAttributes}
         type="button"
+        aria-disabled={htmlAttributes.disabled}
+        aria-label={htmlAttributes['aria-label'] || tooltip}
         className={twMerge(
-          classNames(
-            'night-aware',
-            "inline-flex items-center justify-center whitespace-nowrap rounded border-none font-['Source_Sans_Pro'] font-semibold",
-            'disabled:pointer-events-none disabled:opacity-35',
-            'focus-visible:outline focus-visible:outline-[2px] focus-visible:outline-teal-500',
-            {
-              // small
-              'gap-6 px-6 py-3 text-sm leading-[18px]': isSmall,
-              '[&_svg]:h-16 [&_svg]:w-16': isSmall,
-              'h-24 w-24 p-0': isSmall && isIconOnly,
-
-              // medium
-              'gap-10 px-10 py-4 text-base': isMedium,
-              '[&_svg]:h-18 [&_svg]:w-18': isMedium,
-              'h-32 w-32 p-0': isMedium && isIconOnly,
-
-              // large
-              'gap-8 px-12 py-8 text-base': isLarge,
-              'h-40 w-40 p-0': isLarge && isIconOnly,
-
-              // primary
-              'bg-teal-500 text-white hover:bg-teal-450': isPrimary,
-              'bg-teal-450': isPrimary && active,
-
-              // secondary & ghost
-              'bg-oblivion/[0.05] dark:bg-moonbeam/[0.05]': isSecondary,
-              'text-moon-800 dark:text-moon-200': isSecondary || isGhost,
-              'hover:bg-teal-300/[0.48] hover:text-teal-600 dark:hover:bg-teal-700/[0.48] dark:hover:text-teal-400':
-                isSecondary || isGhost,
-              'bg-teal-300/[0.48] text-teal-600 dark:bg-teal-700/[0.48] dark:text-teal-400':
-                (isSecondary || isGhost) && active,
-
-              // quiet
-              'text-moon-500': isQuiet,
-              'bg-oblivion/[0.05] text-moon-800 dark:bg-moonbeam/[0.05] dark:text-moon-200':
-                isQuiet && active,
-              'hover:text-moon-800 dark:hover:text-moon-200': isQuiet,
-              'hover:bg-oblivion/[0.05] dark:hover:bg-moonbeam/[0.05]': isQuiet,
-
-              // destructive
-              'bg-red-500 text-white hover:bg-red-450': isDestructive,
-              'bg-red-450': isDestructive && active,
-            },
-            className
-          )
-        )}
-        {...htmlAttributes}>
-        {startIcon ? <Icon name={startIcon} /> : null}
-        {children}
-        {endIcon ? <Icon name={endIcon} /> : null}
+          'night-aware',
+          "inline-flex items-center justify-center whitespace-nowrap rounded border-none font-['Source_Sans_Pro'] font-semibold",
+          'disabled:pointer-events-none disabled:opacity-35',
+          'focus-visible:outline focus-visible:outline-[2px] focus-visible:outline-teal-500',
+          sizeMap[size] + (icon && !children ? ' h-24 w-24 p-0' : ''),
+          variantMap[variant] + (active ? ' bg-teal-450' : ''),
+          className
+        )}>
+        <ButtonInner />
       </button>
     );
 
     const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
     if (tooltip) {
       return (
         <Tailwind style={wrapperStyles}>
@@ -150,7 +148,9 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             <Tooltip.Root open={isTooltipOpen} onOpenChange={setIsTooltipOpen}>
               <Tooltip.Trigger asChild>
                 {/* span is needed so tooltip works on disabled buttons */}
-                <span>{button}</span>
+                <span>
+                  <ButtonInternal />
+                </span>
               </Tooltip.Trigger>
               <Tooltip.Portal>
                 <Tooltip.Content {...tooltipProps}>{tooltip}</Tooltip.Content>
@@ -161,6 +161,10 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       );
     }
 
-    return <Tailwind style={wrapperStyles}>{button}</Tailwind>;
+    return (
+      <Tailwind style={wrapperStyles}>
+        <ButtonInternal />
+      </Tailwind>
+    );
   }
 );
