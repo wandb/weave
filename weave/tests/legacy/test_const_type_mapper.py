0ff6b3378914a47e0a6837ce3ec053f3465cf46b
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-21 13:38:35 -0400

    chore(weave): Split out legacy tests (#2179)

---
+++ weave/tests/legacy/test_const_type_mapper.py
@@ -0,0 +1,28 @@
+import typing
+
+import weave
+from weave.legacy import context_state
+
+from ... import weave_internal
+
+_loading_builtins_token = context_state.set_loading_built_ins()
+
+MetricRows = typing.TypeVar("MetricRows")
+
+
+@weave.type()
+class _TestType(typing.Generic[MetricRows]):
+    metrics: MetricRows
+
+
+context_state.clear_loading_built_ins(_loading_builtins_token)
+
+
+def test_const_type_mapper():
+    node = weave_internal.make_const_node(
+        _TestType.WeaveType(weave.types.List(weave.types.Number())), None
+    )
+    fn_node = weave_internal.define_fn(
+        {"row": weave.types.Number()}, lambda row: row + 1
+    )
+    node.metrics.map(fn_node)
