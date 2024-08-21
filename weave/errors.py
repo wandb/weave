9fecdfc676af9ddc07c578dadd6d65e59b03aff1
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-20 20:59:37 -0400

    test

--- weave/errors.py
+++ weave/errors.py
@@ -13,6 +13,13 @@ class WeaveBaseWarning(Warning):
     pass
 
 
+# Only use this if you actually want to return an Http 400
+# to the client. This should only happen in cases where the
+# client is wrong.
+class WeaveBadRequest(WeaveBaseError):
+    pass
+
+
 class WeaveInternalError(WeaveBaseError):
     """Internal Weave Error (a programming error)"""
 
