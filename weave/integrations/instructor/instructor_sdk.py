083367b8e8d33f510a958ff71c4cf5792613a968
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-08-22 21:17:51 +0530

    add: support for async

--- weave/integrations/instructor/instructor_sdk.py
+++ weave/integrations/instructor/instructor_sdk.py
@@ -1,13 +1,31 @@
 import importlib
-from typing import Callable
+from functools import wraps
+from typing import Any, Callable
 
 import weave
 from weave.trace.patcher import MultiPatcher, SymbolPatcher
 
 
-def instructor_wrapper(name: str) -> Callable[[Callable], Callable]:
+def instructor_wrapper_sync(name: str) -> Callable[[Callable], Callable]:
     def wrapper(fn: Callable) -> Callable:
         op = weave.op()(fn)
+        op.name = name  # type: ignore
+        return op
+
+    return wrapper
+
+
+def instructor_wrapper_async(name: str) -> Callable[[Callable], Callable]:
+    def wrapper(fn: Callable) -> Callable:
+        def _fn_wrapper(fn: Callable) -> Callable:
+            @wraps(fn)
+            async def _async_wrapper(*args: Any, **kwargs: Any) -> Any:
+                return await fn(*args, **kwargs)
+
+            return _async_wrapper
+
+        "We need to do this so we can check if `stream` is used"
+        op = weave.op()(_fn_wrapper(fn))
         op.name = name  # type: ignore
         return op
 
@@ -19,7 +37,12 @@ instructor_patcher = MultiPatcher(
         SymbolPatcher(
             lambda: importlib.import_module("instructor.client"),
             "Instructor.create",
-            instructor_wrapper(name="Instructor.create"),
+            instructor_wrapper_sync(name="Instructor.create"),
+        ),
+        SymbolPatcher(
+            lambda: importlib.import_module("instructor.client"),
+            "AsyncInstructor.create",
+            instructor_wrapper_async(name="AsyncInstructor.create"),
         ),
     ]
 )
