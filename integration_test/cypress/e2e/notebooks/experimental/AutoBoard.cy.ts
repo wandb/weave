65ba610dbf7dfff4d8d7119f86cc33a83275a19b
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-24 16:01:02 -0400

    move to legacy: old query panel examples and notebooks

--- integration_test/cypress/e2e/notebooks/experimental/AutoBoard.cy.ts
+++ integration_test/cypress/e2e/notebooks/experimental/AutoBoard.cy.ts
@@ -1,7 +1,8 @@
 import {checkWeaveNotebookOutputs} from '../notebooks';
 
-describe('../examples/experimental/AutoBoard.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/experimental/AutoBoard.ipynb')
-    );
+describe('../weave/legacy/examples/experimental/AutoBoard.ipynb notebook test', () => {
+  it('passes', () =>
+    checkWeaveNotebookOutputs(
+      '../weave/legacy/examples/experimental/AutoBoard.ipynb'
+    ));
 });
--- integration_test/cypress/e2e/notebooks/reference/layout/Each.cy.ts
+++ integration_test/cypress/e2e/notebooks/reference/layout/Each.cy.ts
@@ -1,7 +1,8 @@
 import {checkWeaveNotebookOutputs} from '../../notebooks';
 
-describe('../examples/reference/layout/Each.ipynb notebook test', () => {
-    it('passes', () =>
-        checkWeaveNotebookOutputs('../examples/reference/layout/Each.ipynb')
-    );
+describe('../weave/legacy/examples/reference/layout/Each.ipynb notebook test', () => {
+  it('passes', () =>
+    checkWeaveNotebookOutputs(
+      '../weave/legacy/examples/reference/layout/Each.ipynb'
+    ));
 });
