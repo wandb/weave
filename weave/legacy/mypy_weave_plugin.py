729cc5255e4c30b6bb54eb94851fe1b89349051e
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-25 17:14:55 -0400

    split: break mypy.ini into trace and legacy pieces and update pre-commit

--- weave/legacy/mypy_weave_plugin.py
+++ weave/legacy/mypy_weave_plugin.py
@@ -7,24 +7,23 @@ This tells mypy to treat it as such.
 from typing import Callable, Optional
 
 from mypy.plugin import ClassDefContext, Plugin
+from mypy.plugins import dataclasses
 
 
 class WeavePlugin(Plugin):
     def get_class_decorator_hook(
         self, fullname: str
     ) -> Optional[Callable[[ClassDefContext], None]]:
-        from mypy.plugins import dataclasses
 
-        if fullname in ["weave.decorator_type.type"]:
+        if fullname in ["weave.legacy.weave.decorator_type.type"]:
             return dataclasses.dataclass_tag_callback
         return None
 
     def get_class_decorator_hook_2(
         self, fullname: str
     ) -> Optional[Callable[[ClassDefContext], bool]]:
-        from mypy.plugins import dataclasses
 
-        if fullname in ["weave.decorator_type.type"]:
+        if fullname in ["weave.legacy.weave.decorator_type.type"]:
             return dataclasses.dataclass_class_maker_callback
         return None
 
