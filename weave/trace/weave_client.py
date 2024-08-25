d064148886bf045f86e7a2fcd4b016342151aa27
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-25 17:14:55 -0400

    split: break mypy.ini into trace and legacy pieces and update pre-commit

--- weave/trace/weave_client.py
+++ weave/trace/weave_client.py
@@ -4,7 +4,7 @@ import platform
 import sys
 import typing
 from functools import lru_cache
-from typing import Any, Dict, Iterator, Optional, Sequence, Union
+from typing import TYPE_CHECKING, Any, Dict, Iterator, Optional, Sequence, Union
 
 import pydantic
 from requests import HTTPError
@@ -50,6 +50,9 @@ from weave.trace_server.trace_server_interface import (
     TableSchemaForInsert,
     TraceServerInterface,
 )
+
+if TYPE_CHECKING:
+    from weave.legacy.weave import ref_base
 
 # Controls if objects can have refs to projects not the WeaveClient project.
 # If False, object refs with with mismatching projects will be recreated.
@@ -900,10 +903,10 @@ class WeaveClient:
     def _remove_call_display_name(self, call: Call) -> None:
         self._set_call_display_name(call, None)
 
-    def _ref_input_to(self, ref: Ref) -> Sequence[Call]:
+    def _ref_input_to(self, ref: "ref_base.Ref") -> Sequence[Call]:
         raise NotImplementedError()
 
-    def _ref_value_input_to(self, ref: Ref) -> list[Call]:
+    def _ref_value_input_to(self, ref: "ref_base.Ref") -> list[Call]:
         raise NotImplementedError()
 
     def _ref_output_of(self, ref: ObjectRef) -> typing.Optional[Call]:
