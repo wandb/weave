a34f36d45683e6bc9a7dd0b6196cb9151460326f
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-26 09:34:39 -0400

    split: further split trace and legacy env

--- weave/trace/env.py
+++ weave/trace/env.py
@@ -5,3 +5,7 @@ WEAVE_PARALLELISM = "WEAVE_PARALLELISM"
 
 def get_weave_parallelism() -> int:
     return int(os.getenv(WEAVE_PARALLELISM, "20"))
+
+
+def wandb_production() -> bool:
+    return os.getenv("WEAVE_ENV") == "wandb_production"
