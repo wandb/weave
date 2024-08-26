1d0dd0f8d082cfd5e807e8c6293b066522e15de6
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-26 13:45:09 -0400

    test

--- weave/autopatch.py
+++ weave/autopatch.py
@@ -17,16 +17,16 @@ def autopatch() -> None:
     from .integrations.mistral.mistral import mistral_patcher
     from .integrations.openai.openai_sdk import openai_patcher
 
-    openai_patcher.attempt_patch()
-    mistral_patcher.attempt_patch()
+    anthropic_patcher.attempt_patch()
+    cohere_patcher.attempt_patch()
+    dspy_patcher.attempt_patch()
+    groq_patcher.attempt_patch()
+    instructor_patcher.attempt_patch()
+    langchain_patcher.attempt_patch()
     litellm_patcher.attempt_patch()
     llamaindex_patcher.attempt_patch()
-    langchain_patcher.attempt_patch()
-    anthropic_patcher.attempt_patch()
-    groq_patcher.attempt_patch()
-    dspy_patcher.attempt_patch()
-    cohere_patcher.attempt_patch()
-    instructor_patcher.attempt_patch()
+    mistral_patcher.attempt_patch()
+    openai_patcher.attempt_patch()
 
 
 def reset_autopatch() -> None:
@@ -41,13 +41,13 @@ def reset_autopatch() -> None:
     from .integrations.mistral.mistral import mistral_patcher
     from .integrations.openai.openai_sdk import openai_patcher
 
-    openai_patcher.undo_patch()
-    mistral_patcher.undo_patch()
+    anthropic_patcher.undo_patch()
+    cohere_patcher.undo_patch()
+    dspy_patcher.undo_patch()
+    groq_patcher.undo_patch()
+    instructor_patcher.undo_patch()
+    langchain_patcher.undo_patch()
     litellm_patcher.undo_patch()
     llamaindex_patcher.undo_patch()
-    langchain_patcher.undo_patch()
-    anthropic_patcher.undo_patch()
-    groq_patcher.undo_patch()
-    dspy_patcher.undo_patch()
-    cohere_patcher.undo_patch()
-    instructor_patcher.undo_patch()
+    mistral_patcher.undo_patch()
+    openai_patcher.undo_patch()
