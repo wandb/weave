4b9b3b93790d34a916e4cfb40e6eab669d809796
gtarpenning <griffin.tarpenning@wandb.com>
2024-08-27 16:05:35 -0700

    order

--- weave/trace_server/trace_server_common.py
+++ weave/trace_server/trace_server_common.py
@@ -8,6 +8,37 @@ from weave.trace_server import trace_server_interface as tsi
 from weave.trace_server.refs_internal import InternalObjectRef, parse_internal_uri
 
 
+def make_derived_summary_fields(
+    call_dict: Dict[str, Any], summary_key: str
+) -> tsi.SummaryMap:
+    display_name = call_dict.get("display_name")
+    started_at_dt = _make_datetime_from_any(call_dict["started_at"])
+    ended_at_dt = _make_datetime_from_any(call_dict.get("ended_at"))
+    status = _make_call_status_from_exception_and_ended_at(
+        call_dict.get("exception"), ended_at_dt
+    )
+
+    latency = None
+    if ended_at_dt and started_at_dt:
+        latency = (ended_at_dt - started_at_dt).microseconds
+
+    if not display_name:
+        op = parse_internal_uri(call_dict["op_name"])
+        if isinstance(op, InternalObjectRef):
+            display_name = op.name
+        else:
+            display_name = call_dict["op_name"]
+
+    weave_derived_fields = tsi.WeaveSummarySchema(
+        nice_trace_name=display_name,
+        status=status,
+        latency=latency,
+    )
+    summary = _load_json_maybe(call_dict.get(summary_key)) or {}
+    summary["weave"] = weave_derived_fields
+    return cast(tsi.SummaryMap, summary)
+
+
 def _make_call_status_from_exception_and_ended_at(
     exception: Optional[str], ended_at: Optional[datetime.datetime]
 ) -> Literal["success", "error", "running"]:
@@ -44,37 +75,6 @@ def _load_json_maybe(value: Any) -> Any:
     elif isinstance(value, dict):
         return value
     return None
-
-
-def make_derived_summary_fields(
-    call_dict: Dict[str, Any], summary_key: str
-) -> tsi.SummaryMap:
-    display_name = call_dict.get("display_name")
-    started_at_dt = _make_datetime_from_any(call_dict["started_at"])
-    ended_at_dt = _make_datetime_from_any(call_dict.get("ended_at"))
-    status = _make_call_status_from_exception_and_ended_at(
-        call_dict.get("exception"), ended_at_dt
-    )
-
-    latency = None
-    if ended_at_dt and started_at_dt:
-        latency = (ended_at_dt - started_at_dt).microseconds
-
-    if not display_name:
-        op = parse_internal_uri(call_dict["op_name"])
-        if isinstance(op, InternalObjectRef):
-            display_name = op.name
-        else:
-            display_name = call_dict["op_name"]
-
-    weave_derived_fields = tsi.WeaveSummarySchema(
-        nice_trace_name=display_name,
-        status=status,
-        latency=latency,
-    )
-    summary = _load_json_maybe(call_dict.get(summary_key)) or {}
-    summary["weave"] = weave_derived_fields
-    return cast(tsi.SummaryMap, summary)
 
 
 def get_nested_key(d: Dict[str, Any], col: str) -> Optional[Any]:
