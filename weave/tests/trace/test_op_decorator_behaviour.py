f36647620465b64d6bc2b5fff804277fc6f13e27
Andrew Truong <itsandrewtruong@gmail.com>
2024-08-29 01:18:46 -0400

    test

--- weave/tests/trace/test_op_decorator_behaviour.py
+++ weave/tests/trace/test_op_decorator_behaviour.py
@@ -265,17 +265,31 @@ async def test_gotten_object_method_is_callable_with_call_func(client, weave_obj
     assert call3.output == call4.output
 
 
-def test_op_display_name_str():
+def test_op_display_name_str(client):
     @op(display_name="example")
     def func():
         return 1
 
     assert func.display_name == "example"
 
+    func()
 
-def test_op_display_name_callable():
+    calls = list(client.calls())
+    call = calls[0]
+
+    assert call.display_name == "example"
+
+
+def test_op_display_name_callable(client):
     @op(display_name=lambda: "example")
     def func():
         return 1
 
     assert func.display_name == "example"
+
+    func()
+
+    calls = list(client.calls())
+    call = calls[0]
+
+    assert call.display_name == "example"
