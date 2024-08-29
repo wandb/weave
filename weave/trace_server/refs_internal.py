ff8b62dc742acb971c35fa6e9213a7b461ad9ce0
Tim Sweeney <tim@wandb.com>
2024-08-29 08:33:03 -0700

    fix: Relax Ref Constraints and Add Helper Text to Error (#2274)
    * init
    
    * init
    
    * init
    
    * init
    
    * fixed
    
    * fixed
    
    * fixed

--- weave/trace_server/refs_internal.py
+++ weave/trace_server/refs_internal.py
@@ -8,8 +8,6 @@ import dataclasses
 import urllib
 from typing import Any, Union
 
-from . import validation
-
 WEAVE_INTERNAL_SCHEME = "weave-trace-internal"
 WEAVE_SCHEME = "weave"
 WEAVE_PRIVATE_SCHEME = "weave-private"
@@ -106,7 +104,8 @@ class InternalObjectRef:
         validate_no_slashes(self.version, "version")
         validate_no_colons(self.version, "version")
         validate_extra(self.extra)
-        validation.object_id_validator(self.name)
+        validate_no_slashes(self.name, "name")
+        validate_no_colons(self.name, "name")
 
     def uri(self) -> str:
         u = f"{WEAVE_INTERNAL_SCHEME}:///{self.project_id}/object/{self.name}:{self.version}"
