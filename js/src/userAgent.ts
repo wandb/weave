c5c043fbf50180b234aed8916636daad8db5de40
Shawn Lewis <shlewis@gmail.com>
2024-08-30 16:52:10 -0700

    Include version in UserAgent headers

--- js/src/userAgent.ts
+++ js/src/userAgent.ts
@@ -1,4 +1,16 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+let packageVersion: string;
+
+try {
+    const packageJson = JSON.parse(readFileSync(join(__dirname, '..', 'package.json'), 'utf8'));
+    packageVersion = packageJson.version;
+} catch (error) {
+    console.warn('Failed to read package.json:', error);
+    packageVersion = 'unknown';
+}
+
 export function userAgent() {
-    // TODO: include version.
-    return `Weave JS Client`
+    return `Weave JS Client ${packageVersion}`;
 }
