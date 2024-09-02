eece0908bf6836b1e4c72ed81fc56c5303ac1cdc
Shawn Lewis <shlewis@gmail.com>
2024-09-02 11:53:37 -0700

    Move netrc stuff to settings.ts

---
+++ js/src/settings.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+
+export function readApiKeyFromNetrc(host: string): string | undefined {
+    const netrcPath = path.join(os.homedir(), '.netrc');
+    if (!fs.existsSync(netrcPath)) {
+        return undefined;
+    }
+
+    const netrcContent = fs.readFileSync(netrcPath, 'utf-8');
+    const lines = netrcContent.split('\n');
+    let foundMachine = false;
+    for (const line of lines) {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith('machine') && trimmedLine.includes(host)) {
+            foundMachine = true;
+        } else if (foundMachine && trimmedLine.startsWith('password')) {
+            return trimmedLine.split(' ')[1];
+        }
+    }
+    return undefined;
+}
