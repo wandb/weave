a43e5fc42a9e5c0009f88e5201a5a96f8da8af04
Shawn Lewis <shlewis@gmail.com>
2024-09-03 15:54:48 -0700

    Use our own concurrency limiter

--- js/src/clientApi.ts
+++ js/src/clientApi.ts
@@ -1,9 +1,9 @@
-// import pLimit from 'p-limit';
 import { WeaveClient } from "./weaveClient";
 import { readApiKeyFromNetrc } from './settings';
 import { WandbServerApi } from './wandbServerApi';
 import { Api as TraceServerApi } from './traceServerApi';
 import { InMemoryTraceServer } from './inMemoryTraceServer';
+import { ConcurrencyLimiter } from './concurrencyLimit';
 
 // Global client instance
 export let globalClient: WeaveClient | null = null;
@@ -27,22 +27,20 @@ export async function init(projectName: string): Promise<WeaveClient> {
         const projectId = `${defaultEntityName}/${projectName}`;
 
         // Limit to N concurrent requests to traceServer
-        // const limit = pLimit(20);
-        // const concurrencyLimitedFetch = (() => {
-        //     return (...fetchParams: Parameters<typeof fetch>) => {
-        //         return limit(() => {
-        //             // console.log(`Active: ${limit.activeCount} Pending: ${limit.pendingCount}`);
-        //             return fetch(...fetchParams);
-        //         });
-        //     }
-        // })()
+        const concurrencyLimiter = new ConcurrencyLimiter(20);
+        const concurrencyLimitedFetch = concurrencyLimiter.limitFunction(async (...fetchParams: Parameters<typeof fetch>) => {
+            const result = await fetch(...fetchParams);
+            // Useful for debugging
+            // console.log(`Active: ${concurrencyLimiter.active} Pending: ${concurrencyLimiter.pending}`);
+            return result
+        });
 
         const traceServerApi = new TraceServerApi({
             baseUrl: 'https://trace.wandb.ai',
             baseApiParams: {
                 headers: headers,
             },
-            // customFetch: concurrencyLimitedFetch
+            customFetch: concurrencyLimitedFetch
         });
 
         globalClient = new WeaveClient(traceServerApi, wandbServerApi, projectId);
---
+++ js/src/concurrencyLimit.ts
@@ -0,0 +1,47 @@
+export class ConcurrencyLimiter {
+    private activeCount = 0;
+    private queue: (() => void)[] = [];
+
+    constructor(private limit: number) { }
+
+    get active(): number {
+        return this.activeCount;
+    }
+
+    get pending(): number {
+        return this.queue.length;
+    }
+
+    private tryExecuteNext() {
+        if (this.queue.length > 0 && this.activeCount < this.limit) {
+            const nextTask = this.queue.shift();
+            this.activeCount++;
+            nextTask!();
+        }
+    }
+
+    limitFunction<T extends any[], R>(asyncFn: (...args: T) => Promise<R>): (...args: T) => Promise<R> {
+        return async (...args: T): Promise<R> => {
+            return new Promise<R>((resolve, reject) => {
+                const task = async () => {
+                    try {
+                        const result = await asyncFn(...args);
+                        resolve(result);
+                    } catch (error) {
+                        reject(error);
+                    } finally {
+                        this.activeCount--;
+                        this.tryExecuteNext();
+                    }
+                };
+
+                if (this.activeCount < this.limit) {
+                    this.activeCount++;
+                    task();
+                } else {
+                    this.queue.push(task);
+                }
+            });
+        };
+    }
+}
