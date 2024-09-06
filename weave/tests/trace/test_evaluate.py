b556f291e9b363c895cd13b4a9eef2fab32bb6ce
gtarpenning <griffin.tarpenning@wandb.com>
2024-09-06 13:47:52 -0700

    test: fix test mean latency flake

--- weave/tests/trace/test_evaluate.py
+++ weave/tests/trace/test_evaluate.py
@@ -16,7 +16,7 @@ dataset = Dataset(rows=dataset_rows)
 expected_eval_result = {
     "model_output": {"mean": 9.5},
     "score": {"true_count": 1, "true_fraction": 0.5},
-    "model_latency": {"mean": pytest.approx(0, abs=0.05)},
+    "model_latency": {"mean": pytest.approx(0, abs=1)},
 }
 
 
@@ -63,7 +63,7 @@ def test_predict_can_receive_other_params(client):
         "model_output": {"mean": 18.5},
         "score": {"true_count": 0, "true_fraction": 0.0},
         "model_latency": {
-            "mean": pytest.approx(0, abs=0.05),
+            "mean": pytest.approx(0, abs=1),
         },
     }
 
@@ -127,7 +127,7 @@ def test_score_as_class(client):
         "model_output": {"mean": 9.5},
         "MyScorer": {"true_count": 1, "true_fraction": 0.5},
         "model_latency": {
-            "mean": pytest.approx(0, abs=0.05),
+            "mean": pytest.approx(0, abs=1),
         },
     }
 
@@ -153,7 +153,7 @@ def test_score_with_custom_summarize(client):
         "model_output": {"mean": 9.5},
         "MyScorer": {"awesome": 3},
         "model_latency": {
-            "mean": pytest.approx(0, abs=0.05),
+            "mean": pytest.approx(0, abs=1),
         },
     }
 
@@ -179,6 +179,6 @@ def test_multiclass_f1_score(client):
             "b": {"f1": 0, "precision": 0, "recall": 0.0},
         },
         "model_latency": {
-            "mean": pytest.approx(0, abs=0.05),
+            "mean": pytest.approx(0, abs=1),
         },
     }
