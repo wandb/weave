335a8c4d01097d7ce079e10de681ce1cdf251d0c
jwlee64 <josiah.lee@wandb.com>
2024-09-11 14:51:25 -0700

    re add clear cache script

---
+++ weave/clear_cache.py
@@ -0,0 +1,17 @@
+# DO NOT MOVE OR DELETE OR RENAME THIS FILE
+# This file is used by the clear_cache job in the weave-python helm chart
+import os
+import time
+
+from weave.legacy.weave import cache
+
+# Script to run to delete expired caches
+if __name__ == "__main__":
+    print("Starting clear cache job", flush=True)
+    hour_interval = int(os.getenv("WEAVE_CACHE_CLEAR_INTERVAL", 24))
+    print("Clearing expired caches every " + str(hour_interval) + " hours", flush=True)
+    while True:
+        curTime = time.strftime("%x, %X", time.localtime())
+        print("Clearing cache " + curTime, flush=True)
+        cache.clear_cache()
+        time.sleep(60 * 60 * hour_interval)
