fb720073443889bd7c5e42c944fd014103752e87
Tim Sweeney <tim@wandb.com>
2024-09-10 22:45:22 -0700

    a little more

--- weave/context_state.py
+++ weave/context_state.py
@@ -1,8 +1,6 @@
 import contextlib
 import contextvars
-import dataclasses
 import typing
-
 
 # Throw an error if op saving encounters an unknonwn condition.
 # The default behavior is to warn.
@@ -20,4 +18,3 @@ def strict_op_saving(enabled: bool) -> typing.Generator[bool, None, None]:
     token = _strict_op_saving.set(enabled)
     yield _strict_op_saving.get()
     _strict_op_saving.reset(token)
-
