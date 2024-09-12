4dc2c878a64668f0176f57df7d2419bc4d360765
gtarpenning <griffin.tarpenning@wandb.com>
2024-09-12 16:27:22 -0700

    first try, totally broken

---
+++ weave/type_serializers/JSONBlob/jsonblob.py
@@ -0,0 +1,24 @@
+import json
+from typing import Any
+
+from weave.trace import serializer
+from weave.trace.custom_objs import MemTraceFilesArtifact
+
+
+class JSONBlob:
+    def __init__(self, obj: Any) -> None:
+        self.obj = obj
+
+
+def save(obj: JSONBlob, artifact: MemTraceFilesArtifact, name: str) -> None:
+    with artifact.new_file("blob.json", binary=False) as f:
+        json.dump(obj.obj, f)
+
+
+def load(artifact: MemTraceFilesArtifact, name: str) -> JSONBlob:
+    with artifact.path("blob.json") as f:
+        return JSONBlob(json.load(f))
+
+
+def register() -> None:
+    serializer.register_serializer(JSONBlob, save, load)
