6a827af81aab6fbf396acbb6be3c9dfca047bbdd
jwlee64 <josiah.lee@wandb.com>
2024-09-12 17:27:30 -0700

    update return type

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/callsTableQuery.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/callsTableQuery.ts
@@ -36,6 +36,7 @@ export const useCallsForQuery = (
   expandedColumns: Set<string>,
   includeCosts: boolean = false
 ): {
+  costsLoading: boolean;
   result: CallSchema[];
   loading: boolean;
   total: number;
