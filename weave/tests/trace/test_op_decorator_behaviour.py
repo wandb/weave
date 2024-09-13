b886171f7e2b484016e85aaf65a1bf73b8d21b8f
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-13 13:17:12 -0400

    fix(weave): Remove erroneous self-binding of weave ops on classes (#2363)

--- weave/tests/trace/test_op_decorator_behaviour.py
+++ weave/tests/trace/test_op_decorator_behaviour.py
@@ -125,7 +125,7 @@ def test_sync_method(client, weave_obj, py_obj):
 
 
 def test_sync_method_call(client, weave_obj, py_obj):
-    res, call = weave_obj.method.call(1)
+    res, call = weave_obj.method.call(weave_obj, 1)
     assert isinstance(call, Call)
     assert call.inputs == {
         "self": ObjectRef(
@@ -145,7 +145,7 @@ def test_sync_method_call(client, weave_obj, py_obj):
         weave_obj_method2 = weave_obj_method_ref.get()
 
     with pytest.raises(errors.OpCallError):
-        call2 = py_obj.amethod.call(1)
+        res2, call2 = py_obj.amethod.call(1)
 
 
 @pytest.mark.asyncio
@@ -160,7 +160,7 @@ async def test_async_method(client, weave_obj, py_obj):
 
 @pytest.mark.asyncio
 async def test_async_method_call(client, weave_obj, py_obj):
-    res, call = await weave_obj.amethod.call(1)
+    res, call = await weave_obj.amethod.call(weave_obj, 1)
     assert isinstance(call, Call)
     assert call.inputs == {
         "self": ObjectRef(
@@ -180,7 +180,7 @@ async def test_async_method_call(client, weave_obj, py_obj):
         weave_obj_amethod2 = weave_obj_amethod_ref.get()
 
     with pytest.raises(errors.OpCallError):
-        call2 = await py_obj.amethod.call(1)
+        res2, call2 = await py_obj.amethod.call(1)
 
 
 def test_sync_func_patching_passes_inspection(func):
@@ -216,7 +216,7 @@ def test_sync_method_calls(client, weave_obj):
         weave_obj.method(x)
 
     for x in range(3):
-        weave_obj.method.call(x)
+        weave_obj.method.call(weave_obj, x)
 
     calls = weave_obj.method.calls()
     calls = list(calls)
@@ -230,7 +230,7 @@ async def test_async_method_calls(client, weave_obj):
         await weave_obj.amethod(x)
 
     for x in range(3):
-        await weave_obj.amethod.call(x)
+        await weave_obj.amethod.call(weave_obj, x)
 
     calls = weave_obj.amethod.calls()
     calls = list(calls)
@@ -252,14 +252,14 @@ async def test_gotten_object_method_is_callable_with_call_func(client, weave_obj
     ref = weave.publish(weave_obj)
 
     weave_obj2 = ref.get()
-    res, call = weave_obj.method.call(1)
-    res2, call2 = weave_obj2.method.call(1)
+    res, call = weave_obj.method.call(weave_obj, 1)
+    res2, call2 = weave_obj2.method.call(weave_obj2, 1)
     assert res == res2
     assert call.inputs == call2.inputs
     assert call.output == call2.output
 
-    res3, call3 = await weave_obj.amethod.call(1)
-    res4, call4 = await weave_obj2.amethod.call(1)
+    res3, call3 = await weave_obj.amethod.call(weave_obj, 1)
+    res4, call4 = await weave_obj2.amethod.call(weave_obj2, 1)
     assert res3 == res4
     assert call3.inputs == call4.inputs
     assert call3.output == call4.output
