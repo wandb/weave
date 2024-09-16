a55b4f393206970a79f3dee046506b3dc8f2edf1
Shawn Lewis <shlewis@gmail.com>
2024-09-16 14:51:32 -0700

    Fixes: export boundOp. Don't rely on obj.className(), use javascript methods instead

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
-export { init } from './clientApi';
-export { weaveImage } from './media';
-export { op } from './op';
-export { wrapOpenAI } from './integrations';
-export { Dataset } from './dataset';
-export { Evaluation } from './evaluation';
+export { init } from "./clientApi";
+export { weaveImage } from "./media";
+export { op, boundOp } from "./op";
+export { wrapOpenAI } from "./integrations";
+export { Dataset } from "./dataset";
+export { Evaluation } from "./evaluation";
--- js/src/op.ts
+++ js/src/op.ts
@@ -1,88 +1,125 @@
-import { globalClient } from './clientApi';
-import { OpOptions, Op } from './opType';
+import { globalClient } from "./clientApi";
+import { OpOptions, Op } from "./opType";
 
 export function op<T extends (...args: any[]) => any>(
-    fn: T,
-    options?: OpOptions<T>
+  fn: T,
+  options?: OpOptions<T>
 ): Op<(...args: Parameters<T>) => Promise<ReturnType<T>>> {
-    const fnName = options?.originalFunction?.name || fn.name || 'anonymous';
-    let actualOpName = fnName;
-    const thisArg = options?.bindThis;
-    if (options?.bindThis) {
-        actualOpName = `${options.bindThis.className()}.${fnName}`;
+  const fnName = options?.originalFunction?.name || fn.name || "anonymous";
+  let actualOpName = fnName;
+  const thisArg = options?.bindThis;
+  if (options?.bindThis) {
+    const className = Object.getPrototypeOf(options.bindThis).constructor.name;
+    actualOpName = `${className}.${fnName}`;
+  }
+  if (options?.name) {
+    actualOpName = options.name;
+  }
+
+  const opWrapper = async function (
+    ...params: Parameters<T>
+  ): Promise<ReturnType<T>> {
+    if (!globalClient) {
+      return await fn(...params);
     }
-    if (options?.name) {
-        actualOpName = options.name;
+
+    const { newStack, currentCall, parentCall } = globalClient.pushNewCall();
+    const startTime = new Date();
+    if (!globalClient.quiet && parentCall == null) {
+      console.log(
+        `🍩 https://wandb.ai/${globalClient.projectId}/r/call/${currentCall.callId}`
+      );
     }
+    const startCallPromise = globalClient.startCall(
+      opWrapper,
+      params,
+      options?.parameterNames,
+      thisArg,
+      currentCall,
+      parentCall,
+      startTime
+    );
 
-    const opWrapper = async function (...params: Parameters<T>): Promise<ReturnType<T>> {
-        if (!globalClient) {
-            return await fn(...params);
-        }
+    try {
+      let result = await globalClient.runWithCallStack(newStack, async () => {
+        return await fn(...params);
+      });
 
-        const { newStack, currentCall, parentCall } = globalClient.pushNewCall();
-        const startTime = new Date();
-        if (!globalClient.quiet && parentCall == null) {
-            console.log(`🍩 https://wandb.ai/${globalClient.projectId}/r/call/${currentCall.callId}`);
-        }
-        const startCallPromise = globalClient.startCall(opWrapper, params, options?.parameterNames, thisArg, currentCall, parentCall, startTime);
+      if (options?.streamReducer && Symbol.asyncIterator in result) {
+        const { initialState, reduceFn } = options.streamReducer;
+        let state = initialState;
 
-        try {
-            let result = await globalClient.runWithCallStack(newStack, async () => {
-                return await fn(...params);
-            });
-
-            if (options?.streamReducer && Symbol.asyncIterator in result) {
-                const { initialState, reduceFn } = options.streamReducer;
-                let state = initialState;
-
-                const wrappedIterator = {
-                    [Symbol.asyncIterator]: async function* () {
-                        try {
-                            for await (const chunk of result as AsyncIterable<any>) {
-                                state = reduceFn(state, chunk);
-                                yield chunk;
-                            }
-                        } finally {
-                            if (globalClient) { // Check if globalClient still exists
-                                const endTime = new Date();
-                                await globalClient.finishCall(state, currentCall, parentCall, options?.summarize, endTime, startCallPromise);
-                            }
-                        }
-                    }
-                };
-
-                return wrappedIterator as unknown as ReturnType<T>;
-            } else {
+        const wrappedIterator = {
+          [Symbol.asyncIterator]: async function* () {
+            try {
+              for await (const chunk of result as AsyncIterable<any>) {
+                state = reduceFn(state, chunk);
+                yield chunk;
+              }
+            } finally {
+              if (globalClient) {
+                // Check if globalClient still exists
                 const endTime = new Date();
-                globalClient.finishCall(result, currentCall, parentCall, options?.summarize, endTime, startCallPromise);
-                return result;
+                await globalClient.finishCall(
+                  state,
+                  currentCall,
+                  parentCall,
+                  options?.summarize,
+                  endTime,
+                  startCallPromise
+                );
+              }
             }
-        } catch (error) {
-            // console.error(`Op ${actualOpName} failed:`, error);
-            const endTime = new Date();
-            globalClient.finishCallWithException(error, currentCall, endTime, startCallPromise);
-            throw error;
-        } finally {
-            // No need to do anything here.
-        }
-    };
+          },
+        };
 
-    opWrapper.__name = actualOpName;
-    opWrapper.__isOp = true as true;
-    if (options?.originalFunction) {
-        opWrapper.__wrappedFunction = options.originalFunction;
-    } else {
-        opWrapper.__wrappedFunction = fn;
+        return wrappedIterator as unknown as ReturnType<T>;
+      } else {
+        const endTime = new Date();
+        globalClient.finishCall(
+          result,
+          currentCall,
+          parentCall,
+          options?.summarize,
+          endTime,
+          startCallPromise
+        );
+        return result;
+      }
+    } catch (error) {
+      // console.error(`Op ${actualOpName} failed:`, error);
+      const endTime = new Date();
+      globalClient.finishCallWithException(
+        error,
+        currentCall,
+        endTime,
+        startCallPromise
+      );
+      throw error;
+    } finally {
+      // No need to do anything here.
     }
+  };
 
-    if (options?.bindThis !== undefined) {
-        opWrapper.__boundThis = options.bindThis;
-    }
+  opWrapper.__name = actualOpName;
+  opWrapper.__isOp = true as true;
+  if (options?.originalFunction) {
+    opWrapper.__wrappedFunction = options.originalFunction;
+  } else {
+    opWrapper.__wrappedFunction = fn;
+  }
 
-    return opWrapper as Op<T>;
+  if (options?.bindThis !== undefined) {
+    opWrapper.__boundThis = options.bindThis;
+  }
+
+  return opWrapper as Op<T>;
 }
 
-export function boundOp(bindThis: any, fn: (...args: any[]) => any, options?: OpOptions<any>) {
-    return op(fn.bind(bindThis), { originalFunction: fn, bindThis, ...options });
+export function boundOp(
+  bindThis: any,
+  fn: (...args: any[]) => any,
+  options?: OpOptions<any>
+) {
+  return op(fn.bind(bindThis), { originalFunction: fn, bindThis, ...options });
 }
