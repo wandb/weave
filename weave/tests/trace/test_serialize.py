642ab9acdecdc37b038cb1b0a779a4515cce5167
Tim Sweeney <tim@wandb.com>
2024-09-17 18:13:55 -0700

    Done

--- weave/tests/trace/test_serialize.py
+++ weave/tests/trace/test_serialize.py
@@ -1,28 +1,50 @@
-from PIL import Image
+import json
 
-import weave
-from weave.trace import serialize
+from weave.trace import serialize, serializer
+from weave.trace.custom_objs import MemTraceFilesArtifact
+
+
+class MyCustomClass:
+    def __init__(self, value):
+        self.value = value
+
+
+def save(obj: MyCustomClass, artifact: MemTraceFilesArtifact, name: str) -> None:
+    with artifact.new_file("obj.json", binary=True) as f:
+        f.write(json.dumps({"value": obj.value}).encode("utf-8"))
+
+
+def load(artifact: MemTraceFilesArtifact, name: str) -> MyCustomClass:
+    with open(artifact.path("obj.json"), "r") as f:
+        return MyCustomClass(json.load(f)["value"])
+
+
+serializer.register_serializer(MyCustomClass, save, load)
 
 
 def test_serialize_caching(client):
-    serialize._custom_weave_type_cache_map[client._project_id()].reset()
-    img = Image.new("RGB", (100, 100))
-    serialized_img = serialize._to_json_custom_weave_type(
-        img, client._project_id(), client.server
+    # Technically, we could use PIL images in this test, but I like
+    # the purity of not having external dependencies in our tests.
+    # Furthermore, when running with other tests, it is possible
+    # that they will have already serialized this object, and
+    # so our tests should be resilient to that.
+    obj = MyCustomClass(1)
+    serialized_obj = serialize._to_json_custom_weave_type(
+        obj, client._project_id(), client.server
     )
     client._flush()
     access_log = client.server.attribute_access_log
     methods_1 = [log for log in access_log if not log.startswith("_")]
     assert methods_1 == [
         "ensure_project_exists",  # default initialization
-        "file_create",  # creates the deserialize op code
-        "obj_create",  # creates the deserialize op object
-        "file_create",  # creates the image file
+        "file_create",  # creates the serialized op code
+        "obj_create",  # creates the serialized op object
+        "file_create",  # creates the custom object file
     ]
 
     # Serialize again
-    serialized_img = serialize._to_json_custom_weave_type(
-        img, client._project_id(), client.server
+    serialized_obj = serialize._to_json_custom_weave_type(
+        obj, client._project_id(), client.server
     )
     client._flush()
     access_log = client.server.attribute_access_log
@@ -31,11 +53,11 @@ def test_serialize_caching(client):
     assert methods_2 == methods_1
 
     # Make sure we can deserialize
-    deserialize_img = serialize._from_json_custom_weave_type(
-        serialized_img, client._project_id(), client.server
+    deserialize_obj = serialize._from_json_custom_weave_type(
+        serialized_obj, client._project_id(), client.server
     )
-    assert isinstance(deserialize_img, Image.Image)
-    assert deserialize_img.tobytes() == img.tobytes()
+    assert isinstance(deserialize_obj, MyCustomClass)
+    assert deserialize_obj.value == obj.value
 
     # Again, no new methods should be called!
     client._flush()
@@ -47,8 +69,8 @@ def test_serialize_caching(client):
     serialize._custom_weave_type_cache_map[client._project_id()].reset()
 
     # Deserialize should trigger new methods
-    deserialize_img = serialize._from_json_custom_weave_type(
-        serialized_img, client._project_id(), client.server
+    deserialize_obj = serialize._from_json_custom_weave_type(
+        serialized_obj, client._project_id(), client.server
     )
     client._flush()
     access_log = client.server.attribute_access_log
@@ -60,8 +82,8 @@ def test_serialize_caching(client):
     ]
 
     # Reserialize should not trigger any new methods
-    serialized_img = serialize._to_json_custom_weave_type(
-        deserialize_img, client._project_id(), client.server
+    serialized_obj = serialize._to_json_custom_weave_type(
+        deserialize_obj, client._project_id(), client.server
     )
     client._flush()
     access_log = client.server.attribute_access_log
