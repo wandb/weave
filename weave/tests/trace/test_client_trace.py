2fdd643c1772d01f70e9ff7841d0e660c24456ef
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-18 21:44:32 -0400

    test

--- weave/tests/trace/test_client_trace.py
+++ weave/tests/trace/test_client_trace.py
@@ -2871,3 +2871,25 @@ def test_inline_pydantic_basemodel_generates_no_refs_in_object(client):
         )
     )
     assert len(res.objs) == 1  # Just the weave object, and not the pydantic model
+
+
+def test_weave_finish_unsets_client(client):
+    @weave.op
+    def foo():
+        return 1
+
+    weave.trace.client_context.weave_client.set_weave_client_global(None)
+    weave.trace.weave_init._current_inited_client = (
+        weave.trace.weave_init.InitializedClient(client)
+    )
+    weave_client = weave.trace.weave_init._current_inited_client.client
+    assert weave.trace.weave_init._current_inited_client is not None
+
+    foo()
+    assert len(list(weave_client.get_calls())) == 1
+
+    weave.finish()
+
+    foo()
+    assert len(list(weave_client.get_calls())) == 1
+    assert weave.trace.weave_init._current_inited_client is None
