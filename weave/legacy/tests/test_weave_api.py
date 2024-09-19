65d91d2163cd908346e3a712b5c6fa889071cc81
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-19 18:39:23 -0400

    chore(weave): Split out Trace and Query tests; move trace tests into dedicated tox envs across py39-312 (#2403)

--- weave/legacy/tests/test_weave_api.py
+++ weave/legacy/tests/test_weave_api.py
@@ -33,24 +33,3 @@ def test_create_list_rename_delete():
     arts = weave.use(weave.legacy.weave.ops.local_artifacts())
     assert len(arts) == 0
 
-
-def test_weave_finish_unsets_client(client):
-    @weave.op
-    def foo():
-        return 1
-
-    weave.trace.client_context.weave_client.set_weave_client_global(None)
-    weave.trace.weave_init._current_inited_client = (
-        weave.trace.weave_init.InitializedClient(client)
-    )
-    weave_client = weave.trace.weave_init._current_inited_client.client
-    assert weave.trace.weave_init._current_inited_client is not None
-
-    foo()
-    assert len(list(weave_client.get_calls())) == 1
-
-    weave.finish()
-
-    foo()
-    assert len(list(weave_client.get_calls())) == 1
-    assert weave.trace.weave_init._current_inited_client is None
