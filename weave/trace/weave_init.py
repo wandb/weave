2241cbea3c9a9e66ab2d1c337ef977c746289ca7
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-19 11:26:05 -0400

    test

--- weave/trace/weave_init.py
+++ weave/trace/weave_init.py
@@ -18,7 +18,7 @@ class InitializedClient:
 
 
 def get_username() -> typing.Optional[str]:
-    from weave.legacy.weave import wandb_api
+    from weave.wandb_interface import wandb_api
 
     api = wandb_api.get_wandb_api_sync()
     try:
@@ -28,7 +28,7 @@ def get_username() -> typing.Optional[str]:
 
 
 def get_entity_project_from_project_name(project_name: str) -> tuple[str, str]:
-    from weave.legacy.weave import wandb_api
+    from weave.wandb_interface import wandb_api
 
     fields = project_name.split("/")
     if len(fields) == 1:
@@ -76,7 +76,7 @@ def init_weave(
         else:
             _current_inited_client.reset()
 
-    from weave.legacy.weave import wandb_api
+    from weave.wandb_interface import wandb_api
 
     # Must init to read ensure we've read auth from the environment, in
     # case we're on a new thread.
