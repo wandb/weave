d2944b61a03f6afe436424efaa511fbfc1ea0d7b
Tim Sweeney <tim@wandb.com>
2024-09-18 17:35:28 -0700

    init

---
+++ weave/tests/trace/test_table_query.py
@@ -0,0 +1,36 @@
+from weave.trace.weave_client import WeaveClient
+from weave.trace_server import trace_server_interface as tsi
+
+def generate_table_data(client: WeaveClient, n_rows: int, n_cols: int):
+    data = [
+        {
+            "id": i,
+            **{f"col_{j}": f"value_{i}_{j}" for j in range(n_cols)},
+        }
+        for i in range(n_rows)
+    ]
+
+    res = client.server.table_create(
+        tsi.TableCreateReq(
+            table=tsi.TableSchemaForInsert(rows=data,             project_id=client._project_id(),
+),
+        )
+    )
+    digest = res.digest
+
+    return digest, data
+
+def test_table_query(client: WeaveClient):
+    digest, data = generate_table_data(client, 10, 10)
+
+    res = client.server.table_query(
+        tsi.TableQueryReq(
+            project_id=client._project_id(),
+            digest=digest,
+        )
+    )
+
+    row_data = [r.val for r in res.rows]
+
+    assert row_data == data
+    
