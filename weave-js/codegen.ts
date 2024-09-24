b7b9dc3c9ddb55bf1bdb820f15291b12b3c0979b
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-24 12:31:47 -0400

    test2

--- weave-js/codegen.ts
+++ weave-js/codegen.ts
@@ -2,7 +2,7 @@ import {CodegenConfig} from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: './wb_schema.gql',
+  schema: '../wb_schema.gql',
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
     './src/generated/': {
