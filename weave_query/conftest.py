d96da421115eef643e3b4b4982642a76d005ce34
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-24 20:03:07 -0400

    fixture_fakewandb

--- weave_query/conftest.py
+++ weave_query/conftest.py
@@ -10,6 +10,7 @@ import numpy as np
 import pytest
 from flask.testing import FlaskClient
 
+from weave_query.tests import fixture_fakewandb
 from weave_query.weave_query import client as client_legacy
 from weave_query.weave_query import context_state, environment, io_service, serialize
 from weave_query.weave_query.language_features.tagging.tag_store import (
@@ -114,6 +115,13 @@ def cereal_csv():
         cereal_path = os.path.join(d, "cereal.csv")
         shutil.copy("testdata/cereal.csv", cereal_path)
         yield cereal_path
+        
+
+@pytest.fixture()
+def fake_wandb():
+    setup_response = fixture_fakewandb.setup()
+    yield setup_response
+    fixture_fakewandb.teardown(setup_response)
 
 
 @pytest.fixture()
