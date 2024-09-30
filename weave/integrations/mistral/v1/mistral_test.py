118a575c2f8fe38d0c7538947f513e82a3271033
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-30 13:15:44 -0400

    test

--- weave/integrations/mistral/v1/mistral_test.py
+++ weave/integrations/mistral/v1/mistral_test.py
@@ -54,21 +54,19 @@ Each of these cheeses has its unique characteristics, so the "best" one depends 
 
     assert call.exception is None and call.ended_at is not None
     output = call.output
-    assert output["choices"][0]["message"]["content"] == exp
-    assert output["choices"][0]["finish_reason"] == "stop"
-    assert output["id"] == chat_response.id
-    assert output["model"] == chat_response.model
-    assert output["object"] == chat_response.object
-    assert output["created"] == chat_response.created
+    assert output.choices[0].message.content == exp
+    assert output.choices[0].finish_reason == "stop"
+    assert output.id == chat_response.id
+    assert output.model == chat_response.model
+    assert output.object == chat_response.object
+    assert output.created == chat_response.created
     summary = call.summary
     assert summary is not None
-    model_usage = summary["usage"][output["model"]]
+    model_usage = summary["usage"][output.model]
     assert model_usage["requests"] == 1
-    assert (
-        output["usage"]["completion_tokens"] == model_usage["completion_tokens"] == 406
-    )
-    assert output["usage"]["prompt_tokens"] == model_usage["prompt_tokens"] == 10
-    assert output["usage"]["total_tokens"] == model_usage["total_tokens"] == 416
+    assert output.usage.completion_tokens == model_usage["completion_tokens"] == 406
+    assert output.usage.prompt_tokens == model_usage["prompt_tokens"] == 10
+    assert output.usage.total_tokens == model_usage["total_tokens"] == 416
 
 
 @pytest.mark.skip_clickhouse_client
@@ -114,21 +112,19 @@ Each of these cheeses offers a unique taste and texture, so the "best" one is a 
 
     assert call.exception is None and call.ended_at is not None
     output = call.output
-    assert output["choices"][0]["message"]["content"] == exp
-    assert output["choices"][0]["finish_reason"] == "stop"
-    assert output["id"] == chat_response.id
-    assert output["model"] == chat_response.model
-    assert output["object"] == chat_response.object
-    assert output["created"] == chat_response.created
+    assert output.choices[0].message.content == exp
+    assert output.choices[0].finish_reason == "stop"
+    assert output.id == chat_response.id
+    assert output.model == chat_response.model
+    assert output.object == chat_response.object
+    assert output.created == chat_response.created
     summary = call.summary
     assert summary is not None
-    model_usage = summary["usage"][output["model"]]
+    model_usage = summary["usage"][output.model]
     assert model_usage["requests"] == 1
-    assert (
-        output["usage"]["completion_tokens"] == model_usage["completion_tokens"] == 363
-    )
-    assert output["usage"]["prompt_tokens"] == model_usage["prompt_tokens"] == 10
-    assert output["usage"]["total_tokens"] == model_usage["total_tokens"] == 373
+    assert output.usage.completion_tokens == model_usage["completion_tokens"] == 363
+    assert output.usage.prompt_tokens == model_usage["prompt_tokens"] == 10
+    assert output.usage.total_tokens == model_usage["total_tokens"] == 373
 
 
 @pytest.mark.skip_clickhouse_client
@@ -175,21 +171,19 @@ Each of these cheeses offers a unique taste and texture, so the "best" one depen
 
     assert call.exception is None and call.ended_at is not None
     output = call.output
-    assert output["choices"][0]["message"]["content"] == exp
-    assert output["choices"][0]["finish_reason"] == "stop"
-    assert output["id"] == chunk.data.id
-    assert output["model"] == chunk.data.model
-    assert output["object"] == chunk.data.object
-    assert output["created"] == chunk.data.created
+    assert output.choices[0].message.content == exp
+    assert output.choices[0].finish_reason == "stop"
+    assert output.id == chunk.data.id
+    assert output.model == chunk.data.model
+    assert output.object == chunk.data.object
+    assert output.created == chunk.data.created
     summary = call.summary
     assert summary is not None
-    model_usage = summary["usage"][output["model"]]
+    model_usage = summary["usage"][output.model]
     assert model_usage["requests"] == 1
-    assert (
-        output["usage"]["completion_tokens"] == model_usage["completion_tokens"] == 350
-    )
-    assert output["usage"]["prompt_tokens"] == model_usage["prompt_tokens"] == 10
-    assert output["usage"]["total_tokens"] == model_usage["total_tokens"] == 360
+    assert output.usage.completion_tokens == model_usage["completion_tokens"] == 350
+    assert output.usage.prompt_tokens == model_usage["prompt_tokens"] == 10
+    assert output.usage.total_tokens == model_usage["total_tokens"] == 360
 
 
 @pytest.mark.skip_clickhouse_client
@@ -245,18 +239,16 @@ Each of these cheeses has its unique characteristics, so the "best" one depends 
 
     assert call.exception is None and call.ended_at is not None
     output = call.output
-    assert output["choices"][0]["message"]["content"] == exp
-    assert output["choices"][0]["finish_reason"] == "stop"
-    assert output["id"] == chunk.data.id
-    assert output["model"] == chunk.data.model
-    assert output["object"] == chunk.data.object
-    assert output["created"] == chunk.data.created
+    assert output.choices[0].message.content == exp
+    assert output.choices[0].finish_reason == "stop"
+    assert output.id == chunk.data.id
+    assert output.model == chunk.data.model
+    assert output.object == chunk.data.object
+    assert output.created == chunk.data.created
     summary = call.summary
     assert summary is not None
-    model_usage = summary["usage"][output["model"]]
+    model_usage = summary["usage"][output.model]
     assert model_usage["requests"] == 1
-    assert (
-        output["usage"]["completion_tokens"] == model_usage["completion_tokens"] == 459
-    )
-    assert output["usage"]["prompt_tokens"] == model_usage["prompt_tokens"] == 10
-    assert output["usage"]["total_tokens"] == model_usage["total_tokens"] == 469
+    assert output.usage.completion_tokens == model_usage["completion_tokens"] == 459
+    assert output.usage.prompt_tokens == model_usage["prompt_tokens"] == 10
+    assert output.usage.total_tokens == model_usage["total_tokens"] == 469
