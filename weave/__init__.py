c8fd6f067f9d6c48a5e72ac7808947e3f3eb2709
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-29 21:33:15 -0400

    cleanup flow imports

--- weave/__init__.py
+++ weave/__init__.py
@@ -3,15 +3,12 @@
 from weave import version
 
 from weave.trace.api import *
+from weave.flow import *
+
+from weave.trace.util import ThreadPoolExecutor, Thread
 
 __version__ = version.VERSION
 
-from weave.flow.obj import Object
-from weave.flow.dataset import Dataset
-from weave.flow.model import Model
-from weave.flow.eval import Evaluation, Scorer
-from weave.flow.agent import Agent, AgentState
-from weave.trace.util import ThreadPoolExecutor, Thread
 
 # Special object informing doc generation tooling which symbols
 # to document & to associate with this module.
---
+++ weave/flow/__init__.py
@@ -0,0 +1,6 @@
+from weave.flow.obj import Object
+from weave.flow.dataset import Dataset
+from weave.flow.model import Model
+from weave.flow.eval import Evaluation, Scorer
+from weave.flow.agent import Agent, AgentState
+from weave.trace.util import ThreadPoolExecutor, Thread
