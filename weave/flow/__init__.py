d08c4c36e8f6d40d4bfa331555046fbe4d3640e6
Andrew Truong <itsandrewtruong@gmail.com>
2024-09-29 21:35:54 -0400

    tidy

--- weave/flow/__init__.py
+++ weave/flow/__init__.py
@@ -1,6 +1,15 @@
-from weave.flow.obj import Object
-from weave.flow.dataset import Dataset
-from weave.flow.model import Model
-from weave.flow.eval import Evaluation, Scorer
 from weave.flow.agent import Agent, AgentState
-from weave.trace.util import ThreadPoolExecutor, Thread
+from weave.flow.dataset import Dataset
+from weave.flow.eval import Evaluation, Scorer
+from weave.flow.model import Model
+from weave.flow.obj import Object
+
+__docspec__ = [
+    Object,
+    Dataset,
+    Model,
+    Evaluation,
+    Scorer,
+    Agent,
+    AgentState,
+]
