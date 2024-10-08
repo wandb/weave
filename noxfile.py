ed02aa35d24d09d4c4d14a3574553942919c384b
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-07 20:46:47 -0400

    test

--- noxfile.py
+++ noxfile.py
@@ -56,11 +56,8 @@ def tests(session, shard):
 
     test_dirs = test_dirs_dict.get(shard, default_test_dirs)
 
-    has_n_arg = any(
-        arg.startswith("-n") and (len(arg) == 2 or arg[2:].isdigit())
-        for arg in session.posargs
-    )
-    if not has_n_arg:
+    # seems to resolve ci issues
+    if shard == "llamaindex":
         session.posargs.insert(0, "-n4")
 
     session.run("pytest", *session.posargs, *test_dirs, env=env)
