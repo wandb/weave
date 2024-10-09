d67e04d9750602fd6abfdf849b54731f380af07d
Tim Sweeney <tim@wandb.com>
2024-10-09 15:05:14 -0700

    init

--- tests/trace/type_serializers/Image/image_test.py
+++ tests/trace/type_serializers/Image/image_test.py
@@ -77,9 +77,14 @@ def image_as_input_and_output_part(in_img: Image.Image) -> dict:
     return {"out_img": in_img}
 
 
+reruns = 0
+
+
 @pytest.mark.flaky(reruns=3)
 def test_image_as_call_io(client: WeaveClient) -> None:
-    client.project = "test_image_as_call_io"
+    global reruns
+    client.project = f"test_image_as_call_io_{reruns}"
+    reruns += 1
     non_published_img = image_as_solo_output(publish_first=False)
     img_dict = image_as_input_and_output_part(non_published_img)
 
