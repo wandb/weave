970ef27e9bd4ae5f95601282dd45ddf17e6a33b8
Tim Sweeney <tim@wandb.com>
2024-10-10 16:53:38 -0700

    init

--- weave_query/weave_query/async_queue.py
+++ weave_query/weave_query/async_queue.py
@@ -1,6 +1,8 @@
+import atexit
 import aioprocessing
 import janus
 from typing import Optional, Generic, TypeVar
+import asyncio
 
 QueueItemType = TypeVar("QueueItemType")
 
@@ -95,4 +97,34 @@ class ThreadQueue(Queue, Generic[QueueItemType]):
         self.queue.sync_q.task_done()
 
     def init(self) -> None:
+        """
+        This is some old code that previously just has one line:
+        `self._queue: janus.Queue = janus.Queue(maxsize=self.maxsize)`
+
+        However, `init` is not `async` and therefore cannot sure we are in the event loop.
+        Moreover, these objects are created and stored globally! There are some race cases
+        where the event loop is not running when the object is created. This is a super
+        hack to avoid a huge refactor and instead, just create a new event loop and set it
+        as the current event loop. When the janus.Queue is created, it internally maintains
+        a reference to the event loop that was current when it was created. This means
+        that we cannot immediately close it. Instead, we register a function to close it at  
+        the end of the program.
+
+        This is a hack and should be removed when possible.
+        """
+        not_in_event_loop = not _in_event_loop()
+        if not_in_event_loop:
+            # Yikes! we are going to do something hacky:
+            if not hasattr(self, "_local_loop"):
+                self._local_loop = asyncio.new_event_loop()
+                atexit.register(self._local_loop.close)
+            asyncio.set_event_loop(self._local_loop)
         self._queue: janus.Queue = janus.Queue(maxsize=self.maxsize)
+        if not_in_event_loop:
+            asyncio.set_event_loop(None)
+
+def _in_event_loop() -> bool:
+    try:
+        return asyncio.get_event_loop().is_running()
+    except RuntimeError:
+        return False
