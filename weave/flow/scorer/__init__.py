3d2e35212b2bf7a1c7f928b272ee46e248c64ec2
Thomas Capelle <tcapelle@pm.me>
2024-10-10 19:01:10 +0200

    missing import

--- weave/flow/scorer/__init__.py
+++ weave/flow/scorer/__init__.py
@@ -1,4 +1,4 @@
 from weave.flow.scorer.base_scorer import *
 from weave.flow.scorer.regex_scorer import RegexScorer
 from weave.flow.scorer.json_scorer import JSONScorer
-from weave.flow.scorer.llm_scorer import LLMScorer, EmbeddingScorer
+from weave.flow.scorer.llm_scorer import LLMScorer, EmbeddingScorer, OpenAIModerationScorer
