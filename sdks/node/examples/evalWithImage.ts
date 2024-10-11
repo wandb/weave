26a3345fe75c848e010786a75f31a31967495b8a
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-10 23:07:58 -0400

    format examples

--- sdks/node/examples/evalWithImage.ts
+++ sdks/node/examples/evalWithImage.ts
@@ -1,22 +1,22 @@
-import "source-map-support/register";
-import { OpenAI } from "openai";
-import { init, wrapOpenAI, op, Dataset, Evaluation, weaveImage } from "weave";
+import { OpenAI } from 'openai';
+import 'source-map-support/register';
+import { Dataset, Evaluation, init, op, wrapOpenAI } from 'weave';
 
 const sentences = [
-  "There are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy.",
-  "Pounits are a bright green color and are more savory than sweet.",
-  "Finally, there are fruits called glowls, which have a very sour and bitter taste which is acidic and caustic, and a pale orange tinge to them.",
-  "There are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy.",
+  'There are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy.',
+  'Pounits are a bright green color and are more savory than sweet.',
+  'Finally, there are fruits called glowls, which have a very sour and bitter taste which is acidic and caustic, and a pale orange tinge to them.',
+  'There are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy.',
 ];
 const labels = [
-  { fruit: "neoskizzles", color: "purple", flavor: "candy" },
-  { fruit: "pounits", color: "bright green", flavor: "savory" },
-  { fruit: "glowls", color: "pale orange", flavor: "sour and bitter" },
+  { fruit: 'neoskizzles', color: 'purple', flavor: 'candy' },
+  { fruit: 'pounits', color: 'bright green', flavor: 'savory' },
+  { fruit: 'glowls', color: 'pale orange', flavor: 'sour and bitter' },
 ];
 const examples = [
-  { id: "0", sentence: sentences[0], target: labels[0] },
-  { id: "1", sentence: sentences[1], target: labels[1] },
-  { id: "2", sentence: sentences[2], target: labels[2] },
+  { id: '0', sentence: sentences[0], target: labels[0] },
+  { id: '1', sentence: sentences[1], target: labels[1] },
+  { id: '2', sentence: sentences[2], target: labels[2] },
 ];
 
 const openaiClient = wrapOpenAI(new OpenAI());
@@ -24,24 +24,24 @@ const openaiClient = wrapOpenAI(new OpenAI());
 const model = op(async function myModel({ datasetRow }) {
   const prompt = `Extract fields ("fruit": <str>, "color": <str>, "flavor") from the following text, as json: ${datasetRow.sentence}`;
   const response = await openaiClient.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-    response_format: { type: "json_object" },
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: prompt }],
+    response_format: { type: 'json_object' },
   });
   const result = response.choices[0].message.content;
   if (result == null) {
-    throw new Error("No response from model");
+    throw new Error('No response from model');
   }
-  if (datasetRow.id == "3") {
-    throw new Error("This is an error");
+  if (datasetRow.id == '3') {
+    throw new Error('This is an error');
   }
   return JSON.parse(result);
 });
 
 async function main() {
-  await init("weavejsdev-evalwithimage");
+  await init('weavejsdev-evalwithimage');
   const ds = new Dataset({
-    id: "Fruit Dataset",
+    id: 'Fruit Dataset',
     rows: examples,
   });
   const evaluation = new Evaluation({
@@ -56,8 +56,8 @@ async function main() {
         const result = await openaiClient.images.generate({
           prompt: `A fruit that's ${modelOutput.color} and ${modelOutput.flavor}`,
           n: 1,
-          size: "256x256",
-          response_format: "b64_json",
+          size: '256x256',
+          response_format: 'b64_json',
         });
         return result.data[0];
         // console.log('RESULT', result)
--- sdks/node/examples/evaluate.ts
+++ sdks/node/examples/evaluate.ts
@@ -1,22 +1,22 @@
-import "source-map-support/register";
-import { init, op, Dataset, Evaluation } from "weave";
+import 'source-map-support/register';
+import { Dataset, Evaluation, init, op } from 'weave';
 
 async function main() {
-  await init("weavejsdev-eval6");
+  await init('weavejsdev-eval6');
   const ds = new Dataset({
-    id: "My Dataset",
-    description: "This is a dataset",
+    id: 'My Dataset',
+    description: 'This is a dataset',
     rows: [
-      { name: "Alice", age: 25 },
-      { name: "Bob", age: 30 },
-      { name: "Charlie", age: 34 },
+      { name: 'Alice', age: 25 },
+      { name: 'Bob', age: 30 },
+      { name: 'Charlie', age: 34 },
     ],
   });
   const evaluation = new Evaluation({
     dataset: ds,
     scorers: [
       op(({ modelOutput, datasetItem }) => modelOutput == datasetItem.age, {
-        name: "isEqual",
+        name: 'isEqual',
       }),
     ],
   });
