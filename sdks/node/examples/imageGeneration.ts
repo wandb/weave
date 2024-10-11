94245c5eb4731f384696b169e60bbf783dc73404
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-10 23:12:24 -0400

    ts-ify examples

---
+++ sdks/node/examples/imageGeneration.ts
@@ -0,0 +1,19 @@
+import OpenAI from 'openai';
+import { init, wrapOpenAI } from 'weave';
+
+async function main() {
+  const client = await init('weavejs-img');
+  const openai = wrapOpenAI(new OpenAI());
+
+  // Generate an image
+  const result = await openai.images.generate({
+    prompt: 'A cute baby sea otter',
+    n: 3,
+    size: '256x256',
+    response_format: 'b64_json',
+  });
+
+  console.log('Generated image result:', result);
+}
+
+main();
