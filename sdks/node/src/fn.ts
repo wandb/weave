fcf34c21febb1b4fde4e4cab4004668b95f0f16d
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-14 11:55:21 -0400

    tidy fn

--- sdks/node/src/fn.ts
+++ sdks/node/src/fn.ts
@@ -8,8 +8,11 @@ export interface Fn<I, O> {
   trials: (n: number, input: I) => Promise<O[]>;
   // map: (over: I[]) => O[]
 }
+export type FnInputs<T extends Fn<any, any>> = T extends Fn<infer I, any> ? I : never;
+export type FnOutput<T extends Fn<any, any>> = T extends Fn<any, infer O> ? O : never;
 
-export class BaseFn<I, O> extends WeaveObject implements Fn<I, O> {
+// In python this is currently called `Model`
+export abstract class BaseFn<I, O> extends WeaveObject implements Fn<I, O> {
   constructor({ id, description }: { id?: string; description?: string } = {}) {
     super({ id, description });
     this.trials = boundOp(this, this.trials, {
@@ -22,14 +25,6 @@ export class BaseFn<I, O> extends WeaveObject implements Fn<I, O> {
     return this._baseParameters.description ?? '';
   }
 
-  async invoke(input: I): Promise<O> {
-    throw new Error('Method not implemented.');
-  }
-  async trials(n: number, input: I): Promise<O[]> {
-    throw new Error('Method not implemented.');
-  }
+  abstract invoke(input: I): Promise<O>;
+  abstract trials(n: number, input: I): Promise<O[]>;
 }
-
-export type FnInputs<T extends Fn<any, any>> = T extends Fn<infer I, any> ? I : never;
-
-export type FnOutput<T extends Fn<any, any>> = T extends Fn<any, infer O> ? O : never;
