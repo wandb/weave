94fbcca4185c310831177278a550b9c75082ae72
Thomas Capelle <tcapelle@pm.me>
2024-10-15 12:14:22 +0200

    add key for tests

--- noxfile.py
+++ noxfile.py
@@ -58,6 +58,11 @@ def tests(session, shard):
             "WEAVE_SERVER_DISABLE_ECOSYSTEM",
         ]
     }
+    # Add the GOOGLE_API_KEY environment variable for the "google" shard
+    if shard == "google_ai_studio":
+        env["GOOGLE_API_KEY"] = session.env.get("GOOGLE_API_KEY")
+
+    
 
     default_test_dirs = [f"integrations/{shard}/"]
     test_dirs_dict = {
