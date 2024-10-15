f1fbc3675d1cacd06b66af95b9aa4cb9c1e11b27
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-14 23:49:29 -0400

    fmt

--- tests/trace/test_custom_objs.py
+++ tests/trace/test_custom_objs.py
@@ -6,9 +6,11 @@ from weave.trace.custom_objs import decode_custom_obj, encode_custom_obj
 def test_decode_custom_obj_known_type(client):
     img = Image.new("RGB", (100, 100))
     encoded = encode_custom_obj(img)
-    
+
     # Even though something is wrong with the deserializer op, we can still decode
-    decoded = decode_custom_obj(encoded["weave_type"], encoded["files"], "weave:///totally/invalid/uri")
-    
+    decoded = decode_custom_obj(
+        encoded["weave_type"], encoded["files"], "weave:///totally/invalid/uri"
+    )
+
     assert isinstance(decoded, Image.Image)
     assert decoded.tobytes() == img.tobytes()
