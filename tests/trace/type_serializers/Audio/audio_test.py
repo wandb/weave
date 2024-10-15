6948f30ea7515b61e846764eea6e4a81aa425f50
gtarpenning <griffin.tarpenning@wandb.com>
2024-10-15 16:26:21 -0700

    tests

--- tests/trace/type_serializers/Audio/audio_test.py
+++ tests/trace/type_serializers/Audio/audio_test.py
@@ -1,8 +1,8 @@
-import weave
-import wave
 import math
 import tempfile
+import wave
 
+import weave
 from weave.trace.weave_client import WeaveClient, get_ref
 
 
@@ -22,7 +22,8 @@ def make_audio_file(filename: str) -> None:
             wav_file.writeframes(bytes(sound_wave(440, 2.5)))
 
 
-def test_audio_publish(client: weave.WeaveClient) -> None:
+def test_audio_publish(client: WeaveClient) -> None:
+    client.project = "test_audio_publish"
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
         make_audio_file(temp_file.name)
         audio = wave.open(temp_file.name, "rb")
@@ -32,3 +33,51 @@ def test_audio_publish(client: weave.WeaveClient) -> None:
         assert ref is not None
         gotten_audio = weave.ref(ref.uri()).get()
         assert audio.readframes(1) == gotten_audio.readframes(1)
+
+
+def test_audio_as_property(client: WeaveClient) -> None:
+    client.project = "test_audio_as_property"
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+        make_audio_file(temp_file.name)
+        audio = wave.open(temp_file.name, "rb")
+        weave.publish(audio)
+
+        ref = get_ref(audio)
+        assert ref is not None
+        gotten_audio = weave.ref(ref.uri()).get()
+        assert audio.readframes(1) == gotten_audio.readframes(1)
+
+
+def test_audio_as_dataset_cell(client: WeaveClient) -> None:
+    client.project = "test_audio_as_dataset_cell"
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+        make_audio_file(temp_file.name)
+        audio = wave.open(temp_file.name, "rb")
+        weave.publish(audio)
+
+        ref = get_ref(audio)
+        assert ref is not None
+
+
+@weave.op
+def audio_as_solo_output(publish_first: bool) -> wave.Wave_read:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+        make_audio_file(temp_file.name)
+        audio = wave.open(temp_file.name, "rb")
+        if publish_first:
+            weave.publish(audio)
+        return audio
+
+
+@weave.op
+def audio_as_input_and_output_part(in_audio: wave.Wave_read) -> dict:
+    return {"out_audio": in_audio}
+
+
+def test_audio_as_call_io(client: WeaveClient) -> None:
+    client.project = "test_audio_as_call_io"
+    non_published_audio = audio_as_solo_output(publish_first=False)
+    audio_dict = audio_as_input_and_output_part(non_published_audio)
+
+    exp_bytes = non_published_audio.readframes(1)
+    assert audio_dict["out_audio"].readframes(1) == exp_bytes
