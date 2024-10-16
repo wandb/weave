30e9d0180b824d625d9719144694f1899a3e3d03
Thomas Capelle <tcapelle@pm.me>
2024-10-16 17:19:49 +0200

    pass API env vars

--- noxfile.py
+++ noxfile.py
@@ -63,6 +63,14 @@ def tests(session, shard):
     # Add the GOOGLE_API_KEY environment variable for the "google" shard
     if shard == "google_ai_studio":
         env["GOOGLE_API_KEY"] = session.env.get("GOOGLE_API_KEY")
+    
+    # we are doing some integration test in test_llm_integrations.py that requires
+    # setting some environment variables for the LLM providers
+    if shard == "scorers":
+        env["GOOGLE_API_KEY"] = session.env.get("GOOGLE_API_KEY")
+        env["ANTHROPIC_API_KEY"] = session.env.get("ANTHROPIC_API_KEY")
+        env["MISTRAL_API_KEY"] = session.env.get("MISTRAL_API_KEY")
+        env["OPENAI_API_KEY"] = session.env.get("OPENAI_API_KEY")
 
     default_test_dirs = [f"integrations/{shard}/"]
     test_dirs_dict = {
