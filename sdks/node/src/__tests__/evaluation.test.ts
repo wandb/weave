2e1810c0db2e595e88b2a4176ddf6e011cf68f83
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-16 00:24:56 -0400

    tests

--- sdks/node/src/__tests__/evaluation.test.ts
+++ sdks/node/src/__tests__/evaluation.test.ts
@@ -1,70 +1,86 @@
-import { Evaluation } from '../evaluation';
 import { Dataset } from '../dataset';
-// import { Op } from "../opType";
+import { Evaluation } from '../evaluation';
 import { op } from '../op';
 
-describe('Evaluation', () => {
-  test('summarizeResults with failed predictions and scorers', async () => {
-    // Mock dataset
-    const mockDataset = new Dataset({
-      rows: [
-        { id: 0, text: 'Example 0' },
-        { id: 1, text: 'Example 1' },
-        { id: 2, text: 'Example 2' },
-        { id: 3, text: 'Example 3' },
-        { id: 4, text: 'Example 4' },
-      ],
-    });
+const createMockDataset = () =>
+  new Dataset({
+    rows: [
+      { id: 0, text: 'Example 0' },
+      { id: 1, text: 'Example 1' },
+      { id: 2, text: 'Example 2' },
+      { id: 3, text: 'Example 3' },
+      { id: 4, text: 'Example 4' },
+    ],
+  });
 
-    // Mock model
-    const mockModel = op(async function mockPrediction({
+const createMockModel = (failable: boolean) => {
+  return op(async function mockPrediction({ datasetRow }: { datasetRow: { id: number; text: string } }) {
+    if (failable && datasetRow.id === 0) throw new Error('Model failed');
+    return `Prediction for ${datasetRow.text}`;
+  });
+};
+
+const createMockScorers = (failable: boolean) => {
+  return [
+    op(async function lengthScorer({
+      datasetRow,
+      modelOutput,
+    }: {
+      datasetRow: { id: number; text: string };
+      modelOutput: string;
+    }) {
+      if (failable && datasetRow.id === 3) throw new Error('Scorer 1 failed');
+      return {
+        explanation: 'length is ' + modelOutput.length,
+        length: modelOutput.length,
+      };
+    }),
+    op(async function inclusionScorer({
+      modelOutput,
       datasetRow,
     }: {
-      datasetRow: {
-        id: number;
-        text: string;
-      };
+      modelOutput: string;
+      datasetRow: { id: number; text: string };
     }) {
-      if (datasetRow.id === 0) throw new Error('Model failed');
-      return `Prediction for ${datasetRow.text}`;
-    });
+      return modelOutput.includes(datasetRow.text);
+    }),
+  ];
+};
 
-    // Mock scorers
-    const mockScorers = [
-      op(async function lengthScorer({
-        datasetRow,
-        modelOutput,
-      }: {
-        datasetRow: { id: number; text: string };
-        modelOutput: string;
-      }) {
-        if (datasetRow.id === 3) throw new Error('Scorer 1 failed');
-        return {
-          explanation: 'length is ' + modelOutput.length,
-          length: modelOutput.length,
-        };
-      }),
-      op(async function inclusionScorer({
-        modelOutput,
-        datasetRow,
-      }: {
-        modelOutput: string;
-        datasetRow: { id: number; text: string };
-      }) {
-        return modelOutput.includes(datasetRow.text);
-      }),
-    ];
+const createMockEvaluation = (failable: boolean) => {
+  return new Evaluation({
+    dataset: createMockDataset(),
+    scorers: createMockScorers(failable),
+  });
+};
 
-    // Create Evaluation instance
-    const evaluation = new Evaluation({
-      dataset: mockDataset,
-      scorers: mockScorers,
-    });
+describe('Evaluation', () => {
+  test('summarizeResults', async () => {
+    const mockEval = createMockEvaluation(false);
+    const mockModel = createMockModel(false);
 
-    // Run evaluation
-    const results = await evaluation.evaluate({ model: mockModel });
+    const results = await mockEval.evaluate({ model: mockModel });
+    const expectedResults = {
+      model_success: { true_count: 5, true_fraction: 1 },
+      inclusionScorer: {
+        true_count: 5,
+        true_fraction: 1,
+      },
+      lengthScorer: {
+        length: {
+          mean: 24,
+        },
+      },
+      model_latency: { mean: expect.any(Number) },
+    };
 
-    // Expected summarized results
+    expect(results).toEqual(expectedResults);
+  });
+  test('summarizeResults with failed predictions and scorers', async () => {
+    const mockEval = createMockEvaluation(true);
+    const mockModel = createMockModel(true);
+
+    const results = await mockEval.evaluate({ model: mockModel });
     const expectedResults = {
       model_success: { true_count: 4, true_fraction: 0.8 },
       inclusionScorer: {
@@ -79,7 +95,6 @@ describe('Evaluation', () => {
       model_latency: { mean: expect.any(Number) },
     };
 
-    // Check results
     expect(results).toEqual(expectedResults);
   });
 });
