9efaf8b0f64973396240e98af7db6ce53c1c4c42
gtarpenning <griffin.tarpenning@wandb.com>
2024-10-16 14:02:22 -0700

    add test

--- tests/trace/type_serializers/Audio/audio_test.py
+++ tests/trace/type_serializers/Audio/audio_test.py
@@ -1,13 +1,14 @@
 import math
 import tempfile
 import wave
+from typing import Optional
 
 import weave
 from weave.trace.weave_client import WeaveClient, get_ref
 
 
-def make_audio_file(filename: str) -> None:
-    FRAMES_PER_SECOND = 44100
+def make_audio_file(filename: str, nframes: Optional[int] = None) -> None:
+    FRAMES_PER_SECOND = 24_000
 
     def sound_wave(frequency, num_seconds):
         for frame in range(round(num_seconds * FRAMES_PER_SECOND)):
@@ -17,8 +18,10 @@ def make_audio_file(filename: str) -> None:
 
     with wave.open(filename, mode="wb") as wav_file:
         wav_file.setnchannels(1)
-        wav_file.setsampwidth(1)
+        wav_file.setsampwidth(2)
         wav_file.setframerate(FRAMES_PER_SECOND)
+        if nframes is not None:
+            wav_file.setnframes(nframes)
         wav_file.writeframes(bytes(sound_wave(440, 2.5)))
 
 
@@ -87,3 +90,27 @@ def test_audio_as_call_io(client: WeaveClient) -> None:
 
         assert input_output_part_call.inputs["in_audio"].readframes(5) == exp_bytes
         assert input_output_part_call.output["out_audio"].readframes(5) == exp_bytes
+
+
+def test_audio_with_max_nframes(client: WeaveClient) -> None:
+    client.project = "test_audio_with_max_nframes"
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+        two_gb_nframes = (1 * 1024 * 1024 * 1024) - 1
+        make_audio_file(temp_file.name, nframes=two_gb_nframes)
+        audio = wave.open(temp_file.name, "rb")
+        weave.publish(audio)
+
+        ref = get_ref(audio)
+        assert ref is not None
+        gotten_audio = weave.ref(ref.uri()).get()
+        assert audio.readframes(10) == gotten_audio.readframes(10)
+
+        # now with 0 nframes
+        make_audio_file(temp_file.name, nframes=0)
+        audio = wave.open(temp_file.name, "rb")
+        weave.publish(audio)
+
+        ref = get_ref(audio)
+        assert ref is not None
+        gotten_audio = weave.ref(ref.uri()).get()
+        assert audio.readframes(10) == gotten_audio.readframes(10)
