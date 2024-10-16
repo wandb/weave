3ce2a2a8ce2ec81dbd35d8a01f896ea2b0b822c3
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-15 22:03:35 -0400

    tests

--- tests/integrations/google_ai_studio/google_ai_studio_test.py
+++ tests/integrations/google_ai_studio/google_ai_studio_test.py
@@ -5,11 +5,37 @@ import pytest
 from weave.integrations.integration_utilities import op_name_from_ref
 
 
+# NOTE: These asserts are slightly more relaxed than other integrations because we can't yet save
+# the output with vcrpy.  When VCR.py supports GRPC, we should add recordings for these tests!
+def assert_correct_output_shape(output: dict):
+    assert "candidates" in output
+    assert isinstance(output["candidates"], list)
+    for candidate in output["candidates"]:
+        assert isinstance(parts := candidate["content"]["parts"], list)
+        for part in parts:
+            assert isinstance(part["text"], str)
+
+        # https://cloud.google.com/vertex-ai/generative-ai/docs/reference/python/latest/vertexai.preview.generative_models.FinishReason
+        # 0 is FINISH_REASON_UNSPECIFIED
+        # 1 is STOP
+        assert candidate["finish_reason"] in (0, 1)
+        assert isinstance(candidate["index"], int)
+        assert isinstance(candidate["safety_ratings"], list)
+        assert isinstance(candidate["token_count"], int)
+        assert isinstance(candidate["grounding_attributions"], list)
+        assert isinstance(candidate["avg_logprobs"], float)
+    assert isinstance(output["usage_metadata"], dict)
+    assert isinstance(output["usage_metadata"]["prompt_token_count"], int)
+    assert isinstance(output["usage_metadata"]["candidates_token_count"], int)
+    assert isinstance(output["usage_metadata"]["total_token_count"], int)
+    assert isinstance(output["usage_metadata"]["cached_content_token_count"], int)
+
+
 @pytest.mark.skip_clickhouse_client
 def test_content_generation(client):
     import google.generativeai as genai
 
-    genai.configure(api_key=os.environ.get("GOOGLE_GENAI_KEY"))
+    genai.configure(api_key=os.getenv("GOOGLE_GENAI_KEY"))
     model = genai.GenerativeModel("gemini-1.5-flash")
     model.generate_content("Write a story about an AI and magic")
 
@@ -24,13 +50,14 @@ def test_content_generation(client):
     )
     output = call.output
     assert output is not None
+    assert_correct_output_shape(output)
 
 
 @pytest.mark.skip_clickhouse_client
 def test_content_generation_stream(client):
     import google.generativeai as genai
 
-    genai.configure(api_key=os.environ.get("GOOGLE_GENAI_KEY"))
+    genai.configure(api_key=os.getenv("GOOGLE_GENAI_KEY"))
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(
         "Write a story about an AI and magic", stream=True
@@ -49,6 +76,7 @@ def test_content_generation_stream(client):
     )
     output = call.output
     assert output is not None
+    assert_correct_output_shape(output)
 
 
 @pytest.mark.asyncio
@@ -56,7 +84,7 @@ def test_content_generation_stream(client):
 async def test_content_generation_async(client):
     import google.generativeai as genai
 
-    genai.configure(api_key=os.environ.get("GOOGLE_GENAI_KEY"))
+    genai.configure(api_key=os.getenv("GOOGLE_GENAI_KEY"))
     model = genai.GenerativeModel("gemini-1.5-flash")
 
     _ = await model.generate_content_async("Write a story about an AI and magic")
@@ -72,3 +100,4 @@ async def test_content_generation_async(client):
     )
     output = call.output
     assert output is not None
+    assert_correct_output_shape(output)
