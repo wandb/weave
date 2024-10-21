d1e748c24d6ae5b3514f4dd000d8147e093a73c4
Thomas Capelle <tcapelle@pm.me>
2024-10-21 10:54:01 +0200

    warn new scorers path

--- weave/flow/scorer.py
+++ weave/flow/scorer.py
@@ -1,3 +1,5 @@
 # Keeping this file for now to avoid breaking changes.
 # In future, users should import all scoring functionality from weave.scorers
 from weave.scorers import *
+
+raise Warning("This file is deprecated. Please import all scoring functionality from weave.scorers")
