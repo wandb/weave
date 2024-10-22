652ae756260f7ff20a785eb82fb089f85d917162
Morgan McGuire <morganmcg1@users.noreply.github.com>
2024-10-22 10:09:37 +0100

    add deprecation warning to flow/scorer.py

--- weave/flow/scorer.py
+++ weave/flow/scorer.py
@@ -1,7 +1,12 @@
 # Keeping this file for now to avoid breaking changes.
 # In future, users should import all scoring functionality from weave.scorers
+import warnings
+
 from weave.scorers import *
 
-raise Warning(
-    "This file is deprecated. Please import all scoring functionality from weave.scorers"
+warnings.warn(
+    "Importing from weave.flow.scorer is deprecated. "
+    "Please import from weave.scorers in the future.",
+    DeprecationWarning,
+    stacklevel=2,
 )
