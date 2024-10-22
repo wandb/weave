4cee957849d38851698e7854ded1dfa7b1e99b04
Tim Sweeney <tim@wandb.com>
2024-10-21 18:00:14 -0700

    init

---
+++ tests/trace/test_actions.py
@@ -0,0 +1,13 @@
+
+from weave.trace.weave_client import WeaveClient
+
+
+def test_action_flow(client: WeaveClient):
+    # 1. Bootstrap builtin actions
+    # 2. Query Available Actions
+    # Run an op
+    # 3. Create a 1-off batch action using mapping
+    # 4. Create an online trigger
+    # Run more ops
+    # 5. Query back the feedback results.
+    pass
