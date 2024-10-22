1510f63efcec36d2d51f1609885c0f9ab546eeb8
harukatab <haruka@wandb.com>
2024-10-22 00:17:29 -0700

    font color for manage columns popup

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
@@ -179,6 +179,8 @@ const CallPageInnerVertical: FC<{
     callId: selectedCall.callId,
   });
 
+  console.log({rows, call, costLoading, selectedCall});
+
   const assumeCallIsSelectedCall = path == null || path === '';
   const [currentCall, setCurrentCall] = useState(call);
 
