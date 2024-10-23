af30efb87fcac28a46417e91251dc7b0b77b7170
Marie Barr-Ramsey <marie.barrramsey@wandb.com>
2024-10-23 13:28:40 -0700

    chart working

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/Charts.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/Charts.tsx
@@ -35,13 +35,27 @@ type GradientAreaChartProps = {
 export const GradientAreaChart = ({
   costAndTimeData,
 }: GradientAreaChartProps) => {
-  const uniqueMonths = Array.from(
-    new Set(costAndTimeData.map(d => moment(d.timestamp).format('YYYY-MM')))
-  );
   const formattedData = costAndTimeData.map(d => ({
     ...d,
     timestamp: moment(d.timestamp).valueOf(), // Convert to milliseconds
   }));
+
+  // Generate an array of unique monthly ticks
+  const startMonth = moment(formattedData[0]?.timestamp).startOf('month');
+  const endMonth = moment(
+    formattedData[formattedData.length - 1]?.timestamp
+  ).endOf('month');
+  const monthlyTicks = [];
+
+  let currentMonth = startMonth.clone();
+  while (currentMonth <= endMonth) {
+    monthlyTicks.push(currentMonth.valueOf());
+    currentMonth.add(1, 'month');
+  }
+
+  const startTimestamp = formattedData[0]?.timestamp;
+  const endTimestamp = formattedData[formattedData.length - 1]?.timestamp;
+
   return (
     <ResponsiveContainer width="100%" height={400}>
       <AreaChart
@@ -56,16 +70,18 @@ export const GradientAreaChart = ({
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis
           dataKey="timestamp"
-          //   scale="time"
           type="number"
-          domain={['auto', 'auto']}
+          //   scale='point'
+          //   domain={['auto', 'auto']}
+          domain={['dataMin', 'dataMax']} // Use 'dataMin' and 'dataMax' for exact fit
+          ticks={monthlyTicks} // Use generated monthly ticks
           tickFormatter={tick => moment(tick).format('MMM YYYY')}
-          interval="preserveStartEnd"
         />
 
         <YAxis />
         <Tooltip
           labelFormatter={label => moment(label).format('MMM DD, YYYY')} // Format tooltip
+          cursor={{stroke: TEAL_400, strokeWidth: 2}} // Set crosshair color and thickness
         />
         <Area
           type="monotone"
