35f79fba3fb09eebd557911c0db9847171239040
Andrew Levin <andrew.levin@wandb.com>
2024-10-24 15:29:12 -0700

    cleanup

--- weave-js/src/components/PagePanelComponents/Home/Browse2/SmallRef.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse2/SmallRef.tsx
@@ -84,32 +84,40 @@ export const objectRefDisplayName = (
 
 export const SmallArtifactRef: FC<{
   objRef: WandbArtifactRef;
-  wfTable?: WFDBTableType;
   iconOnly?: boolean;
-}> = ({objRef , wfTable, iconOnly = false}) => {
-  const {
-    useObjectVersion,
-    derived: {useRefsType},
-  } = useWFHooks();
+}> = ({objRef }) => {
+  const Item = (
+      <Box display="flex" alignItems="center">
+        <Box
+            mr="4px"
+            bgcolor={hexToRGB(MOON_300, 0.48)}
+            sx={{
+              height: '22px',
+              width: '22px',
+              borderRadius: '16px',
+              display: 'flex',
+              flex: '0 0 22px',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+          <Icon name={IconNames.OpenNewTab} width={14} height={14} />
+        </Box>
+        <Box
+            sx={{
+              height: '22px',
+              flex: 1,
+              minWidth: 0,
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+            }}>
+          {objRef.artifactName}:{objRef.artifactVersion}
+        </Box>
+      </Box>
+  );
 
-  // TODO check if artifact present before creating possible link to it
-
-  // let objVersionKey: ObjectVersionKey | null = null;
-
-  // call out the use of this as a link back to model registry
-  // objVersionKey = {
-  //   scheme: 'wandb-artifact',
-  //   entity: objRef.entityName,
-  //   project: objRef.projectName,
-  //   objectId: objRef.artifactName,
-  //   versionHash: objRef.artifactVersion,
-  //   path: objRef.artifactPath,
-  //   refExtra: objRef.artifactRefExtra,
-  // };
-
-  // const apolloClient = useApolloClient();
-  //
-  // // VIEWER_QUERY for inspo
+  // TODO lookup the artifact by name (where name is name:version) so you can verify it's a good link
+    // AND check that 'model' is the right type
   // const ARTIFACT_BY_NAME_QUERY = gql`
   //   query ArtifactByName(
   //     $entityName: String!,
@@ -129,42 +137,6 @@ export const SmallArtifactRef: FC<{
   //     }
   //   }
   // `;
-  //
-  // apolloClient.query({query: ARTIFACT_BY_NAME_QUERY as any}).then(result => {
-  //   const info = result.data.viewer;
-  // });
-
-  const Item = (
-      <Box display="flex" alignItems="center">
-        <Box
-            mr="4px"
-            bgcolor={hexToRGB(MOON_300, 0.48)}
-            sx={{
-              height: '22px',
-              width: '22px',
-              borderRadius: '16px',
-              display: 'flex',
-              flex: '0 0 22px',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-          <Icon name={IconNames.OpenNewTab} width={14} height={14} />
-        </Box>
-        {!iconOnly && (
-            <Box
-                sx={{
-                  height: '22px',
-                  flex: 1,
-                  minWidth: 0,
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                  textOverflow: 'ellipsis',
-                }}>
-              {objRef.artifactName}:{objRef.artifactVersion}
-            </Box>
-        )}
-      </Box>
-  );
 
   return (
   <Link
@@ -176,7 +148,7 @@ export const SmallArtifactRef: FC<{
             alignItems: 'center',
           }}
       as="a"
-      href={`${window.location.origin}/${objRef.entityName}/${objRef.projectName}/artifacts/model/${objRef.artifactName}/${objRef.artifactVersion}/overview`}
+      href={`${window.location.origin}/${objRef.entityName}/${objRef.projectName}/artifacts/model/${objRef.artifactName}/${objRef.artifactVersion}`}
       target="_blank" rel="noopener noreferrer"
     >
     {Item}
