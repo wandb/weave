3b4a0793fed490390688db3da6f21ad502aaca3f
Peyton Sarmiento <peytonsarmiento@Peyton-Sarmiento-H4QDYT9T4Y.local>
2024-10-23 17:24:15 -0700

    maybe use decorators

--- weave/trace_server/llm_completion.py
+++ weave/trace_server/llm_completion.py
@@ -2,14 +2,19 @@ import litellm
 from typing import Any, List, Dict
 import json
 from weave.trace_server import trace_server_interface as tsi
-        
+from weave.trace import weave_init as w
+import weave_query as weave
+      
 
-def call_llm(api_key: str, model_name: str, messages: List[Dict[str, Any]]) -> tsi.CallsLLMRes:
-    litellm.api_key = api_key
-    try:
+def call_llm(project_id: str, api_key: str, model_name: str, messages: List[Dict[str, Any]]) -> tsi.CallsLLMRes:
+
+    w.init_weave(project_id)
+
+    @weave.op()
+    def llm_complete(api_key: str, model_name: str, messages: List[Dict[str, Any]]) -> tsi.CallsLLMRes:
+        litellm.api_key = api_key
         res = litellm.completion(messages=messages, model=model_name)
         return tsi.CallsLLMRes(response=res.model_dump())
-    except Exception as e:
-        return tsi.CallsLLMRes(response={"error": e.message})
     
+    return llm_complete(api_key, model_name, messages)
         
