828af234475cc8372c5fa020fe7af67d34cfa953
Andrew Levin <andrew.levin@wandb.com>
2024-10-25 09:06:02 -0700

    prettier

--- weave-js/src/components/PagePanelComponents/Home/Browse2/SmallRef.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse2/SmallRef.tsx
@@ -79,38 +79,36 @@ export const objectRefDisplayName = (
 };
 
 export const SmallRefBox: FC<{
-    iconName: IconName;
-    text: string;
-}> = ({ iconName, text }) => (
-    <Box display="flex" alignItems="center">
-        <Box
-            mr="4px"
-            bgcolor={hexToRGB(MOON_300, 0.48)}
-            sx={{
-                height: '22px',
-                width: '22px',
-                borderRadius: '16px',
-                display: 'flex',
-                flex: '0 0 22px',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <Icon name={iconName} width={14} height={14} />
-        </Box>
-        <Box
-            sx={{
-                height: '22px',
-                flex: 1,
-                minWidth: 0,
-                overflow: 'hidden',
-                whiteSpace: 'nowrap',
-                textOverflow: 'ellipsis',
-            }}
-        >
-            {text}
-        </Box>
+  iconName: IconName;
+  text: string;
+}> = ({iconName, text}) => (
+  <Box display="flex" alignItems="center">
+    <Box
+      mr="4px"
+      bgcolor={hexToRGB(MOON_300, 0.48)}
+      sx={{
+        height: '22px',
+        width: '22px',
+        borderRadius: '16px',
+        display: 'flex',
+        flex: '0 0 22px',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Icon name={iconName} width={14} height={14} />
     </Box>
+    <Box
+      sx={{
+        height: '22px',
+        flex: 1,
+        minWidth: 0,
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+      }}>
+      {text}
+    </Box>
+  </Box>
 );
 
 export const SmallArtifactRef: FC<{
@@ -141,7 +139,6 @@ export const SmallArtifactRef: FC<{
   `;
   */
 
-
   return (
     <Link
       $variant="secondary"
@@ -155,7 +152,10 @@ export const SmallArtifactRef: FC<{
       href={`${window.location.origin}/${objRef.entityName}/${objRef.projectName}/artifacts/model/${objRef.artifactName}/${objRef.artifactVersion}`}
       target="_blank"
       rel="noopener noreferrer">
-      <SmallRefBox iconName={IconNames.OpenNewTab} text={`${objRef.artifactName}:${objRef.artifactVersion}`} />
+      <SmallRefBox
+        iconName={IconNames.OpenNewTab}
+        text={`${objRef.artifactName}:${objRef.artifactVersion}`}
+      />
     </Link>
   );
 };
@@ -222,9 +222,7 @@ export const SmallWeaveRef: FC<{
   } else if (rootTypeName === 'OpDef') {
     icon = IconNames.JobProgramCode;
   }
-  const Item = (
-      <SmallRefBox iconName={icon} text={label} />
-  );
+  const Item = <SmallRefBox iconName={icon} text={label} />;
 
   if (refTypeQuery.loading) {
     return Item;
