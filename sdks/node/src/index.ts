52797d849f1bb8161ead1a0726e7d4c4781a6123
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-25 12:35:01 -0700

    add imports

--- sdks/node/src/index.ts
+++ sdks/node/src/index.ts
@@ -1,6 +1,7 @@
-export { init, login, requireCurrentChildSummary } from './clientApi';
+export { init, login, requireCurrentCallStackEntry, requireCurrentChildSummary } from './clientApi';
 export { Dataset } from './dataset';
 export { Evaluation } from './evaluation';
+export { CallSchema, CallsFilter } from './generated/traceServerApi';
 export { wrapOpenAI } from './integrations';
 export { weaveAudio, weaveImage } from './media';
 export { op } from './op';
