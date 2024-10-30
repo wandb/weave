9be50667e6df83156ed6dfc5e70b24b54d6b2d14
gtarpenning <griffin.tarpenning@wandb.com>
2024-10-29 17:22:02 -0700

    use schema

--- tests/trace/test_human_feedback.py
+++ tests/trace/test_human_feedback.py
@@ -1,45 +1,55 @@
-from weave.flow.human_feedback import (
-    BinaryFeedback,
-    CategoricalFeedback,
-    HumanFeedback,
-    NumericalFeedback,
-    TextFeedback,
-)
+from weave.flow.human_feedback import HumanAnnotationColumn
 from weave.trace_server.trace_server_interface import ObjQueryReq
 
 
 def test_human_feedback_basic(client):
     # create a human feedback spec
 
-    binary_feedback = BinaryFeedback(display_name="my binary")
-    text_feedback = TextFeedback(display_name="my text", max_length=100)
-    categorical_feedback = CategoricalFeedback(
-        display_name="my categorical", options=["option1", "option2"]
+    col1 = HumanAnnotationColumn(
+        name="Numerical field #1",
+        description="A numerical field with a range of -1 to 1",
+        json_schema={
+            "type": "number",
+            "min": -1,
+            "max": 1,
+        },
+        unique_among_creators=True,
+        op_scope=None,
     )
-    numerical_feedback = NumericalFeedback(display_name="my numerical", min=0, max=100)
+    ref1 = client.save(col1, "my numerical spec")
+    assert ref1
 
-    human_feedback = HumanFeedback(
-        feedback_fields=[
-            binary_feedback,
-            text_feedback,
-            categorical_feedback,
-            numerical_feedback,
-        ]
+    col2 = HumanAnnotationColumn(
+        name="Text field #1",
+        json_schema={"type": "string", "maxLength": 100},
+        op_scope=["fake-op"],
     )
-
-    ref = client.save(human_feedback, "my spec")
-    assert ref
+    ref2 = client.save(col2, "my text spec")
+    assert ref2
 
     # query it by object type
     objects = client.server.objs_query(
         ObjQueryReq.model_validate(
             {
                 "project_id": client._project_id(),
-                "filter": {"base_object_classes": ["HumanFeedback"]},
+                "filter": {"base_object_classes": ["HumanAnnotationColumn"]},
             }
         )
     )
 
-    assert len(objects.objs) == 1
-    spec = objects.objs[0]
-    assert len(spec.val["feedback_fields"]) == 4
+    assert len(objects.objs) == 2
+    assert objects.objs[0].val["name"] == "Numerical field #1"
+    assert objects.objs[1].val["name"] == "Text field #1"
+    assert (
+        objects.objs[0].val["description"]
+        == "A numerical field with a range of -1 to 1"
+    )
+    assert not objects.objs[1].val["description"]
+    assert not objects.objs[0].val["op_scope"]
+    assert objects.objs[1].val["op_scope"] == ["fake-op"]
+    assert objects.objs[0].val["json_schema"] == {
+        "type": "number",
+        "min": -1,
+        "max": 1,
+    }
+    assert objects.objs[1].val["json_schema"] == {"type": "string", "maxLength": 100}
--- weave/flow/human_feedback.py
+++ weave/flow/human_feedback.py
@@ -1,35 +1,16 @@
 from typing import Optional
 
-from pydantic import BaseModel
-
 from weave.flow.obj import Object
 
 
-class FeedbackType(BaseModel):
-    display_name: str
+class HumanAnnotationColumn(Object):
+    json_schema: dict
 
     # If true, all unique creators will have their
     # own value for this feedback type. Otherwise,
     # by default, the value is shared and can be edited.
     unique_among_creators: bool = False
 
-
-class HumanFeedback(Object):
-    feedback_fields: list[FeedbackType]
-
-
-class BinaryFeedback(FeedbackType):
-    pass
-
-
-class CategoricalFeedback(FeedbackType):
-    options: list[str]
-
-
-class NumericalFeedback(FeedbackType):
-    min: Optional[float] = None
-    max: Optional[float] = None
-
-
-class TextFeedback(FeedbackType):
-    max_length: Optional[int] = None
+    # If provided, this feedback type will only be shown
+    # when a call is generated from the given op
+    op_scope: Optional[list[str]] = None
