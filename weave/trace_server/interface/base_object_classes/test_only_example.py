45c1756e553ca54f2afa1b4f2abb868f8302bbe5
Tim Sweeney <tim@wandb.com>
2024-10-31 03:08:48 -0600

    clean

--- weave/trace_server/interface/base_object_classes/test_only_example.py
+++ weave/trace_server/interface/base_object_classes/test_only_example.py
@@ -17,6 +17,9 @@ class TestOnlyExample(base_object_def.BaseObject):
     # Important: `RefStr` is just an alias for `str`. When defining `BaseObject`s, we
     # should never have a property point to another `BaseObject`. This is because each
     # base object is stored in the database and should be treated like a foreign key.
+    #
+    # It would be nice to have a way to ensure that no `BaseObject` has any `BaseObject`
+    # properties.
     nested_base_object: base_object_def.RefStr
 
 
