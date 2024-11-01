4ac791addc3ba9a51d1de5d95d39a8490d8f1943
Andrew Truong <itsandrewtruong@gmail.com>
2024-10-31 22:13:11 -0400

    add sample test

---
+++ sdks/node/src/__tests__/playwright/jsLogging.spec.ts
@@ -0,0 +1,14 @@
+import {expect, test} from '@playwright/test';
+
+test('defaults to JS page', async ({page}) => {
+  test.setTimeout(5000);
+
+  // TODO: We should really be calling the API here instead of going to this static link
+  await page.goto(
+    'https://wandb.ai/megatruong/examples/weave/traces?peekPath=/megatruong/examples/calls/0192c64a-253e-7604-867f-ed23a23ab14f'
+  );
+
+  // Nav to the "use" tab and confirm it's on the TypeScript page by default (not Python)
+  await page.getByRole('tab', {name: 'use'}).click();
+  await expect(page.getByText(`import * as weave from 'weave';`)).toBeVisible();
+});
