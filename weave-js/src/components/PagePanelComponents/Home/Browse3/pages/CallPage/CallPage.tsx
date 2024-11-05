abe4fea53eed31c61686d747a0f71435c91f957e
Griffin Tarpenning <griffin.tarpenning@wandb.com>
2024-11-05 08:52:25 -0800

    feat(ui): navigate up/down on the calls page from the peek drawer (#2798)

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallPage/CallPage.tsx
@@ -1,14 +1,19 @@
 import Box from '@mui/material/Box';
 import {Loading} from '@wandb/weave/components/Loading';
 import {useViewTraceEvent} from '@wandb/weave/integrations/analytics/useViewEvents';
-import React, {FC, useCallback, useEffect, useState} from 'react';
+import React, {FC, useCallback, useContext, useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom';
 
 import {makeRefCall} from '../../../../../../util/refs';
 import {Button} from '../../../../../Button';
 import {Tailwind} from '../../../../../Tailwind';
 import {Browse2OpDefCode} from '../../../Browse2/Browse2OpDefCode';
-import {TRACETREE_PARAM, useWeaveflowCurrentRouteContext} from '../../context';
+import {TableRowSelectionContext} from '../../../Browse3';
+import {
+  TRACETREE_PARAM,
+  useWeaveflowCurrentRouteContext,
+  WeaveflowPeekContext,
+} from '../../context';
 import {FeedbackGrid} from '../../feedback/FeedbackGrid';
 import {NotFoundPanel} from '../../NotFoundPanel';
 import {isCallChat} from '../ChatView/hooks';
@@ -196,6 +201,66 @@ const CallPageInnerVertical: FC<{
     }
   }, [callComplete]);
 
+  // Call navigation by arrow keys and buttons
+  const {getNextRowId, getPreviousRowId, rowIdsConfigured} = useContext(
+    TableRowSelectionContext
+  );
+  const {isPeeking} = useContext(WeaveflowPeekContext);
+  const showPaginationContols = isPeeking && rowIdsConfigured;
+  const onNextCall = useCallback(() => {
+    const nextCallId = getNextRowId?.(currentCall.callId);
+    if (nextCallId) {
+      history.replace(
+        currentRouter.callUIUrl(
+          currentCall.entity,
+          currentCall.project,
+          currentCall.traceId,
+          nextCallId,
+          path,
+          showTraceTree
+        )
+      );
+    }
+  }, [currentCall, currentRouter, history, path, showTraceTree, getNextRowId]);
+  const onPreviousCall = useCallback(() => {
+    const previousRowId = getPreviousRowId?.(currentCall.callId);
+    if (previousRowId) {
+      history.replace(
+        currentRouter.callUIUrl(
+          currentCall.entity,
+          currentCall.project,
+          currentCall.traceId,
+          previousRowId,
+          path,
+          showTraceTree
+        )
+      );
+    }
+  }, [
+    currentCall,
+    currentRouter,
+    history,
+    path,
+    showTraceTree,
+    getPreviousRowId,
+  ]);
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'ArrowDown' && event.shiftKey) {
+        onNextCall();
+      } else if (event.key === 'ArrowUp' && event.shiftKey) {
+        onPreviousCall();
+      }
+    },
+    [onNextCall, onPreviousCall]
+  );
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
   const callTabs = useCallTabs(currentCall);
 
   if (loading && !assumeCallIsSelectedCall) {
@@ -205,14 +270,39 @@ const CallPageInnerVertical: FC<{
   return (
     <SimplePageLayoutWithHeader
       headerExtra={
-        <Box>
-          <Button
-            icon="layout-tabs"
-            tooltip={`${showTraceTree ? 'Hide' : 'Show'} trace tree`}
-            variant="ghost"
-            active={showTraceTree ?? false}
-            onClick={onToggleTraceTree}
-          />
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          {showPaginationContols && (
+            <Box>
+              <Button
+                icon="sort-ascending"
+                tooltip="Previous call. (Shift + Arrow Up)"
+                variant="ghost"
+                onClick={onPreviousCall}
+                className="mr-2"
+              />
+              <Button
+                icon="sort-descending"
+                tooltip="Next call. (Shift + Arrow Down)"
+                variant="ghost"
+                onClick={onNextCall}
+              />
+            </Box>
+          )}
+          <Box sx={{marginLeft: showPaginationContols ? 0 : 'auto'}}>
+            <Button
+              icon="layout-tabs"
+              tooltip={`${showTraceTree ? 'Hide' : 'Show'} trace tree`}
+              variant="ghost"
+              active={showTraceTree ?? false}
+              onClick={onToggleTraceTree}
+            />
+          </Box>
         </Box>
       }
       isSidebarOpen={showTraceTree}
