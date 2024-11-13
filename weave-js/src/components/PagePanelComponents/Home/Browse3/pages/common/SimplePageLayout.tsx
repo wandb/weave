0dbe7f03b7668daa2f619209a4c62408fdf7934c
Tim Sweeney <tim@wandb.com>
2024-11-12 18:36:09 -0800

    added line

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/SimplePageLayout.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/SimplePageLayout.tsx
@@ -272,21 +272,23 @@ export const SimplePageLayoutWithHeader: FC<{
                 {props.headerContent}
               </Box>
               {(!props.hideTabsIfSingle || tabs.length > 1) && (
-                <Tabs.Root
-                  style={{margin: '12px 16px 0 16px'}}
-                  value={tabs[tabValue].label}
-                  onValueChange={handleTabChange}>
-                  <Tabs.List>
-                    {tabs.map(tab => (
-                      <Tabs.Trigger
-                        key={tab.label}
-                        value={tab.label}
-                        className="h-[30px] text-sm">
-                        {tab.label}
-                      </Tabs.Trigger>
-                    ))}
-                  </Tabs.List>
-                </Tabs.Root>
+                <Box sx={{borderBottom: '1px solid #e0e0e0'}}>
+                  <Tabs.Root
+                    style={{padding: '12px 16px 0 16px'}}
+                    value={tabs[tabValue].label}
+                    onValueChange={handleTabChange}>
+                    <Tabs.List style={{borderBottom: 'none'}}>
+                      {tabs.map(tab => (
+                        <Tabs.Trigger
+                          key={tab.label}
+                          value={tab.label}
+                          className="h-[30px] text-sm">
+                          {tab.label}
+                        </Tabs.Trigger>
+                      ))}
+                    </Tabs.List>
+                  </Tabs.Root>
+                </Box>
               )}
               <Box
                 sx={{
