d402062f02cabd36474c301506b6288ca7211c41
Tim Sweeney <tim@wandb.com>
2024-11-14 14:57:16 -0800

    fixed

---
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/ScorersPage/ActionScorerForm.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+import {ScorerFormProps} from './ScorerForms';
+
+export const ActionScorerForm: FC<ScorerFormProps<any>> = ({
+  data,
+  onDataChange,
+}) => {
+  return <div>Action Scorer Form</div>;
+};
