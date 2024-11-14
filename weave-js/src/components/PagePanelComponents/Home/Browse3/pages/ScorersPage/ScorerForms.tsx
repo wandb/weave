0afb6a6e2920a3f4a377f2622ad3a040a42ecd64
Tim Sweeney <tim@wandb.com>
2024-11-14 10:23:16 -0800

    init

---
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/ScorersPage/ScorerForms.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+
+import {ScorerFormProps} from './NewScorerDrawer';
+
+export const AnnotationScorerForm: FC<ScorerFormProps> = ({onDataChange}) => {
+  // Implementation for annotation scorer form
+  return <div>Annotation Scorer Form</div>;
+};
+
+export const ActionScorerForm: FC<ScorerFormProps> = ({onDataChange}) => {
+  // Implementation for action scorer form
+  return <div>Action Scorer Form</div>;
+};
+
+export const ProgrammaticScorerForm: FC<ScorerFormProps> = ({onDataChange}) => {
+  // Implementation for programmatic scorer form
+  return <div>Programmatic Scorer Form</div>;
+};
