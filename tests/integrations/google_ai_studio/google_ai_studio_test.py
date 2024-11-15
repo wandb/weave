f8826d122daeb8fd3dcb15bb179949b9da5be298
Andrew Truong <itsandrewtruong@gmail.com>
2024-11-15 14:09:52 -0500

    test

--- tests/integrations/google_ai_studio/google_ai_studio_test.py
+++ tests/integrations/google_ai_studio/google_ai_studio_test.py
@@ -47,6 +47,7 @@ def assert_correct_summary(summary: dict, trace_name: str):
     assert summary["weave"]["latency_ms"] > 0
 
 
+@pytest.mark.skip
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
 @pytest.mark.skip_clickhouse_client
 def test_content_generation(client):
@@ -69,6 +70,7 @@ def test_content_generation(client):
     assert_correct_summary(call.summary, trace_name)
 
 
+@pytest.mark.skip
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
 @pytest.mark.skip_clickhouse_client
 def test_content_generation_stream(client):
@@ -95,6 +97,7 @@ def test_content_generation_stream(client):
     assert_correct_summary(call.summary, trace_name)
 
 
+@pytest.mark.skip
 @pytest.mark.flaky(reruns=5, reruns_delay=2)
 @pytest.mark.asyncio
 @pytest.mark.skip_clickhouse_client
