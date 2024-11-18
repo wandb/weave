b2f087617c949f359e28b3e5cf3c3a78fc88c49d
Josiah Lee <josiahwlee@gmail.com>
2024-11-18 10:28:30 -0800

    style(weave): chatview styling (#2977)
    * add playground context
    
    * clean up
    
    * types
    
    * style try
    
    * style audit
    
    * pr comments

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/ChatView/MessagePanel.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/ChatView/MessagePanel.tsx
@@ -1,3 +1,4 @@
+import {Callout} from '@wandb/weave/components/Callout';
 import classNames from 'classnames';
 import _ from 'lodash';
 import React from 'react';
@@ -7,34 +8,96 @@ import {ToolCalls} from './ToolCalls';
 import {Message} from './types';
 
 type MessagePanelProps = {
+  index: number;
   message: Message;
   isStructuredOutput?: boolean;
+  isNested?: boolean;
 };
 
 export const MessagePanel = ({
   message,
   isStructuredOutput,
+  isNested,
 }: MessagePanelProps) => {
   const isUser = message.role === 'user';
-  const bg = isUser ? 'bg-cactus-300/[0.48]' : 'bg-moon-100';
+  const isSystemPrompt = message.role === 'system';
+  const isTool = message.role === 'tool';
+  const hasToolCalls =
+    message.tool_calls != null && message.tool_calls.length > 0;
+  const hasContent = message.content != null && message.content.length > 0;
+
   return (
-    <div className={classNames('rounded-[8px] px-16 py-8', bg)}>
-      <div style={{fontVariantCaps: 'all-small-caps'}}>{message.role}</div>
-      {message.content && (
-        <div>
-          {_.isString(message.content) ? (
-            <MessagePanelPart
-              value={message.content}
-              isStructuredOutput={isStructuredOutput}
+    <div className={classNames('flex gap-8', {'mt-24': !isTool})}>
+      {!isNested && !isSystemPrompt && (
+        <div className="w-32">
+          {!isUser && !isTool && (
+            <Callout
+              size="small"
+              icon="robot-service-member"
+              color="moon"
+              className="h-32 w-32"
             />
-          ) : (
-            message.content.map((p, i) => (
-              <MessagePanelPart key={i} value={p} />
-            ))
           )}
         </div>
       )}
-      {message.tool_calls && <ToolCalls toolCalls={message.tool_calls} />}
+
+      <div
+        className={classNames('relative overflow-visible rounded-lg', {
+          'border-t border-moon-250': isTool,
+          'bg-moon-100': isSystemPrompt,
+          'bg-cactus-300/[0.24]': isUser,
+          'w-full': !isUser,
+          'max-w-3xl': isUser,
+          'ml-auto': isUser,
+          'mr-auto': !isUser,
+          'py-8': hasContent,
+        })}>
+        <div>
+          {isSystemPrompt && (
+            <div className="flex justify-between px-16">
+              <div className="text-sm text-moon-500">
+                {message.role.charAt(0).toUpperCase() + message.role.slice(1)}
+              </div>
+            </div>
+          )}
+
+          {isTool && (
+            <div className={classNames({'px-16': isNested}, 'pb-8')}>
+              <div className="text-sm font-semibold text-moon-500">
+                Response
+              </div>
+            </div>
+          )}
+
+          <div className={classNames('w-full overflow-y-hidden')}>
+            {hasContent && (
+              <div
+                className={classNames(hasToolCalls ? 'pb-8' : '', ' text-sm', {
+                  'px-16': isSystemPrompt || isUser,
+                })}>
+                {_.isString(message.content) ? (
+                  <MessagePanelPart
+                    value={message.content}
+                    isStructuredOutput={isStructuredOutput}
+                  />
+                ) : (
+                  message.content!.map((p, i) => (
+                    <MessagePanelPart key={i} value={p} />
+                  ))
+                )}
+              </div>
+            )}
+            {hasToolCalls && (
+              <div
+                className={classNames({
+                  'border-t border-moon-250 pt-8': hasContent,
+                })}>
+                <ToolCalls toolCalls={message.tool_calls!} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
