5b752e815f9ec6fd9bcc762580d0e48c24a445f9
gtarpenning <griffin.tarpenning@wandb.com>
2024-11-19 13:01:04 -0800

    chore(weave): feedback payload 1024 -> 1024*1024

--- weave/trace_server/clickhouse_trace_server_batched.py
+++ weave/trace_server/clickhouse_trace_server_batched.py
@@ -1350,7 +1350,8 @@ class ClickHouseTraceServer(tsi.TraceServerInterface):
         created_at = datetime.datetime.now(ZoneInfo("UTC"))
         # TODO: Any validation on weave_ref?
         payload = _dict_value_to_dump(req.payload)
-        MAX_PAYLOAD = 1024
+        # 1MB max payload size
+        MAX_PAYLOAD = 1024 * 1024
         if len(payload) > MAX_PAYLOAD:
             raise InvalidRequest("Feedback payload too large")
         row: Row = {
