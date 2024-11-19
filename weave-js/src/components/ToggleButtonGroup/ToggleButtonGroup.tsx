cda585ca2230c54961d0f7a5b21d096d7b25d0c8
Mollie Anderson <mollie.anderson@wandb.com>
2024-11-18 17:08:23 -0800

    fiix gap, update styling

--- weave-js/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
+++ weave-js/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -12,12 +12,12 @@ export type ToggleButtonGroupProps = {
   value: string;
   size: ToggleButtonGroupSize;
   isDisabled?: boolean;
-  icon?: IconName;
+  icons?: (IconName | undefined)[];
   onValueChange: (value: string) => void;
 };
 
 export const ToggleButtonGroup = React.forwardRef<HTMLDivElement, ToggleButtonGroupProps>(
-  ({ options, value, size, isDisabled, icon, onValueChange }, ref) => {
+  ({ options, value, size, isDisabled, icons, onValueChange }, ref) => {
     const handleValueChange = (newValue: string) => {
         if (newValue !== value) {
           onValueChange(newValue);
@@ -29,27 +29,31 @@ export const ToggleButtonGroup = React.forwardRef<HTMLDivElement, ToggleButtonGr
           type="single"
           value={value}
           onValueChange={handleValueChange}
-          className="flex space-x-2"
+          className="flex gap-[1px] my-16"
           ref={ref}
         >
-          {options.map((option) => (
+          {options.map((option, index) => (
             <ToggleGroup.Item
               key={option}
-              value={option}>
-                <Button
-                icon={icon}
+              value={option}
+              active={value === option}
+              disabled={isDisabled}
+            >
+              <Button
+                icon={icons?.[index]}
                 className={twMerge(
-                "inline-flex items-center justify-center whitespace-nowrap font-['Source_Sans_Pro'] font-semibold gap-[1px] my-10", // TODO: don't use my-10 here
+                "flex justify-center items-center",
                   size === 'small' && 'px-6 py-3 text-sm leading-[18px]',
                   size === 'medium' && 'px-10 py-4 text-base leading-normal',
                   size === 'large' && 'px-12 py-8 text-base leading-normal',
                   isDisabled && 'pointer-events-none opacity-35',
-                  value === option ? 'bg-[#a9edf2]/50 text-[#038194] hover:bg-[#a9edf2]/50]' : 'bg-[#0d0f12]/5 text-[#565c66] hover:text-[#0d0f12] hover:bg-[#0d0f12]/10',
+                  value === option 
+                    ? 'bg-teal-300/[0.48] text-teal-600 hover:bg-teal-300/[0.48]' 
+                    : 'bg-oblivion/5 text-[#565c66] hover:text-[#0d0f12] hover:bg-moonbeam/[0.05]',
                   'first:rounded-l-sm', // First button rounded left
-                  'last:rounded-r-sm' // Last button rounded right
+                  'last:rounded-r-sm', // Last button rounded right
+                  'gap-3'
                 )}
-                active={value === option}
-                disabled={isDisabled}
               >
                 {option}
               </Button>
