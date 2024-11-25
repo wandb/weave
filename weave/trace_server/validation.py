85a2b4cbe564b6f70469c9807c587fe2f74f5605
gtarpenning <griffin.tarpenning@wandb.com>
2024-11-25 10:48:02 -0800

    lint

--- weave/trace_server/validation.py
+++ weave/trace_server/validation.py
@@ -2,29 +2,7 @@ import re
 from typing import Any, Literal, Optional
 
 from weave.trace_server import refs_internal, validation_util
-<<<<<<< HEAD
 from weave.trace_server.errors import InvalidFieldError, InvalidRequest
-=======
-from weave.trace_server.constants import (
-    MAX_DISPLAY_NAME_LENGTH,
-    MAX_OBJECT_NAME_LENGTH,
-    MAX_OP_NAME_LENGTH,
-)
-from weave.trace_server.errors import InvalidRequest
-
-# Temporary flag to disable database-side validation of object ids.
-# We want to enable this be default, but we need to wait until >95% of users
-# are on weave>=0.51.1, when we can enforce the charset check on the db
-# side.
-#
-# Actions:
-# 1. (ETA: Sept 30) - Verify that 95% of users are on weave>=0.51.1, or
-#    that 95% of new objects have the valid charset.
-# 2. Remove this flag (thereby setting this to True), and add a check to the
-#    server-side validation code to ensure that the charset is valid.
-# 3. Release and deploy backend.
-SHOULD_ENFORCE_OBJ_ID_CHARSET = False
->>>>>>> master
 
 
 def project_id_validator(s: str) -> str:
