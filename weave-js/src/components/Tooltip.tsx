e192cdfe9809dbbe3df9f2033a169acd130a49ed
Jamie Rasmussen <jamie.rasmussen@wandb.com>
2024-11-27 00:22:40 -0600

    chore(ui): new Radix based tooltip implementation

--- weave-js/src/components/Tooltip.tsx
+++ weave-js/src/components/Tooltip.tsx
@@ -1,33 +1,91 @@
-import {Popup} from 'semantic-ui-react';
-import styled from 'styled-components';
+import React, {useState} from 'react';
 
-import {
-  hexToRGB,
-  MOON_650,
-  MOON_800,
-  OBLIVION,
-  WHITE,
-} from '../common/css/globals.styles';
+import {Content, Portal, Provider, Root, Trigger} from './RadixTooltip';
 
-export const Tooltip = styled(Popup).attrs({
-  basic: true, // This removes the pointing arrow.
-  mouseEnterDelay: 500,
-  popperModifiers: {
-    preventOverflow: {
-      // Prevent popper from erroneously constraining the popup.
-      // Without this, tooltips in single row table cells get positioned under the cursor,
-      // causing them to immediately close.
-      boundariesElement: 'viewport',
-    },
-  },
-})`
-  && {
-    color: ${WHITE};
-    background: ${MOON_800};
-    border-color: ${MOON_650};
-    box-shadow: 0px 4px 6px ${hexToRGB(OBLIVION, 0.2)};
-    font-size: 14px;
-    line-height: 140%;
-    max-width: 300px;
+type TooltipPosition =
+  | 'top left'
+  | 'top right'
+  | 'bottom right'
+  | 'bottom left'
+  | 'right center'
+  | 'left center'
+  | 'top center'
+  | 'bottom center';
+
+type Side = 'top' | 'right' | 'bottom' | 'left';
+type Align = 'start' | 'center' | 'end';
+
+type TooltipProps = {
+  /** Primary content. */
+  children?: React.ReactNode;
+
+  content?: React.ReactNode;
+
+  /** Element to be rendered in-place where the popup is defined. */
+  trigger: React.ReactNode;
+
+  side?: Side;
+  align?: Align;
+
+  /** Position for the popover. This is for backwards compatibility. Prefer side/align */
+  position?: TooltipPosition;
+};
+
+type ParsedPosition = {
+  side: Side;
+  align: Align;
+};
+
+const parsePosition = (position?: TooltipPosition): ParsedPosition => {
+  const parsed: ParsedPosition = {
+    side: 'top',
+    align: 'center',
+  };
+  if (position) {
+    const [positionSide, positionAlign] = position.split(' ');
+    parsed.side = positionSide as Side;
+    if (positionAlign) {
+      if (positionAlign === 'center') {
+        parsed.align = 'center';
+      } else if (positionAlign === 'left') {
+        parsed.align = 'start';
+      } else if (positionAlign === 'right') {
+        parsed.align = 'end';
+      }
+    }
   }
-`;
+  return parsed;
+};
+
+export const Tooltip = ({
+  trigger,
+  content,
+  side,
+  align,
+  position,
+  children,
+}: TooltipProps) => {
+  const defaultPosition = parsePosition(position);
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+  return (
+    <Provider>
+      <Root open={isTooltipOpen} onOpenChange={setIsTooltipOpen}>
+        <Trigger asChild>
+          {/* span is needed so tooltip works on disabled buttons */}
+          <span className="[display:inherit]">{trigger}</span>
+        </Trigger>
+        <Portal>
+          <Content
+            style={{
+              // it's hard to state how silly this is, but the zIndex on semantic's modal is 2147483605 - so, that + 1
+              zIndex: 2147483606,
+            }}
+            side={side ?? defaultPosition.side}
+            align={align ?? defaultPosition.align}>
+            {children ?? content}
+          </Content>
+        </Portal>
+      </Root>
+    </Provider>
+  );
+};
