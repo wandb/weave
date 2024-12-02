1dd2a6bc2e24ac99d06d8dc33dc925c3ae383ee6
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-12-02 19:51:27 +0530

    fix(weave): dictify for vertexai integration (#3110)
    * fix: dictify for vertexai integration
    
    * add: patching for send_message
    
    * update: vertexai tests
    
    * fix: lint
    
    * update: pyproject.toml

--- weave/integrations/vertexai/vertexai_sdk.py
+++ weave/integrations/vertexai/vertexai_sdk.py
@@ -5,10 +5,23 @@ from typing import TYPE_CHECKING, Any, Callable, Optional
 import weave
 from weave.trace.op_extensions.accumulator import add_accumulator
 from weave.trace.patcher import MultiPatcher, SymbolPatcher
+from weave.trace.serialize import dictify
 from weave.trace.weave_client import Call
 
 if TYPE_CHECKING:
     from vertexai.generative_models import GenerationResponse
+
+
+def vertexai_postprocess_inputs(inputs: dict[str, Any]) -> dict[str, Any]:
+    if "self" in inputs:
+        model_name = (
+            inputs["self"]._model_name
+            if hasattr(inputs["self"], "_model_name")
+            else inputs["self"]._model._model_name
+        )
+        inputs["self"] = dictify(inputs["self"])
+        inputs["model_name"] = model_name
+    return inputs
 
 
 def vertexai_accumulator(
@@ -51,11 +64,12 @@ def vertexai_accumulator(
 def vertexai_on_finish(
     call: Call, output: Any, exception: Optional[BaseException]
 ) -> None:
-    original_model_name = call.inputs["self"]._model_name
+    original_model_name = call.inputs["model_name"]
     model_name = original_model_name.split("/")[-1]
     usage = {model_name: {"requests": 1}}
     summary_update = {"usage": usage}
     if output:
+        call.output = dictify(output)
         usage[model_name].update(
             {
                 "prompt_tokens": output.usage_metadata.prompt_token_count,
@@ -69,7 +83,7 @@ def vertexai_on_finish(
 
 def vertexai_wrapper_sync(name: str) -> Callable[[Callable], Callable]:
     def wrapper(fn: Callable) -> Callable:
-        op = weave.op()(fn)
+        op = weave.op(postprocess_inputs=vertexai_postprocess_inputs)(fn)
         op.name = name  # type: ignore
         op._set_on_finish_handler(vertexai_on_finish)
         return add_accumulator(
@@ -92,7 +106,7 @@ def vertexai_wrapper_async(name: str) -> Callable[[Callable], Callable]:
             return _async_wrapper
 
         "We need to do this so we can check if `stream` is used"
-        op = weave.op()(_fn_wrapper(fn))
+        op = weave.op(postprocess_inputs=vertexai_postprocess_inputs)(_fn_wrapper(fn))
         op.name = name  # type: ignore
         op._set_on_finish_handler(vertexai_on_finish)
         return add_accumulator(
@@ -118,6 +132,16 @@ vertexai_patcher = MultiPatcher(
             vertexai_wrapper_async(
                 name="vertexai.GenerativeModel.generate_content_async"
             ),
+        ),
+        SymbolPatcher(
+            lambda: importlib.import_module("vertexai.generative_models"),
+            "ChatSession.send_message",
+            vertexai_wrapper_sync(name="vertexai.ChatSession.send_message"),
+        ),
+        SymbolPatcher(
+            lambda: importlib.import_module("vertexai.generative_models"),
+            "ChatSession.send_message_async",
+            vertexai_wrapper_async(name="vertexai.ChatSession.send_message_async"),
         ),
         SymbolPatcher(
             lambda: importlib.import_module("vertexai.preview.vision_models"),
