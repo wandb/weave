e46282714682d08b36b85ba8fe7e139ef6babb37
Tim Sweeney <tim@wandb.com>
2024-12-12 15:19:10 -0800

    lint pass 5

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--weave-server",
         action="store",
-        default="clickhouse",
+        default="sqlite",
         help="Specify the client object to use: sqlite or clickhouse",
     )
 
