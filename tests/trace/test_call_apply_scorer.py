506a09aeacfcb4e9227d249566fcdff84d7bf5e8
Tim Sweeney <tim@wandb.com>
2025-01-03 13:20:07 -0800

    small typing

--- tests/trace/test_call_apply_scorer.py
+++ tests/trace/test_call_apply_scorer.py
@@ -1,38 +1,37 @@
+from weave.trace.weave_client import WeaveClient
 
 
-def test_scorer_op_no_context(client):
-
-
+def test_scorer_op_no_context(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_op_with_context():
+def test_scorer_op_with_context(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_op_incorrect_args():
+def test_scorer_op_incorrect_args(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_async_scorer_op():
+def test_async_scorer_op(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_obj_no_context():
+def test_scorer_obj_no_context(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_obj_with_context():
+def test_scorer_obj_with_context(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_obj_incorrect_args():
+def test_scorer_obj_incorrect_args(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_scorer_obj_with_arg_mapping():
+def test_scorer_obj_with_arg_mapping(client: WeaveClient):
     raise NotImplementedError()
 
 
-def test_async_scorer_obj():
+def test_async_scorer_obj(client: WeaveClient):
     raise NotImplementedError()
