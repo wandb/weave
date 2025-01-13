3f1c5c1f241bf03b4553afd2190aabff6889f951
Andrew Truong <itsandrewtruong@gmail.com>
2025-01-12 22:51:40 -0500

    test

--- tests/trace/test_uri_get.py
+++ tests/trace/test_uri_get.py
@@ -70,8 +70,10 @@ async def test_gotten_methods(client):
     ]
     # TODO: Replace with client version of this query when available
     res = client.server.objs_query(
-        ObjQueryReq(project_id=client._project_id()),
-        filter=ObjectVersionFilter(object_ids=relevant_object_ids),
+        ObjQueryReq(
+            project_id=client._project_id(),
+            filter=ObjectVersionFilter(object_ids=relevant_object_ids),
+        )
     )
     for obj in res:
         assert obj.version_index == 0
