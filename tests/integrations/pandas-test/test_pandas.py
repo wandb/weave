96810c9283ffe65c836afad46a6f3f410499db3d
Andrew Truong <itsandrewtruong@gmail.com>
2025-01-12 23:39:22 -0500

    test

--- tests/integrations/pandas-test/test_pandas.py
+++ tests/integrations/pandas-test/test_pandas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 import weave
+from weave import Dataset
 
 
 def test_op_save_with_global_df(client):
@@ -20,3 +21,15 @@ def test_op_save_with_global_df(client):
     call = list(my_op.calls())[0]
     assert call.inputs == {"a": "d"}
     assert call.output == "a"
+
+
+def test_dataset(client):
+    rows = [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}]
+    ds = Dataset(rows=rows)
+    df = ds.to_pandas()
+
+    df2 = pd.DataFrame(rows)
+    ds2 = Dataset.from_pandas(df2)
+
+    assert df.equals(df2)
+    assert ds.rows == ds2.rows
