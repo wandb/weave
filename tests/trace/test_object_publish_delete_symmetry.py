c80f8efcfcbdc78291908f60436b725995ee8cfd
Andrew Truong <itsandrewtruong@gmail.com>
2025-01-22 12:20:10 -0500

    test

--- tests/trace/test_object_publish_delete_symmetry.py
+++ tests/trace/test_object_publish_delete_symmetry.py
@@ -24,8 +24,16 @@ def custom_object() -> weave.Object:
     return CustomObject(a=1, b="hello")
 
 
-def test_object_saving(client, custom_object):
+def test_object_publish(client, custom_object):
     custom_object.publish()
+    obj2 = custom_object.ref.get()
+    assert isinstance(obj2, CustomObject)
+    assert obj2.a == 1
+    assert obj2.b == "hello"
+
+
+def test_object_publish_imperative(client, custom_object):
+    weave.publish(custom_object)
     obj2 = custom_object.ref.get()
     assert isinstance(obj2, CustomObject)
     assert obj2.a == 1
@@ -41,6 +49,14 @@ def test_object_delete(client, custom_object):
         obj2.ref.get()
 
 
+def test_object_delete_imperative(client, custom_object):
+    custom_object.publish()
+
+    weave.delete(custom_object)
+    with pytest.raises(ObjectDeletedError):
+        custom_object.ref.get()
+
+
 def test_object_lifecycle(client, custom_object):
     ref = custom_object.publish()
 
@@ -50,7 +66,11 @@ def test_object_lifecycle(client, custom_object):
     custom_object.b = "world"
     ref3 = custom_object.publish()
 
+    custom_object.a = 3
+    ref4 = custom_object.publish()
+
     ref2.delete()
+    weave.delete(ref4)
 
     res = ref.get()
     assert res.a == 1
@@ -63,15 +83,18 @@ def test_object_lifecycle(client, custom_object):
     assert res.a == 2
     assert res.b == "world"
 
+    with pytest.raises(ObjectDeletedError):
+        ref4.get()
+
 
 def test_failed_publish_maintains_old_object_ref(client, custom_object, monkeypatch):
     old_ref = custom_object.ref
 
-    with pytest.raises(Exception):
+    with pytest.raises(RuntimeError):
         with monkeypatch.context() as m:
 
             def fail_publish(*args, **kwargs):
-                raise Exception("Publish failed")  # noqa: TRY002
+                raise RuntimeError("Publish failed")
 
             m.setattr("weave._publish", fail_publish)
             custom_object.publish()
@@ -108,7 +131,9 @@ def test_delete_symmetry(client, custom_object):
     ref = weave.publish(custom_object)
     ref2 = weave.publish(custom_object2)
 
-    ref.delete()
+    obj = ref.get()
+    weave.delete(obj)
+
     obj2 = ref2.get()
     obj2.delete()
 
