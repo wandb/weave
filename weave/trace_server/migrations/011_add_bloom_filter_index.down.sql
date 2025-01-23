5289efc11bddfdfa9b7cba2a7edafff1b89ed1db
gtarpenning <griffin.tarpenning@wandb.com>
2025-01-23 15:55:29 -0800

    bloom_filter

---
+++ weave/trace_server/migrations/011_add_bloom_filter_index.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE calls_merged
+    DROP INDEX bf_inputs_dump;
+
+ALTER TABLE calls_merged
+    DROP INDEX bf_output_dump;
---
+++ weave/trace_server/migrations/011_add_bloom_filter_index.up.sql
@@ -0,0 +1,12 @@
+ALTER TABLE calls_merged
+    ADD INDEX bf_inputs_dump inputs_dump TYPE bloom_filter GRANULARITY 1;
+
+ALTER TABLE calls_merged
+    ADD INDEX bf_output_dump output_dump TYPE bloom_filter GRANULARITY 1;
+
+ALTER TABLE calls_merged
+    MATERIALIZE INDEX bf_inputs_dump;
+
+ALTER TABLE calls_merged
+    MATERIALIZE INDEX bf_output_dump;
+
