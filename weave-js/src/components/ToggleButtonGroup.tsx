d3ed204e27f2329fa0d4ddc6a0ff98bb6dbc020a
ericakdiaz <erica.diaz@wandb.com>
2025-01-29 12:10:48 -0800

    use "group" for rounded borders

--- weave-js/src/components/ToggleButtonGroup.tsx
+++ weave-js/src/components/ToggleButtonGroup.tsx
@@ -55,48 +55,44 @@ export const ToggleButtonGroup = React.forwardRef<
         className="flex gap-px"
         ref={ref}>
         {options.map(
-          (
-            {
-              value: optionValue,
-              icon,
-              isDisabled: optionIsDisabled,
-              iconOnly = false,
-            },
-            i
-          ) => (
-            <ToggleGroup.Item
-              key={optionValue}
-              value={optionValue}
-              disabled={isDisabled}
-              asChild>
-              <Button
-                icon={icon}
-                size={size}
-                className={twMerge(
-                  'night-aware',
-                  size === 'small' &&
-                    (icon ? 'gap-4 px-4 py-3 text-sm' : 'px-8 py-3 text-sm'),
-                  size === 'medium' &&
-                    (icon
-                      ? 'gap-5 px-7 py-4 text-base'
-                      : 'px-10 py-4 text-base'),
-                  size === 'large' &&
-                    (icon
-                      ? 'gap-6 px-10 py-8 text-base'
-                      : 'px-12 py-8 text-base'),
-                  (isDisabled || optionIsDisabled) && 'cursor-auto opacity-35',
-                  value === optionValue
-                    ? 'bg-teal-300/[0.48] text-teal-600 hover:bg-teal-300/[0.48] dark:bg-teal-700/[0.48] dark:text-teal-400'
-                    : 'hover:bg-oblivion/7 bg-oblivion/5 text-moon-600 hover:text-moon-800 dark:bg-moonbeam/[0.05] hover:dark:bg-teal-700/[0.48] hover:dark:text-teal-400',
-                  i === 0
-                    ? 'rounded-l-sm rounded-r-none' // First button rounded left
-                    : i === options.length - 1
-                    ? 'rounded-l-none rounded-r-sm' // Last button rounded right
-                    : 'rounded-none'
-                )}>
-                {!iconOnly ? optionValue : <></>}
-              </Button>
-            </ToggleGroup.Item>
+          ({
+            value: optionValue,
+            icon,
+            isDisabled: optionIsDisabled,
+            iconOnly = false,
+          }) => (
+            <div className="group" key={optionValue}>
+              <ToggleGroup.Item
+                key={optionValue}
+                value={optionValue}
+                disabled={isDisabled}
+                asChild>
+                <Button
+                  icon={icon}
+                  size={size}
+                  className={twMerge(
+                    'night-aware',
+                    size === 'small' &&
+                      (icon ? 'gap-4 px-4 py-3 text-sm' : 'px-8 py-3 text-sm'),
+                    size === 'medium' &&
+                      (icon
+                        ? 'gap-5 px-7 py-4 text-base'
+                        : 'px-10 py-4 text-base'),
+                    size === 'large' &&
+                      (icon
+                        ? 'gap-6 px-10 py-8 text-base'
+                        : 'px-12 py-8 text-base'),
+                    (isDisabled || optionIsDisabled) &&
+                      'cursor-auto opacity-35',
+                    value === optionValue
+                      ? 'bg-teal-300/[0.48] text-teal-600 hover:bg-teal-300/[0.48] dark:bg-teal-700/[0.48] dark:text-teal-400'
+                      : 'hover:bg-oblivion/7 bg-oblivion/5 text-moon-600 hover:text-moon-800 dark:bg-moonbeam/[0.05] hover:dark:bg-teal-700/[0.48] hover:dark:text-teal-400',
+                    'rounded-none group-first:rounded-l-sm group-first:rounded-r-none group-last:rounded-l-none group-last:rounded-r-sm'
+                  )}>
+                  {!iconOnly ? optionValue : <></>}
+                </Button>
+              </ToggleGroup.Item>
+            </div>
           )
         )}
       </ToggleGroup.Root>
