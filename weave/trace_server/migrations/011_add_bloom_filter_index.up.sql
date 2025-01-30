e10752b2c7605760ba8092b30fff31fea442f0bc
gtarpenning <griffin.tarpenning@wandb.com>
2025-01-29 17:38:11 -0800

    more!

--- weave/trace_server/migrations/011_add_bloom_filter_index.up.sql
+++ weave/trace_server/migrations/011_add_bloom_filter_index.up.sql
@@ -16,3 +16,14 @@ ALTER TABLE calls_merged
 ALTER TABLE calls_merged
     MATERIALIZE INDEX minmax_id_idx;
 
+ALTER TABLE calls_merged
+    ADD INDEX minmax_started_at (started_at) TYPE minmax GRANULARITY 1;
+
+ALTER TABLE calls_merged
+    ADD INDEX minmax_ended_at (ended_at) TYPE minmax GRANULARITY 1;
+
+ALTER TABLE calls_merged
+    MATERIALIZE INDEX minmax_started_at;
+
+ALTER TABLE calls_merged
+    MATERIALIZE INDEX minmax_ended_at;
