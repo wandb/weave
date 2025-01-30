3dc51ce12dee7479606c2c53b7831d37c2b3872b
gtarpenning <griffin.tarpenning@wandb.com>
2025-01-29 16:46:08 -0800

    fix(ui): additional filters lookup the proper operator

--- weave-js/src/components/PagePanelComponents/Home/Browse3/filters/FilterRow.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/filters/FilterRow.tsx
@@ -3,7 +3,7 @@
  */
 
 import {GridFilterItem} from '@mui/x-data-grid-pro';
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo} from 'react';
 
 import {Button} from '../../../../Button';
 import {
@@ -35,8 +35,11 @@ export const FilterRow = ({
     if (item.id == null) {
       onAddFilter(field);
     } else {
-      // TODO: May need to get new operator or value?
-      onUpdateFilter({...item, field});
+      // If this is an additional filter, we need to get new operator
+      // because the default is string
+      const newOperatorOptions = getGroupedOperatorOptions(field);
+      const operator = newOperatorOptions[0].options[0].value;
+      onUpdateFilter({...item, field, operator});
     }
   };
 
