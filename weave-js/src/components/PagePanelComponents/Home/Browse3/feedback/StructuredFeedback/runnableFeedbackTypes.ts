e88177e506fe6819ddb96f9df15d4c4fb7e7d99d
Tim Sweeney <tim@wandb.com>
2025-01-30 19:27:59 -0800

    UI aspect

--- weave-js/src/components/PagePanelComponents/Home/Browse3/feedback/StructuredFeedback/runnableFeedbackTypes.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/feedback/StructuredFeedback/runnableFeedbackTypes.ts
@@ -1,1 +1,4 @@
 export const RUNNABLE_FEEDBACK_TYPE_PREFIX = 'wandb.runnable';
+
+export const callQueryFieldForScorerOutput = (runnableName: string) =>
+  `feedback.[${RUNNABLE_FEEDBACK_TYPE_PREFIX}.${runnableName}].payload.output`;
