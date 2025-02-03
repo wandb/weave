c85b44a9db4c0a5017a692b0906eefa118b12400
Thomas Capelle <tcapelle@pm.me>
2025-02-03 18:14:00 +0100

    move import litellm to init

--- weave/scorers/llm_utils.py
+++ weave/scorers/llm_utils.py
@@ -1,3 +1,10 @@
+try:
+    from litellm import amoderation
+except ImportError:
+    raise ImportError(
+        "litellm is not installed. Please install it with `pip install litellm`."
+    )
+
 OPENAI_DEFAULT_MODEL = "gpt-4o"
 OPENAI_DEFAULT_EMBEDDING_MODEL = "text-embedding-3-small"
 OPENAI_DEFAULT_MODERATION_MODEL = "text-moderation-latest"
@@ -9,3 +16,4 @@ MISTRAL_DEFAULT_EMBEDDING_MODEL = "mistral-embed"
 
 DEFAULT_MAX_TOKENS = 4096
 DEFAULT_TEMPERATURE = 0.7
+
