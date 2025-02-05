e7d705db0e0582384c3aaa67633f076c232f8c07
Morgan McGuire <morganmcg1@users.noreply.github.com>
2025-02-05 15:39:55 +0000

    Add download from custom artifact path to BiasScorer

--- weave/scorers/moderation_scorer.py
+++ weave/scorers/moderation_scorer.py
@@ -268,6 +268,8 @@ class BiasScorer(RollingWindowScorer):
         """Initialize the bias model and tokenizer."""
         if os.path.isdir(self.model_name_or_path):
             self._local_model_path = self.model_name_or_path
+        elif self.model_name_or_path != "":
+            self._local_model_path = download_model(self.model_name_or_path)
         else:
             self._local_model_path = download_model(MODEL_PATHS["bias_scorer"])
 
