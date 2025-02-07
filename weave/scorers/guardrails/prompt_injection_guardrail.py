794739a3820736b1d2342bd5469eeb853c6ee0dd
soumik12345 <19soumik.rakshit96@gmail.com>
2025-02-07 20:59:13 +0800

    fix: PromptInjectionLLMGuardrail

--- weave/scorers/guardrails/prompt_injection_guardrail.py
+++ weave/scorers/guardrails/prompt_injection_guardrail.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Optional
 
 from litellm import acompletion
 from pydantic import BaseModel
@@ -7,6 +7,7 @@ import weave
 from weave.scorers.default_models import OPENAI_DEFAULT_MODEL
 from weave.scorers.guardrails.prompts import (
     PROMPT_INJECTION_GUARDRAIL_SYSTEM_PROMPT,
+    PROMPT_INJECTION_GUARDRAIL_USER_PROMPT,
     PROMPT_INJECTION_SURVEY_PAPER_SUMMARY,
 )
 from weave.scorers.llm_scorer import LLMScorer
@@ -21,28 +22,35 @@ class LLMGuardrailReasoning(BaseModel):
 
 
 class LLMGuardrailResponse(BaseModel):
-    safe: bool
-    reasoning: LLMGuardrailReasoning
+    flagged: bool
+    reason: LLMGuardrailReasoning
 
 
 class PromptInjectionLLMGuardrail(LLMScorer):
+    """
+    The `PromptInjectionLLMGuardrail` uses an LLM to assess whether a prompt
+    is a prompt injection attack or not.
+
+    Attributes:
+        system_prompt (str): The prompt describing the task of detecting prompt injection attacks.
+            The system prompt is a summarized version of the research paper
+            [An Early Categorization of Prompt Injection Attacks on Large Language Models](https://arxiv.org/abs/2402.00898)
+            that contains the taxonomy of prompt injection attacks and the criteria and definitions for each attack type.
+        model_id (str): The LLM model name, depends on the LLM's providers to be used `client` being used.
+        temperature (float): LLM temperature setting.
+        max_tokens (int): Maximum number of tokens in the LLM's response.
+    """
+
     system_prompt: str = PROMPT_INJECTION_GUARDRAIL_SYSTEM_PROMPT
     model_id: str = OPENAI_DEFAULT_MODEL
     temperature: float = 0.7
     max_tokens: int = 4096
 
     @weave.op
-    async def score(self, output: str) -> LLMGuardrailResponse:
-        user_prompt = (
-            PROMPT_INJECTION_SURVEY_PAPER_SUMMARY
-            + f"""
-You are given the following user prompt that you are suppossed to assess whether it is a prompt injection attack or not:
-
-
-<input_prompt>
-{stringify(output)}
-</input_prompt>
-"""
+    async def score(self, output: str) -> dict[str, Any]:
+        user_prompt = PROMPT_INJECTION_GUARDRAIL_USER_PROMPT.format(
+            research_paper_summary=PROMPT_INJECTION_SURVEY_PAPER_SUMMARY,
+            prompt=stringify(output),
         )
         response = await acompletion(
             messages=[
@@ -58,5 +66,5 @@ You are given the following user prompt that you are suppossed to assess whether
             response.choices[0].message.content
         )
         return LLMGuardrailResponse(
-            safe=not response.injection_prompt, reasoning=response
+            flagged=response.injection_prompt, reason=response
         ).model_dump()
