b85dcdb278c527d802532362ba36d8f8fab4b134
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2025-02-18 23:13:11 +0530

    feat(weave): refactor `PromptInjectionLLMGuardrail` (#3692)
    * refactor: PromptInjectionLLMGuardrail
    
    * fix: imports

--- weave/scorers/__init__.py
+++ weave/scorers/__init__.py
@@ -6,7 +6,6 @@ check_litellm_installation()
 from weave.scorers.classification_scorer import (
     MultiTaskBinaryClassificationF1,
 )
-from weave.scorers.guardrails import PromptInjectionLLMGuardrail
 from weave.scorers.hallucination_scorer import HallucinationFreeScorer
 from weave.scorers.json_scorer import ValidJSONScorer
 from weave.scorers.moderation_scorer import (
@@ -16,6 +15,9 @@ from weave.scorers.moderation_scorer import (
 )
 from weave.scorers.presidio_entity_recognition_guardrail import (
     PresidioEntityRecognitionGuardrail,
+)
+from weave.scorers.prompt_injection_guardrail import (
+    PromptInjectionLLMGuardrail,
 )
 from weave.scorers.pydantic_scorer import PydanticScorer
 from weave.scorers.ragas_scorer import (
