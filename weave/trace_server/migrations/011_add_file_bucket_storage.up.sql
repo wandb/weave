62a34cb4764a2929a080c432e2283c949529f241
Tim Sweeney <tim@wandb.com>
2025-02-21 15:35:23 -0700

    Better stats

--- weave/trace_server/migrations/011_add_file_bucket_storage.up.sql
+++ weave/trace_server/migrations/011_add_file_bucket_storage.up.sql
@@ -1,2 +1,22 @@
 ALTER TABLE files ADD COLUMN bytes_stored Nullable(UInt32);
 ALTER TABLE files ADD COLUMN file_storage_uri Nullable(String);
+
+ALTER TABLE files_stats ADD COLUMN bytes_stored Nullable(UInt32);
+ALTER TABLE files_stats ADD COLUMN file_storage_uri Nullable(String);
+
+ALTER TABLE files_stats_view MODIFY QUERY
+SELECT
+    files.project_id,
+    files.digest,
+    files.chunk_index,
+    anySimpleState(files.n_chunks) as n_chunks,
+    anySimpleState(files.name) as name,
+    anySimpleState(length(files.val_bytes) + files.bytes_stored) AS size_bytes,
+    anySimpleState(files.file_storage_uri) AS file_storage_uri,
+    minSimpleState(files.created_at) AS created_at,
+    maxSimpleState(files.created_at) AS updated_at
+FROM files
+GROUP BY
+    files.project_id,
+    files.digest,
+    files.chunk_index;
