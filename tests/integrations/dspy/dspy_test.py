536309406ddb49660d06817c5967e644d6ccdfea
soumik12345 <19soumik.rakshit96@gmail.com>
2025-02-24 13:24:46 +0530

    add: test for classification

--- tests/integrations/dspy/dspy_test.py
+++ tests/integrations/dspy/dspy_test.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 
 import pytest
 
@@ -127,6 +128,75 @@ def test_dspy_chain_of_thought_module(client) -> None:
     assert trace_name == "litellm.completion"
 
     call = calls[6]
+    assert call.started_at < call.ended_at
+    trace_name = op_name_from_ref(call.op_name)
+    assert trace_name == "openai.chat.completions.create"
+
+
+@pytest.mark.vcr(
+    filter_headers=[
+        "authorization",
+        "x-api-key",
+        "cookie",
+        "set-cookie",
+        "x-request-id",
+        "x-ratelimit-remaining-requests",
+        "x-ratelimit-remaining-tokens",
+    ],
+    allowed_hosts=["api.wandb.ai", "localhost", "trace.wandb.ai"],
+)
+@pytest.mark.skip_clickhouse_client
+def test_dspy_classification_module(client) -> None:
+    import dspy
+
+    dspy.configure(
+        lm=dspy.LM(
+            "openai/gpt-4o-mini",
+            cache=False,
+            api_key=os.environ.get("OPENAI_API_KEY", "DUMMY_API_KEY"),
+        ),
+        callbacks=[WeaveCallback()],
+    )
+
+    class Classify(dspy.Signature):
+        """Classify sentiment of a given sentence."""
+
+        sentence: str = dspy.InputField()
+        sentiment: Literal["positive", "negative", "neutral"] = dspy.OutputField()
+        confidence: float = dspy.OutputField()
+
+    classify = dspy.Predict(Classify)
+    response = classify(
+        sentence="This book was super fun to read, though not the last chapter."
+    )
+    assert response.sentiment == "positive"
+    assert response.confidence >= 0.5
+
+    calls = list(client.calls())
+    assert len(calls) == 6
+
+    call = calls[0]
+    assert call.started_at < call.ended_at
+    trace_name = op_name_from_ref(call.op_name)
+    assert trace_name == "dspy.Predict"
+    assert response["sentiment"] == "positive"
+    assert response["confidence"] >= 0.5
+    assert (
+        call.inputs["self"]["signature"]["description"]
+        == "Classify sentiment of a given sentence."
+    )
+
+    call = calls[3]
+    assert call.started_at < call.ended_at
+    trace_name = op_name_from_ref(call.op_name)
+    assert trace_name == "dspy.LM"
+
+    call = calls[4]
+    assert call.started_at < call.ended_at
+    trace_name = op_name_from_ref(call.op_name)
+    assert trace_name == "litellm.completion"
+
+    call = calls[5]
     assert call.started_at < call.ended_at
     trace_name = op_name_from_ref(call.op_name)
     assert trace_name == "openai.chat.completions.create"
