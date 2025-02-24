e4cee2531e2663c9177868f455b4d1da9e9c2afb
gtarpenning <griffin.tarpenning@wandb.com>
2025-02-24 09:33:41 -0800

    typo

--- weave/trace_server/migrations/012_bloom_filter_inputs_outputs.down.sql
+++ weave/trace_server/migrations/012_bloom_filter_inputs_outputs.down.sql
@@ -4,5 +4,5 @@ ALTER TABLE calls_merged
 ALTER TABLE calls_merged
     ADD INDEX bf_output_dump output_dump TYPE BLOOM_FILTER GRANULARITY 1;
 
-ALTER TABLE calls_merged MATERLIAZE INDEX bf_inputs_dump;
-ALTER TABLE calls_merged MATERLIAZE INDEX bf_output_dump;
+ALTER TABLE calls_merged MATERIALIZE INDEX bf_inputs_dump;
+ALTER TABLE calls_merged MATERIALIZE INDEX bf_output_dump;
