5ee4c746f48f303280f67e05555cb8fef3b8e56e
Andrew Truong <itsandrewtruong@gmail.com>
2025-02-25 16:11:27 -0500

    test

--- tests/trace/test_list_objects.py
+++ tests/trace/test_list_objects.py
@@ -12,39 +12,45 @@ def generate_objects(weave_client: WeaveClient, obj_count: int, version_count: i
             weave.publish({"i": i, "j": j}, name=f"obj_{i}")
 
 
-def test_list_objects_basic(client: WeaveClient):
+def test_list_objects_basic_and_iteration(client: WeaveClient):
     generate_objects(client, obj_count=5, version_count=3)
 
     collections = client.list_objects()
     assert len(collections) == 5
 
-    for collection in collections:
+    for i, collection in enumerate(collections):
         assert len(collection) == 3
         assert isinstance(collection, ObjectVersionCollection)
-        assert collection.object_id.startswith("obj_")
+        assert collection.object_id.startswith(f"obj_{i}")
 
-
-def test_list_objects_iteration(client: WeaveClient):
-    generate_objects(client, obj_count=2, version_count=2)
-
-    collections = client.list_objects()
-
-    for collection in collections:
-        for obj in collection:
-            assert isinstance(obj, (WeaveDict))
+        for j, obj in enumerate(collection):
+            assert isinstance(obj, WeaveDict)
             assert "i" in obj
             assert "j" in obj
+            assert obj["i"] == i
+            assert obj["j"] == j
 
 
 def test_list_objects_with_filter(client: WeaveClient):
-    generate_objects(client, obj_count=5, version_count=2)
+    generate_objects(client, obj_count=5, version_count=3)
 
     filtered_collections = client.list_objects(
         filter=tsi.ObjectVersionFilter(object_ids=["obj_0", "obj_1"])
     )
-
     assert len(filtered_collections) == 2
     assert {c.object_id for c in filtered_collections} == {"obj_0", "obj_1"}
+
+    for i, collection in enumerate(filtered_collections):
+        assert len(collection) == 3
+        assert isinstance(collection, ObjectVersionCollection)
+        assert collection.object_id.startswith(f"obj_{i}")
+
+        for j, obj in enumerate(collection):
+            assert isinstance(obj, WeaveDict)
+            assert "i" in obj
+            assert "j" in obj
+            assert obj["i"] == i
+            assert obj["j"] == j
 
 
 def test_list_objects_lazy_loading(client: WeaveClient):
