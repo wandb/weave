e05d04055497ba34bc4113070b196787d7698ea9
gtarpenning <griffin.tarpenning@wandb.com>
2025-02-26 14:11:09 -0800

    DO NOT ADD is_heavy TO QUERYBUILDERFIELD

--- weave/trace_server/calls_query_builder.py
+++ weave/trace_server/calls_query_builder.py
@@ -65,9 +65,6 @@ class QueryBuilderField(BaseModel):
     def as_select_sql(self, pb: ParamBuilder, table_alias: str) -> str:
         return f"{self.as_sql(pb, table_alias)} AS {self.field}"
 
-    def is_heavy(self) -> bool:
-        return False
-
 
 class CallsMergedField(QueryBuilderField):
     def is_heavy(self) -> bool:
