f36f3d5db0a19368fca89d3fb1ef340a2f3c871a
soumik12345 <19soumik.rakshit96@gmail.com>
2025-03-05 01:29:01 +0530

    update: noxflie

--- noxfile.py
+++ noxfile.py
@@ -65,8 +65,8 @@ def tests(session, shard):
     if session.python.startswith("3.13") and shard in PY313_INCOMPATIBLE_SHARDS:
         session.skip(f"Skipping {shard=} as it is not compatible with Python 3.13")
 
-    # if session.python.startswith("3.9") and shard in PY309_INCOMPATIBLE_SHARDS:
-    #     session.skip(f"Skipping {shard=} as it is not compatible with Python 3.9")
+    if session.python.startswith("3.9") and shard in PY309_INCOMPATIBLE_SHARDS:
+        session.skip(f"Skipping {shard=} as it is not compatible with Python 3.9")
 
     session.install("-e", f".[{shard},test]")
     session.chdir("tests")
