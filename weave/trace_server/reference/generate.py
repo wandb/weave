75363bdafae003ff0f0ef16b1be3479c3e5e6648
Andrew Truong <itsandrewtruong@gmail.com>
2025-03-07 09:37:55 -0500

    test

--- weave/trace_server/reference/generate.py
+++ weave/trace_server/reference/generate.py
@@ -94,6 +94,28 @@ def generate_routes(
 
     # This order is done to minimize diff to the current OpenAPI spec.  Once everything
     # settles, we should refactor this to be in the order of the TraceServerInterface.
+
+    @router.post("/call/start", tags=[CALLS_TAG_NAME])
+    def call_start(
+        req: tsi.CallStartReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CallStartRes:
+        return server.call_start(req)
+
+    @router.post("/call/end", tags=[CALLS_TAG_NAME])
+    def call_end(
+        req: tsi.CallEndReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CallEndRes:
+        return server.call_end(req)
+
+    @router.post("/call/upsert_batch", tags=[CALLS_TAG_NAME])
+    def call_start_batch(
+        req: tsi.CallCreateBatchReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CallCreateBatchRes:
+        return server.call_start_batch(req)
+
     @router.post("/calls/delete", tags=[CALLS_TAG_NAME])
     def calls_delete(
         req: tsi.CallsDeleteReq,
@@ -123,7 +145,7 @@ def generate_routes(
         return server.calls_query_stats(req)
 
     @router.post(
-        "/calls/query_stream",
+        "/calls/stream_query",
         tags=[CALLS_TAG_NAME],
         response_class=StreamingResponse,
         responses={
@@ -147,75 +169,12 @@ def generate_routes(
     ) -> StreamingResponse:
         return StreamingResponse(server.calls_query_stream(req), media_type=accept)
 
-    @router.post("/call/start", tags=[CALLS_TAG_NAME])
-    def call_start(
-        req: tsi.CallStartReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CallStartRes:
-        return server.call_start(req)
-
-    @router.post("/call/end", tags=[CALLS_TAG_NAME])
-    def call_end(
-        req: tsi.CallEndReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CallEndRes:
-        return server.call_end(req)
-
-    @router.post("/call/upsert_batch", tags=[CALLS_TAG_NAME])
-    def call_start_batch(
-        req: tsi.CallCreateBatchReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CallCreateBatchRes:
-        return server.call_start_batch(req)
-
     @router.post("/calls/query", tags=[CALLS_TAG_NAME], include_in_schema=False)
     def calls_query(
         req: tsi.CallsQueryReq,
         server: tsi.TraceServerInterface = Depends(get_server),
     ) -> tsi.CallsQueryRes:
         return server.calls_query(req)
-
-    @router.post("/op/create", tags=[OPS_TAG_NAME])
-    def op_create(
-        req: tsi.OpCreateReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.OpCreateRes:
-        return server.op_create(req)
-
-    @router.post("/op/read", tags=[OPS_TAG_NAME])
-    def op_read(
-        req: tsi.OpReadReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.OpReadRes:
-        return server.op_read(req)
-
-    @router.post("/ops/query", tags=[OPS_TAG_NAME])
-    def ops_query(
-        req: tsi.OpQueryReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.OpQueryRes:
-        return server.ops_query(req)
-
-    @router.post("/cost/create", tags=[COST_TAG_NAME])
-    def cost_create(
-        req: tsi.CostCreateReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CostCreateRes:
-        return server.cost_create(req)
-
-    @router.post("/cost/query", tags=[COST_TAG_NAME])
-    def cost_query(
-        req: tsi.CostQueryReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CostQueryRes:
-        return server.cost_query(req)
-
-    @router.post("/cost/purge", tags=[COST_TAG_NAME])
-    def cost_purge(
-        req: tsi.CostPurgeReq,
-        server: tsi.TraceServerInterface = Depends(get_server),
-    ) -> tsi.CostPurgeRes:
-        return server.cost_purge(req)
 
     @router.post("/obj/create", tags=[OBJECTS_TAG_NAME])
     def obj_create(
@@ -327,11 +286,54 @@ def generate_routes(
         res = server.file_content_read(req)
         return StreamingResponse(iter([res.content]))
 
+    @router.post("/op/create", tags=[OPS_TAG_NAME])
+    def op_create(
+        req: tsi.OpCreateReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.OpCreateRes:
+        return server.op_create(req)
+
+    @router.post("/op/read", tags=[OPS_TAG_NAME])
+    def op_read(
+        req: tsi.OpReadReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.OpReadRes:
+        return server.op_read(req)
+
+    @router.post("/ops/query", tags=[OPS_TAG_NAME])
+    def ops_query(
+        req: tsi.OpQueryReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.OpQueryRes:
+        return server.ops_query(req)
+
+    @router.post("/cost/create", tags=[COST_TAG_NAME])
+    def cost_create(
+        req: tsi.CostCreateReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CostCreateRes:
+        return server.cost_create(req)
+
+    @router.post("/cost/query", tags=[COST_TAG_NAME])
+    def cost_query(
+        req: tsi.CostQueryReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CostQueryRes:
+        return server.cost_query(req)
+
+    @router.post("/cost/purge", tags=[COST_TAG_NAME])
+    def cost_purge(
+        req: tsi.CostPurgeReq,
+        server: tsi.TraceServerInterface = Depends(get_server),
+    ) -> tsi.CostPurgeRes:
+        return server.cost_purge(req)
+
     @router.post("/feedback/create", tags=[FEEDBACK_TAG_NAME])
     def feedback_create(
         req: tsi.FeedbackCreateReq,
         server: tsi.TraceServerInterface = Depends(get_server),
     ) -> tsi.FeedbackCreateRes:
+        """Add feedback to a call or object."""
         return server.feedback_create(req)
 
     @router.post("/feedback/query", tags=[FEEDBACK_TAG_NAME])
@@ -339,6 +341,7 @@ def generate_routes(
         req: tsi.FeedbackQueryReq,
         server: tsi.TraceServerInterface = Depends(get_server),
     ) -> tsi.FeedbackQueryRes:
+        """Query for feedback."""
         return server.feedback_query(req)
 
     @router.post("/feedback/purge", tags=[FEEDBACK_TAG_NAME])
@@ -346,6 +349,7 @@ def generate_routes(
         req: tsi.FeedbackPurgeReq,
         server: tsi.TraceServerInterface = Depends(get_server),
     ) -> tsi.FeedbackPurgeRes:
+        """Permanently delete feedback."""
         return server.feedback_purge(req)
 
     @router.post("/feedback/replace", tags=[FEEDBACK_TAG_NAME])
