d008824685a33279a6c573814037e9fc8e77b937
gtarpenning <griffin.tarpenning@wandb.com>
2025-03-17 15:17:44 -0700

    undo comment delete

--- weave-js/src/components/PagePanelComponents/Home/Browse3/filters/FilterRow.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/filters/FilterRow.tsx
@@ -35,6 +35,8 @@ export const FilterRow = ({
     if (item.id == null) {
       onAddFilter(field);
     } else {
+      // If this is an additional filter, we need to get new operator
+      // because the default is string
       const newOperatorOptions = getGroupedOperatorOptions(field);
       const operator = newOperatorOptions[0].options[0].value;
       onUpdateFilter({...item, field, operator});
