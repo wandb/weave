fe2198dd7b9e25c51a76fb8df36a6b96e27d3d7f
Martin Mark <martin.mark@wandb.com>
2025-03-16 21:49:49 -0700

    feat(weave): New trace view: design tweaks (#3868)
    * Pre: move icons over
    
    * >> Trace lines added
    
    * >> Tracebar trimming info
    
    * Fixed divider tooltip (general)
    
    * >> Search algo child / parents - added emphasis
    
    * Conditionally show/hide breadcrumb buttons when they don't show
    
    * >> Expand collapse scrubbers
    
    * Added tooltip for treeview (for truncating)
    
    * Use button for cancel search
    
    * Lint
    
    * Scrubber: colors + collapse alignment
    
    * Re-added latency / added toggle / lint
    
    * Colors
    
    * Tweaked colors
    
    * Fix: don't add siblings that are linked from parents on filter
    
    * Fixes: Scrubber expand/collapse / padding
    
    * Added strict search to treeview

--- weave-js/src/components/PagePanelComponents/Home/Browse3/components/TraceNavigator/TraceScrubber/index.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/components/TraceNavigator/TraceScrubber/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {Icon} from '@wandb/weave/components/Icon';
+import React, {useState, useMemo} from 'react';
 
 import {BaseScrubberProps} from './components/BaseScrubber';
 import {
@@ -8,7 +9,7 @@ import {
   StackScrubber,
   TimelineScrubber,
 } from './components/scrubbers';
-import {Container} from './styles';
+import {CollapseButton, CollapseWrapper, Container} from './styles';
 
 export type ScrubberOption =
   | 'timeline'
@@ -22,20 +23,42 @@ const TraceScrubber: React.FC<
     allowedScrubbers?: ScrubberOption[];
   }
 > = props => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   const showScrubber = (scrubber: ScrubberOption) => {
     if (!props.allowedScrubbers) {
       return true;
     }
     return props.allowedScrubbers.includes(scrubber);
   };
+
+  // Count how many scrubbers are visible
+  // Only show collapse functionality if there are 4 or more scrubbers
+  const visibleScrubberCount = useMemo(() => {
+    const scrubberOptions: ScrubberOption[] = ['timeline', 'peer', 'codePath', 'sibling', 'stack'];
+    return scrubberOptions.filter(scrubber => showScrubber(scrubber)).length;
+  }, [props.allowedScrubbers]);
+  const showCollapseButton = visibleScrubberCount >= 4;
+  
   return (
-    <Container>
-      {showScrubber('timeline') && <TimelineScrubber {...props} />}
-      {showScrubber('peer') && <PeerScrubber {...props} />}
-      {showScrubber('codePath') && <CodePathScrubber {...props} />}
-      {showScrubber('sibling') && <SiblingScrubber {...props} />}
-      {showScrubber('stack') && <StackScrubber {...props} />}
-    </Container>
+    <CollapseWrapper>
+      {showCollapseButton && (
+        <CollapseButton onClick={() => setIsCollapsed(!isCollapsed)}>
+          <Icon
+            name={isCollapsed ? 'chevron-up' : 'chevron-down'}
+            size="small"
+            className="max-w-14 max-h-14"
+          />
+        </CollapseButton>
+      )}
+      <Container $isCollapsed={showCollapseButton ? isCollapsed : false}>
+        {showScrubber('timeline') && <TimelineScrubber {...props} />}
+        {showScrubber('peer') && <PeerScrubber {...props} />}
+        {showScrubber('codePath') && <CodePathScrubber {...props} />}
+        {showScrubber('sibling') && <SiblingScrubber {...props} />}
+        {showScrubber('stack') && <StackScrubber {...props} />}
+      </Container>
+    </CollapseWrapper>
   );
 };
 
