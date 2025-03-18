cda912f1bbcea71c4d0be06b9b8c96ff68e10e69
Connie Lee <connie.lee@wandb.com>
2025-03-18 13:14:48 -0700

    feat(ui): Add truncation features to Tag

--- weave-js/src/components/Tag/Tag.tsx
+++ weave-js/src/components/Tag/Tag.tsx
@@ -1,24 +1,20 @@
 import classNames from 'classnames';
-import React, {
-  FC,
-  ReactElement,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import React, {FC, ReactElement, useMemo, useRef} from 'react';
 import {twMerge} from 'tailwind-merge';
 
 import {Icon, IconName} from '../Icon';
 import {Tailwind} from '../Tailwind';
+import {
+  TruncateByCharsProps,
+  TruncateByCharsWithTooltip,
+} from '../TruncateByCharsWithTooltip';
 import {RemoveAction} from './RemoveAction';
-import {TagTooltip} from './TagTooltip';
 import {
   getRandomTagColor,
   getTagColorByString,
   getTagColorClass,
   getTagHoverClass,
-  isTagLabelTruncated,
+  TAG_DEFAULT_MAX_CHARS,
   TagColorName,
 } from './utils';
 
@@ -55,15 +51,12 @@ export function useTagClasses({
   );
 }
 
-export type TagProps = {
+export type TagProps = TruncateByCharsProps & {
   label: string;
   color?: TagColorName;
   showIcon?: boolean;
   iconName?: IconName;
   endIconName?: IconName;
-  // Wrapping the Tag in Tailwind can be a problem if the Tailwind wrapper is supplied higher up
-  // and there is a need to position the Tag as a direct child for something like flexbox
-  Wrapper?: React.ComponentType<any> | null;
   isInteractive?: boolean;
 };
 
@@ -73,79 +66,65 @@ export const Tag: FC<TagProps> = ({
   showIcon = false,
   iconName,
   endIconName,
+  maxChars = TAG_DEFAULT_MAX_CHARS,
+  truncatedPart,
   Wrapper = Tailwind,
   isInteractive = false,
 }) => {
   const classes = useTagClasses({color, isInteractive, label});
-
-  const nakedTag = (
-    <div className={twMerge(classes, showIcon ? 'pl-4 pr-6' : 'px-6')}>
-      {showIcon && (
-        <Icon
-          role="presentation"
-          className="mr-4 h-14 w-14"
-          name={iconName ?? DEFAULT_TAG_ICON}
-        />
+  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper};
+  return (
+    <TruncateByCharsWithTooltip {...truncationProps}>
+      {({truncatedText}) => (
+        <div className={twMerge(classes, showIcon ? 'pl-4 pr-6' : 'px-6')}>
+          {showIcon && (
+            <Icon
+              role="presentation"
+              className="mr-4 h-14 w-14"
+              name={iconName ?? DEFAULT_TAG_ICON}
+            />
+          )}
+          <span>{truncatedText}</span>
+          {endIconName && (
+            <Icon className="ml-4 h-14 w-14" name={endIconName} />
+          )}
+        </div>
       )}
-      <span className="max-w-[24ch] overflow-hidden text-ellipsis whitespace-nowrap">
-        {label}
-      </span>
-      {endIconName && <Icon className="ml-4 h-14 w-14" name={endIconName} />}
-    </div>
+    </TruncateByCharsWithTooltip>
   );
-  if (Wrapper) {
-    return <Wrapper>{nakedTag}</Wrapper>;
-  }
-
-  return nakedTag;
 };
 
-export type RemovableTagProps = Omit<TagProps, 'isInteractive'> & {
-  removeAction: ReactElement<typeof RemoveAction>;
-};
+export type RemovableTagProps = TruncateByCharsProps &
+  Omit<TagProps, 'isInteractive'> & {
+    removeAction: ReactElement<typeof RemoveAction>;
+  };
 export const RemovableTag: FC<RemovableTagProps> = ({
   label,
   removeAction,
   color,
   showIcon = false,
   iconName,
+  maxChars = TAG_DEFAULT_MAX_CHARS,
+  truncatedPart,
   Wrapper = Tailwind,
 }) => {
-  const labelRef = useRef<HTMLParagraphElement>(null);
-  const [isTruncated, setIsTruncated] = useState(false);
-
-  useEffect(() => {
-    if (labelRef.current) {
-      setIsTruncated(isTagLabelTruncated(labelRef));
-    }
-  }, [label]);
-
   const classes = useTagClasses({color, isInteractive: true, label});
-
-  const nakedTag = (
-    <TagTooltip value={label} disabled={!isTruncated}>
-      <div className={twMerge(classes, showIcon ? 'px-4' : 'pl-6 pr-4')}>
-        {showIcon && (
-          <Icon
-            role="presentation"
-            className="mr-4 h-14 w-14"
-            name={iconName ?? DEFAULT_TAG_ICON}
-          />
-        )}
-        <p
-          className={twMerge(
-            'max-w-[172px]', // 172px =  MAX_TAG_LABEL_WIDTH_PX + 14 (account for remove action button)
-            'overflow-hidden text-ellipsis whitespace-nowrap'
+  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper};
+  return (
+    <TruncateByCharsWithTooltip {...truncationProps}>
+      {({truncatedText}) => (
+        <div className={twMerge(classes, showIcon ? 'px-4' : 'pl-6 pr-4')}>
+          {showIcon && (
+            <Icon
+              role="presentation"
+              className="mr-4 h-14 w-14"
+              name={iconName ?? DEFAULT_TAG_ICON}
+            />
           )}
-          ref={labelRef}>
-          {label}
-        </p>
-        {removeAction}
-      </div>
-    </TagTooltip>
+          <span>{truncatedText}</span>
+          {removeAction}
+        </div>
+      )}
+    </TruncateByCharsWithTooltip>
   );
-  if (Wrapper) {
-    return <Wrapper>{nakedTag}</Wrapper>;
-  }
-  return nakedTag;
 };
