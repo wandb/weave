4b16df7af2617817b88f997dbbf02403d0c47e89
Connie Lee <connie.lee0317@gmail.com>
2025-03-18 12:32:23 -0700

    chore(ui): Update Alias, Pill, and Tag attributes (#3897)

--- weave-js/src/components/Tag/Alias.tsx
+++ weave-js/src/components/Tag/Alias.tsx
@@ -1,10 +1,11 @@
-import classNames from 'classnames';
 import React, {FC, ReactElement} from 'react';
+import {twMerge} from 'tailwind-merge';
 
 import {Icon} from '../Icon';
 import {Tailwind} from '../Tailwind';
 import {RemoveAction} from './RemoveAction';
-import {getTagColorClass, TagColorName} from './utils';
+import {useTagClasses} from './Tag';
+import {TagColorName} from './utils';
 
 export const DEFAULT_ALIAS_ICON = 'email-at';
 
@@ -14,18 +15,21 @@ export type AliasProps = {
   showIcon?: boolean;
 };
 export const Alias: FC<AliasProps> = ({label, color, showIcon = false}) => {
+  const classes = useTagClasses({color, label});
   return (
     <Tailwind>
       <div
-        key={`pill-${label}`}
-        className={classNames(
-          'night-aware',
-          'min-h-22 flex max-h-22 w-fit items-center rounded-[3px] font-mono text-[13px]',
-          showIcon ? 'pl-4 pr-6' : 'px-6',
-          getTagColorClass(color)
+        className={twMerge(
+          classes,
+          'font-mono text-[13px]',
+          showIcon ? 'pl-4 pr-6' : 'px-6'
         )}>
         {showIcon && (
-          <Icon className="mr-4 h-14 w-14" name={DEFAULT_ALIAS_ICON} />
+          <Icon
+            role="presentation"
+            className="mr-4 h-14 w-14"
+            name={DEFAULT_ALIAS_ICON}
+          />
         )}
         <span className="max-w-[24ch] overflow-hidden text-ellipsis whitespace-nowrap">
           {label}
@@ -47,17 +51,16 @@ export const RemovableAlias: FC<RemovableAliasProps> = ({
   color,
   showIcon = false,
 }) => {
+  const classes = useTagClasses({color, label, isInteractive: true});
   return (
     <Tailwind>
-      <div
-        key={`pill-${label}`}
-        className={classNames(
-          'night-aware',
-          'min-h-22 flex max-h-22 w-fit items-center rounded-[3px] px-4 font-mono text-[13px]',
-          getTagColorClass(color)
-        )}>
+      <div className={twMerge(classes, 'px-4 font-mono text-[13px]')}>
         {showIcon && (
-          <Icon className="mr-4 h-14 w-14" name={DEFAULT_ALIAS_ICON} />
+          <Icon
+            role="presentation"
+            className="mr-4 h-14 w-14"
+            name={DEFAULT_ALIAS_ICON}
+          />
         )}
         <span className="max-w-[24ch] overflow-hidden text-ellipsis whitespace-nowrap">
           {label}
