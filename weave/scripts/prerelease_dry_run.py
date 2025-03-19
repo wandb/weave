c9693930ba86f1035c9eebc6e08154c5a28c2d55
Tim Sweeney <tim@wandb.com>
2025-03-19 12:43:07 -0700

    Remove graph view

--- weave/scripts/prerelease_dry_run.py
+++ weave/scripts/prerelease_dry_run.py
@@ -7,31 +7,42 @@
 
 # Run this script with `uv run prerelease_dry_run.py`
 
-import datetime
+# import datetime
+
+# import weave
+
+# # This uniq id ensures that the op is not cached
+# uniq_id = datetime.datetime.now().timestamp()
+
+
+# @weave.op
+# def func(a: int) -> float:
+#     return a + uniq_id
+
+
+# def main() -> None:
+#     client = weave.init("test-project")
+#     res = func(42)
+
+#     client._flush()
+#     calls = func.calls()
+
+#     assert len(calls) == 1
+#     assert calls[0].output == res
+#     assert calls[0].inputs == {"a": 42}
+
+
+# if __name__ == "__main__":
+#     main()
+#     print("Dry run passed")
+
 
 import weave
 
-# This uniq id ensures that the op is not cached
-uniq_id = datetime.datetime.now().timestamp()
+weave.init("test-large-eval")
+
+dataset = weave.Dataset(rows=[{"a": i} for i in range(10_000)])
 
 
-@weave.op
-def func(a: int) -> float:
-    return a + uniq_id
 
 
-def main() -> None:
-    client = weave.init("test-project")
-    res = func(42)
-
-    client._flush()
-    calls = func.calls()
-
-    assert len(calls) == 1
-    assert calls[0].output == res
-    assert calls[0].inputs == {"a": 42}
-
-
-if __name__ == "__main__":
-    main()
-    print("Dry run passed")
