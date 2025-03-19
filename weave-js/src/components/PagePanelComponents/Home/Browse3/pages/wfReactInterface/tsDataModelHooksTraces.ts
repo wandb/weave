00b8c8c5037309d23e3224286a0fa34e5cc40848
Tim Sweeney <tim@wandb.com>
2025-03-19 13:22:27 -0700

    better fix and lint

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/tsDataModelHooksTraces.ts
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/tsDataModelHooksTraces.ts
@@ -27,12 +27,35 @@ export const useBareTraceCalls = (
       try {
         const client = getClient();
         const resProm = fetchBareTraceCalls(client, entity, project, traceId);
-        // TODO: Add back costs
+        // The backend has a bug where calls that are unfinished do not return at all
+        // when requesting costs. Therefore, we have to do a second fetch to get costs
+        // and zip the results together - BAD!. FIXME.
         resProm.then(res => {
           if (mounted) {
             setTraceCalls(res);
             setLoading(false);
             setError(null);
+            const resWithCostsProm = fetchBareTraceCallsWithCosts(
+              client,
+              entity,
+              project,
+              traceId
+            );
+            resWithCostsProm.then(resWithCosts => {
+              if (mounted) {
+                setTraceCalls(currCalls => {
+                  // First, make a map of the calls with cost by id:
+                  const callsWithCosts = new Map<string, TraceCallSchema>();
+                  resWithCosts.forEach(call => {
+                    callsWithCosts.set(call.id, call);
+                  });
+                  // Then, update the calls in the current list with the costs:
+                  return currCalls.map(call => {
+                    return callsWithCosts.get(call.id) ?? call;
+                  });
+                });
+              }
+            });
           }
         });
       } catch (err) {
