6254a9754a27f231ce81ef21e2c99afd6bcf06d2
Connie Lee <connie.lee0317@gmail.com>
2025-03-19 14:24:01 -0700

    feat(ui): Improve Tag/Pill/Alias truncation (#3904)
    * feat(ui): Create TruncateByCharsWithTooltip HOC
    
    * feat(ui): Add truncation features to Alias
    
    * feat(ui): Add truncation features to Pill
    
    * feat(ui): Add truncation features to Tag
    
    * chore(ui): Remove unused code
    
    * chore(ui): Add presentation role on icon
    
    * feat(ui): Add edge cases
    
    * refactor: Update tooltip props

--- weave-js/src/components/Tag/Alias.tsx
+++ weave-js/src/components/Tag/Alias.tsx
@@ -2,44 +2,55 @@ import React, {FC, ReactElement} from 'react';
 import {twMerge} from 'tailwind-merge';
 
 import {Icon} from '../Icon';
-import {Tailwind} from '../Tailwind';
+import {
+  TruncateByCharsProps,
+  TruncateByCharsWithTooltip,
+} from '../TruncateByCharsWithTooltip';
 import {RemoveAction} from './RemoveAction';
 import {useTagClasses} from './Tag';
-import {TagColorName} from './utils';
+import {TAG_DEFAULT_MAX_CHARS, TagColorName} from './utils';
 
 export const DEFAULT_ALIAS_ICON = 'email-at';
 
-export type AliasProps = {
+export type AliasProps = TruncateByCharsProps & {
   label: string;
   color?: TagColorName;
   showIcon?: boolean;
 };
-export const Alias: FC<AliasProps> = ({label, color, showIcon = false}) => {
+export const Alias: FC<AliasProps> = ({
+  label,
+  color,
+  showIcon = false,
+  maxChars = TAG_DEFAULT_MAX_CHARS,
+  truncatedPart,
+  Wrapper,
+}) => {
   const classes = useTagClasses({color, label});
+  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper};
   return (
-    <Tailwind>
-      <div
-        className={twMerge(
-          classes,
-          'font-mono text-[13px]',
-          showIcon ? 'pl-4 pr-6' : 'px-6'
-        )}>
-        {showIcon && (
-          <Icon
-            role="presentation"
-            className="mr-4 h-14 w-14"
-            name={DEFAULT_ALIAS_ICON}
-          />
-        )}
-        <span className="max-w-[24ch] overflow-hidden text-ellipsis whitespace-nowrap">
-          {label}
-        </span>
-      </div>
-    </Tailwind>
+    <TruncateByCharsWithTooltip {...truncationProps}>
+      {({truncatedText}) => (
+        <div
+          className={twMerge(
+            classes,
+            'font-mono text-[13px]',
+            showIcon ? 'pl-4 pr-6' : 'px-6'
+          )}>
+          {showIcon && (
+            <Icon
+              role="presentation"
+              className="mr-4 h-14 w-14"
+              name={DEFAULT_ALIAS_ICON}
+            />
+          )}
+          <span>{truncatedText}</span>
+        </div>
+      )}
+    </TruncateByCharsWithTooltip>
   );
 };
 
-export type RemovableAliasProps = {
+export type RemovableAliasProps = TruncateByCharsProps & {
   label: string;
   removeAction: ReactElement<typeof RemoveAction>;
   color?: TagColorName;
@@ -50,23 +61,27 @@ export const RemovableAlias: FC<RemovableAliasProps> = ({
   removeAction,
   color,
   showIcon = false,
+  maxChars = TAG_DEFAULT_MAX_CHARS,
+  truncatedPart,
+  Wrapper,
 }) => {
   const classes = useTagClasses({color, label, isInteractive: true});
+  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper};
   return (
-    <Tailwind>
-      <div className={twMerge(classes, 'px-4 font-mono text-[13px]')}>
-        {showIcon && (
-          <Icon
-            role="presentation"
-            className="mr-4 h-14 w-14"
-            name={DEFAULT_ALIAS_ICON}
-          />
-        )}
-        <span className="max-w-[24ch] overflow-hidden text-ellipsis whitespace-nowrap">
-          {label}
-        </span>
-        {removeAction}
-      </div>
-    </Tailwind>
+    <TruncateByCharsWithTooltip {...truncationProps}>
+      {({truncatedText}) => (
+        <div className={twMerge(classes, 'px-4 font-mono text-[13px]')}>
+          {showIcon && (
+            <Icon
+              role="presentation"
+              className="mr-4 h-14 w-14"
+              name={DEFAULT_ALIAS_ICON}
+            />
+          )}
+          <span>{truncatedText}</span>
+          {removeAction}
+        </div>
+      )}
+    </TruncateByCharsWithTooltip>
   );
 };
