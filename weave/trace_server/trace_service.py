d315da6c4d159dfd7b50d5c8b480f27017c2044b
andrewtruong <itsandrewtruong@gmail.com>
2025-03-21 14:28:26 -0400

    lint

--- weave/trace_server/trace_service.py
+++ weave/trace_server/trace_service.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from weave.trace_server.trace_server_interface import TraceServerInterface
-
-
 from typing import Protocol
+
+from pydantic import BaseModel
+
+from weave.trace_server.trace_server_interface import TraceServerInterface
 
 
 class ServerInfoRes(BaseModel):
@@ -11,15 +11,16 @@ class ServerInfoRes(BaseModel):
 
 class TraceService(Protocol):
     """This protocol defines the interface definition for a trace service.
-    
+
     TraceService wraps a TraceServerInterface and additionally provides methods
     that are generic across all interfaces, e.g. getting the server info or
     server health.
-    
+
     The intent is to provide a simple interface for weave clients to interact
     with the trace server.  We also have tooling in `weave/trace_server/reference`
     that consumes this interface and provides a convenient FastAPI router.
     """
+
     trace_server_interface: TraceServerInterface
 
     def server_info(self) -> ServerInfoRes: ...
