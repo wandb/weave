637205fb005cc66625bde524fba7e3aeea3bd7a0
Connie Lee <connie.lee@wandb.com>
2025-03-27 10:41:08 -0700

    chore(ui): Fix nested buttons in removable tag/alias

--- weave-js/src/components/Tag/Alias.tsx
+++ weave-js/src/components/Tag/Alias.tsx
@@ -2,6 +2,7 @@ import React, {FC, ReactElement} from 'react';
 import {twMerge} from 'tailwind-merge';
 
 import {Icon} from '../Icon';
+import {Tailwind} from '../Tailwind';
 import {
   TruncateByCharsProps,
   TruncateByCharsWithTooltip,
@@ -63,25 +64,28 @@ export const RemovableAlias: FC<RemovableAliasProps> = ({
   showIcon = false,
   maxChars = TAG_DEFAULT_MAX_CHARS,
   truncatedPart,
-  Wrapper,
+  Wrapper = Tailwind,
 }) => {
   const classes = useTagClasses({color, label, isInteractive: true});
-  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper};
+  const truncationProps = {text: label, maxChars, truncatedPart, Wrapper: null};
+  if (Wrapper === null) {
+    Wrapper = React.Fragment;
+  }
   return (
-    <TruncateByCharsWithTooltip {...truncationProps}>
-      {({truncatedText}) => (
-        <div className={twMerge(classes, 'px-4 font-mono text-[13px]')}>
-          {showIcon && (
-            <Icon
-              role="presentation"
-              className="mr-4 h-14 w-14"
-              name={DEFAULT_ALIAS_ICON}
-            />
-          )}
-          <span>{truncatedText}</span>
-          {removeAction}
-        </div>
-      )}
-    </TruncateByCharsWithTooltip>
+    <Wrapper>
+      <div className={twMerge(classes, 'px-4 font-mono text-[13px]')}>
+        {showIcon && (
+          <Icon
+            role="presentation"
+            className="mr-4 h-14 w-14"
+            name={DEFAULT_ALIAS_ICON}
+          />
+        )}
+        <TruncateByCharsWithTooltip {...truncationProps}>
+          {({truncatedText}) => <span>{truncatedText}</span>}
+        </TruncateByCharsWithTooltip>
+        {removeAction}
+      </div>
+    </Wrapper>
   );
 };
