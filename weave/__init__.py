b3a9c67bfc8cd868d42ac53c40536702bcfcac37
Emmanuel Turlay <emmanuel.turlay@wandb.com>
2025-03-31 10:03:37 -0700

    First pass at

--- weave/__init__.py
+++ weave/__init__.py
@@ -15,6 +15,7 @@ from weave.flow.model import Model
 from weave.flow.obj import Object
 from weave.flow.prompt.prompt import EasyPrompt, MessagesPrompt, Prompt, StringPrompt
 from weave.flow.scorer import Scorer
+from weave.flow.monitor import Monitor
 from weave.initialization import *
 from weave.trace.util import Thread as Thread
 from weave.trace.util import ThreadPoolExecutor as ThreadPoolExecutor
@@ -46,4 +47,5 @@ __docspec__ = [
     Scorer,
     AnnotationSpec,
     Markdown,
+    Monitor,
 ]
