9f5daec74ec6238d62d6914270de69f59fad0904
ruhiparvatam <ruhi.parvatam@wandb.com>
2025-04-01 15:50:07 -0400

    linting

--- weave-js/src/components/Panel2/PanelDir/Component.tsx
+++ weave-js/src/components/Panel2/PanelDir/Component.tsx
@@ -155,14 +155,14 @@ const SubfileRow: React.FC<SubfileRowProps> = props => {
 
   return (
     <Table.Row
-    style={
-      !isDownloadable
-        ? {
-            backgroundColor: 'inherit',
-            pointerEvents: 'none',
-          }
-        : undefined
-    }
+      style={
+        !isDownloadable
+          ? {
+              backgroundColor: 'inherit',
+              pointerEvents: 'none',
+            }
+          : undefined
+      }
       onClick={() => {
         if (file.ref) {
           if (file.ref.startsWith('http://')) {
