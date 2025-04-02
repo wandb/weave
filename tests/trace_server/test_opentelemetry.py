037599eb6f217895aee51a8d5b3fbc6a7b58c347
zbirenbaum <zachary.birenbaum@wandb.com>
2025-04-02 12:05:56 -0700

    lint

--- tests/trace_server/test_opentelemetry.py
+++ tests/trace_server/test_opentelemetry.py
@@ -145,7 +145,7 @@ def test_otel_export_clickhouse(client: weave_client.WeaveClient):
     call = res.calls[0]
     export_span = export_req.traces.resource_spans[0].scope_spans[0].spans[0]
     decoded_trace = hexlify(export_span.trace_id).decode("ascii")
-    decoded_span= hexlify(export_span.span_id).decode("ascii")
+    decoded_span = hexlify(export_span.span_id).decode("ascii")
 
     # TODO: Once postprocessing is added we should have tests for input, output and summary propogation
     assert call.id == decoded_span
