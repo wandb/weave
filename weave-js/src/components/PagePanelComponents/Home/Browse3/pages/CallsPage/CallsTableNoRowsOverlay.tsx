3cd4e6155e211f9256c31dfa6c39d90d71415ec7
gtarpenning <griffin.tarpenning@wandb.com>
2025-04-04 11:02:12 -0700

    better filter handling

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/CallsTableNoRowsOverlay.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/CallsTableNoRowsOverlay.tsx
@@ -62,7 +62,6 @@ export const CallsTableNoRowsOverlay: React.FC<
       return (
         <DateFilterEmptyState
           filterModelResolved={filterModelResolved}
-          opCreatedAt={opCreatedAt ?? null}
           clearFilters={clearFilters}
           setFilterModel={setFilterModel}
         />
@@ -77,7 +76,6 @@ export const CallsTableNoRowsOverlay: React.FC<
     return (
       <DateFilterEmptyState
         filterModelResolved={filterModelResolved}
-        opCreatedAt={opCreatedAt ?? null}
         clearFilters={clearFilters}
         setFilterModel={setFilterModel}
       />
@@ -89,74 +87,54 @@ export const CallsTableNoRowsOverlay: React.FC<
 
 type DateFilterEmptyStateProps = {
   filterModelResolved: GridFilterModel;
-  opCreatedAt: number | null;
   clearFilters?: () => void;
   setFilterModel?: (model: GridFilterModel) => void;
 };
 
 const DateFilterEmptyState: React.FC<DateFilterEmptyStateProps> = ({
   filterModelResolved,
-  opCreatedAt,
   clearFilters,
   setFilterModel,
 }) => {
+  const existingFilter = filterModelResolved.items.find(
+    item => item.field === 'started_at' && item.operator === '(date): after'
+  );
+
+  const now = new Date();
+  const existingFilterDays = existingFilter
+    ? Math.round(
+        (now.getTime() - new Date(existingFilter.value).getTime()) /
+          (24 * 60 * 60 * 1000)
+      )
+    : 0;
+
+  const clearingFilters = !setFilterModel || existingFilterDays > 7;
+
   const expandDateRange = () => {
     if (!setFilterModel) {
       clearFilters?.();
       return;
     }
-    const currentFilterModel = {...filterModelResolved};
-    const items = [...currentFilterModel.items];
 
-    const existingFilter = items.find(
-      item => item.field === 'started_at' && item.operator === '(date): after'
+    const currentFilterModel = {...filterModelResolved};
+    const filteredItems = currentFilterModel.items.filter(
+      item => item.field !== 'started_at'
     );
-    // Remove any existing started_at filters
-    const filteredItems = items.filter(item => item.field !== 'started_at');
 
     // Determine new date range based on opCreatedAt
     let newDateFilter;
-    if (opCreatedAt) {
-      const filterDate = new Date(opCreatedAt);
-      const now = new Date();
-
-      const daysSinceOpCreated = Math.round(
-        (now.getTime() - filterDate.getTime()) / (24 * 60 * 60 * 1000)
-      );
-
-      // If there's an existing filter, calculate how many days it spans
-      // default to the number of days since last op
-      let existingFilterDays = daysSinceOpCreated;
-      if (existingFilter) {
-        const existingDate = new Date(existingFilter.value);
-        existingFilterDays = Math.round(
-          (now.getTime() - existingDate.getTime()) / (24 * 60 * 60 * 1000)
-        );
-      }
-
-      // Determine the appropriate bucket size, ensuring it's larger than the existing filter
-      if (existingFilterDays < 4) {
-        newDateFilter = makeDateFilter(7);
-      } else if (existingFilterDays <= 7) {
-        // Month is a special case, depends on the # of days in last month
-        newDateFilter = makeMonthFilter();
-      } else if (existingFilterDays <= 31) {
-        newDateFilter = makeDateFilter(365);
-      } else {
-        // If we're already at the largest bucket, remove the date filter
-        setFilterModel({
-          ...currentFilterModel,
-          items: filteredItems,
-        });
-        return;
-      }
-    } else {
-      // Impossible (block conditioned on hasDateFilter), but default to 3 months
+    if (existingFilterDays < 4) {
+      newDateFilter = makeDateFilter(7);
+    } else if (existingFilterDays <= 7) {
       newDateFilter = makeMonthFilter();
+    } else {
+      newDateFilter = undefined;
     }
 
     // Add the new date filter
-    filteredItems.push(newDateFilter);
+    if (newDateFilter) {
+      filteredItems.push(newDateFilter);
+    }
 
     // Update the filter model
     setFilterModel({
@@ -165,16 +143,17 @@ const DateFilterEmptyState: React.FC<DateFilterEmptyStateProps> = ({
     });
   };
 
+  const dateRangeText = clearingFilters
+    ? 'clearing your date range'
+    : 'expanding your date range';
+
   return (
     <EmptyStateBox>
       No calls found for the specified date range.{' '}
       <>
         Try{' '}
-        <ClearFiltersAction
-          onClick={expandDateRange}
-          text="expanding your date range"
-        />{' '}
-        or looking for data in a different time period.
+        <ClearFiltersAction onClick={expandDateRange} text={dateRangeText} /> or
+        looking for data in a different time period.
       </>
     </EmptyStateBox>
   );
