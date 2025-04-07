e670d58dd90d69cd26f3a7a2a8a06677288a7da6
andrewtruong <itsandrewtruong@gmail.com>
2025-04-07 17:18:18 -0600

    test

--- tests/trace/test_evaluation_imperative.py
+++ tests/trace/test_evaluation_imperative.py
@@ -1,6 +1,7 @@
 import pytest
 
 from weave import ImperativeEvaluationLogger
+from weave.integrations.integration_utilities import op_name_from_call
 
 
 @pytest.fixture
@@ -19,12 +20,68 @@ def user_model():
 def test_basic_evaluation(client, user_dataset, user_model):
     ev = ImperativeEvaluationLogger()
 
+    model_outputs = []
+    score1_results = []
+    score2_results = []
     for row in user_dataset:
-        model_output = user_model(row["a"], row["b"])
+        model_outputs.append(model_output := user_model(row["a"], row["b"]))
         pred = ev.log_prediction(inputs=row, output=model_output)
-        pred.log_score(scorer_name="greater_than_2_scorer", score=model_output > 2)
-        pred.log_score(scorer_name="greater_than_4_scorer", score=model_output > 4)
+
+        score1_results.append(score1_result := model_output > 2)
+        pred.log_score(scorer_name="greater_than_2_scorer", score=score1_result)
+
+        score2_results.append(score2_result := model_output > 2)
+        pred.log_score(scorer_name="greater_than_4_scorer", score=score2_result)
 
     ev.log_summary({"avg_score": 1.0, "total_examples": 3})
 
     client.flush()
+
+    calls = client.get_calls()
+    assert len(calls) == 14
+
+    evaluate_call = calls[0]
+    assert op_name_from_call(evaluate_call) == "Evaluation.evaluate"
+    assert evaluate_call.inputs["self"]._class_name == "Evaluation"
+    assert evaluate_call.inputs["model"]._class_name == "Model"
+    assert evaluate_call.output == {"avg_score": 1.0, "total_examples": 3}
+
+    for i, (inputs, outputs, score1, score2) in enumerate(
+        zip(user_dataset, model_outputs, score1_results, score2_results)
+    ):
+        predict_index = 1 + i * 4
+
+        predict_and_score_call = calls[predict_index]
+        assert op_name_from_call(predict_and_score_call) == "predict_and_score"
+        assert predict_and_score_call.inputs["self"]._class_name == "Evaluation"
+        assert predict_and_score_call.inputs["model"]._class_name == "Model"
+        assert predict_and_score_call.inputs["inputs"] == inputs
+        assert predict_and_score_call.output["model_output"] == outputs
+
+        feedbacks = list(predict_and_score_call.feedback)
+        assert len(feedbacks) == 2
+        assert feedbacks[0].feedback_type == "wandb.runnable.greater_than_2_scorer"
+        assert feedbacks[1].feedback_type == "wandb.runnable.greater_than_4_scorer"
+
+        predict_call = calls[predict_index + 1]
+        assert op_name_from_call(predict_call) == "predict"
+        assert predict_call.inputs["self"]._class_name == "Model"
+        assert predict_call.inputs["inputs"] == inputs
+        assert predict_call.output == outputs
+
+        scorer1_call = calls[predict_index + 2]
+        assert op_name_from_call(scorer1_call) == "greater_than_2_scorer"
+        assert scorer1_call.inputs["output"]["model_output"] == outputs
+        assert scorer1_call.inputs["inputs"]["inputs"] == inputs
+        assert scorer1_call.output == score1
+
+        scorer2_call = calls[predict_index + 3]
+        assert op_name_from_call(scorer2_call) == "greater_than_4_scorer"
+        assert scorer2_call.inputs["output"]["model_output"] == outputs
+        assert scorer2_call.inputs["inputs"]["inputs"] == inputs
+        assert scorer2_call.output == score2
+
+    summarize_call = calls[13]
+    assert op_name_from_call(summarize_call) == "summarize"
+    assert summarize_call.inputs["self"]._class_name == "Evaluation"
+    assert summarize_call.output == {"avg_score": 1.0, "total_examples": 3}
