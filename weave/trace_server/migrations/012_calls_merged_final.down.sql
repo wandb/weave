b4696600b0895919079d0bc1874c25178b0a2f31
gtarpenning <griffin.tarpenning@wandb.com>
2025-04-07 16:21:39 -0700

    interesting temp table idea

--- weave/trace_server/migrations/012_calls_merged_final.down.sql
+++ weave/trace_server/migrations/012_calls_merged_final.down.sql
@@ -1,2 +1,3 @@
 DROP VIEW IF EXISTS calls_merged_final_view;
 DROP TABLE IF EXISTS calls_merged_final;
+DROP TABLE IF EXISTS temp;
--- weave/trace_server/migrations/012_calls_merged_final.up.sql
+++ weave/trace_server/migrations/012_calls_merged_final.up.sql
@@ -44,6 +44,60 @@ CREATE TABLE IF NOT EXISTS calls_merged_final (
 ) ENGINE = AggregatingMergeTree
 ORDER BY (project_id, inv_started_at);
 
+CREATE TABLE IF NOT EXISTS temp(
+    id String,
+    project_id String,
+    started_at DateTime64(6),
+    created_at DateTime64(6),
+    inv_started_at DateTime64(6),
+    wb_run_id String,
+    wb_user_id String,
+    trace_id String,
+    parent_id String,
+    op_name String,
+    ended_at DateTime64(6),
+    attributes_dump String,
+    inputs_dump String,
+    output_dump String,
+    summary_dump String,
+    display_name String,
+    input_refs Array(String) DEFAULT [],
+    output_refs Array(String) DEFAULT [],
+    exception String,
+    deleted_at DateTime64(6),
+) ENGINE=Null;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS calls_merged_final_view TO calls_merged_final AS
+SELECT
+    id,
+    project_id,
+    anySimpleState(coalesce(temp.started_at, temp.created_at)) as started_at,
+    anySimpleState(-toUnixTimestamp(coalesce(temp.started_at, temp.created_at))) AS inv_started_at,
+
+    anySimpleState(wb_run_id) as wb_run_id,
+    anySimpleStateIf(wb_user_id, isNotNull(temp.started_at)) as wb_user_id,
+    anySimpleState(trace_id) as trace_id,
+    anySimpleState(parent_id) as parent_id,
+    anySimpleState(op_name) as op_name,
+    anySimpleState(ended_at) as ended_at,
+    anySimpleState(attributes_dump) as attributes_dump,
+    anySimpleState(inputs_dump) as inputs_dump,
+    anySimpleState(coalesce(output_dump, '{}')) as output_dump,
+    array_concat_aggSimpleState(input_refs) as input_refs,
+    array_concat_aggSimpleState(output_refs) as output_refs,
+    anySimpleState(exception) as exception,
+    anySimpleState(deleted_at) as deleted_at,
+    anySimpleState(coalesce(summary_dump, '{}')) as summary_dump,
+    anySimpleState(display_name) as display_name
+FROM temp
+GROUP BY
+    project_id,
+    id;
+
+INSERT INTO temp SELECT *, -toUnixTimestamp(coalesce(started_at, created_at)) AS inv_started_at FROM call_parts;
+drop table calls_merged_final_view;
+drop table temp;
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS calls_merged_final_view TO calls_merged_final AS
 SELECT
     id,
@@ -70,3 +124,6 @@ FROM call_parts
 GROUP BY
     project_id,
     id;
+
+
+-- IDEA: https://gist.github.com/den-crane/f7382cd4f1f859ff6ac46afe7dc9925a
