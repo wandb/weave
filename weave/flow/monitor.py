74746e46e03134afc8ad12ec4b1c2dfad2ca45f9
Emmanuel Turlay <emmanuel.turlay@wandb.com>
2025-04-16 09:47:17 -0700

    Merge Tim's fix

--- weave/flow/monitor.py
+++ weave/flow/monitor.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Optional
 
 from pydantic import Field
 from typing_extensions import Self
@@ -6,7 +6,6 @@ from typing_extensions import Self
 from weave.flow.casting import Scorer
 from weave.flow.obj import Object
 from weave.trace.api import ObjectRef, publish
-from weave.trace.object_record import ObjectRecord
 from weave.trace.objectify import register_object
 from weave.trace.vals import WeaveObject
 from weave.trace_server.interface.query import Query
@@ -78,43 +77,4 @@ class Monitor(Object):
 
     @classmethod
     def from_obj(cls, obj: WeaveObject) -> Self:
-        field_values = {}
-        for field_name in cls.model_fields:
-            if hasattr(obj, field_name):
-                field_obj = getattr(obj, field_name)
-
-                # Special-casing the query field to convert
-                # a WeaveObject(ObjectRecord(...)) to a Query
-                if field_name == "query" and field_obj is not None:
-                    field_obj = Query(**_obj_rec_to_query_dict(field_obj._val))
-
-                field_values[field_name] = field_obj
-
-        return cls(**field_values)
-
-
-def _obj_rec_to_query_dict(obj: ObjectRecord) -> dict:
-    """Converts an ObjectRecord to a dictionary that can be used to create a Query."""
-
-    def _treat_value(v: Any) -> Any:
-        if isinstance(v, ObjectRecord):
-            return _obj_rec_to_query_dict(v)
-        elif isinstance(v, list):
-            return [_treat_value(i) for i in v]
-        elif isinstance(v, dict):
-            return {k: _treat_value(v) for k, v in v.items()}
-        else:
-            return v
-
-    def _snake_to_camel(snake_str: str) -> str:
-        components = snake_str.split("_")
-        return components[0] + "".join(x.title() for x in components[1:])
-
-    def _treat_key(k: str) -> str:
-        return f"${_snake_to_camel(k[:-1])}"
-
-    return {
-        _treat_key(k): _treat_value(v)
-        for k, v in obj.__dict__.items()
-        if k != "_class_name" and k != "_bases"
-    }
+        return cls.model_validate(obj.unwrap())
