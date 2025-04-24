98e5190f8b00f529587f4fd8a223d2645ffb7b05
Chance An <chance.an@wandb.com>
2025-04-24 15:27:06 -0700

    properly fixes the table_query method returns duplicated results

--- weave/trace_server/clickhouse_trace_server_batched.py
+++ weave/trace_server/clickhouse_trace_server_batched.py
@@ -1047,8 +1047,8 @@ class ClickHouseTraceServer(tsi.TraceServerInterface):
 
         res = self.table_query_stats_batch(batch_req)
 
-        # if len(res.tables) != 1:
-        #     raise RuntimeError("Unexpected number of results", res)
+        if len(res.tables) != 1:
+            raise RuntimeError("Unexpected number of results", res)
 
         count = res.tables[0].count
         return tsi.TableQueryStatsRes(count=count)
@@ -1063,7 +1063,7 @@ class ClickHouseTraceServer(tsi.TraceServerInterface):
 
         query = """
         SELECT digest, length(row_digests)
-        FROM tables
+        FROM tables_deduped
         WHERE project_id = {project_id:String} AND digest IN {digests:Array(String)}
         """
 
