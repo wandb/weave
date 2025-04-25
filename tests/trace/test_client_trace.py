7c8487750ddc165e8dea6606c26a56dacc012a4e
Andrew Truong <itsandrewtruong@gmail.com>
2025-04-24 21:34:49 -0400

    chore(weave): Add new testing shards (#4278)

--- tests/trace/test_client_trace.py
+++ tests/trace/test_client_trace.py
@@ -122,20 +122,6 @@ def test_simple_op(client):
     )
 
 
-def test_dataset(client):
-    from weave.flow.dataset import Dataset
-
-    d = Dataset(rows=[{"a": 5, "b": 6}, {"a": 7, "b": 10}])
-    ref = weave.publish(d)
-    d2 = weave.ref(ref.uri()).get()
-
-    # This might seem redundant, but it is useful to ensure that the
-    # dataset can be re-iterated over multiple times and equality is preserved.
-    assert list(d2.rows) == list(d2.rows)
-    assert list(d.rows) == list(d2.rows)
-    assert list(d.rows) == list(d.rows)
-
-
 def test_trace_server_call_start_and_end(client):
     call_id = generate_id()
     trace_id = generate_id()
