a79d93ec70f8f6978d0247ae232915c275206f9a
Emmanuel Turlay <emmanuel.turlay@wandb.com>
2025-04-25 10:56:36 -0700

    Cleanup after merge

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/TypeVersionCategoryChip.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/TypeVersionCategoryChip.tsx
@@ -16,6 +16,7 @@ const colorMap: Record<KnownBaseObjectClassType, TagColorName> = {
   EvaluationResults: 'moon',
   Provider: 'gold',
   ProviderModel: 'blue',
+  Monitor: 'gold',
 };
 
 export const TypeVersionCategoryChip: React.FC<{
