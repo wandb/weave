536d359592f1ef7413709459203c66f4335244f0
Martin Mark <martin@martinmark.com>
2025-04-30 13:15:56 -0400

    Add proper loader to RedirectToLastView

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/RedirectToLastView.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CallsPage/RedirectToLastView.tsx
@@ -4,7 +4,7 @@ import {Redirect, useLocation} from 'react-router-dom';
 import {useProjectInfo} from '../../../../../../common/hooks/useProjectInfo';
 import {safeLocalStorage} from '../../../../../../core/util/localStorage';
 import {Alert} from '../../../../../Alert';
-import {Loading} from '../../../../../Loading';
+import {WaveLoader} from '../../../../../Loaders/WaveLoader';
 import {getDefaultViewId} from '../SavedViews/savedViewUtil';
 
 type RedirectToLastViewProps = {
@@ -24,7 +24,23 @@ export const RedirectToLastView = ({
     project
   );
   if (loadingProjectInfo) {
-    return <Loading />;
+    return (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+          zIndex: 1,
+        }}>
+        <WaveLoader size="huge" />
+      </div>
+    );
   }
   if (!projectInfo) {
     return <Alert severity="error">Invalid project: {project}</Alert>;
