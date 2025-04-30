ffe4761bc72ade968766a92f3da1435757f2072b
zbirenbaum <zachary.birenbaum@wandb.com>
2025-04-30 13:01:16 -0700

    support MP3 files

--- weave/type_handlers/Audio/audio.py
+++ weave/type_handlers/Audio/audio.py
@@ -1,17 +1,38 @@
 import wave
 
+from typing import TYPE_CHECKING, Any, Union, Protocol
+from enum import Enum
+import os
+import shutil
 from weave.trace.serialization import serializer
 from weave.trace.serialization.custom_objs import MemTraceFilesArtifact
+from .utils import DEFAULT_VIDEO_FORMAT, AudioFile, AudioFormat, SUPPORTED_FORMATS, get_format_from_filename
 
-AUDIO_FILE_NAME = "audio.wav"
+try:
+    import pydub
+    has_pydub = True
+except ImportError:
+    has_pydub = False
+
+if TYPE_CHECKING:
+    import pydub
 
 
-def save(obj: wave.Wave_read, artifact: MemTraceFilesArtifact, name: str) -> None:
+class SupportsAudioSegmentExport(Protocol):
+    def export(self, out_f=None, format='mp3', codec=None, bitrate=None, parameters=None, tags=None, id3v2_version='4', cover=None):
+        ...
+
+DEFAULT_AUDIO_FORMAT = AudioFormat.MP3
+
+AudioType = Union[wave.Wave_read, AudioFile, "pydub.AudioSegment"]
+
+
+def save_wave(obj: wave.Wave_read, artifact: MemTraceFilesArtifact, name: str) -> None:
     original_frame_position = obj.tell()
     obj.rewind()
     frames = obj.readframes(obj.getnframes())
     params = obj.getparams()
-    with artifact.writeable_file_path(AUDIO_FILE_NAME) as fp:
+    with artifact.writeable_file_path('audio.wav') as fp:
         with wave.open(fp, "w") as wav_file:
             # Exclude nframes param, it is often set as the maximum number of frames
             # which bumps into the 4GB max file size when creating the wave.Wave_write
@@ -24,11 +45,50 @@ def save(obj: wave.Wave_read, artifact: MemTraceFilesArtifact, name: str) -> Non
     # Rewind to the original position
     obj.setpos(original_frame_position)
 
+def save(obj: AudioType, artifact: MemTraceFilesArtifact, name: str) -> None:
+    if isinstance(obj, AudioFile):
+        exists = os.path.exists(obj.path)
+        if not exists:
+            raise ValueError(f"Audio file does not exist: {obj.path}")
+        ext = obj.extension
+        # If it's not supported we can't copy it directly
+        if ext not in SUPPORTED_FORMATS:
+            raise ValueError(f"Unsupported audio format: {ext} - Supported formats are: {' '.join(SUPPORTED_FORMATS)}")
+        fname = f"audio.{ext}"
+        with artifact.writeable_file_path(fname) as fp:
+            # Copy the file to the artifact
+            obj.export(fp)
+    elif isinstance(obj, "pydub.AudioSegment"):
+        obj.export(artifact.writeable_file_path(f"audio.{DEFAULT_VIDEO_FORMAT}"), format=DEFAULT_VIDEO_FORMAT)
+    else:
+        # Object is a wave.Wave_read object
+        save_wave(obj, artifact, name)
 
-def load(artifact: MemTraceFilesArtifact, name: str) -> wave.Wave_read:
-    path = artifact.path(AUDIO_FILE_NAME)
-    return wave.open(path, "rb")
 
+def load(artifact: MemTraceFilesArtifact, name: str) -> wave.Wave_read | pydub.AudioSegment:
+    for filename in artifact.path_contents:
+        path = artifact.path(filename)
+        if filename.startswith("video."):
+            fmt = get_format_from_filename(filename)
+            if fmt == AudioFormat.UNSUPPORTED:
+                raise ValueError(f"Unsupported audio format: {filename} - Supported formats are: {' '.join(SUPPORTED_FORMATS)}")
+            elif fmt != AudioFormat.WAV:
+                if not has_pydub:
+                    raise ValueError(f"Pydub is required to retrieve {fmt.value} audio files")
+                # If the file is in a supported format, we can load it directly
+                return pydub.AudioSegment.from_file(path, format=fmt.value)
+            else:
+                # File is in WAV format, return it as a wave.Wave_read object
+                wave_file = wave.open(path, "rb")
+                return wave_file
+
+    raise ValueError("No audio found for artifact")
+
+def is_audio_instance(obj: Any) -> bool:
+    if has_pydub:
+        return isinstance(obj, (AudioFile, pydub.AudioSegment, wave.Wave_read))
+    else:
+        return isinstance(obj, (AudioFile, wave.Wave_read))
 
 def register() -> None:
     serializer.register_serializer(wave.Wave_read, save, load)
---
+++ weave/type_handlers/Audio/utils.py
@@ -0,0 +1,69 @@
+import wave
+
+from typing import Any, Protocol, TypeVar, Union
+from enum import Enum
+import shutil
+from weave.trace.serialization import serializer
+from weave.trace.serialization.custom_objs import MemTraceFilesArtifact
+
+try:
+    from pydub import AudioSegment
+    has_pydub = True
+except ImportError:
+    AudioSegment = None
+    has_pydub = False
+
+from os import PathLike
+class AudioFormat(str, Enum):
+    """
+    These are NOT the list of formats we accept from the user
+    Rather, these are the list of formats we can save to weave servers
+    If we detect that the file is in these formats, we copy it over directly
+    Otherwise, we encode it to one of these formats using ffmpeg (mp3 by default)
+    """
+
+    MP3 = "mp3"
+    WAV = "wav"
+    UNSUPPORTED = "unsupported"
+
+    def __str__(self) -> str:
+        return self.value
+
+    @classmethod
+    def _missing_(cls, value: Any) -> "AudioFormat":
+        return cls.UNSUPPORTED
+
+SUPPORTED_FORMATS = [fmt.value for fmt in AudioFormat if fmt != AudioFormat.UNSUPPORTED]
+
+DEFAULT_VIDEO_FORMAT = AudioFormat.MP3
+
+def get_format_from_filename(filename: str) -> AudioFormat:
+    """Get the file format from a filename.
+    Args:
+        filename: The filename to extract the format from
+    Returns:
+        The format string or None if no extension is found
+    """
+    # Get last dot position
+    last_dot = filename.rfind(".")
+
+    # If there's no dot or it's the last character, return None
+    if last_dot == -1 or last_dot == len(filename) - 1:
+        return AudioFormat.UNSUPPORTED
+
+    # Get the extension without the dot
+    return AudioFormat(filename[last_dot + 1 :])
+
+class AudioFile(PathLike):
+    path: str
+    extension: AudioFormat
+
+    def __init__(self, path: str):
+        self.extension = get_format_from_filename(path)
+        if self.extension == AudioFormat.UNSUPPORTED:
+            raise ValueError(f"Unsupported or missing file format: {path} - Supported Extensions: {' '.join(SUPPORTED_FORMATS)}")
+        self.path = path
+
+    def export(self, fp: str) -> None:
+        shutil.copyfile(self.path, fp)
+
