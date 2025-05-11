5352d072064771f0cf7ea9a965976d8d8ff213de
Nicholas Pun <182540099+nicholaspun-wandb@users.noreply.github.com>
2025-05-10 21:29:19 -0700

    use predownload context

--- weave_query/weave_query/ops_domain/table.py
+++ weave_query/weave_query/ops_domain/table.py
@@ -7,6 +7,7 @@ import typing
 from weave_query import (
     artifact_fs,
     artifact_wandb,
+    context_state,
     engine_trace,
     errors,
     io_service,
@@ -19,6 +20,7 @@ from weave_query import timestamp as weave_timestamp
 from weave_query import weave_types as types
 from weave_query.api import op, weave_class
 from weave_query.ops_domain import trace_tree, wbmedia
+
 
 @dataclasses.dataclass(frozen=True)
 class TableType(types.ObjectType):
@@ -680,11 +682,15 @@ def _get_table_data_from_file(file: artifact_fs.FilesystemArtifactFile) -> dict:
             data = json.load(f)
     return data
 
+
 def _get_incremental_table_awl_from_file(
-    data: dict,
-    file: artifact_fs.FilesystemArtifactFile
+    data: dict, file: artifact_fs.FilesystemArtifactFile
 ) -> ops_arrow.ArrowWeaveList:
-    from weave_query.ops_domain.wb_util import escape_artifact_path, _filesystem_artifact_file_from_artifact_path
+    from weave_query.ops_domain.wb_util import (
+        _filesystem_artifact_file_from_artifact_path,
+        escape_artifact_path,
+    )
+
     all_awls: list[ops_arrow.ArrowWeaveList] = []
     files = {}
     if "previous_increments_paths" in data:
@@ -692,20 +698,26 @@ def _get_incremental_table_awl_from_file(
         # If we have more than 100, otherwise use all increments
         all_increment_paths = data["previous_increments_paths"]
         MAX_PREVIOUS_INCREMENTS = 99
-        increment_paths = all_increment_paths[-MAX_PREVIOUS_INCREMENTS:] if len(all_increment_paths) > MAX_PREVIOUS_INCREMENTS else all_increment_paths
+        increment_paths = (
+            all_increment_paths[-MAX_PREVIOUS_INCREMENTS:]
+            if len(all_increment_paths) > MAX_PREVIOUS_INCREMENTS
+            else all_increment_paths
+        )
         escaped_paths = [escape_artifact_path(path) for path in increment_paths]
         for path in escaped_paths:
             fs_art_file = _filesystem_artifact_file_from_artifact_path(path)
             files[fs_art_file.path] = fs_art_file
 
     files[file.path] = file
-    
+
     asyncio.run(ensure_files(files))
     rrows: list[list] = []
     object_types: list[types.Type] = []
     for incremental_file in files.values():
         incremental_data = _get_table_data_from_file(incremental_file)
-        rows, object_type = _get_rows_and_object_type_awl_from_file(incremental_data, file)
+        rows, object_type = _get_rows_and_object_type_awl_from_file(
+            incremental_data, file
+        )
         rrows.append(rows)
         object_types.append(object_type)
 
@@ -718,8 +730,6 @@ def _get_incremental_table_awl_from_file(
     return arrow_weave_list
 
 
-
-
 def _get_table_like_awl_from_file(
     file: typing.Union[
         artifact_fs.FilesystemArtifactFile, artifact_fs.FilesystemArtifactDir, None
@@ -728,7 +738,13 @@ def _get_table_like_awl_from_file(
 ) -> _TableLikeAWLFromFileResult:
     if file is None or isinstance(file, artifact_fs.FilesystemArtifactDir):
         raise errors.WeaveInternalError("File is None or a directory")
-    data = _get_table_data_from_file(file)
+
+    file_table_predownload = context_state.get_file_table_predownload()
+    maybe_file_content = file_table_predownload.get(
+        file.artifact._read_artifact_uri.with_path(file.path), None
+    )
+    data = maybe_file_content or _get_table_data_from_file(file)
+
     if "log_mode" in data and data["log_mode"] == "INCREMENTAL":
         awl = _get_incremental_table_awl_from_file(data, file)
     elif file.path.endswith(".joined-table.json"):
@@ -838,7 +854,7 @@ async def ensure_files(files: dict[str, artifact_fs.FilesystemArtifactFile]):
     client = io_service.get_async_client()
     loop = asyncio.get_running_loop()
     tasks = set()
-    
+
     async with client.connect() as conn:
         for file in files.values():
             if (
