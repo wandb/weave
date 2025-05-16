4004ba275e2b9ff49b4f50f30ec8904d7c10d24f
domphan-wandb <dominic.phan@wandb.com>
2025-05-15 17:14:15 -0700

    chore(weave_query): Only load incremental tables if in a run workspace (#4445)

--- weave_query/weave_query/ops_domain/table.py
+++ weave_query/weave_query/ops_domain/table.py
@@ -684,7 +684,6 @@ def _get_incremental_table_awl_from_file(
     data: dict,
     file: artifact_fs.FilesystemArtifactFile
 ) -> ops_arrow.ArrowWeaveList:
-    from weave_query.ops_domain.wb_util import escape_artifact_path, _filesystem_artifact_file_from_artifact_path
     all_awls: list[ops_arrow.ArrowWeaveList] = []
     files = {}
     if "previous_increments_paths" in data:
@@ -725,11 +724,12 @@ def _get_table_like_awl_from_file(
         artifact_fs.FilesystemArtifactFile, artifact_fs.FilesystemArtifactDir, None
     ],
     num_parts: int = 1,
+    load_increments: bool = False
 ) -> _TableLikeAWLFromFileResult:
     if file is None or isinstance(file, artifact_fs.FilesystemArtifactDir):
         raise errors.WeaveInternalError("File is None or a directory")
     data = _get_table_data_from_file(file)
-    if "log_mode" in data and data["log_mode"] == "INCREMENTAL":
+    if load_increments and "log_mode" in data and data["log_mode"] == "INCREMENTAL":
         awl = _get_incremental_table_awl_from_file(data, file)
     elif file.path.endswith(".joined-table.json"):
         awl = _get_joined_table_awl_from_file(data, file)
@@ -898,6 +898,19 @@ def file_table(file: artifact_fs.FilesystemArtifactFile) -> typing.Optional[Tabl
     # Prevent a panel crash from stale file handle errors
     # There are rare stale file handle errors that cause panel crashes as noted:
     # https://wandb.atlassian.net/browse/WB-22355
+    except OSError as e:
+        import errno
+
+        if e.errno == errno.ESTALE:
+            return None
+        raise
+
+@op(name="file-table_with_increments", hidden=True)
+def file_table_with_increments(file: artifact_fs.FilesystemArtifactFile) -> typing.Optional[Table]:
+    try:
+        return Table(_get_table_like_awl_from_file(file, load_increments=True).awl)
+    except FileNotFoundError as e:
+        return None
     except OSError as e:
         import errno
 
