0c1aff96151d020e262ad8be026cb61a3b055495
Tim Sweeney <tim@wandb.com>
2025-05-21 15:48:45 -0700

    chore(weave): final direct codex change

--- bin/codex_setup.sh
+++ bin/codex_setup.sh
@@ -3,7 +3,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv tool install nox
 uv tool install pre-commit
-nox --install-only -e "tests-3.12(shard='lint')"
+nox --install-only -e lint
 nox --install-only -e "tests-3.12(shard='custom')"
 nox --install-only -e "tests-3.12(shard='trace')"
 nox --install-only -e "tests-3.12(shard='flow')"
