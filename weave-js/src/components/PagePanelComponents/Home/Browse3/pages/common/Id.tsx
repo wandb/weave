44afdb04906b14bb55c86e08db321cf9d4838343
Martin Mark <martin@martinmark.com>
2025-05-24 11:56:31 -0400

    Same treatment for all ID hovers

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/Id.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/common/Id.tsx
@@ -2,7 +2,7 @@
  * Show truncated ID with full value in a tooltip.
  */
 
-import {GREEN_600, MOON_150} from '@wandb/weave/common/css/color.styles';
+import {GREEN_600, TEAL_500, MOON_150} from '@wandb/weave/common/css/color.styles';
 import {IconCheckmark} from '@wandb/weave/components/Icon';
 import {Tooltip} from '@wandb/weave/components/Tooltip';
 import {TooltipDeprecated} from '@wandb/weave/components/TooltipDeprecated';
@@ -20,6 +20,9 @@ export const IdPanel = styled.div<{clickable?: boolean}>`
   font-size: 10px;
   line-height: 20px;
   cursor: ${props => (props.clickable ? 'pointer' : 'default')};
+  &:hover {
+    color: ${TEAL_500};
+  }
 `;
 IdPanel.displayName = 'S.IdPanel';
 
