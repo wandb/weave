2d786d048afc41f34dd6e6147a2afe9bbd20744a
Tim Sweeney <tim@wandb.com>
2025-05-27 06:48:06 +1000

    init

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CompareEvaluationsPage/sections/ExampleCompareSection/ExampleCompareSectionDetail.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/CompareEvaluationsPage/sections/ExampleCompareSection/ExampleCompareSectionDetail.tsx
@@ -42,6 +42,7 @@ import {
 } from '../../ecpUtil';
 import {usePeekCall} from '../../hooks';
 import {HorizontalBox, VerticalBox} from '../../Layout';
+import { EvaluationCallLink } from '../ComparisonDefinitionSection/EvaluationDefinition';
 import {
   ComparisonPill,
   SCORER_VARIATION_WARNING_EXPLANATION,
@@ -499,10 +500,11 @@ export const ExampleCompareSectionDetail: React.FC<{
     const evaluationCall = props.state.summary.evaluationCalls[currEvalCallId];
     if (trialEntity && trialProject && trialOpName && trialCallId) {
       return (
-        <Box
+        <HorizontalBox
           style={{
             overflow: 'hidden',
           }}>
+          <EvaluationCallLink callId={currEvalCallId} state={props.state} />|
           <CallLink
             entityName={trialEntity}
             projectName={trialProject}
@@ -511,7 +513,7 @@ export const ExampleCompareSectionDetail: React.FC<{
             icon={<Icon name="filled-circle" color={evaluationCall.color} />}
             color={MOON_800}
           />
-        </Box>
+        </HorizontalBox>
       );
     }
     return null;
