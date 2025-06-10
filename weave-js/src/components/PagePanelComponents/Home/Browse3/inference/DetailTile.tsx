ffd976a8bc8d933080d3c3630211538347c95c9c
Jamie Rasmussen <jamie.rasmussen@wandb.com>
2025-06-09 21:53:49 -0500

    chore(ui): more model catalog feedback

--- weave-js/src/components/PagePanelComponents/Home/Browse3/inference/DetailTile.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/inference/DetailTile.tsx
@@ -14,8 +14,10 @@ const NON_BREAKING_SPACE = '\u00A0';
 
 export const DetailTile = ({header, footer, children}: DetailTileProps) => {
   return (
-    <div className="flex h-[96px] w-[150px] flex-col border border-moon-250">
-      <div className="text-center text-sm font-semibold">{header}</div>
+    <div className="flex h-[96px] w-[170px] flex-col rounded-lg border border-moon-250 p-8">
+      <div className="text-center text-sm font-semibold text-moon-600">
+        {header}
+      </div>
       <div className="flex flex-grow items-center justify-center">
         {children}
       </div>
