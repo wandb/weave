9839b1bb05183b2e4249f5d81a0e26e79e5751af
Martin Mark <martin@martinmark.com>
2025-06-11 06:57:40 -0400

    Add line-clamp to playground settings

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/PlaygroundPage/PlaygroundSettings/PlaygroundSettings.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/PlaygroundPage/PlaygroundSettings/PlaygroundSettings.tsx
@@ -59,7 +59,7 @@ export const PlaygroundSettings: React.FC<PlaygroundSettingsProps> = ({
     <div className="relative flex h-full w-[320px] shrink-0 flex-col border-l border-moon-250 pb-4">
       <div className="flex items-center justify-between gap-8 border-b border-moon-250 px-16 py-8">
         {/* Header */}
-        <div className="flex items-center gap-8">
+        <div className="flex items-center line-clamp-1 [&>span]:line-clamp-1 gap-8">
           <Tag label={`${settingsTab + 1}`} />
           <Tooltip
             content={playgroundStates[settingsTab].model}
