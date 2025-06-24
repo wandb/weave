f230f3378470354ef28294aaee138337af6dad7b
zbirenbaum <zachary.birenbaum@wandb.com>
2025-06-24 13:21:16 -0700

    Fallback

--- weave-js/src/components/PagePanelComponents/Home/Browse3/typeViews/Content/ContentView.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/typeViews/Content/ContentView.tsx
@@ -1,12 +1,102 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import {LoadingDots} from '@wandb/weave/components/LoadingDots';
 import {useWFHooks} from '@wandb/weave/components/PagePanelComponents/Home/Browse3/pages/wfReactInterface/context';
-import {ContentMetadata, ContentViewProps} from './types';
+import {ContentMetadata, ContentViewProps, ContentViewMetadataLoadedProps} from './types';
 import {ImageContent} from './ImageView';
 import {AudioContent} from './AudioView';
 import { PDFContent } from './PDFView';
 import { VideoContent } from './VideoView';
+import {TailwindContents} from '@wandb/weave/components/Tailwind';
+import {ContentMetadataTooltip, ContentTooltipWrapper, DownloadButton, getIconName, IconWithText} from './Shared';
+
+// Save a Blob as a content in the user's downloads folder in a
+// cross-browser compatible way.
+const saveBlob = (blob: Blob, filename: string) => {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  });
+};
+
+const FallbackContent = (props: ContentViewMetadataLoadedProps) => {
+  const [contentResult, setContentResult] = useState<Blob | null>(null);
+  const [isDownloading, setIsDownloading] = useState(false);
+  const {useFileContent} = useWFHooks();
+  const {metadata, project, entity, content} = props;
+  const {filename, size, mimetype} = metadata;
+
+  const contentContent = useFileContent({
+    entity,
+    project,
+    digest: content,
+    skip: !isDownloading,
+  });
+
+  // Store the last non-null content content result in state
+  useEffect(() => {
+    if (contentContent.result) {
+      const blob = new Blob([contentContent.result], {
+        type: mimetype,
+      });
+
+      setContentResult(blob);
+      setIsDownloading(false);
+    }
+  }, [contentContent.result, mimetype]);
+
+  const doSave = useCallback(() => {
+    if (!contentResult) {
+      if (!isDownloading) {
+        setIsDownloading(true);
+      }
+      return;
+    }
+    saveBlob(contentResult, filename);
+  }, [contentResult, filename, isDownloading]);
+
+  const iconName = getIconName(mimetype);
+
+  const iconWithText = (
+    <div>
+      <IconWithText
+        iconName={iconName}
+        filename={filename}
+      />
+    </div>
+  );
+
+  const tooltipTrigger = (
+    <ContentTooltipWrapper
+      showPreview={false}
+      tooltipHint="Click button to download"
+      body={iconWithText}
+    >
+      <ContentMetadataTooltip
+        filename={filename}
+        mimetype={mimetype}
+        size={size}
+      />
+    </ContentTooltipWrapper>
+  );
+
+  return (
+    <TailwindContents>
+      <div className="group flex items-center gap-4">
+        {tooltipTrigger}
+        <div className="opacity-0 group-hover:opacity-100">
+          <DownloadButton isDownloading={isDownloading} doSave={doSave} />
+        </div>
+      </div>
+    </TailwindContents>
+  );
+};
 
 export const ContentView = ({entity, project, data}: ContentViewProps) => {
   const {useFileContent} = useWFHooks();
@@ -36,9 +126,10 @@ export const ContentView = ({entity, project, data}: ContentViewProps) => {
   }
 
   const content = data.files['content'];
+  let contentView;
 
   if (metadataJson.mimetype.startsWith('image')) {
-    return (
+    contentView = (
       <ImageContent
         entity={entity}
         project={project}
@@ -48,7 +139,7 @@ export const ContentView = ({entity, project, data}: ContentViewProps) => {
     )
   }
   else if (metadataJson.mimetype.startsWith('audio')) {
-    return (
+    contentView = (
       <AudioContent
         entity={entity}
         project={project}
@@ -58,18 +149,28 @@ export const ContentView = ({entity, project, data}: ContentViewProps) => {
     )
   }
   else if (metadataJson.mimetype.startsWith('video')) {
-    return (
+    contentView = (
       <VideoContent
         entity={entity}
         project={project}
         metadata={metadataJson}
         content={content}
       />
-    )
+    );
   }
   else if (metadataJson.mimetype === ('application/pdf')) {
-    return (
+    contentView = (
       <PDFContent
+        entity={entity}
+        project={project}
+        metadata={metadataJson}
+        content={content}
+      />
+    );
+  }
+  else {
+    contentView = (
+      <FallbackContent
         entity={entity}
         project={project}
         metadata={metadataJson}
@@ -77,17 +178,5 @@ export const ContentView = ({entity, project, data}: ContentViewProps) => {
       />
     )
   }
-  return (
-    <div>
-      Fallback
-    </div>
-  )
-  //   <ContentViewMetadataLoaded
-  //     data={data}
-  //     entity={entity}
-  //     project={project}
-  //     metadata={metadataJson}
-  //     content={content}
-  //   />
-  // );
+  return contentView;
 };
