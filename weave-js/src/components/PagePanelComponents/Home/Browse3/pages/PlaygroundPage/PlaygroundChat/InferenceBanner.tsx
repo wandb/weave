2a8833786f7a300b6edcb1d51d0e00a0a62c39ca
Jamie Rasmussen <jamie.rasmussen@wandb.com>
2025-06-25 09:57:53 -0500

    Fix banner states

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/PlaygroundPage/PlaygroundChat/InferenceBanner.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/PlaygroundPage/PlaygroundChat/InferenceBanner.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 import {Alert} from '../../../../../../Alert';
 import {AccountPlanType} from '../../../inference/types';
 import {useInferenceContext} from '../../../inference/util';
-import {differenceInCalendarDaysUTC, isAfter} from './date';
-
-const TODAY = new Date();
 
 export const InferenceBanner = () => {
   const inferenceContext = useInferenceContext();
@@ -15,9 +12,7 @@ export const InferenceBanner = () => {
     accountPlanType,
     usage,
     inferenceSafetyLimit,
-    billingPeriodStart,
     billingPeriodEnd,
-    formatAsDollar,
     getFormattedBillingDate,
   } = billing;
 
@@ -26,47 +21,31 @@ export const InferenceBanner = () => {
 
   switch (accountPlanType) {
     case AccountPlanType.Free:
-    case AccountPlanType.ThirtyDayTrial:
-      if (usage >= inferenceSafetyLimit) {
-        exceededLimitBoldStatusMsg = 'Out of W&B Inference credits.';
-        exceededLimitStatusMsg =
-          'To continue using Inference, please upgrade your plan.';
-      }
-      break;
-    case AccountPlanType.Teams:
-    case AccountPlanType.Pro:
-      if (usage >= inferenceSafetyLimit) {
-        exceededLimitBoldStatusMsg = `You've hit your ${formatAsDollar(
-          inferenceSafetyLimit / 100
-        )} monthly W&B Inference spend limit.`;
-        exceededLimitStatusMsg = `Want a higher cap? Talk to support or wait for your limit to reset on ${getFormattedBillingDate(
-          billingPeriodEnd
-        )}`;
-      }
-      break;
-    case AccountPlanType.Enterprise:
-      if (
-        inferenceSafetyLimit === 0 ||
-        isAfter(TODAY, billingPeriodEnd) ||
-        isAfter(billingPeriodStart, TODAY)
-      ) {
-        break; // no banner if outside the billing period or no limit set
-      } else if (usage >= inferenceSafetyLimit) {
-        exceededLimitBoldStatusMsg = `You've hit your ${formatAsDollar(
-          inferenceSafetyLimit / 100
-        )} W&B Inference spend limit.`;
-        exceededLimitStatusMsg = 'Want a higher cap? Talk to support.';
-      } else if (differenceInCalendarDaysUTC(billingPeriodEnd, TODAY) <= 30) {
-        exceededLimitBoldStatusMsg = `You have ${differenceInCalendarDaysUTC(
-          billingPeriodEnd,
-          TODAY
-        )} days remaining in your W&B Inference trial.`;
-        exceededLimitStatusMsg =
-          'Once your trial expires access to W&B Inference will be restricted.';
-      } else if (usage >= inferenceSafetyLimit * 0.9) {
+      if (inferenceSafetyLimit * 0.9 <= usage && usage < inferenceSafetyLimit) {
         exceededLimitBoldStatusMsg = 'Almost out of W&B Inference credits.';
-        exceededLimitStatusMsg =
-          'To continue using W&B Inference without disruption, talk to sales.';
+        exceededLimitStatusMsg = 'Upgrade your plan to keep going.';
+      }
+      break;
+    case AccountPlanType.ThirtyDayTrial:
+      if (inferenceSafetyLimit * 0.9 <= usage && usage < inferenceSafetyLimit) {
+        exceededLimitBoldStatusMsg = 'Almost out of W&B Inference credits.';
+        exceededLimitStatusMsg = 'To avoid interruption, upgrade your plan.';
+      }
+      break;
+    case AccountPlanType.Pro:
+      if (inferenceSafetyLimit * 0.9 <= usage && usage < inferenceSafetyLimit) {
+        exceededLimitBoldStatusMsg = 'Almost out of W&B Inference credits.';
+        exceededLimitStatusMsg = `You'll receive more credits on ${getFormattedBillingDate(
+          billingPeriodEnd
+        )}. Once you're out, usage will switch to pay-as-you-go pricing.`;
+      }
+      break;
+    case AccountPlanType.Academic:
+      if (inferenceSafetyLimit * 0.9 <= usage && usage < inferenceSafetyLimit) {
+        exceededLimitBoldStatusMsg = 'Almost out of W&B Inference credits.';
+        exceededLimitStatusMsg = `Credits refresh on ${getFormattedBillingDate(
+          billingPeriodEnd
+        )}. W&B Inference will pause after the credit limit is reached.`;
       }
       break;
   }
