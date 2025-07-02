efb877f48446f212d305b49fddddc7dbc8c60eea
zbirenbaum <zachary.birenbaum@wandb.com>
2025-07-02 12:53:12 -0700

    Add back File with deprecation

--- weave/type_handlers/__init__.py
+++ weave/type_handlers/__init__.py
@@ -1,10 +1,12 @@
 from weave.type_handlers.Audio import audio
 from weave.type_handlers.Content import content
 from weave.type_handlers.DateTime import datetime
+from weave.type_handlers.File import file
 from weave.type_handlers.Image import image
 from weave.type_handlers.Markdown import markdown
 from weave.type_handlers.Video import video
 
+file.register()
 content.register()
 image.register()
 audio.register()
