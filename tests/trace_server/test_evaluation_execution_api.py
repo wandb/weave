56931511770a1210c1994205c66a17cc54034fa7
Tim Sweeney <tim@wandb.com>
2025-07-01 18:33:13 -0700

    complete initial work

--- tests/trace_server/test_evaluation_execution_api.py
+++ tests/trace_server/test_evaluation_execution_api.py
@@ -1,5 +1,6 @@
 import pytest
 
+from tests.trace_server.completions_util import with_simple_mock_litellm_completion
 from weave.trace.refs import ObjectRef
 from weave.trace_server.trace_server_interface import (
     ObjCreateReq,
@@ -20,7 +21,8 @@ async def test_run_model(trace_server: TraceServerInterface):
         "default_params": {
             "messages_template": [
                 {"role": "system", "content": "You are a helpful assistant."},
-            ]
+            ],
+            "response_format": "text",
         },
     }
     model_create_res = trace_server.obj_create(
@@ -37,22 +39,24 @@ async def test_run_model(trace_server: TraceServerInterface):
     )
     model_digest = model_create_res.digest
 
-    model_run_res = await trace_server.run_model(
-        RunModelReq.model_validate(
-            {
-                "project_id": project_id,
-                "model_ref": ObjectRef(
-                    entity=entity,
-                    project=project,
-                    name=model_object_id,
-                    _digest=model_digest,
-                ).uri(),
-                "inputs": {
-                    "input_type": "value",
-                    "value": {"user_input": "Hello, how are you?"},
-                },
-            }
+    expected_output = "Fantastic - how are you?"
+    with with_simple_mock_litellm_completion(expected_output):
+        model_run_res = await trace_server.run_model(
+            RunModelReq.model_validate(
+                {
+                    "project_id": project_id,
+                    "model_ref": ObjectRef(
+                        entity=entity,
+                        project=project,
+                        name=model_object_id,
+                        _digest=model_digest,
+                    ).uri(),
+                    "inputs": {
+                        "input_type": "value",
+                        "value": {"user_input": "Hello, how are you?"},
+                    },
+                }
+            )
         )
-    )
 
-    assert model_run_res.output == "NO WAY THIS MATCHES"
+    assert model_run_res.output == expected_output
