ecb7b3de17acccfc13f44e3132d6f4d15dc2fbc5
zbirenbaum <zachary.birenbaum@wandb.com>
2025-07-06 20:10:56 -0700

    cleanup

--- weave-js/src/components/PagePanelComponents/Home/Browse3/pages/DocumentView/DocumentView.tsx
+++ weave-js/src/components/PagePanelComponents/Home/Browse3/pages/DocumentView/DocumentView.tsx
@@ -1,39 +1,7 @@
+import React, {useMemo} from 'react';
+import {ParsedCall, WeaveDocumentSchema} from './schemas';
+import {DocumentDropdown} from './DropdownMenu';
 
-import React, {useMemo, useState} from 'react';
-import { Box, Typography } from '@mui/material';
-import { ParsedCall, WeaveDocumentSchema } from './schemas';
-import { ObjectViewerSection } from '../CallPage/ObjectViewerSection';
-import { DropdownSection, DocumentDropdown } from './DropdownMenu';
-
-// interface DocumentCardProps {
-//   doc: WeaveDocumentSchema;
-// }
-
-// export const DocumentCard: React.FC<DocumentCardProps> = ({ doc }) => {
-//   const metadata = useMemo(() => {
-//     if (doc.metadata && typeof doc.metadata === 'object' && doc.content) {
-//       return doc.metadata;
-//     }
-//     return null;
-//   }, [doc]);
-//
-//   return (
-//     <Box
-//       border={1}
-//       borderColor={"#DFE0E2"}
-//       borderRadius={"8px"}
-//       paddingTop={"12px"}
-//       paddingLeft={"16px"}
-//       paddingBottom={"12px"}
-//     >
-//       <Typography variant="body2" color="text.secondary">
-//         {doc.content}
-//       </Typography>
-//     </Box>
-//   );
-// };
-//
-//
 export const DocumentView: React.FC<{ data: ParsedCall<WeaveDocumentSchema>, isExpanded: boolean}> = ({ data }) => {
   const {inputsDocuments, outputDocuments} = useMemo(() => {
     const inputs = data.inputs?.filter(parsed => parsed.schema == "Document") ?? [];
