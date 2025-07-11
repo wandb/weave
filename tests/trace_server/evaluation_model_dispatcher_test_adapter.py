0d7eb841375a9dbcbccf0089c5ca0888424346d4
Tim Sweeney <tim@wandb.com>
2025-07-11 15:50:37 -0700

    lint

--- tests/trace_server/evaluation_model_dispatcher_test_adapter.py
+++
@@ -1,9 +0,0 @@
-from weave.trace_server.workers.evaluate_model_worker.evaluate_model_worker import (
-    EvaluateModelArgs,
-    EvaluateModelDispatcher,
-)
-
-
-class EvaluationModelDispatcherTestAdapter(EvaluateModelDispatcher):
-    def dispatch(self, args: EvaluateModelArgs) -> None:
-        raise NotImplementedError("Not implemented")
