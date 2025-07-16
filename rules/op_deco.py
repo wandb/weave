c6661b1efc256c6bd01aa85e77a18a422ade8354
andrewtruong <itsandrewtruong@gmail.com>
2025-07-16 11:34:40 -0700

    rule

---
+++ rules/op_deco.py
@@ -0,0 +1,22 @@
+import libcst
+from fixit import InvalidTestCase, LintRule, ValidTestCase
+
+
+class OpDecoRule(LintRule):
+    VALID = [ValidTestCase("@weave.op")]
+    INVALID = [InvalidTestCase("@weave.op()")]
+
+    def visit_Decorator(self, node: libcst.Decorator) -> None:
+        if (
+            isinstance(node.decorator, libcst.Call)
+            and isinstance(attr := node.decorator.func, libcst.Attribute)
+            and isinstance(name := attr.value, libcst.Name)
+            and name.value == "weave"
+            and node.decorator.args == ()
+        ):
+            op_without_parens = libcst.Decorator(decorator=attr)
+            self.report(
+                node,
+                "Prefer @weave.op over @weave.op()",
+                replacement=op_without_parens,
+            )
