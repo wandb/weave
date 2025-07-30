17813e59be5909237a56873a2bf324e5c676002f
andrewtruong <itsandrewtruong@gmail.com>
2025-07-30 15:51:26 -0400

    test

--- weave/flow/eval.py
+++ weave/flow/eval.py
@@ -144,9 +144,6 @@ class Evaluation(Object):
             field_values["scorers"] = scorers
         # End mega-hack
 
-        # if not field_values.get("dataset"):
-        #     field_values["dataset"] = weave.Dataset(rows=[{"a": 1}])
-
         if not field_values.get("ref"):
             entity, project = obj.project_id.split("/")
             field_values["ref"] = ObjectRef(
