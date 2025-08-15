6b981609693a1917c354c4257d4c770281542ad6
andrewtruong <itsandrewtruong@gmail.com>
2025-08-15 12:01:30 -0400

    test

--- weave/flow/saved_view.py
+++ weave/flow/saved_view.py
@@ -11,8 +11,8 @@ from weave.trace.api import publish as weave_publish
 from weave.trace.api import ref as weave_ref
 from weave.trace.context import weave_client_context
 from weave.trace.display.grid import Grid
-from weave.trace.refs import ObjectRef, parse_op_uri
 from weave.trace.display.rich import pydantic_util
+from weave.trace.refs import ObjectRef, parse_op_uri
 from weave.trace.traverse import ObjectPath, get_paths
 from weave.trace.vals import WeaveObject
 from weave.trace.weave_client import CallsIter
