acdbe434f48869064731f5a831e629225b829eab
andrewtruong <itsandrewtruong@gmail.com>
2025-08-26 15:34:48 -0400

    test

--- weave/utils/sanitize.py
+++ weave/utils/sanitize.py
@@ -9,11 +9,11 @@ REDACTED_VALUE = "REDACTED"
 
 def add_redact_key(key: str) -> None:
     """Add a key to the list of keys that should be redacted.
-    
+
     Args:
         key: The key name to add to the redaction list.
              The key will be matched case-insensitively.
-    
+
     Example:
         >>> import weave
         >>> from weave.utils import sanitize
@@ -26,10 +26,10 @@ def add_redact_key(key: str) -> None:
 
 def remove_redact_key(key: str) -> None:
     """Remove a key from the list of keys that should be redacted.
-    
+
     Args:
         key: The key name to remove from the redaction list.
-    
+
     Note:
         This will not remove the default keys (api_key, auth_headers, authorization)
         unless explicitly called.
@@ -39,7 +39,7 @@ def remove_redact_key(key: str) -> None:
 
 def get_redact_keys() -> set[str]:
     """Get a copy of the current set of keys that will be redacted.
-    
+
     Returns:
         A copy of the set of keys that will be redacted.
     """
