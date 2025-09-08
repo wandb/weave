937a9d376a91fbd086add0bc099715cb1095342c
andrewtruong <itsandrewtruong@gmail.com>
2025-09-08 16:32:31 -0400

    test

--- weave/type_handlers/__init__.py
+++ weave/type_handlers/__init__.py
@@ -1,11 +1,10 @@
+from weave.type_handlers import Video
 from weave.type_handlers.Audio import audio
 from weave.type_handlers.Content import content
 from weave.type_handlers.DateTime import datetime
 from weave.type_handlers.File import file
 from weave.type_handlers.Image import image
 from weave.type_handlers.Markdown import markdown
-from weave.type_handlers.Video import video
-from weave.type_handlers.Video.lazy_import import install_hook
 
 file.register()
 content.register()
@@ -14,5 +13,4 @@ audio.register()
 datetime.register()
 markdown.register()
 
-# Install the lazy import hook for MoviePy instead of registering immediately
-install_hook()
+Video.install_hook()
