ad3965591fcbfca7a5f29d2be3b42ddbbef2ff6f
gtarpenning <griffin.tarpenning@wandb.com>
2025-09-09 08:36:32 -0700

    lint

--- tests/trace_server/test_environment.py
+++ tests/trace_server/test_environment.py
@@ -15,7 +15,7 @@ def test_kafka_producer_max_buffer_size():
     os.environ["KAFKA_PRODUCER_MAX_BUFFER_SIZE"] = "10"
     assert kafka_producer_max_buffer_size() == 10
     os.environ["KAFKA_PRODUCER_MAX_BUFFER_SIZE"] = "10.5"
-    assert kafka_producer_max_buffer_size() == None
+    assert kafka_producer_max_buffer_size() is None
     os.environ["KAFKA_PRODUCER_MAX_BUFFER_SIZE"] = "invalid"
     assert kafka_producer_max_buffer_size() is None
     del os.environ["KAFKA_PRODUCER_MAX_BUFFER_SIZE"]
