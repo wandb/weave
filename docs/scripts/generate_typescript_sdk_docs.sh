b8611fc2688d4ee7a1b72e579d2b448e47a59693
dbrian57 <daniel.brian@gmail.com>
2025-09-16 12:18:39 -0400

    Removes unnecessary comment

--- docs/scripts/generate_typescript_sdk_docs.sh
+++ docs/scripts/generate_typescript_sdk_docs.sh
@@ -1,5 +1,4 @@
 cd ../sdks/node
-# Install dependencies if node_modules doesn't exist
 pnpm typedoc src/index.ts \
     --plugin typedoc-plugin-markdown \
     --out ../../docs/docs/reference/typescript-sdk/weave/ \
