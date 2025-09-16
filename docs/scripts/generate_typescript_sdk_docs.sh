a1bac4d817bd36f0226d6c2bb93b248444783082
dbrian57 <daniel.brian@gmail.com>
2025-09-16 12:18:01 -0400

    Removes unnecessary code

--- docs/scripts/generate_typescript_sdk_docs.sh
+++ docs/scripts/generate_typescript_sdk_docs.sh
@@ -1,10 +1,6 @@
 cd ../sdks/node
 # Install dependencies if node_modules doesn't exist
-if [ ! -d "node_modules" ]; then
-    echo "Installing dependencies..."
-    npm install
-fi
-npx typedoc src/index.ts \
+pnpm typedoc src/index.ts \
     --plugin typedoc-plugin-markdown \
     --out ../../docs/docs/reference/typescript-sdk/weave/ \
     --readme none
