39c96e7511cdffbc658ff87cb2ed60892d3dfe90
gtarpenning <griffin.tarpenning@wandb.com>
2025-10-10 14:26:59 -0700

    chore(weave): disable content conversion

--- weave/trace_server/clickhouse_trace_server_batched.py
+++ weave/trace_server/clickhouse_trace_server_batched.py
@@ -299,7 +299,8 @@ class ClickHouseTraceServer(tsi.TraceServerInterface):
         # This does validation and conversion of the input data as well
         # as enforcing business rules and defaults
 
-        req = process_call_req_to_content(req, self)
+        # TODO: re-enable when perf is addressed
+        # req = process_call_req_to_content(req, self)
         ch_call = _start_call_for_insert_to_ch_insertable_start_call(req.start, self)
 
         # Inserts the call into the clickhouse database, verifying that
@@ -321,7 +322,8 @@ class ClickHouseTraceServer(tsi.TraceServerInterface):
         # Converts the user-provided call details into a clickhouse schema.
         # This does validation and conversion of the input data as well
         # as enforcing business rules and defaults
-        req = process_call_req_to_content(req, self)
+        
+        # TODO: re-enable when perf is addressed
         ch_call = _end_call_for_insert_to_ch_insertable_end_call(req.end, self)
 
         # Inserts the call into the clickhouse database, verifying that
