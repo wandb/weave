1b7b3a9d6079c99e5688fb0d5b5d2c30bf45ef9b
andrewtruong <itsandrewtruong@gmail.com>
2025-10-13 11:34:11 -0400

    fmt

--- weave/trace/op.py
+++ weave/trace/op.py
@@ -111,7 +111,9 @@ class Sentinel:
 _sentinels_to_check = [
     Sentinel(package="openai", path="openai._types", name="NOT_GIVEN"),
     Sentinel(package="openai", path="openai._types", name="omit"),
-    Sentinel(package="openai", path="openai._types", name="Omit"),  # Class, not instance
+    Sentinel(
+        package="openai", path="openai._types", name="Omit"
+    ),  # Class, not instance
     Sentinel(package="cohere", path="cohere.base_client", name="COHERE_NOT_GIVEN"),
     Sentinel(package="anthropic", path="anthropic._types", name="NOT_GIVEN"),
     Sentinel(package="cerebras", path="cerebras.cloud.sdk._types", name="NOT_GIVEN"),
