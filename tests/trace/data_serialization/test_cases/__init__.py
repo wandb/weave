70df9fb5d2f999654e7cf8be5d5d0dfefeda4814
Tim Sweeney <tim@wandb.com>
2025-10-15 12:02:46 -0700

    chore(weave): Add serialization assertions for the remaining data types: Evaluation Library Types (4) (#5578)
    * init list
    
    * init list
    
    * Added Adio
    
    * Moved Files
    
    * File moving
    
    * File moving
    
    * File moving
    
    * File moving
    
    * File split 1
    
    * File split 2
    
    * Added Video
    
    * Added Video
    
    * Finished Media Types
    
    * Fixed up
    
    * Some more fixes
    
    * Version fix
    
    * Version fix
    
    * Version fix
    
    * Added container types
    
    * init
    
    * init
    
    * MORE
    
    * merged
    
    * revert content adjustments
    
    * small adjust
    
    * fixed
    
    * fixed
    
    * logging data
    
    * logging data
    
    * logging data
    
    * merged
    
    * working

--- tests/trace/data_serialization/test_cases/__init__.py
+++ tests/trace/data_serialization/test_cases/__init__.py
@@ -1,6 +1,7 @@
 from tests.trace.data_serialization.spec import SerializationTestCase
 from tests.trace.data_serialization.test_cases.container_cases import container_cases
 from tests.trace.data_serialization.test_cases.core_types import core_cases
+from tests.trace.data_serialization.test_cases.library_cases import library_cases
 from tests.trace.data_serialization.test_cases.media_cases import media_cases
 from tests.trace.data_serialization.test_cases.primitive_cases import primitive_cases
 
@@ -35,16 +36,16 @@ from tests.trace.data_serialization.test_cases.primitive_cases import primitive_
 [x] Op
 [x] Object
 
-## Weave Library Objects:
-[] Model
-[] Scorer
-[] Evaluation
-[] Dataset
-[] Prompt
+## Weave Evaluation Library Objects:
+[x] Model
+[x] Scorer
+[x] Evaluation
+[x] Dataset
+[x] Prompt
 
-### Weave Library Specialized Objects:
-[] LLMStructuredCompletionModel
-[] LLMAsAJudgeScorer
+### Weave Evaluation Library Specialized Objects:
+[x] LLMStructuredCompletionModel
+[x] LLMAsAJudgeScorer
 
 ## Weave Config Objects:
 [] AnnotationSpec
@@ -58,4 +59,5 @@ cases: list[SerializationTestCase] = [
     *media_cases,
     *container_cases,
     *core_cases,
+    *library_cases,
 ]
