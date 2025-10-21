cf52f4ba31b45064ddc57c7c20877465293f4d48
andrewtruong <itsandrewtruong@gmail.com>
2025-10-21 15:19:56 -0400

    test

--- tests/trace/data_serialization/test_cases/media_cases.py
+++ tests/trace/data_serialization/test_cases/media_cases.py
@@ -255,7 +255,9 @@ media_cases = [
             "weave_type": {"type": "weave.type_wrappers.Content.content.Content"},
             "files": {
                 "content": "xfOhnNfgQxRzgWZ6DC1QEGt9vrJWcathymKPPZQmmIw",
-                "metadata.json": "0tY4LYkQE9BXzCQDItzaUoFLd3lesQ0RkuHNMuXQJIk",
+                "metadata.json": "v5gYkptifYVGd0JYbW3U0jYQPSKkLzEI3eamc0cnCG8"
+                if sys.platform == "win32"
+                else "0tY4LYkQE9BXzCQDItzaUoFLd3lesQ0RkuHNMuXQJIk",
             },
             "load_op": "weave:///shawn/test-project/op/load_weave.type_wrappers.Content.content.Content:eZCocNGYXrXPrJMWO0MECHxVUOWlrWSma9iGydnlxE0",
         },
@@ -272,7 +274,9 @@ media_cases = [
         ],
         exp_files=[
             {
-                "digest": "0tY4LYkQE9BXzCQDItzaUoFLd3lesQ0RkuHNMuXQJIk",
+                "digest": "v5gYkptifYVGd0JYbW3U0jYQPSKkLzEI3eamc0cnCG8"
+                if sys.platform == "win32"
+                else "0tY4LYkQE9BXzCQDItzaUoFLd3lesQ0RkuHNMuXQJIk",
                 "exp_content": b'{"size": 88244, "mimetype": "audio/x-wav", "digest": "c5f3a19cd7e043147381667a0c2d50106b7dbeb25671ab61ca628f3d9426988c", "filename": "audio.wav", "content_type": "file", "input_type": "str", "encoding": "utf-8", "metadata": null, "extension": ".wav"}',
             },
             {
