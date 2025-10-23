5e1e7e8ed792d53fed4f2c98d23e4c6b92270839
andrewtruong <itsandrewtruong@gmail.com>
2025-10-22 20:47:25 -0400

    test

--- weave/trace_server_bindings/caching_middleware_trace_server.py
+++ weave/trace_server_bindings/caching_middleware_trace_server.py
@@ -273,6 +273,14 @@ class CachingMiddlewareTraceServer(tsi.FullTraceServerInterface):
     def get_cache_recorder(self) -> CacheRecorder:
         return self._cache_recorder.copy()
 
+    def __getattr__(self, name: str) -> Any:
+        """Delegate any non-cached methods to the underlying trace server.
+
+        This proxy pattern allows us to only implement cached methods explicitly,
+        while all other methods are automatically delegated to _next_trace_server.
+        """
+        return getattr(self._next_trace_server, name)
+
     # Cacheable Methods:
     def obj_read(self, req: tsi.ObjReadReq) -> tsi.ObjReadRes:
         if not digest_is_cacheable(req.digest):
@@ -415,126 +423,7 @@ class CachingMiddlewareTraceServer(tsi.FullTraceServerInterface):
             tsi.FilesStatsRes,
         )
 
-    # Remaining Un-cacheable Methods:
-
-    def ensure_project_exists(
-        self, entity: str, project: str
-    ) -> tsi.EnsureProjectExistsRes:
-        return self._next_trace_server.ensure_project_exists(entity, project)
-
-    # Call API
-    def call_start(self, req: tsi.CallStartReq) -> tsi.CallStartRes:
-        return self._next_trace_server.call_start(req)
-
-    def call_end(self, req: tsi.CallEndReq) -> tsi.CallEndRes:
-        return self._next_trace_server.call_end(req)
-
-    def call_read(self, req: tsi.CallReadReq) -> tsi.CallReadRes:
-        return self._next_trace_server.call_read(req)
-
-    def calls_query(self, req: tsi.CallsQueryReq) -> tsi.CallsQueryRes:
-        return self._next_trace_server.calls_query(req)
-
-    def calls_query_stream(self, req: tsi.CallsQueryReq) -> Iterator[tsi.CallSchema]:
-        return self._next_trace_server.calls_query_stream(req)
-
-    def calls_delete(self, req: tsi.CallsDeleteReq) -> tsi.CallsDeleteRes:
-        return self._next_trace_server.calls_delete(req)
-
-    def calls_query_stats(self, req: tsi.CallsQueryStatsReq) -> tsi.CallsQueryStatsRes:
-        return self._next_trace_server.calls_query_stats(req)
-
-    def call_update(self, req: tsi.CallUpdateReq) -> tsi.CallUpdateRes:
-        return self._next_trace_server.call_update(req)
-
-    # OTEL API
-    def otel_export(self, req: tsi.OtelExportReq) -> tsi.OtelExportRes:
-        return self._next_trace_server.otel_export(req)
-
-    # Op API
-    def op_create(self, req: tsi.OpCreateReq) -> tsi.OpCreateRes:
-        return self._next_trace_server.op_create(req)
-
-    def op_read(self, req: tsi.OpReadReq) -> tsi.OpReadRes:
-        return self._next_trace_server.op_read(req)
-
-    def ops_query(self, req: tsi.OpQueryReq) -> tsi.OpQueryRes:
-        return self._next_trace_server.ops_query(req)
-
-    # Cost API
-    def cost_create(self, req: tsi.CostCreateReq) -> tsi.CostCreateRes:
-        return self._next_trace_server.cost_create(req)
-
-    def cost_query(self, req: tsi.CostQueryReq) -> tsi.CostQueryRes:
-        return self._next_trace_server.cost_query(req)
-
-    def cost_purge(self, req: tsi.CostPurgeReq) -> tsi.CostPurgeRes:
-        return self._next_trace_server.cost_purge(req)
-
-    def objs_query(self, req: tsi.ObjQueryReq) -> tsi.ObjQueryRes:
-        return self._next_trace_server.objs_query(req)
-
-    # Table API
-    def table_create(self, req: tsi.TableCreateReq) -> tsi.TableCreateRes:
-        return self._next_trace_server.table_create(req)
-
-    def table_create_from_digests(
-        self, req: tsi.TableCreateFromDigestsReq
-    ) -> tsi.TableCreateFromDigestsRes:
-        return self._next_trace_server.table_create_from_digests(req)
-
-    def table_update(self, req: tsi.TableUpdateReq) -> tsi.TableUpdateRes:
-        return self._next_trace_server.table_update(req)
-
-    def feedback_create(self, req: tsi.FeedbackCreateReq) -> tsi.FeedbackCreateRes:
-        return self._next_trace_server.feedback_create(req)
-
-    def feedback_create_batch(
-        self, req: tsi.FeedbackCreateBatchReq
-    ) -> tsi.FeedbackCreateBatchRes:
-        return self._next_trace_server.feedback_create_batch(req)
-
-    def feedback_query(self, req: tsi.FeedbackQueryReq) -> tsi.FeedbackQueryRes:
-        return self._next_trace_server.feedback_query(req)
-
-    def feedback_purge(self, req: tsi.FeedbackPurgeReq) -> tsi.FeedbackPurgeRes:
-        return self._next_trace_server.feedback_purge(req)
-
-    def feedback_replace(self, req: tsi.FeedbackReplaceReq) -> tsi.FeedbackReplaceRes:
-        return self._next_trace_server.feedback_replace(req)
-
-    # Action API
-    def actions_execute_batch(
-        self, req: tsi.ActionsExecuteBatchReq
-    ) -> tsi.ActionsExecuteBatchRes:
-        return self._next_trace_server.actions_execute_batch(req)
-
-    # Execute LLM API
-    def completions_create(
-        self, req: tsi.CompletionsCreateReq
-    ) -> tsi.CompletionsCreateRes:
-        return self._next_trace_server.completions_create(req)
-
-    def project_stats(self, req: tsi.ProjectStatsReq) -> tsi.ProjectStatsRes:
-        return self._next_trace_server.project_stats(req)
-
-    def threads_query_stream(
-        self, req: tsi.ThreadsQueryReq
-    ) -> Iterator[tsi.ThreadSchema]:
-        return self._next_trace_server.threads_query_stream(req)
-
-    def evaluate_model(self, req: tsi.EvaluateModelReq) -> tsi.EvaluateModelRes:
-        return self._next_trace_server.evaluate_model(req)
-
-    def evaluation_status(
-        self, req: tsi.EvaluationStatusReq
-    ) -> tsi.EvaluationStatusRes:
-        return self._next_trace_server.evaluation_status(req)
-
-    # === V2 APIs ===
-
-    def op_create_v2(self, req: tsi.OpCreateV2Req) -> tsi.OpCreateV2Res:
-        return self._next_trace_server.op_create_v2(req)
+    # === Cacheable V2 APIs ===
 
     def op_read_v2(self, req: tsi.OpReadV2Req) -> tsi.OpReadV2Res:
         if not digest_is_cacheable(req.digest):
@@ -543,61 +432,12 @@ class CachingMiddlewareTraceServer(tsi.FullTraceServerInterface):
             self._next_trace_server.op_read_v2, req, tsi.OpReadV2Res
         )
 
-    def op_list_v2(self, req: tsi.OpListV2Req) -> Iterator[tsi.OpReadV2Res]:
-        return self._next_trace_server.op_list_v2(req)
-
-    def op_delete_v2(self, req: tsi.OpDeleteV2Req) -> tsi.OpDeleteV2Res:
-        return self._next_trace_server.op_delete_v2(req)
-
-    def dataset_create_v2(self, req: tsi.DatasetCreateV2Req) -> tsi.DatasetCreateV2Res:
-        return self._next_trace_server.dataset_create_v2(req)
-
     def dataset_read_v2(self, req: tsi.DatasetReadV2Req) -> tsi.DatasetReadV2Res:
         if not digest_is_cacheable(req.digest):
             return self._next_trace_server.dataset_read_v2(req)
         return self._with_cache_pydantic(
             self._next_trace_server.dataset_read_v2, req, tsi.DatasetReadV2Res
         )
-
-    def dataset_list_v2(
-        self, req: tsi.DatasetListV2Req
-    ) -> Iterator[tsi.DatasetReadV2Res]:
-        return self._next_trace_server.dataset_list_v2(req)
-
-    def dataset_delete_v2(self, req: tsi.DatasetDeleteV2Req) -> tsi.DatasetDeleteV2Res:
-        return self._next_trace_server.dataset_delete_v2(req)
-
-    def scorer_create_v2(self, req: tsi.ScorerCreateV2Req) -> tsi.ScorerCreateV2Res:
-        return self._next_trace_server.scorer_create_v2(req)
-
-    def scorer_read_v2(self, req: tsi.ScorerReadV2Req) -> tsi.ScorerReadV2Res:
-        return self._next_trace_server.scorer_read_v2(req)
-
-    def scorer_list_v2(self, req: tsi.ScorerListV2Req) -> Iterator[tsi.ScorerReadV2Res]:
-        return self._next_trace_server.scorer_list_v2(req)
-
-    def scorer_delete_v2(self, req: tsi.ScorerDeleteV2Req) -> tsi.ScorerDeleteV2Res:
-        return self._next_trace_server.scorer_delete_v2(req)
-
-    def evaluation_create_v2(
-        self, req: tsi.EvaluationCreateV2Req
-    ) -> tsi.EvaluationCreateV2Res:
-        return self._next_trace_server.evaluation_create_v2(req)
-
-    def evaluation_read_v2(
-        self, req: tsi.EvaluationReadV2Req
-    ) -> tsi.EvaluationReadV2Res:
-        return self._next_trace_server.evaluation_read_v2(req)
-
-    def evaluation_list_v2(
-        self, req: tsi.EvaluationListV2Req
-    ) -> Iterator[tsi.EvaluationReadV2Res]:
-        return self._next_trace_server.evaluation_list_v2(req)
-
-    def evaluation_delete_v2(
-        self, req: tsi.EvaluationDeleteV2Req
-    ) -> tsi.EvaluationDeleteV2Res:
-        return self._next_trace_server.evaluation_delete_v2(req)
 
 
 def pydantic_bytes_safe_dump(obj: BaseModel) -> str:
