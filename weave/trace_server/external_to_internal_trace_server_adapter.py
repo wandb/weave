631aba5f96ca27943c676c75b22fccb830a8a735
zbirenbaum <zachary.birenbaum@wandb.com>
2025-11-04 13:51:50 -0800

    remove comment

--- weave/trace_server/external_to_internal_trace_server_adapter.py
+++ weave/trace_server/external_to_internal_trace_server_adapter.py
@@ -251,12 +251,10 @@ class ExternalTraceServer(tsi.FullTraceServerInterface):
         return self._ref_apply(self._internal_trace_server.obj_create, req)
 
     def obj_create_batch(self, req: tsi.ObjCreateBatchReq) -> tsi.ObjCreateBatchRes:
-        # Convert project and user IDs for each object in the batch
         for obj in req.batch:
             obj.project_id = self._idc.ext_to_int_project_id(obj.project_id)
             if obj.wb_user_id is not None:
                 obj.wb_user_id = self._idc.ext_to_int_user_id(obj.wb_user_id)
-        # Apply internal method and convert any refs in the response
         return self._ref_apply(self._internal_trace_server.obj_create_batch, req)
 
     def obj_read(self, req: tsi.ObjReadReq) -> tsi.ObjReadRes:
