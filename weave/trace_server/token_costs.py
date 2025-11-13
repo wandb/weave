faa525203e8e244f5da8c501ec06922c8e2aa81a
gtarpenning <griffin.tarpenning@wandb.com>
2025-11-13 09:07:20 -0800

    lint

--- weave/trace_server/token_costs.py
+++ weave/trace_server/token_costs.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 
 from weave.trace_server import trace_server_interface as tsi
+from weave.trace_server.calls_query_builder.cte import CTE
 from weave.trace_server.clickhouse_schema import SelectableCHCallSchema
 from weave.trace_server.errors import InvalidRequest
 from weave.trace_server.orm import (
@@ -17,7 +18,6 @@ from weave.trace_server.validation import (
     validate_purge_req_multiple,
     validate_purge_req_one,
 )
-from weave.trace_server.calls_query_builder.cte import CTE
 
 if TYPE_CHECKING:
     from weave.trace_server.calls_query_builder.calls_query_builder import OrderField
