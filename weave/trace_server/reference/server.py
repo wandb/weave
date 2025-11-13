ad4c494184fa63526ef038a677419836f9d8a058
Andrew Truong <itsandrewtruong@gmail.com>
2025-11-13 01:58:42 -0500

    chore(weave): Rename V2 API to Objects API #5730

--- weave/trace_server/reference/server.py
+++ weave/trace_server/reference/server.py
@@ -8,7 +8,7 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from weave.trace_server.reference.generate import (
     ServiceDependency,
     generate_routes,
-    generate_routes_v2,
+    generate_v2_routes,
     noop_trace_server_factory,
 )
 
@@ -26,7 +26,7 @@ def authenticate(
 
 server_dependency = ServiceDependency(service_factory=noop_trace_server_factory)
 trace_service_router = generate_routes(APIRouter(), server_dependency)
-v2_router = generate_routes_v2(APIRouter(prefix="/v2"), server_dependency)
+v2_router = generate_v2_routes(APIRouter(prefix="/object"), server_dependency)
 app = FastAPI()
 app.include_router(trace_service_router, dependencies=[Depends(authenticate)])
 app.include_router(v2_router, dependencies=[Depends(authenticate)])
