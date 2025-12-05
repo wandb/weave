402c722d19f4499ef25e9b992ea80620e098b76b
Tim Sweeney <tim@wandb.com>
2025-12-05 12:36:31 -0800

    fix(weave): Fixes non-deterministic ordering of large-table evals (#5815)
    * fixed
    
    * fixed
    
    * lint

--- weave/trace/concurrent/futures.py
+++ weave/trace/concurrent/futures.py
@@ -116,8 +116,11 @@ class FutureExecutor:
 
         def callback() -> None:
             try:
-                done, _ = wait(futures)
-                results = [f.result() for f in done]
+                _, _ = wait(futures)
+                # Use `futures` here not the result of `wait` to
+                # ensure correct order of results. `wait` returns
+                # a set of done futures (seemingly non deterministic order).
+                results = [f.result() for f in futures]
             except Exception as e:
                 result_future.set_exception(e)
                 return
