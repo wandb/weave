90ac7d64a85a35ccb92026e546f16f766a10e282
Tim Sweeney <tim@wandb.com>
2025-12-16 13:03:39 -0800

    tests

--- tests/trace/test_weave_client.py
+++ tests/trace/test_weave_client.py
@@ -331,7 +331,7 @@ def test_call_create(client):
     client.finish_call(call, "hello")
     result = client.get_call(call.id)
     expected = weave.trace.call.Call(
-        _op_name="weave:///shawn/test-project/op/x:IMZjfLeSAoLTYu8QLPrVVXrNIFX6jZlxAYAeRDEEezE",
+        _op_name="weave:///shawn/test-project/op/x:6jAV4T6F42RKlabeB2RO0BXkbFFPrKyU2yyQedpotB8",
         project_id="shawn/test-project",
         trace_id=RegexStringMatcher(".*"),
         parent_id=None,
@@ -375,7 +375,7 @@ def test_calls_query(client):
     result = list(client.get_calls(filter=tsi.CallsFilter(op_names=[call1.op_name])))
     assert len(result) == 2
     assert result[0] == weave.trace.call.Call(
-        _op_name="weave:///shawn/test-project/op/x:IMZjfLeSAoLTYu8QLPrVVXrNIFX6jZlxAYAeRDEEezE",
+        _op_name="weave:///shawn/test-project/op/x:6jAV4T6F42RKlabeB2RO0BXkbFFPrKyU2yyQedpotB8",
         project_id="shawn/test-project",
         trace_id=RegexStringMatcher(".*"),
         parent_id=None,
@@ -401,7 +401,7 @@ def test_calls_query(client):
         ended_at=None,
     )
     assert result[1] == weave.trace.call.Call(
-        _op_name="weave:///shawn/test-project/op/x:IMZjfLeSAoLTYu8QLPrVVXrNIFX6jZlxAYAeRDEEezE",
+        _op_name="weave:///shawn/test-project/op/x:6jAV4T6F42RKlabeB2RO0BXkbFFPrKyU2yyQedpotB8",
         project_id="shawn/test-project",
         trace_id=RegexStringMatcher(".*"),
         parent_id=call0.id,
