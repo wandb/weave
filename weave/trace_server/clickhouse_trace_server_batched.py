aeb7f7814cf829a76a6e21bfa0b9c9becce2f11f
gtarpenning <griffin.tarpenning@wandb.com>
2025-12-17 10:46:03 -0800

    fixmerge

--- weave/trace_server/clickhouse_trace_server_batched.py
+++ weave/trace_server/clickhouse_trace_server_batched.py
@@ -150,6 +150,7 @@ from weave.trace_server.project_version.types import (
     ProjectDataResidence,
     ReadTable,
     WriteTarget,
+    ReadTable
 )
 from weave.trace_server.secret_fetcher_context import _secret_fetcher_context
 from weave.trace_server.table_query_builder import (
