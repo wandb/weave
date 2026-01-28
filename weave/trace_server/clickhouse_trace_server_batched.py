2b42f8ecf4073f1150f9265bdacce8e41400dea2
Griffin Tarpenning <griffin.tarpenning@wandb.com>
2026-01-28 14:15:49 -0800

    chore(weave): add mutation paths for calls_complete (#6003)

--- weave/trace_server/clickhouse_trace_server_batched.py
+++ weave/trace_server/clickhouse_trace_server_batched.py
@@ -56,7 +56,9 @@ from weave.trace_server.calls_query_builder.calls_query_builder import (
     OrderField,
     QueryBuilderDynamicField,
     QueryBuilderField,
+    build_calls_complete_delete_query,
     build_calls_complete_update_end_query,
+    build_calls_complete_update_query,
     build_calls_stats_query,
     combine_conditions,
 )
@@ -1190,6 +1192,14 @@ class ClickHouseTraceServer(tsi.FullTraceServerInterface):
             all_calls=all_calls,
         )
 
+        write_target = self.table_routing_resolver.resolve_v1_write_target(
+            req.project_id,
+            self.ch_client,
+        )
+        if write_target == WriteTarget.CALLS_COMPLETE:
+            self._delete_calls_complete(req.project_id, all_descendants)
+            return tsi.CallsDeleteRes(num_deleted=len(all_descendants))
+
         deleted_at = datetime.datetime.now()
         insertables = [
             CallDeleteCHInsertable(
@@ -1207,6 +1217,19 @@ class ClickHouseTraceServer(tsi.FullTraceServerInterface):
 
         return tsi.CallsDeleteRes(num_deleted=len(all_descendants))
 
+    @ddtrace.tracer.wrap(name="clickhouse_trace_server_batched._delete_calls_complete")
+    def _delete_calls_complete(self, project_id: str, call_ids: list[str]) -> None:
+        pb = ParamBuilder()
+        project_id_param = pb.add_param(project_id)
+        call_ids_param = pb.add_param(call_ids)
+        delete_query = build_calls_complete_delete_query(
+            "calls_complete",
+            project_id_param,
+            call_ids_param,
+            cluster_name=self.clickhouse_cluster_name,
+        )
+        self._query(delete_query, pb.get_params())
+
     def _ensure_valid_update_field(self, req: tsi.CallUpdateReq) -> None:
         valid_update_fields = ["display_name"]
         for field in valid_update_fields:
@@ -1221,6 +1244,15 @@ class ClickHouseTraceServer(tsi.FullTraceServerInterface):
     def call_update(self, req: tsi.CallUpdateReq) -> tsi.CallUpdateRes:
         assert_non_null_wb_user_id(req)
         self._ensure_valid_update_field(req)
+
+        write_target = self.table_routing_resolver.resolve_v1_write_target(
+            req.project_id,
+            self.ch_client,
+        )
+        if write_target == WriteTarget.CALLS_COMPLETE:
+            self._update_calls_complete(req.project_id, req.call_id, req.display_name)
+            return tsi.CallUpdateRes()
+
         renamed_insertable = CallUpdateCHInsertable(
             project_id=req.project_id,
             id=req.call_id,
@@ -1230,6 +1262,23 @@ class ClickHouseTraceServer(tsi.FullTraceServerInterface):
         self._insert_call(renamed_insertable)
 
         return tsi.CallUpdateRes()
+
+    @ddtrace.tracer.wrap(name="clickhouse_trace_server_batched._update_calls_complete")
+    def _update_calls_complete(
+        self, project_id: str, call_id: str, display_name: str
+    ) -> None:
+        pb = ParamBuilder()
+        project_id_param = pb.add_param(project_id)
+        call_id_param = pb.add_param(call_id)
+        display_name_param = pb.add_param(display_name)
+        update_query = build_calls_complete_update_query(
+            "calls_complete",
+            project_id_param,
+            call_id_param,
+            display_name_param,
+            cluster_name=self.clickhouse_cluster_name,
+        )
+        self._query(update_query, pb.get_params())
 
     @ddtrace.tracer.wrap(name="clickhouse_trace_server_batched.obj_create")
     def obj_create(self, req: tsi.ObjCreateReq) -> tsi.ObjCreateRes:
