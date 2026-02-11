3a0f7f1a25a9d623d2a601c88e7c72b9ad6f0789
Andrew Truong <itsandrewtruong@gmail.com>
2026-02-11 13:47:36 -0500

    chore(weave): Remove duplicate tests #6036

--- tests/trace/test_anonymous_ops.py
+++ tests/trace/test_anonymous_ops.py
@@ -2,28 +2,6 @@ from weave.trace import weave_client
 from weave.trace_server.trace_server_interface import CallsQueryReq
 
 
-def test_named_op(client: weave_client.WeaveClient) -> str:
-    call = client.create_call("anonymous_op", {"a": 1})
-    client.finish_call(call, {"c": 3}, None)
-
-    call_res = client.server.calls_query(
-        CallsQueryReq(
-            project_id=client._project_id(),
-        )
-    )
-    calls = call_res.calls
-
-    assert len(calls) == 1
-    call = calls[0]
-    assert (
-        call.op_name
-        == "weave:///shawn/test-project/op/anonymous_op:IMZjfLeSAoLTYu8QLPrVVXrNIFX6jZlxAYAeRDEEezE"
-    )
-    assert call.inputs == {"a": 1}
-    assert call.output == {"c": 3}
-    assert call.exception is None
-
-
 def test_anonymous_op(client: weave_client.WeaveClient) -> str:
     call = client.create_call("anonymous_op", {"a": 1})
     client.finish_call(call, {"c": 3}, None)
