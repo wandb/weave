d0408a61b6ae0da53f01e6b235693e1abe93aeed
Chance An <chance.an@wandb.com>
2026-02-11 09:42:12 -0800

    chore(weave): API for editing an annotation queue metadata

--- weave/trace_server/external_to_internal_trace_server_adapter.py
+++ weave/trace_server/external_to_internal_trace_server_adapter.py
@@ -470,6 +470,12 @@ class ExternalTraceServer(tsi.FullTraceServerInterface):
         req.project_id = self._idc.ext_to_int_project_id(req.project_id)
         return self._ref_apply(self._internal_trace_server.annotation_queue_read, req)
 
+    def annotation_queue_update(
+        self, req: tsi.AnnotationQueueUpdateReq
+    ) -> tsi.AnnotationQueueUpdateRes:
+        req.project_id = self._idc.ext_to_int_project_id(req.project_id)
+        return self._ref_apply(self._internal_trace_server.annotation_queue_update, req)
+
     def annotation_queue_delete(
         self, req: tsi.AnnotationQueueDeleteReq
     ) -> tsi.AnnotationQueueDeleteRes:
