c44c50e049092e25140160f479d7abfddb0f4e1c
gtarpenning <griffin.tarpenning@wandb.com>
2026-02-17 17:04:11 -0800

    test -- remove codecove html

--- noxfile.py
+++ noxfile.py
@@ -218,8 +218,8 @@ def tests(session: nox.Session, shard: str):
         "--durations=20",
         "--strict-markers",
         "--cov=weave",
-        "--cov-report=html",
         "--cov-report=xml",
+        "--cov-report=term:skip-covered",
         "--cov-branch",
     ]
 
