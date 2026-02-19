157bb4ef18876aa3360327964d9e85456045c153
Mike Scavezze <244614145+mscavezze-cw@users.noreply.github.com>
2026-02-19 12:31:30 -0800

    fix tests

--- tests/trace/data_serialization/test_cases/config_cases.py
+++ tests/trace/data_serialization/test_cases/config_cases.py
@@ -99,9 +99,74 @@ config_cases = [
                     ]
                 }
             },
+            debounced_scoring_enabled=True,
+            debounced_scoring_aggregation_field="trace_id",
+            debounced_scoring_timeout_seconds=60.1
         ),
         inline_call_param=False,
         is_legacy=False,
+        exp_json={
+            "_type": "Monitor",
+            "name": "test_monitor",
+            "description": None,
+            "sampling_rate": 0.5,
+            "scorers": [],
+            "op_names": ["example_op_name"],
+            "query": {
+                "_type": "Query",
+                "$expr": {
+                    "_type": "GtOperation",
+                    "$gt": [
+                        {
+                            "_type": "GetFieldOperator",
+                            "$getField": "started_at",
+                            "_class_name": "GetFieldOperator",
+                            "_bases": ["BaseModel"],
+                        },
+                        {
+                            "_type": "LiteralOperation",
+                            "$literal": 1742540400,
+                            "_class_name": "LiteralOperation",
+                            "_bases": ["BaseModel"],
+                        },
+                    ],
+                    "_class_name": "GtOperation",
+                    "_bases": ["BaseModel"],
+                },
+                "_class_name": "Query",
+                "_bases": ["BaseModel"],
+            },
+            "active": False,
+            "debounced_scoring_enabled": True,
+            "debounced_scoring_aggregation_field": "trace_id",
+            "debounced_scoring_timeout_seconds": 60.1,
+            "_class_name": "Monitor",
+            "_bases": ["Object", "BaseModel"],
+        },
+        exp_objects=[],
+        exp_files=[],
+        # Sad ... equality is really a pain to assert here (and is broken)
+        # TODO: Write a good equality check and make it work
+        equality_check=lambda a, b: True,
+    ),
+    SerializationTestCase(
+        id="Monitor (legacy v1, before debouncing)",
+        runtime_object_factory=lambda: Monitor(
+            name="test_monitor",
+            sampling_rate=0.5,
+            scorers=[],
+            op_names=["example_op_name"],
+            query={
+                "$expr": {
+                    "$gt": [
+                        {"$getField": "started_at"},
+                        {"$literal": 1742540400},
+                    ]
+                }
+            },
+        ),
+        inline_call_param=False,
+        is_legacy=True,
         exp_json={
             "_type": "Monitor",
             "name": "test_monitor",
