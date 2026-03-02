0aaedcc9d0016d067f843de9fa0557cc8e62794b
andrewtruong <itsandrewtruong@gmail.com>
2026-03-02 08:53:52 -0500

    test

--- tests/trace_server/test_trace_server_conftest.py
+++ tests/trace_server/test_trace_server_conftest.py
@@ -2,16 +2,16 @@ import pytest
 
 from tests.trace_server.conftest import get_trace_server_flag
 from tests.trace_server.conftest_lib.trace_server_external_adapter import (
-    TestOnlyUserInjectingExternalTraceServer,
+    UserInjectingExternalTraceServer,
 )
 from weave.trace_server.clickhouse_trace_server_batched import ClickHouseTraceServer
 from weave.trace_server.sqlite_trace_server import SqliteTraceServer
 
 
 def test_trace_server_fixture(
-    request, trace_server: TestOnlyUserInjectingExternalTraceServer
+    request, trace_server: UserInjectingExternalTraceServer
 ):
-    assert isinstance(trace_server, TestOnlyUserInjectingExternalTraceServer)
+    assert isinstance(trace_server, UserInjectingExternalTraceServer)
     if get_trace_server_flag(request) == "clickhouse":
         assert isinstance(trace_server._internal_trace_server, ClickHouseTraceServer)
     else:
@@ -20,7 +20,7 @@ def test_trace_server_fixture(
 
 @pytest.mark.skip_clickhouse_client
 def test_skip_clickhouse_client(
-    request, trace_server: TestOnlyUserInjectingExternalTraceServer
+    request, trace_server: UserInjectingExternalTraceServer
 ):
-    assert isinstance(trace_server, TestOnlyUserInjectingExternalTraceServer)
+    assert isinstance(trace_server, UserInjectingExternalTraceServer)
     assert get_trace_server_flag(request) != "clickhouse"
