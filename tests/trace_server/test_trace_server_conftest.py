75790d8fa5ffd8a9d348fbe547ad0a46b427718e
andrewtruong <itsandrewtruong@gmail.com>
2026-03-02 07:31:47 -0500

    test

--- tests/trace_server/test_trace_server_conftest.py
+++ tests/trace_server/test_trace_server_conftest.py
@@ -8,9 +8,7 @@ from weave.trace_server.clickhouse_trace_server_batched import ClickHouseTraceSe
 from weave.trace_server.sqlite_trace_server import SqliteTraceServer
 
 
-def test_trace_server_fixture(
-    request, trace_server: UserInjectingExternalTraceServer
-):
+def test_trace_server_fixture(request, trace_server: UserInjectingExternalTraceServer):
     assert isinstance(trace_server, UserInjectingExternalTraceServer)
     if get_trace_server_flag(request) == "clickhouse":
         assert isinstance(trace_server._internal_trace_server, ClickHouseTraceServer)
