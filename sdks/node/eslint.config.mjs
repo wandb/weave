7aea225d2226da37cec25435cf4100090011dce0
drtangible <dgilbert@coreweave.com>
2026-06-18 14:42:29 -0500

    chore(weave_ts): Fix '@typescript-eslint/no-unnecessary-condition' lint errors.

--- sdks/node/eslint.config.mjs
+++ sdks/node/eslint.config.mjs
@@ -9,6 +9,16 @@ export default defineConfig([
   {
     files: ['**/*.{js,ts}'],
     extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+          './tsconfig.test.json',
+          './examples/tsconfig.examples.json',
+          './scripts/tsconfig.scripts.json'
+        ]
+      },
+    },
     rules: {
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/consistent-type-imports': [
@@ -18,6 +28,7 @@ export default defineConfig([
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'error',
       '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
