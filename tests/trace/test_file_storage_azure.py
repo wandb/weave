e8605bc82054b7378a735d9b34f558eefa40274e
Zachary Blasczyk <zacharyblasczyk@gmail.com>
2026-07-27 14:53:24 -0500

    chore(weave): format Azure storage tests

--- tests/trace/test_file_storage_azure.py
+++ tests/trace/test_file_storage_azure.py
@@ -33,9 +33,7 @@ def test_workload_identity_presign_uses_user_delegation_key():
     service_client = mock.Mock()
     service_client.account_name = "account"
     service_client.get_user_delegation_key.return_value = "delegation-key"
-    service_client.get_container_client.return_value.get_blob_client.return_value.url = (
-        "https://account.blob.core.windows.net/container/exports/result.json"
-    )
+    service_client.get_container_client.return_value.get_blob_client.return_value.url = "https://account.blob.core.windows.net/container/exports/result.json"
 
     client = AzureStorageClient(base_uri, {"default_credential": credential})
     with (
@@ -73,9 +71,7 @@ def test_account_key_presign_remains_supported():
     service_client = mock.Mock()
     service_client.account_name = "account"
     service_client.credential.account_key = "account-key"
-    service_client.get_container_client.return_value.get_blob_client.return_value.url = (
-        "https://account.blob.core.windows.net/container/exports/result.json"
-    )
+    service_client.get_container_client.return_value.get_blob_client.return_value.url = "https://account.blob.core.windows.net/container/exports/result.json"
 
     client = AzureStorageClient(
         base_uri,
