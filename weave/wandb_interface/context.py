b118eb7b6cce05d7a905505be8114f8ba7d4b68e
RyanBuccellato <ryanbuccellato@gmail.com>
2026-08-20 16:04:01 -0400

    Update context.py

--- weave/wandb_interface/context.py
+++ weave/wandb_interface/context.py
@@ -13,6 +13,7 @@ def get_wandb_auth_context() -> WandbCredentials | None:
 
 
 def get_wandb_api_context() -> str | None:
+    """Return the current W&B credential, reading from env var or local files"""
     credentials = get_wandb_auth_context()
     if isinstance(credentials, ApiKeyCredentials):
         return credentials.api_key
